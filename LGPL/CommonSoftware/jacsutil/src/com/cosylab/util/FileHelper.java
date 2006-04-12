8c09ba5517c3c50b0f3a02061b43cf79d4743bee
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2006-04-12 17:44:02 +0000

    added copy method
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@52806 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jacsutil/src/com/cosylab/util/FileHelper.java
+++ LGPL/CommonSoftware/jacsutil/src/com/cosylab/util/FileHelper.java
@@ -7,6 +7,11 @@
 package com.cosylab.util;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 
 /**
  * @author dvitas
@@ -77,6 +82,27 @@ public class FileHelper {
 		} catch (Exception e) {
 			// nop
 		}
+	}
+	
+	public static void copy(File source, File dest) throws IOException {
+	     FileChannel in = null, out = null;
+	     try {          
+	          in = new FileInputStream(source).getChannel();
+	          out = new FileOutputStream(dest).getChannel();
+	 
+	          long size = in.size();
+	          MappedByteBuffer buf = in.map(FileChannel.MapMode.READ_ONLY, 0, size);
+	 
+	          out.write(buf);
+	 
+	     } finally {
+	          if (in != null) {
+	        	  in.close();
+	          }
+	          if (out != null) {
+	        	  out.close();
+	          }
+	     }
 	}
 
 }
