59e64bf64bd17e66d004c87dcdbd529f04eef880
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2006-04-13 12:49:28 +0000

    copy supports preserveTime flag and creates missing target dirs
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@52848 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jacsutil/src/com/cosylab/util/FileHelper.java
+++ LGPL/CommonSoftware/jacsutil/src/com/cosylab/util/FileHelper.java
@@ -84,25 +84,35 @@ public class FileHelper {
 		}
 	}
 	
-	public static void copy(File source, File dest) throws IOException {
-	     FileChannel in = null, out = null;
-	     try {          
-	          in = new FileInputStream(source).getChannel();
-	          out = new FileOutputStream(dest).getChannel();
-	 
-	          long size = in.size();
-	          MappedByteBuffer buf = in.map(FileChannel.MapMode.READ_ONLY, 0, size);
-	 
-	          out.write(buf);
-	 
-	     } finally {
-	          if (in != null) {
-	        	  in.close();
-	          }
-	          if (out != null) {
-	        	  out.close();
-	          }
-	     }
+	/**
+	 * Copies file <code>source</code> to location <code>dest</code>.
+	 * Necessary directories are created automatically.
+	 * The modification time is preserved if <code>preserveTime</code> is <code>true</code>.
+	 */
+	public static void copy(File source, File dest, boolean preserveTime) throws IOException {
+		FileChannel in = null, out = null;
+		try {
+			dest.getParentFile().mkdirs();
+			
+			in = new FileInputStream(source).getChannel();
+			out = new FileOutputStream(dest).getChannel();
+
+			long size = in.size();
+			MappedByteBuffer buf = in.map(FileChannel.MapMode.READ_ONLY, 0, size);
+
+			out.write(buf);
+
+			if (preserveTime) {
+				dest.setLastModified(source.lastModified());
+			}
+		} finally {
+			if (in != null) {
+				in.close();
+			}
+			if (out != null) {
+				out.close();
+			}
+		}
 	}
 
 }
