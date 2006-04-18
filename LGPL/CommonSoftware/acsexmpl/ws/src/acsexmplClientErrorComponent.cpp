7143c855c33d624efb71284ae7cfc5b33d0dfccd
gchiozzi <gchiozzi@523d945c-050c-4681-91ec-863ad3bb968a>
2006-04-18 16:49:39 +0000

    Improved structure of example and added error handling.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@52915 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsexmpl/ws/src/acsexmplClientErrorComponent.cpp
+++ LGPL/CommonSoftware/acsexmpl/ws/src/acsexmplClientErrorComponent.cpp
@@ -20,7 +20,7 @@
 *
 *
 *
-* "@(#) $Id: acsexmplClientErrorComponent.cpp,v 1.1 2006/04/12 16:35:05 gchiozzi Exp $"
+* "@(#) $Id: acsexmplClientErrorComponent.cpp,v 1.2 2006/04/18 16:49:39 gchiozzi Exp $"
 *
 * who       when      what
 * --------  --------  ----------------------------------------------
@@ -48,7 +48,7 @@
     @endhtmlonly
  */
 
-/** @defgroup ACSEXMPLCLIENTHWDOC Client Hello World
+/** @defgroup ACSEXMPLCLIENTERROROCOMPDOC Client Error Component
  *  @{
  * @htmlonly
 <hr size="2" width="100%">
@@ -57,18 +57,19 @@
 This example shows a client that:
 <ul>
   <li>logs into manager via SimpleClient</li>
-  <li>activates the HELLOWORLD component specified from the command-line</li>
-  <li>calls the displayMessage() method</li>
+  <li>activates the ErrorComponent component specified from the command-line</li>
+  <li>calls the methods in this component to show examples of error handling</li>
   <li>releases the component</li>
   <li>logs out of manager</li>
 </ul>
 <br>
+Error handling examples are encapsulated in the ClientErrorComponent class.
+Each method in the class shows an example.
 <br>
 <h2>What can I gain from this example?</h2>
 <ul>
   <li>SimpleClient usage.</li>
-  <li>Using manager directly through SimpleClient.</li>
-  <li>Accessing (remote) components.</li>
+  <li>Dealing with errors accessing (remote) components.</li>
 </ul>
 <br>
 <br>
@@ -86,12 +87,226 @@ This example shows a client that:
 #include <acsexmplErrorComponentC.h>
 #include <ACSErrTypeCommon.h>
 #include <acsutilTimeStamp.h>
+#include <string.h>
 
-ACE_RCSID(acsexmpl, acsexmplErrorComponentClient, "$Id: acsexmplClientErrorComponent.cpp,v 1.1 2006/04/12 16:35:05 gchiozzi Exp $")
+ACE_RCSID(acsexmpl, acsexmplErrorComponentClient, "$Id: acsexmplClientErrorComponent.cpp,v 1.2 2006/04/18 16:49:39 gchiozzi Exp $")
 using namespace maci;
 
 /*******************************************************************************/
-    
+
+/**
+ * This class demonstrates error handling when accessing a (remote)
+ * component.
+ * 
+ * When the class is instantiated, it gets a reference to the 
+ * ErrorComponent counterpart throw the manager services provided
+ * by the SimpleClient.
+ * When the destructor is called, the reference to the component is released.
+ * This strategy ensures a clean handling of references to Components.
+ *
+ * Each of the public methods of the class demonstrates some error handling pattern.
+ * Each method is self-contained and it should never throw exceptions in itself,
+ * but for ACSErrTypeCommon::CouldntAccessComponentEx, to be used when the
+ * reference to the component is not properly initialised.
+ * Other errors are always completely handled internally.
+ */
+class ClientErrorComponent
+{
+  public:
+    ClientErrorComponent(SimpleClient &client, char *errorComponent) 
+	throw(CORBA::SystemException, ACSErrTypeCommon::CouldntAccessComponentExImpl);
+    virtual ~ClientErrorComponent();
+
+    /**
+     * Here everything should go fine.
+     */
+    void TestOk() throw(ACSErrTypeCommon::CouldntAccessComponentExImpl);
+
+    /**
+     * Example 1: Calls a method that throws an exception
+     *            with a stack trace..
+     * <ul>
+     *   <li> Catches the exception, 
+     *   <li> Adds context information
+     *   <li> sends it to the logging system
+     * </ul>
+     */
+    void TestReceiveRemoteException() throw(ACSErrTypeCommon::CouldntAccessComponentExImpl);
+
+    /**
+     * Example 2: Calls a method that returns a completion
+     *            If the completion contains an error, then
+     * <ul>
+     *   <li> Catches the exception, 
+     *   <li> prints it locally 
+     *   <li> sends it to the logging system
+     * </ul>
+     */
+    void TestReceiveRemoteCompletion() throw(ACSErrTypeCommon::CouldntAccessComponentExImpl);
+
+  private:
+    SimpleClient &client_m;
+    std::string   errorComponent_m;
+    acsexmplErrorComponent::ErrorComponent_var foo_m;
+};
+
+ClientErrorComponent::ClientErrorComponent(SimpleClient &client, 
+					   char *errorComponent)
+    throw(CORBA::SystemException, ACSErrTypeCommon::CouldntAccessComponentExImpl): 
+    client_m(client), errorComponent_m(errorComponent)
+{
+    ACS_TRACE("ClientErrorComponent::ClientErrorComponent");
+
+    /*
+     * Get the specific component we have requested
+     * Add exception handling.
+     */
+
+    foo_m = 
+	client_m.get_object<acsexmplErrorComponent::ErrorComponent>(errorComponent_m.c_str(), 0, true);    
+    if (CORBA::is_nil(foo_m.in()) == true)
+	{
+	throw ACSErrTypeCommon::CouldntAccessComponentExImpl(
+				   __FILE__, __LINE__,
+				   "ClientErrorComponent::ClientErrorComponent");
+	}
+}
+
+ClientErrorComponent::~ClientErrorComponent()
+{ 
+    ACS_TRACE("ClientErrorComponent::~ClientErrorComponent");
+
+   /*
+     * Release the component
+     */
+    client_m.manager()->release_component(client_m.handle(), errorComponent_m.c_str());
+}
+
+void ClientErrorComponent::TestOk() 
+    throw(ACSErrTypeCommon::CouldntAccessComponentExImpl)
+{
+    ACS_TRACE("ClientErrorComponent::TestOk");
+    if (CORBA::is_nil(foo_m.in()) == true)
+	{
+	throw ACSErrTypeCommon::CouldntAccessComponentExImpl(
+				   __FILE__, __LINE__,
+				   "ClientErrorComponent::TestOk");
+	}
+
+    try
+	{
+	foo_m->displayMessage();
+	}
+    catch(CORBA::SystemException &ex)
+	{
+	// Map......
+	ACSErrTypeCommon::GenericErrorExImpl displayMessageEx(
+				   __FILE__, __LINE__,
+				   "ClientErrorComponent::TestReceiveRemoteException");
+	displayMessageEx.setErrorDesc("badMethod has thrown a CORBA exception");
+	displayMessageEx.log();
+	}
+    catch(...)
+	{
+	ACSErrTypeCommon::GenericErrorExImpl displayMessageEx(__FILE__, __LINE__,
+		      				 "ClientErrorComponent::TestReceiveRemoteException");
+	displayMessageEx.setErrorDesc("badMethod has thrown an UNEXPECTED exception");
+	displayMessageEx.log();
+	}
+}
+
+void ClientErrorComponent::TestReceiveRemoteException() 
+    throw(ACSErrTypeCommon::CouldntAccessComponentExImpl)
+{
+    ACS_TRACE("ClientErrorComponent::TestReceiveRemoteException");
+
+    if (CORBA::is_nil(foo_m.in()) == true)
+	{
+	throw ACSErrTypeCommon::CouldntAccessComponentExImpl(
+				   __FILE__, __LINE__,
+				   "ClientErrorComponent::TestReceiveRemoteException");
+	}
+    ACS_SHORT_LOG((LM_INFO, "Example 1: Calls a method that throws an exception."));
+    try
+	{
+	foo_m->badMethod(3);
+	}
+    catch(ACSErrTypeCommon::GenericErrorEx &ex)
+	{
+	ACSErrTypeCommon::GenericErrorExImpl badMethodEx(ex,
+				   __FILE__, __LINE__,
+				   "ClientErrorComponent::TestReceiveRemoteException");
+	badMethodEx.setErrorDesc("badMethod has thrown the expected exception");
+	badMethodEx.log();
+
+	ACS::Time timeStamp = badMethodEx.getTimeStamp();
+	ACE_CString tString = getStringifiedUTC(timeStamp);
+	ACS_DEBUG_PARAM("ClientErrorComponent::TestReceiveRemoteException", 
+			"Time of the exception: %s\n", tString.c_str());
+	}
+    catch(CORBA::SystemException &ex)
+	{
+	// Map......
+	ACSErrTypeCommon::GenericErrorExImpl badMethodEx(
+				   __FILE__, __LINE__,
+				   "ClientErrorComponent::TestReceiveRemoteException");
+	badMethodEx.setErrorDesc("badMethod has thrown a CORBA exception");
+	badMethodEx.log();
+
+	ACS::Time timeStamp = badMethodEx.getTimeStamp();
+	ACE_CString tString = getStringifiedUTC(timeStamp);
+	ACS_DEBUG_PARAM("ClientErrorComponent::TestReceiveRemoteException", 
+			"Time of the CORBA exception: %s\n", tString.c_str());
+	}
+    catch(...)
+	{
+	ACSErrTypeCommon::GenericErrorExImpl badMethodEx(__FILE__, __LINE__,
+		      				 "ClientErrorComponent::TestReceiveRemoteException");
+	badMethodEx.setErrorDesc("badMethod has thrown an UNEXPECTED exception");
+	badMethodEx.log();
+
+	ACS::Time timeStamp = badMethodEx.getTimeStamp();
+	ACE_CString tString = getStringifiedUTC(timeStamp);
+	ACS_DEBUG_PARAM("ClientErrorComponent::TestReceiveRemoteException", 
+			"Time of the unexpected exception: %s\n", tString.c_str());
+	}
+
+}
+
+void ClientErrorComponent::TestReceiveRemoteCompletion() 
+    throw(ACSErrTypeCommon::CouldntAccessComponentExImpl)
+{
+    ACS_TRACE("ClientErrorComponent::TestReceiveRemoteCompletion");
+
+    if (CORBA::is_nil(foo_m.in()) == true)
+	{
+	throw ACSErrTypeCommon::CouldntAccessComponentExImpl(
+				   __FILE__, __LINE__,
+				   "ClientErrorComponent::TestReceiveRemoteCompletion");
+	}
+    try
+	{
+	// OK Completion
+	ACS_SHORT_LOG((LM_INFO, "Example 2a: Calls a method that returns an OK completion."));
+	CompletionImpl comp = foo_m->returnCompletion(0);
+	comp.log();
+
+	// ERROR completion with an error trace inside.
+	ACS_SHORT_LOG((LM_INFO, "Example 2b: Calls a method that returns an Error completion."));
+	CompletionImpl comp2 = foo_m->returnCompletion(3);
+	comp2.log();
+	}
+    catch(...)
+	{
+	ACSErrTypeCommon::GenericErrorExImpl badMethodEx(__FILE__, __LINE__,
+							 "ClientErrorComponent::TestReceiveRemoteCompletion");
+	badMethodEx.setErrorDesc("returnCompletion has thrown an UNEXPECTED exception");
+	badMethodEx.log();
+	}
+}
+
+/*******************************************************************************/
+
 int main(int argc, char *argv[])
 {
     // Creates and initializes the SimpleClient object
@@ -105,93 +320,49 @@ int main(int argc, char *argv[])
 	//Must log into manager before we can really do anything
 	client.login();
 	}
-   
-    //Get the specific component we have requested on the command-line
-    acsexmplErrorComponent::ErrorComponent_var foo = client.get_object<acsexmplErrorComponent::ErrorComponent>(argv[1], 0, true);
-    
-    //Call the displayMessage() method existing in the interface for ErrorComponent
-    foo->displayMessage();
 
-    /********************************************************
-     * Example 1: Calls a method that throws an exception
-     *            with a stack trace..
-     * - Catches the exception, 
-     * - Adds context information
-     * - sends it to the logging system
-     */
-    ACS_SHORT_LOG((LM_INFO, "Example 1: Calls a method that throws an exception."));
-    try
-	{
-	foo->badMethod(3);
-	}
-    catch(ACSErrTypeCommon::GenericErrorEx &ex)
-	{
-	ACSErrTypeCommon::GenericErrorExImpl badMethodEx(ex,
-				   __FILE__, __LINE__,
-				   "main");
-	badMethodEx.setErrorDesc("badMethod has thrown the expected exception");
-	badMethodEx.log();
-
-	ACS::Time timeStamp = badMethodEx.getTimeStamp();
-	ACE_CString tString = getStringifiedUTC(timeStamp);
-	ACS_DEBUG_PARAM(argv[0], "Time of the exception: %s\n", tString.c_str());
-	}
-    catch(CORBA::SystemException &ex)
-	{
-	// Map......
-	ACSErrTypeCommon::GenericErrorExImpl badMethodEx(
-				   __FILE__, __LINE__,
-				   "main");
-	badMethodEx.setErrorDesc("badMethod has thrown a CORBA exception");
-	badMethodEx.log();
-
-	ACS::Time timeStamp = badMethodEx.getTimeStamp();
-	ACE_CString tString = getStringifiedUTC(timeStamp);
-	ACS_DEBUG_PARAM(argv[0], "Time of the CORBA exception: %s\n", tString.c_str());
-	}
-    catch(...)
-	{
-	ACSErrTypeCommon::GenericErrorExImpl badMethodEx(__FILE__, __LINE__,
-							 "main");
-	badMethodEx.setErrorDesc("badMethod has thrown an UNEXPECTED exception");
-	badMethodEx.log();
-
-	ACS::Time timeStamp = badMethodEx.getTimeStamp();
-	ACE_CString tString = getStringifiedUTC(timeStamp);
-	ACS_DEBUG_PARAM(argv[0], "Time of the unexpected exception: %s\n", tString.c_str());
-	}
-
-    /********************************************************
-     * Example 2: Calls a method that returns a completion
-     *            If the completion contains an error, then
-     * - Catches the exception, 
-     * - prints it locally 
-     * - sends it to the logging system
+    /**************************************************************************
+     * Here we instantiate the object used to show examples of error handling.
+     * Each method call demonstrate one aspect of error hanlding.
+     * See the class documentation for details.
      */
     try
 	{
-	// OK Completion
-	ACS_SHORT_LOG((LM_INFO, "Example 2a: Calls a method that returns an OK completion."));
-	CompletionImpl comp = foo->returnCompletion(0);
-	comp.log();
-
-	// ERROR completion with an error trace inside.
-	ACS_SHORT_LOG((LM_INFO, "Example 2b: Calls a method that returns an Error completion."));
-	CompletionImpl comp2 = foo->returnCompletion(3);
-	comp2.log();
+	ClientErrorComponent clientErrorComponent(client, argv[1]);
+	
+	//Call the displayMessage() method existing in the interface for ErrorComponent
+	clientErrorComponent.TestOk();
+	clientErrorComponent.TestReceiveRemoteException();
+	clientErrorComponent.TestReceiveRemoteCompletion();
+	}
+    catch(ACSErr::ACSbaseExImpl ex)
+	{
+	/*
+	 * We should never get here, because the methods in the example
+	 * should be all self contained and none of them should throw
+	 * any exception.
+	 */
+	ACSErrTypeCommon::GenericErrorExImpl badMethodEx(ex, __FILE__, __LINE__,
+							 "main");
+	badMethodEx.setErrorDesc("Examples of error handling have thrown an ACS exception");
+	badMethodEx.log();
 	}
     catch(...)
 	{
+	/*
+	 * We should never get here, because the methods in the example
+	 * should be all self contained and none of them should throw
+	 * any exception.
+	 */
 	ACSErrTypeCommon::GenericErrorExImpl badMethodEx(__FILE__, __LINE__,
 							 "main");
-	badMethodEx.setErrorDesc("returnCompletion has thrown an UNEXPECTED exception");
+	badMethodEx.setErrorDesc("Examples of error handling have thrown an UNEXPECTED exception");
 	badMethodEx.log();
 	}
 
     /****************************************************
-     * We release our component and logout from manager
+     * We logout from manager
      */
-    client.manager()->release_component(client.handle(), argv[1]);
     client.logout();
     
     //Sleep for 3 sec to allow everything to cleanup and stablize
