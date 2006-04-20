05edeefcdefe91d9b3ab22c6f53a1e93d5075de4
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2006-04-20 08:53:29 +0000

    added XPathMatchSetups; added logger
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@53011 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jacsutil/src/alma/acs/config/validators/ConfigFileRedeemerXml.java
+++ LGPL/CommonSoftware/jacsutil/src/alma/acs/config/validators/ConfigFileRedeemerXml.java
@@ -23,9 +23,12 @@
 package alma.acs.config.validators;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilder;
@@ -35,8 +38,8 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
 
-import org.w3c.dom.NodeList;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.helpers.DefaultHandler;
 
 
@@ -49,44 +52,78 @@ public class ConfigFileRedeemerXml extends ConfigFileRedeemer {
 
 	protected SAXParser parser;
 	protected DefaultHandler saxHandler;
-//	protected XmlNamespaceContextContainer nsContext;
 	protected List<XPathMatchSetup> xpathMatchSetupList;
+	
+	public static final String XSD_NS = "http://www.w3.org/2001/XMLSchema";
 	
 	/**
 	 * 
 	 */
-	public ConfigFileRedeemerXml() throws Exception {
+	public ConfigFileRedeemerXml(Logger logger) throws Exception {
+		super(logger);
 		xpathMatchSetupList = new ArrayList<XPathMatchSetup>();
 		configure();
-		
-//		nsContext = new XmlNamespaceContextContainer();
-		
-//		SAXParserFactory saxfac = SAXParserFactory.newInstance();
-//		saxfac.setNamespaceAware(true);
-//		parser = saxfac.newSAXParser();
-//		
-//		saxHandler = new DefaultHandler() {
-//			public void startElement(String uri, String localName, String qName, Attributes atts) {
-//				startSaxElement(uri, localName, qName, atts);
-//			}
-//		};
 	}
+
 	
 	/**
 	 * Configures information about known files which should not be accused of being config files.
 	 */
 	protected void configure() {
-//		addXPathMatch(new XPathMatchSetup("/bla:Container", "bla", "urn:schemas-cosylab-com:Container:1.0", XPathConstants.NODE));		
+		// Castor config files
 		addXPathMatch(new XPathMatchSetup("/EntitybuilderSettings/EntitySchema", null, null, XPathConstants.NODESET));
-		
+		// ACS error system type/code definition files
 		addXPathMatch(new XPathMatchSetup("/Type/@type", null, null, XPathConstants.NUMBER));
 		addXPathMatch(new XPathMatchSetup("/bla:Type/@type", "bla", "Alma/ACSError", XPathConstants.NUMBER)); // error schema used to have a target namespace
+
+		// UML-XMI files
+		addXPathMatch(new XPathMatchSetup("/XMI/@xmi.version", null, null, XPathConstants.NUMBER));
+		// APDM entities
+		addXPathMatch(new XPathMatchSetup("/oprp:ObsProposal", "oprp", "Alma/ObsPrep/ObsProposal", XPathConstants.NODE)); 
+		addXPathMatch(new XPathMatchSetup("/oprj:ObsProject", "oprj", "Alma/ObsPrep/ObsProject", XPathConstants.NODE)); 
+		addXPathMatch(new XPathMatchSetup("/sbl:SchedBlock", "sbl", "Alma/ObsPrep/SchedBlock", XPathConstants.NODE)); 		
+		addXPathMatch(new XPathMatchSetup("/xsd:schema[@targetNamespace='Alma/ObsPrep/ObsProposal']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema[@targetNamespace='Alma/ObsPrep/ObsProject']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema[@targetNamespace='Alma/ObsPrep/ObsReview']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema[@targetNamespace='Alma/ObsPrep/SchedBlock']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema[@targetNamespace='Alma/ObsPrep/ProjectStatus']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema[@targetNamespace='Alma/ValueTypes']", "xsd", XSD_NS, XPathConstants.NODE));
+
+		// OAW mappings
+		addXPathMatch(new XPathMatchSetup("/MetaMap", null, null, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/MetaModel", null, null, XPathConstants.NODE));		
 		
 		addXPathMatch(new XPathMatchSetup("/bla:AcsCommandCenterProject", "bla", "Alma/Acs/AcsCommandCenterProject", XPathConstants.NODE));		
+		addXPathMatch(new XPathMatchSetup("/bla:AcsCommandCenterTools", "bla", "Alma/Acs/AcsCommandCenterTools", XPathConstants.NODE));		
+		addXPathMatch(new XPathMatchSetup("/bla:cosydoc", "bla", "urn:schemas-cosylab-com:Document", XPathConstants.NODE));
+		
+		// known ACS schema files
+		addXPathMatch(new XPathMatchSetup("/xsd:schema/xsd:simpleType[@name='ErrorType']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema/xsd:element[@name='AcsCommandCenterProject']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema/xsd:element[@name='AcsCommandCenterTools']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema/xsd:complexType[@name='EventChannel']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema/xsd:element[@name='SimulatedComponent']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema/xsd:complexType[@name='EntityT']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema/xsd:element[@name='IdentifierRange']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema/xsd:element[@name='ComponentHelperInfo']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema/xsd:element[@name='EntitybuilderSettings']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema/xsd:complexType[@name='ComponentInfo']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema/xsd:complexType[@name='Container']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema/xsd:complexType[@name='Manager']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema/xsd:element[@name='Log']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema/xsd:element[@name='paramset']", "xsd", XSD_NS, XPathConstants.NODE));
+		addXPathMatch(new XPathMatchSetup("/xsd:schema/xsd:element[@name='pset']", "xsd", XSD_NS, XPathConstants.NODE));
+		
+		// Ant build files (incl. Eclipse external build files)
+		addXPathMatch(new XPathMatchSetup("/project/@basedir", null, null, XPathConstants.STRING));
+		addXPathMatch(new XPathMatchSetup("/project/target/javadoc", null, null, XPathConstants.NODESET));
+				
+		// addXPathMatch(new XPathMatchSetup("/bla:Container", "bla", "urn:schemas-cosylab-com:Container:1.0", XPathConstants.NODE));		
+		
 	}
 	
 	public String[] getFileEndings() {
-		return new String[] {".xml"};
+		return new String[] {".xml", ".xsd", ".xslt"};
 	}
 
 	
@@ -119,6 +156,14 @@ public class ConfigFileRedeemerXml extends ConfigFileRedeemer {
 					else if (xpathSetup.getReturnType().equals(XPathConstants.NODESET) && ((NodeList)xpathResult).getLength() == 0) {
 						continue;
 					}
+					// STRING must not be empty
+					else if (xpathSetup.getReturnType().equals(XPathConstants.STRING) && ((String)xpathResult).trim().length() == 0) {
+						continue;
+					}
+					else if (xpathSetup.getReturnType().equals(XPathConstants.NODE)) {
+//						Node node = (Node) xpathResult;
+//						System.out.println("*** got a node: " + node.toString());
+					}
 					System.out.println("XML file '" + xmlFile + "' is not a config file. " + 
 							"The query '" + xpathSetup.getExpression() + "' found a matching " + xpathSetup.getReturnType().getLocalPart());
 					return true;
@@ -127,20 +172,13 @@ public class ConfigFileRedeemerXml extends ConfigFileRedeemer {
 			
 			return false;
 			
-//			parser.parse(xmlFile, saxHandler);
-//			return true;
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.err.println("xml file '" + xmlFile + "' failed to be parsed and will be treated as suspect.");
+		}
+		catch (Exception e) {
+			logger.log(Level.WARNING, "xml file '" + xmlFile + "' failed to be parsed and will be treated as suspect.", e);
 			return false;
 		}
 	}
 
-//	protected void startSaxElement(String uri, String localName, String qName, Attributes atts) {
-//		// todo: try to recognize the element.
-//		System.out.println("XML element: " + qName);
-//	}
-	
 	
 	public static class XPathMatchSetup {
 		private String expression;
