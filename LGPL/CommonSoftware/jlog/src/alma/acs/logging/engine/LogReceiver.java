5e64d172e8d9c653bedafebefb9b7a65fe4832b6
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2006-04-27 20:38:11 +0000

    modified version of an unsuccessful previous attempt under ITS/OP/test. Now uses refactored jlog interfaces, which accept external ORB and manager. Not yet tested.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@53434 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/jlog/src/alma/acs/logging/engine/LogReceiver.java
@@ -0,0 +1,426 @@
+/*
+ *    ALMA - Atacama Large Millimiter Array
+ *    (c) European Southern Observatory, 2005
+ *    Copyright by ESO (in the framework of the ALMA collaboration),
+ *    All rights reserved
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *    MA 02111-1307  USA
+ */
+
+package alma.acs.logging.engine;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.omg.CORBA.ORB;
+
+import si.ijs.maci.Manager;
+
+import com.cosylab.logging.engine.ACS.ACSRemoteLogListener;
+import com.cosylab.logging.engine.ACS.LCEngine;
+import com.cosylab.logging.engine.log.ILogEntry;
+import com.cosylab.logging.engine.log.LogEntry;
+
+
+/**
+ * Receives log messages from the ACS Log service 
+ * and sorts them by timestamp, even if the log messages arrive
+ * in different order within a certain time window.
+ * <p>
+ * For a client of this class to consume these log messages, 
+ * method {@link #getLogQueue()} provides a {@link java.util.concurrent.BlockingQueue}
+ * from which the client can extract the messages.
+ * <p>
+ * Before log messages can be received, {@link #initialize(ORB, Manager)} must be called.
+ * To disconnect from the the log stream, call {@link #stop()}.
+ * <p>
+ * As an alternative to <code>getLogQueue</code> and <code>stop</code>, 
+ * this class also offers the method pair {@link #startCaptureLogs(PrintWriter)} 
+ * and {@link #stopCaptureLogs()}. These methods can be used to directly 
+ * print the received log messages to some writer, sparing the effort of listening on the LogQueue.
+ * Method {@link #stopCaptureLogs()} must be called in that case to stop capturing logs. 
+ *   
+ * @author hsommer
+ */
+public class LogReceiver {
+
+
+	private boolean verbose = false;
+
+	protected LCEngine lct = null;
+	protected MyRemoteResponseCallback rrc = null;
+	
+	// the queue into which all log records are stored
+	private DelayQueue<DelayedLogEntry> logDelayQueue;
+	
+	private ArrayList<String> statusReports;
+	
+	private volatile boolean listenForLogs;
+    private final int sortingDelayMillis = 20000;
+
+	
+	public LogReceiver() {
+		super();
+	}
+
+	public boolean isVerbose() {
+		return verbose;
+	}
+	public void setVerbose(boolean verbose) {
+		this.verbose = verbose;
+	}
+	
+    /**
+     * Initializes the connection to the logging channel, which leads
+     * to log entries getting written to the sorting queue.
+     * To reuse an existing ORB and manager reference, use {@link #initialize(ORB, Manager)} instead of this method.
+     * <p>
+     * If you call this method, make sure to subsequently call
+     * {@link #getLogQueue()} and drain the queue at your own responsibility,
+     * or to call {@link #startCaptureLogs(PrintWriter)} (or {@link #startCaptureLogs(PrintWriter, ThreadFactory)})
+     * which will drain the queue automatically.
+     */
+    public void initialize() {
+        initialize(null, null);
+    }
+    
+    /**
+     * Variant of {@link #initialize()} which takes an existing ORB and manager reference.
+     */
+	public void initialize(ORB theORB, Manager manager) {
+		if (verbose) {
+			System.out.println("Attempting to connect to Log channel...");
+		}
+
+		logDelayQueue = new DelayQueue<DelayedLogEntry>();
+		statusReports = new ArrayList<String>();
+		
+		rrc = new MyRemoteResponseCallback(logDelayQueue, statusReports);
+		rrc.setVerbose(verbose);
+
+        lct = new LCEngine(rrc);
+
+		lct.setAccessType("ACS");
+		lct.connect(theORB, manager);
+	}
+	
+    public boolean isInitialized() {
+        return lct.isConnected();
+    }
+    
+	/**
+	 * Gets access to the log record queue, from which the time-sorted log records should be fetched.
+	 * <p>
+	 * The queue elements are of type {@link DelayedLogEntry},
+	 * from which the log record can be extracted
+	 * using the method {@link DelayedLogEntry#getLogEntry()}.
+	 * @return
+	 */
+	public BlockingQueue<DelayedLogEntry> getLogQueue() {
+		return logDelayQueue;
+	}
+	
+	/**
+	 * Gets the status reports. Currently only a forceful disconnect would be shown,
+	 * the rest is ignored.
+	 * TODO: separate good and bad status reports
+	 * @return
+	 */
+	public String[] getStatusReports() {
+		return statusReports.toArray(new String[statusReports.size()]); 
+	}
+
+	public void stop() {
+		if (verbose) {
+			System.out.println("Attempting to destroy LogConnect...");
+		}
+		lct.disconnect();
+	}
+		
+	
+	
+	/**
+	 * Callback class that receives log data from {@link LCEngine}. 
+	 */
+	static class MyRemoteResponseCallback implements ACSRemoteLogListener {
+		private boolean verbose = false;
+		
+		private ArrayList<String> statusReports;
+		private DelayQueue<DelayedLogEntry> logDelayQueue;
+		
+		MyRemoteResponseCallback(DelayQueue<DelayedLogEntry> logDelayQueue, ArrayList<String> statusReports) {
+			this.logDelayQueue = logDelayQueue;
+			this.statusReports = statusReports;
+		}
+		
+        public void acsLogConnConnecting() {
+            if (verbose) {
+                System.out.println("LogReceiver#acsLogConnConnecting()");             
+            }
+        }
+        public void acsLogConnEstablished() {
+            if (verbose) {
+                System.out.println("LogReceiver#acsLogConnEstablished()");             
+            }
+        }
+        public void acsLogConnLost() {
+            if (verbose) {
+                System.out.println("LogReceiver#acsLogConnLost()");             
+            }
+        }
+
+//        public void invocationDestroyed() {
+//			System.out.println("LogReceiver: invocationDestroyed");	
+//			statusReports.add("Log service terminated!");
+//		}
+		
+		public void reportStatus(String status) {
+			if (verbose) {
+				System.out.println("LogReceiver status report: " + status);				
+			}
+//			statusReports.add(status);
+		}
+
+		public void logEntryReceived(ILogEntry logEntry) {
+			if (verbose) {
+				System.out.println("*** received ILogEntry");
+			}
+			DelayedLogEntry delayedLogEntry = new DelayedLogEntry(logEntry);
+			// add the record to the queue
+			logDelayQueue.offer(delayedLogEntry);
+		}
+
+		boolean isVerbose() {
+			return verbose;
+		}
+
+		void setVerbose(boolean verbose) {
+			this.verbose = verbose;
+		}
+		
+// TODO: these are normal status messages which should be ignored,
+// while other messages should raise an error. Currently all messages are ignored.		
+//		Connecting to ACS remote access...
+//		Initializing CORBA...
+//		CORBA initialized.
+//		Resolving corbaloc::acc:3000/Manager manager reference...
+//		Manager reference resolved.
+//		Resolving Naming Service...
+//		Naming Service resolved.
+//		Resolving channel "LoggingChannel" from Notify Service...
+//		Channel "LoggingChannel" resolved.
+//		Creating Consumer Admin...
+//		Consumer Admin created.
+//		Initializing Structured Push Consumer...
+//		Structured Push Consumer initialized.
+//		Connected to ACS remote access.
+
+	}	
+
+	
+	
+	/**
+	 * Wraps an {@link ILogEntry} for storage in a {@link java.util.concurrent.DelayQueue}.
+	 * <p>
+	 * Method {@link #setDelayTimeMillis(long)} sets the buffer time during which log entries
+	 * are not yet available for the consumer, so that late arriving records get a chance
+	 * to be sorted in according to timestamp.
+	 * 
+	 * @author hsommer
+	 */
+	static class DelayedLogEntry implements Delayed {
+
+		/** default delay for sorting by timestamp */
+		private static long delayTimeMillis = 20000;
+		
+		private static final AtomicInteger logRecordCounter = new AtomicInteger();
+
+		int logRecordIndex;
+		ILogEntry logEntry;
+		long triggerTimeMillis;
+
+		DelayedLogEntry(ILogEntry logEntry) {
+			logRecordIndex = logRecordCounter.incrementAndGet();
+			this.logEntry = logEntry;
+			Date logDate = (Date) logEntry.getField(LogEntry.FIELD_TIMESTAMP);
+			triggerTimeMillis = logDate.getTime() + delayTimeMillis;
+		}
+
+		/**
+		 * Returns the <code>ILogEntry</code> class that was wrapped for sorting inside the queue.
+		 * That class represents the log record as it was received from the logging service.
+		 */
+		public ILogEntry getLogEntry() {
+			return logEntry;
+		}
+
+		/**
+		 * This method is used by the queue to determine whether the log record may 
+		 * leave the queue already.
+		 */
+		public long getDelay(TimeUnit unit) {
+			long delay = triggerTimeMillis - System.currentTimeMillis();
+			return unit.convert(delay, TimeUnit.MILLISECONDS);
+		}
+		
+		/**
+		 * This method is used by the queue for sorting.
+		 */
+		public int compareTo(Delayed other) {
+			DelayedLogEntry otherDelayedLogEntry = (DelayedLogEntry) other;
+			long i = triggerTimeMillis;
+			long j = (otherDelayedLogEntry).triggerTimeMillis;
+			int returnValue;
+			if (i < j) {
+				returnValue = -1;
+			} else if (i > j) {
+				returnValue = 1;
+			} else {
+				// if timestamps are equal we sort by arrival order
+				if (this.logRecordIndex < otherDelayedLogEntry.logRecordIndex) {
+					returnValue = -1;
+				} else if (this.logRecordIndex > otherDelayedLogEntry.logRecordIndex) {
+					returnValue = 1;
+				} else {
+					// this should never happen
+					returnValue = 0;
+				}
+			}
+			return returnValue;
+		}
+
+		/**
+		 * Equals method, just to be consistent with <code>compareTo</code>.
+		 */
+		public boolean equals(Object other) {
+			DelayedLogEntry otherDelayedLogEntry = (DelayedLogEntry) other;
+			return ( otherDelayedLogEntry.triggerTimeMillis == triggerTimeMillis &&
+					otherDelayedLogEntry.logRecordIndex == logRecordIndex );
+		}
+
+		static long getDelayTimeMillis() {
+			return delayTimeMillis;
+		}
+
+		static void setDelayTimeMillis(long delayTimeMillis) {
+			DelayedLogEntry.delayTimeMillis = delayTimeMillis;
+		}
+
+	}
+
+    /**
+     * Convenience method to capture logs directly into a PrintWriter.
+     * Method {@link #initialize(ORB, Manager)} must be called as a precondition.
+     * Method {@link #stopCaptureLogs()} must be called to stop writing logs to <code>logWriter</code>.
+     * <p>
+     *  
+     * @param logWriter 
+     * @throws IOException
+     */
+    public void startCaptureLogs(final PrintWriter logWriter) throws IOException {
+        startCaptureLogs(logWriter, null);
+    }
+    
+	/**
+     * Variant of {@link #startCaptureLogs(PrintWriter)} which takes an optional ThreadFactory
+     * which will be used to create the thread that reads out the log queue.
+     * This method could be used if <code>LogReceiver</code> is run inside a container 
+     * or as part of a ComponentClientTestCase.
+	 */
+	public void startCaptureLogs(final PrintWriter logWriter, ThreadFactory threadFactory) throws IOException {
+        if (!isInitialized()) {
+            throw new IllegalStateException("First call LogReceiver#initialize(ORB, Manager), then startCaptureLogs(PrintWriter)");
+        }
+		if (listenForLogs) {
+            if (verbose) {
+                System.out.println("Ignoring call to 'startCaptureLogs' while already capturing logs.");
+            }
+			return;
+		}
+		
+		Runnable logRetriever = new Runnable() {
+
+            public void run() {
+				try {
+					DelayedLogEntry.setDelayTimeMillis(sortingDelayMillis); // delay 20 sec for sorting incoming log records by timestamp
+					BlockingQueue logQueue = getLogQueue();
+					
+					listenForLogs = true;
+					// loop until stopCaptureLogs changes 'listenForLogs' flag
+					while (listenForLogs == true) {
+						try {
+							// extract logs from queue
+							DelayedLogEntry delayedLogEntry = (DelayedLogEntry) logQueue.poll(1L, TimeUnit.SECONDS);
+							if (delayedLogEntry != null) {
+								ILogEntry logEntry = delayedLogEntry.getLogEntry();
+								String xmlLog = logEntry.toXMLString();
+								logWriter.println(xmlLog);
+							}
+							else {
+//								System.out.println("timed out waiting for log records. Will check termination status and try again...");
+							}
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+					// it's over
+					try {
+						// wait for log records to leave the queue (or reduce DelayedLogEntry.delayTimeMillis to speed this up??)
+                        // or use a "queue poison", i.e. a special record, to trigger the destruction?
+						Thread.sleep(sortingDelayMillis);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				} catch (Throwable thr) {
+					System.out.println("Log receiver got exception " + thr);
+					logWriter.println("Log receiver failed with exception " + thr.toString());
+				} finally {
+					logWriter.close();
+                    // we only call stop now, because this way late arriving records will still be added 
+                    // to the queue during the whole waiting time.
+					stop();
+				}
+			}
+		};
+        Thread t = null;
+        if (threadFactory != null) {
+            t = threadFactory.newThread(logRetriever);
+        }
+        else {
+            t = new Thread();
+            t.setDaemon(true);
+        }        
+		t.start();
+	}
+	
+	/**
+     * Stops capturing logs into the PrintWriter that was provided to {@link #startCaptureLogs(PrintWriter)}.
+     * Even though the call to this method returns immediately, all log records that are  
+     * currently residing inside the queue will still be written out, waiting the due time to allow sorting them.
+	 */
+	public void stopCaptureLogs() {
+		listenForLogs = false; // will terminate while loop in startCaptureLogs
+	}	
+}
+
