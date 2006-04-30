b8ab59cc992cec6876215d261e9e8926d0c67bcf
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2006-04-30 21:15:57 +0000

    first check-in
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@53493 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/jcont/test/alma/acs/component/client/ClientWithLogReceiverTest.java
@@ -0,0 +1,88 @@
+/*
+ *    ALMA - Atacama Large Millimiter Array
+ *    (c) European Southern Observatory, 2004
+ *    Copyright by ESO (in the framework of the ALMA collaboration),
+ *    All rights reserved
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *    MA 02111-1307  USA
+ */
+package alma.acs.component.client;
+
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+import com.cosylab.logging.engine.log.ILogEntry;
+
+import alma.acs.logging.engine.LogReceiver;
+import alma.acs.logging.engine.LogReceiver.DelayedLogEntry;
+
+/**
+ * @author hsommer
+ * created 29.04.2006 10:27:51
+ */
+public class ClientWithLogReceiverTest extends ComponentClientTestCase {
+
+    private LogReceiver logReceiver;
+
+    public ClientWithLogReceiverTest() throws Exception {
+        super("ClientWithLogReceiverTest");
+    }
+    
+    protected void setUp() throws Exception {
+        super.setUp();
+        logReceiver = getLogReceiver();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+    
+    public void testInitialized() {
+        assertTrue(logReceiver.isInitialized());
+    }
+    
+    public void testLogQueueNoDelay() throws Exception {
+        logReceiver.setDelayMillis(0);
+//        PrintWriter logWriter = new PrintWriter(System.out); //new BufferedWriter(new FileWriter(logFile)));
+        BlockingQueue<DelayedLogEntry> queue = logReceiver.getLogQueue();
+        
+        final int numLogs = 5;
+        Level[] levels = new Level[] {Level.FINEST, Level.FINE, Level.INFO, Level.WARNING, Level.SEVERE};
+        Random random = new Random(System.currentTimeMillis());
+        for (int i=0; i<numLogs; i++) {
+            Level level = levels[random.nextInt(levels.length)];
+            String logMessage = "This is log number " + i;
+            m_logger.log(level, logMessage);
+            
+            // in spite of zero queue sorting delay, we need a long timeout 
+            // to compensate travel delay when sending log records to Log service,
+            // and then getting them back over the network.
+            long timeoutSec = 15L;
+            DelayedLogEntry delayedLogEntry = queue.poll(timeoutSec, TimeUnit.SECONDS);
+            if (delayedLogEntry != null) {
+                ILogEntry logEntry = delayedLogEntry.getLogEntry();
+                assertEquals(logMessage, logEntry.getField(ILogEntry.FIELD_LOGMESSAGE));
+                System.out.print(logEntry.getField(ILogEntry.FIELD_ENTRYTYPE));
+                System.out.println(" Received back log record #" + i);
+            }
+            else {
+                fail("Did not receive the expected log record #" + i + " within " + timeoutSec + " seconds.");
+            }
+        }
+    }
+}
