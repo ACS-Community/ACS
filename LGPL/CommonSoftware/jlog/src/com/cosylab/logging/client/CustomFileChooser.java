c0a9c9d8d64a07d8a125580691885a82406dcef0
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2006-05-10 09:21:11 +0000

    First working version integrated in the program.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@54018 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jlog/src/com/cosylab/logging/client/CustomFileChooser.java
+++ LGPL/CommonSoftware/jlog/src/com/cosylab/logging/client/CustomFileChooser.java
@@ -1,10 +1,7 @@
 package com.cosylab.logging.client;
 
 import javax.swing.JFileChooser;
-import javax.swing.JPanel;
-import javax.swing.JRootPane;
 import javax.swing.JDialog;
-import javax.swing.JScrollPane;
 import javax.swing.JComponent;
 import javax.swing.JCheckBox;
 
@@ -24,16 +21,35 @@ import java.io.File;
  */
 public class CustomFileChooser extends JDialog implements ActionListener {
 	
+	/**
+	 * The file selected by the user
+	 * It is null if the file has not been selected or the user pressed cancel
+	 */
+	private File selectedFile=null;
+	
+	/**
+	 * The JFileChooser shown in the center of  the main panel
+	 */
 	private JFileChooser fc;
 	
-	private CustomFileChooser(File currentDir,String title) {
+	/**
+	 * The checkbox to make visible hidden files and folfers
+	 *The default is false (i.e. hidden folders are not shown)
+	 */
+	private JCheckBox viewHiddenFiles = new JCheckBox("Show hidden files",false);
+	
+	/**
+	 * Constructor: build and show the modal dialog
+	 *  
+	 * @param currentDir The dir whose content is shown at startup
+	 * @param title The title of the window (it appears also in the button)
+	 */
+	public CustomFileChooser(File currentDir,String title) {
 		super(LoggingClient.getInstance(),title,true);
 		initialize(currentDir);
 		pack();
 		setVisible(true);
 	}
-	
-	private JCheckBox viewHiddenFiles = new JCheckBox("Show hidden files",false);
 	
 	/**
 	 * Initialize the GUI
@@ -47,15 +63,44 @@ public class CustomFileChooser extends JDialog implements ActionListener {
 		fc = new JFileChooser(curDir);
 		fc.setFileHidingEnabled(!viewHiddenFiles.isSelected());
 		fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		fc.addActionListener(this);
 		JComponent fcComponent = fc;
 		fcComponent.setOpaque(true);
 		rootPane.add(fcComponent,BorderLayout.CENTER);
 	}
 	
+	/**
+	 * 
+	 * @return The file selected by the user or null if the user pressed Cancel
+	 */
+	public File getSelectedFile() {
+		return selectedFile;
+	}
+	
+	/**
+	 * @return The current directory
+	 */
+	public File getCurrentDirectory() {
+		return fc.getCurrentDirectory();
+	}
+	
+	/**
+	 * @see java.awt.event.ActionListener
+	 */
 	public void actionPerformed(ActionEvent evt) {
 		if (evt.getSource()==viewHiddenFiles) {
 			fc.setFileHidingEnabled(!viewHiddenFiles.isSelected());
 			fc.rescanCurrentDirectory();
+		} else {
+			if (evt.getActionCommand().equals("ApproveSelection")) {
+				selectedFile=fc.getSelectedFile();
+				setVisible(false);
+				dispose();
+			} else if (evt.getActionCommand().equals("CancelSelection")) {
+				selectedFile=null;
+				setVisible(false);
+				dispose();
+			}
 		}
 	}
 }
