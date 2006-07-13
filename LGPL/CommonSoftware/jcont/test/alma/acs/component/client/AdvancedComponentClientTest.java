d773b4ca9153e9b73eaa11fce2de09e093634125
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2006-07-13 13:58:01 +0000

    first check-in
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@56007 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/jcont/test/alma/acs/component/client/AdvancedComponentClientTest.java
@@ -0,0 +1,47 @@
+package alma.acs.component.client;
+
+import java.util.logging.Logger;
+
+import alma.acs.container.ContainerException;
+import alma.acs.container.ContainerServices;
+import alma.acs.logging.ClientLogManager;
+import alma.jconttest.ContainerServicesTester;
+import alma.jconttest.ContainerServicesTesterHelper;
+
+import junit.framework.TestCase;
+
+public class AdvancedComponentClientTest extends TestCase {
+
+	private AdvancedComponentClient client;
+	private Logger logger;
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		String clientName = getClass().getName();
+		logger = ClientLogManager.getAcsLogManager().getLoggerForApplication(clientName, true);
+		String managerLoc = System.getProperty("ACS.manager").trim();
+		
+		client = new AdvancedComponentClient(logger, managerLoc, clientName);		
+	}
+	
+	public void testCreateContainerServices() throws ContainerException {
+    	String localClientName = "sillyLocalClient";
+    	ContainerServices newCS = client.createContainerServices(localClientName, logger);
+    	assertEquals(newCS.getName(), localClientName);
+    	try {
+    		newCS.getComponentStateManager();
+    		fail("NPE expected in getComponentStateManager call!");
+    	} catch (NullPointerException ex) {
+    		; // that's good
+    	}
+    	
+    	// get references to the same component through two different CS instances
+    	final String compInstance = "CONT_SERVICES_TESTER";
+    	ContainerServicesTester compRef1 = ContainerServicesTesterHelper.narrow(client.getContainerServices().getComponent(compInstance));
+    	assertNotNull(compRef1);
+    	ContainerServicesTester compRef2 = ContainerServicesTesterHelper.narrow(newCS.getComponent(compInstance));    		
+    	assertNotNull(compRef2);
+    	assertNotSame("Should have gotten a new CORBA proxy for the same component.", compRef1, compRef2);
+    	assertEquals(compRef1.name(), compRef2.name());		
+	}
+}
