cdb19f24d76501f0156157d1b8fc54f37609181a
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2006-07-14 11:00:24 +0000

    First implementation with the logger (not tested yet)
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@56063 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/ACSLaserSource/acs-sources/include/ACSAlarmSystemInterfaceProxy.h
+++ LGPL/CommonSoftware/ACSLaserSource/acs-sources/include/ACSAlarmSystemInterfaceProxy.h
@@ -34,6 +34,70 @@
 #error This is a C++ include file and cannot be used from plain C
 #endif
 
+#import <AlarmSystemInterface.h>
+#import <FaultState.h>
+#import <loggingLogger.h>
+#import <loggingACEMACROS.h>
 
+using namespace Logging;
+
+/**
+ * Implementation of a source that log messages instead of sending msg
+ * to the AlarmSrevice
+ */
+class ACSAlarmSystemInterfaceProxy: public laserSource::AlarmSystemInterface {
+	private:
+		/**
+		 * The logger
+		 */
+		Logger::LoggerSmartPtr m_logger;
+	
+	virtual void close() {
+	}
+	
+	public:
+	
+		ACSAlarmSystemInterfaceProxy(string name): AlarmSystemInterface() {
+			setSourceName(name);
+			// Get the logger
+			m_logger=Logger::getGlobalLogger();
+		}
+		
+		virtual ~ACSAlarmSystemInterfaceProxy() {}
+		/**
+	 	 * Push a fault state.
+	 	 * @param state the fault state change to push.
+	 	 */
+		virtual void push(laserSource::FaultState & state) {
+			string msg="Alarm sent: <";
+			msg+=state.getFamily();
+			msg+=",";
+			msg+=state.getMember();
+			msg+=",";
+			msg+=state.getCode();
+			msg+="> ";
+			msg+=state.getDescriptor();
+			m_logger->log(m_logger->LM_ALERT,msg.c_str());
+		}
+	
+		/**
+	 	 * Push a collection of fault states.
+	 	 * @param states
+	 	 */
+		virtual void push(vector<laserSource::FaultState> & states) {
+			for (unsigned int t=0; t<states.size(); t++) {
+				laserSource::FaultState fs = states[t];
+				push(fs);
+			}
+		}
+	
+		/**
+	 	 * Push the set of active fault states.
+	 	 * @param activeFaults the active fault states.
+	 	 */
+		virtual void pushActiveList(vector<laserSource::FaultState> & activeFaults) {
+			push(activeFaults);
+		}
+};
 
 #endif /*!ACS_ALARM_SYSTEM_INTERFACE_PROXY_H*/
