16b13c916775e6c31d38f7199f6d966ca209af2a
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2006-07-17 13:22:38 +0000

    A C++ component to send alarms (ALARM_SOURCE_MOUNTCPP)
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@56124 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/ACSLaser/demo/src/alsysMountImpl.cpp
@@ -0,0 +1,50 @@
+/*******************************************************************************
+*    ALMA - Atacama Large Millimiter Array
+*    (c) European Southern Observatory, 2002
+*    Copyright by ESO (in the framework of the ALMA collaboration)
+*    and Cosylab 2002, All rights reserved
+*
+*    This library is free software; you can redistribute it and/or
+*    modify it under the terms of the GNU Lesser General Public
+*    License as published by the Free Software Foundation; either
+*    version 2.1 of the License, or (at your option) any later version.
+*
+*    This library is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*    Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public
+*    License along with this library; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+* 
+*/
+
+
+#include <alsysMountImpl.h>
+
+using namespace acscomponent;
+
+Mount::Mount(const ACE_CString &name,maci::ContainerServices * containerServices) : 
+    ACSComponentImpl(name, containerServices)
+{
+    ACS_TRACE("::Mount::Mount");
+     
+}
+
+Mount::~Mount()
+{
+}
+
+void Mount::faultMount() throw (CORBA::SystemException ) {
+}
+
+void Mount::terminate_faultMount() throw (CORBA::SystemException ) {
+}
+
+
+/* --------------- [ MACI DLL support functions ] -----------------*/
+
+#include <maciACSComponentDefines.h>
+MACI_DLL_SUPPORT_FUNCTIONS(Mount)
+/* ----------------------------------------------------------------*/
