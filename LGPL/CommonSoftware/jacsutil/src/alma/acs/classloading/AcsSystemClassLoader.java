b5047fc40c8b82023b874ca7c1d467bcd1da611a
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2006-08-01 12:44:31 +0000

    changes for ALMASW2006065: removed distinction between AcsSystemClassLoader and JDK application class loader.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@56669 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jacsutil/src/alma/acs/classloading/AcsSystemClassLoader.java
+++ LGPL/CommonSoftware/jacsutil/src/alma/acs/classloading/AcsSystemClassLoader.java
@@ -22,8 +22,13 @@
 package alma.acs.classloading;
 
 import java.io.File;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 import java.util.StringTokenizer;
 
 import alma.acs.makesupport.AcsJarFileFinder;
@@ -40,11 +45,27 @@ import alma.acs.makesupport.AcsJarFileFinder;
  * <li><code>-Dacs.system.classpath.jardirs=../lib/:$INTROOT/lib:$ACSROOT/lib</code>. 
  * 		If an operating system has a native style path separator different from "<code>:</code>", it can optionally be used instead.
  * </ul>
- * Note that by setting the property <code>java.system.class.loader</code> to this class loader,
- * the method {@link java.lang.ClassLoader#getSystemClassLoader()} "tricks" the caller and returns an instance of this class loader
- * rather than its parent (<code>sun.misc.Launcher$AppClassLoader</code>) which it otherwise would.
- * This is neccessary to make JUnit tests run, since the JUnit framework sets up their own classloader to be a child
- * of the system classloader, shortcutting any "regular" child class loaders. 
+ * Notes on the classloader hierarchies:
+ * <ul>
+ * <li>Don't confuse the inheritance hierarchy and the delegation hierarchy.
+ *     Both this CL as well as its delegation-parent inherit from <code>URLClassLoader</code>.
+ * <li>This classloader is meant to be registered with the JVM using the property <code>java.system.class.loader</code>.
+ * <li>Instead of using the real system classloader (<code>sun.misc.Launcher$AppClassLoader</code>) as the parent, 
+ *     since ACS 6.0 this CL takes over the functionality of the direct parent, and uses the original grandparent CL for parent-delegation.
+ *     Circumventing the original parent CL after copying its CLASSPATH entries, removes the distinction between the CLASSPATH entries
+ *     and the jar files from the <code>acs.system.classpath.jardirs</code> directories.
+ *     <ul>
+ *     <li>The effect is that we can add jar files to the CLASSPATH, which use classes from the normal pool of jar files.
+ *         One use case for this is testing component implementation classes outside of containers: the jar files under
+ *         <code>../lib/ACScomponents</code> are not picked up by the classloader unless we add them to the CLASSPATH, 
+ *         e.g. using <code>acsStartJava -addToClasspath</code>.
+ *     <li>The CL hierarchy becomes (1) Bootstrap CL, (2) Extention CL, (3) AcsSystemClassLoader, [(4) optional component CL etc]
+ *     </ul>
+ * <li>The method {@link java.lang.ClassLoader#getSystemClassLoader()} "tricks" the caller and returns this class loader,
+ *     even though it thinks that the real system CL is our circumvented sibling, the <code>sun.misc.Launcher$AppClassLoader</code>.
+ *     This allows JUnit tests run, since the JUnit framework sets up their own classloader to be a child
+ *     of the system classloader, shortcutting any "regular" child class loaders in between. 
+ * </ul>
  * 
  * @author hsommer created Sep 13, 2004 6:31:32 PM
  */
@@ -55,7 +76,8 @@ public class AcsSystemClassLoader extends URLClassLoader
 	public static final String PROPERTY_CLASSLOADERVERBOSE = "acs.system.classloader.verbose";
 
 	private boolean verbose;
-	private JarOrderOptimizer optimizer;
+	private final JarOrderOptimizer optimizer;
+	private final URLClassLoader appClassLoader;
 	
 	/**
 	 * Constructor will be called by the JVM.
@@ -69,45 +91,51 @@ public class AcsSystemClassLoader extends URLClassLoader
 	 */
 	public AcsSystemClassLoader(ClassLoader parent)
 	{
-		super(new URL[0], parent);
-
-		verbose = Boolean.getBoolean(PROPERTY_CLASSLOADERVERBOSE);
+		super(new URL[0], parent.getParent());
 		
+		if (!(parent instanceof URLClassLoader)) {
+			throw new IllegalStateException("AcsSystemClassLoader expects parent CL of type URLClassLoader!");
+		}
+		appClassLoader = (URLClassLoader) parent;
+		verbose = Boolean.getBoolean(PROPERTY_CLASSLOADERVERBOSE);
+				
 		String jarDirPath = System.getProperty(PROPERTY_JARDIRS);
 		File[] jarDirs = parseJarDirs(jarDirPath);
 
+		// extract jar files from directories
 		AcsJarFileFinder jarFinder = new AcsJarFileFinder(jarDirs, null);
-		File[] allJars = jarFinder.getAllFiles();
+		List<File> jarFileList = new ArrayList<File>(); 
+		jarFileList.addAll(Arrays.asList(jarFinder.getAllFiles()));
 
 		optimizer = new JarOrderOptimizer(verbose);
 
+		// test mode 
 		if (Boolean.getBoolean(PROPERTY_TOPJARSONLY)) {
 			System.out.println("AcsSystemClassLoader running in test mode: will only use jar files from priority list.");
-			allJars = optimizer.getTopJarsOnly(allJars);
+			jarFileList = optimizer.getTopJarsOnly(jarFileList);
 		}
-		optimizer.sortJars(allJars); 
 		
-		for (int i = 0; i < allJars.length; i++) {
+		// sort the jar files
+		optimizer.sortJars(jarFileList); 
+
+		// we prepend the CLASSPATH entries so that this CL can replace the normal JVM application CL 
+		prependCLASSPATHJars(jarFileList);
+
+		// add the jars and directories 
+		for (Iterator<File> iter = jarFileList.iterator(); iter.hasNext();) {
+			File cpEntry = iter.next();
 			try {
-				addURL(allJars[i].toURL());
+				addURL(cpEntry.toURL());
 				if (verbose) {
-					System.out.println("added " + allJars[i].getAbsolutePath() + " to the custom system class loader's path.");
+					System.out.println("added " + cpEntry.getAbsolutePath() + " to the custom system class loader's path.");
 				}
 			}
 			catch (Exception e) {
-				System.err.println("failed to add " + allJars[i].getAbsolutePath() + " to the custom class loader's path." + e.toString());
+				System.err.println("failed to add " + cpEntry.getAbsolutePath() + " to the custom class loader's path." + e.toString());
 			}
-		}
-
-		if (verbose) {		
-//			//test 
-//			URL[] urls = getURLs();
-//			System.out.println("first URL is " + urls[0].toString());
-//			System.out.println("last URL is " + urls[urls.length-1].toString());
 		}
 	}
 
-	
 	private File[] parseJarDirs(String jarDirPath)
 	{
 		StringTokenizer tok = new StringTokenizer(jarDirPath, File.pathSeparator);
@@ -118,17 +146,55 @@ public class AcsSystemClassLoader extends URLClassLoader
 		return jarDirs;
 	}
 	
+	/**
+	 * Extracts the CLASSPATH jar files (or directories) from the parent class loader 
+	 * and adds them at the beginning of the given list.
+	 * <p>
+	 * It is assumed that thanks to the <code>java.system.class.loader</code> property, 
+	 * this AcsSystemClassLoader is a direct child of the application system class loader 
+	 * which normally would handle the CLASSPATH.
+	 */
+	private void prependCLASSPATHJars(List<File> jarFileList) {
+		if (verbose) {
+			System.out.print("Will prepend the following entries from the CLASSPATH to AcsSystemClassLoader's entries: ");
+		}
+		URL[] sysCP = appClassLoader.getURLs();
+		List<File> sysCPList = new ArrayList<File>();
+		for (int i = 0; i < sysCP.length; i++) {
+			try {
+				File entry = new File(sysCP[i].toURI());
+				sysCPList.add(entry);
+				if (verbose) {
+					System.out.print(entry.getAbsolutePath() + ", ");
+				}
+			} catch (URISyntaxException e) {
+				System.err.println("Failed to add CLASSPATH entry '" + sysCP[i] + "' to AcsSystemClassLoader: " + e.toString());
+			}
+		}
+		if (verbose) {
+			System.out.println();
+		}
+		jarFileList.addAll(0, sysCPList);
+	}
 	
-	protected synchronized Class loadClass(String name, boolean resolve)
+
+	
+	protected synchronized Class<?> loadClass(String name, boolean resolve)
 			throws ClassNotFoundException
 	{
+		if (verbose) {
+			if (ClassLoader.getSystemClassLoader() != this) {
+				System.err.println("Warning: AcsSystemClassLoader is not registered as the JVM's system class loader!");
+			}
+		}
+		
 		// First, check if the class has already been loaded by this classloader
 		Class c = findLoadedClass(name);
 		if (c == null) {
-				// don't delegate to parent class loader for classes from this module (this jar must be on the system class path!)
+			// don't delegate to parent class loader for classes from this module (this jar must be on the system class path!)
 			// The shortcutting will also be a tiny performance improvement since all "alma.acs.xyz" classes even from other jar files
 			// are supposed to be loaded through this class loader, in which case the parent would only delegate back to us anyway.
-				if (name.startsWith("alma.acs.") || name.startsWith("com.cosylab.util") ) { 
+			if (name.startsWith("alma.acs.") || name.startsWith("com.cosylab.util") ) { 
 				try {
 					c = findClass(name);
 				}
@@ -162,17 +228,17 @@ public class AcsSystemClassLoader extends URLClassLoader
 	 * sun.awt.resources.
 	 * com.sun.swing.internal.plaf.     
 	 */
-	protected Class findClass(String name) throws ClassNotFoundException
+	protected Class<?> findClass(String name) throws ClassNotFoundException
 	{
 		if (optimizer.isClassKnownToBeUnavailable(name)) {
 			throw new ClassNotFoundException("ACS and application software are not supposed to deliver a class like '" 
 					+ name + "' and therefore AcsSystemClassLoader won't attempt to load it (for IO optimization).");
 		}
-		Class clazz = null;
+		Class<?> clazz = null;
 		try {
 //			System.out.println("****AcsSystemClassLoader will try to find class " + name);
 			clazz = super.findClass(name);
-	}
+		}
 		catch (ClassNotFoundException e) {
 			if (verbose) {
 				System.err.println(AcsSystemClassLoader.class.getName() + " failed to load class " + name);
