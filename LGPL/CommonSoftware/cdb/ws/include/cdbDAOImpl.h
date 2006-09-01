a77280195fc80ac43fb56cea25d621e3a07dd516
gchiozzi <gchiozzi@523d945c-050c-4681-91ec-863ad3bb968a>
2006-09-01 12:33:32 +0000

    Resfactored formattin, to make the code more readable. This should be done in many other files, but I did it here because I wanted to check some things.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@57933 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/cdb/ws/include/cdbDAOImpl.h
+++ LGPL/CommonSoftware/cdb/ws/include/cdbDAOImpl.h
@@ -1,7 +1,7 @@
 /*******************************************************************************
 * E.S.O. - ACS project
 *
-* "@(#) $Id: cdbDAOImpl.h,v 1.27 2006/09/01 02:20:54 cparedes Exp $"
+* "@(#) $Id: cdbDAOImpl.h,v 1.28 2006/09/01 12:33:32 gchiozzi Exp $"
 *
 * who       when        what
 * --------  ----------  ----------------------------------------------
@@ -19,17 +19,17 @@
 
 class CXMLTreeNode
 {
-public:
-	CXMLTreeNode( CXMLTreeNode *pParent );
-	~CXMLTreeNode();
-        void getAttributeNames( String &names );
+  public:
+    CXMLTreeNode( CXMLTreeNode *pParent );
+    ~CXMLTreeNode();
+    void getAttributeNames( String &names );
 
-	CXMLTreeNode* m_parent; // parent in access tree
-	typedef std::map<String, CXMLTreeNode*> MapStringToNode;
-	//
-	MapStringToNode				m_subNodesMap;
-	MapStringToField m_fieldMap;
-	String								m_name;
+    CXMLTreeNode* m_parent; // parent in access tree
+    typedef std::map<String, CXMLTreeNode*> MapStringToNode;
+    //
+    MapStringToNode				m_subNodesMap;
+    MapStringToField m_fieldMap;
+    String								m_name;
 };
 
 /////////////////////////////////////////////////////////////////////////////
@@ -42,118 +42,82 @@ class  DAOImpl : public virtual POA_CDB::DAO
 class  DAOImpl
 #endif
 {
-public:
-  //Constructor 
-  DAOImpl ( const char* curl );
-  DAOImpl ( CDB::DAO_ptr  dao );
-
-  //Destructor 
-  virtual ~DAOImpl (void);
-	virtual Boolean isInitialized() { return m_initialized; }
-
-///////////////////////////////////////////
-// scalars
-virtual CORBA::Long get_long (
-    const char * propertyName
+  public:
+    //Constructor 
+    DAOImpl ( const char* curl );
+    DAOImpl ( CDB::DAO_ptr  dao );
     
-  )
-  throw (
-    CORBA::SystemException,
-    CDB::WrongDataType
-  );
-
-virtual CORBA::Double get_double (
-    const char * propertyName
+    //Destructor 
+    virtual ~DAOImpl (void);
+    virtual Boolean isInitialized() { return m_initialized; }
     
-  )
-  throw (
-    CORBA::SystemException,
-    CDB::WrongDataType
-  );
-
-virtual char * get_string (
-    const char * propertyName
+    ///////////////////////////////////////////
+    // scalars
+    virtual CORBA::Long get_long (const char * propertyName )
+	throw (CORBA::SystemException,
+	       CDB::WrongDataType );
     
-  )
-  throw (
-    CORBA::SystemException,
-    CDB::WrongDataType
-  );
+    virtual CORBA::Double get_double (const char * propertyName)
+	throw (CORBA::SystemException,
+	       CDB::WrongDataType );
+    
+    virtual char * get_string (const char * propertyName )
+	throw (CORBA::SystemException,
+	       CDB::WrongDataType);
 
-virtual char * get_field_data (
-    const char * propertyName
-      
-  )
-  throw (
-    CORBA::SystemException,
-    CDB::WrongDataType,
-    CDB::FieldDoesNotExist
-  );
+    virtual char * get_field_data (const char * propertyName )
+	throw (CORBA::SystemException,
+	       CDB::WrongDataType,
+	       CDB::FieldDoesNotExist);
 
-///////////////////////////////////////////
-// sequences
-virtual ::CDB::stringSeq * get_string_seq (
-	const char * propertyName
-	 
-	)
-	throw (
-		CORBA::SystemException,
-		CDB::WrongDataType,
-		CDB::FieldDoesNotExist
-	);
+    ///////////////////////////////////////////
+    // sequences
+    virtual ::CDB::stringSeq * get_string_seq (const char * propertyName )
+	throw (CORBA::SystemException,
+	       CDB::WrongDataType,
+	       CDB::FieldDoesNotExist);
 
-virtual ::CDB::longSeq * get_long_seq (
-	const char * propertyName
-	  
-	)
-	throw (
-		CORBA::SystemException,
-		CDB::WrongDataType,
-		CDB::FieldDoesNotExist
-	);
+    virtual ::CDB::longSeq * get_long_seq (const char * propertyName )
+	throw (CORBA::SystemException,
+	       CDB::WrongDataType,
+	       CDB::FieldDoesNotExist);
 
-virtual ::CDB::doubleSeq * get_double_seq (
-	const char * propertyName
-	  
-	)
-	throw (
-		CORBA::SystemException,
-		CDB::WrongDataType,
-		CDB::FieldDoesNotExist
-	);
+    virtual ::CDB::doubleSeq * get_double_seq (const char * propertyName )
+	throw (CORBA::SystemException,
+	       CDB::WrongDataType,
+	       CDB::FieldDoesNotExist);
 
-//
-	Boolean get_field( const String &strFieldName, Field &fld );
+    Boolean get_field( const String &strFieldName, Field &fld );
 
-public:
-	ACE_CString	m_name; 
-	ACE_CString	m_errorMessage;
+  public:
+    ACE_CString	m_name; 
+    ACE_CString	m_errorMessage;
 
-// internal operations
-protected:
-	void	Start(const char *el, const char **attr);
-	void	End(const char *el);
+  // internal operations
+  protected:
+    void	Start(const char *el, const char **attr);
+    void	End(const char *el);
 
-	static void start(void *data, const char *el, const char **attr);
-	static void end(void *data, const char *el);
+    static void start(void *data, const char *el, const char **attr);
+    static void end(void *data, const char *el);
 
-// implementation
-protected:
-	ACE_CString m_xml;
-	Boolean			m_initialized;
+  // implementation
+  protected:
+    ACE_CString m_xml;
+    Boolean			m_initialized;
 
-	// temporary variables for array handling 
-	int				m_inArray;
-	String		m_arrayName;
-	String		m_arrayType;
-	String		m_arrayContent;
+    // temporary variables for array handling 
+    int				m_inArray;
+    String		m_arrayName;
+    String		m_arrayType;
+    String		m_arrayContent;
 
-	// XTML tree handling
-	CXMLTreeNode* m_rootNode;
-	CXMLTreeNode* m_currNode;
+    // XTML tree handling
+    CXMLTreeNode* m_rootNode;
+    CXMLTreeNode* m_currNode;
 
-	int						m_remote; // holding info about current location of this object
-	CDB::DAO_var	m_dao;
+    int			m_remote; // holding info about current location of this object
+    CDB::DAO_var	m_dao;
 };
 
 #endif /* DAO_H_  */
