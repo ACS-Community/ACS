1ce02be5d087a442d273e1bf224ae90ca5811b6a
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2006-09-13 07:43:28 +0000

    improved version, with help from Holger
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@58463 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jcont/src/alma/acs/container/archive/UIDLibrary.java
+++ LGPL/CommonSoftware/jcont/src/alma/acs/container/archive/UIDLibrary.java
@@ -21,15 +21,16 @@
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
  *    MA 02111-1307  USA
  */
-package alma.archive.identifier;
+package alma.acs.container.archive;
 
 import java.net.URI;
 import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import alma.acs.container.ContainerException;
 import alma.acs.container.ContainerServices;
-import alma.archive.exceptions.UIDLibraryException;
 import alma.archive.range.IdentifierRange;
 import alma.entities.commonentity.EntityRefT;
 import alma.entities.commonentity.EntityT;
@@ -40,105 +41,87 @@ import alma.xmlstore.IdentifierPackage.NotAvailable;
 import alma.xmlstore.IdentifierPackage.NotFound;
 
 /**
- * @author simon
+ * @author simon, hsommer
  */
 public class UIDLibrary
 {
 	private static UIDLibrary _instance = null;
-	private static int count = 0;
+	private static final AtomicInteger count = new AtomicInteger(0);
 	
-	private Logger logger = null;
-	private ContainerServices cs = null;
-	private IdentifierJ identifier = null;
+	private final Logger logger;
+	private final ContainerServices cs;
+	private final IdentifierJ identifier;	
+	private final Range defaultRange;
 	
-	private Range defaultRange = null;
-	
-	private HashMap idRanges = null;
-	private HashMap refRanges = null;
+	private final HashMap<URI, Range> idRanges;
+	private final HashMap<URI, Range> refRanges;
 	
 	/**
 	 * Return an instance of the UID library
 	 * @param cs
 	 * @param ident
 	 * @return
-	 * @throws UIDLibraryException
+	 * @throws ContainerException
 	 */
-	public static UIDLibrary instance(ContainerServices cs, Identifier ident) 
-		throws 
-			UIDLibraryException
+	public static synchronized UIDLibrary instance(ContainerServices cs, Identifier ident) 
+		throws ContainerException
 	{
 		if (_instance == null)
 		{
 			_instance = new UIDLibrary(cs,ident);
 		}
-		count++;
+		count.incrementAndGet();
 		return _instance;
 	}
 	
+	/**
+	 * TODO: check if this method is useful, given that its sole purpose is to release the bit of memory 
+	 * used for the single instance returned from the {@link #instance(ContainerServices, Identifier)} method.
+	 */
 	public void close()
 	{
-		count--;
-		if (count == 0)
+		if (count.decrementAndGet() == 0)
 		{
 			_instance = null; 
-			logger = null;
-			cs = null;
-			identifier = null;
 		}
 	}
 
 	/**
-	 * Create the UIDLibrary, fetches the default range from the Identifier
-	 * interface.
+	 * Creates the UIDLibrary, fetches the default range from the Identifier interface.
 	 * @param cs
 	 * @param ident
-	 * @throws UIDLibraryException
+	 * @throws ContainerException
 	 */
-	private UIDLibrary(ContainerServices cs, Identifier ident) 
-		throws 
-			UIDLibraryException
+	private UIDLibrary(ContainerServices cs, Identifier ident)
+		throws ContainerException
 	{
 		this.cs = cs;
 		this.logger = cs.getLogger(); 
+		idRanges = new HashMap<URI, Range>();
+		refRanges = new HashMap<URI, Range>();
 		
-		try
-		{
-			identifier = 
-				(IdentifierJ)cs.getTransparentXmlComponent(
-					IdentifierJ.class,ident,IdentifierOperations.class);
-			
-			try{
-				logger.finest("UIDLibrary: Building default range class");
-				IdentifierRange idRange = identifier.getNewRange();
-				defaultRange = new Range(idRange);
-			}
-			catch (NotAvailable e){
-				throw new UIDLibraryException(e);
-			}
+		identifier = (IdentifierJ) cs.getTransparentXmlComponent(
+				IdentifierJ.class, ident, IdentifierOperations.class);
+		try {
+			logger.finest("UIDLibrary: Building default range class");
+			IdentifierRange idRange = identifier.getNewRange();
+			defaultRange = new Range(idRange);
 		}
-		catch (ContainerException e){
-			throw new UIDLibraryException(e);
-		}
-		
-		idRanges = new HashMap();
-		refRanges = new HashMap();
+		catch (NotAvailable e){
+			throw new ContainerException(e);
+		}		
 	}
 
+	
 	/**
-	 * Assigns a uid to the EntityT from the default range.
-	 * @param entity
-	 * @throws UIDLibraryException
+	 * Assigns a uid from the default range to the <code>EntityT</code> castor class 
+	 * of an XML-based entity such as a SchedBlock.
+	 * @throws ContainerException
+	 * @see {@link ContainerServices#assignUniqueEntityId(EntityT)}
 	 */
-	public void assignUniqueEntityId(EntityT entity) 
-		throws 
-			UIDLibraryException
-	{
-		if (defaultRange != null){
-			logger.finest("UIDLibrary: Assigning Default ID to entity");
-			defaultRange.assignUniqueEntityId(entity);
-		}
-		else throw new UIDLibraryException(
-			"No default Range has been configured");
+	public void assignUniqueEntityId(EntityT entity) throws ContainerException {
+		logger.finest("Will assign default UID to entity of type " + entity.getEntityTypeName());
+		defaultRange.assignUniqueEntityId(entity);
 	}
 
 	/**
@@ -147,53 +130,50 @@ public class UIDLibrary
 	 * @return
 	 */
 	public URI getNewRestrictedRange(int size, String user)
-		throws
-			UIDLibraryException
+		throws ContainerException
 	{
 		Range range = null;
 		try
 		{
 			logger.finest("UIDLibrary: Fetching a restricted range");
-			IdentifierRange idRange = 
-				identifier.getNewRestrictedRange(size,user);
+			IdentifierRange idRange = identifier.getNewRestrictedRange(size, user);
 			range = new Range(idRange);
 		}
-		catch (NotAvailable e){
-			throw new UIDLibraryException(e);
+		catch (NotAvailable e) {
+			throw new ContainerException(e);
 		}
 		
 		URI uri = range.rangeId();
-		if (idRanges.containsKey(uri)){
-			throw new UIDLibraryException(
-				"Cannot store new range URI occupied");
+		if (idRanges.containsKey(uri)) {
+			throw new ContainerException("Cannot store new range. URI occupied. This should never have happened by design!!");
 		}
-		logger.finest("UIDLibrary: Storing Range under: " 
-			+ uri.toASCIIString());
+		logger.finest("UIDLibrary: Storing Range under: "+ uri.toASCIIString());
 		
 		idRanges.put(uri,range);
 		
 		return uri;
 	}
 	
+	
 	/**
 	 * Assign a uid to the EntityT from the specified range, not permitted with
 	 * deserialised Ranges
 	 * @param entity
 	 * @param uri
-	 * @throws UIDLibraryException
+	 * @throws ContainerException
 	 */
 	public void assignUniqueEntityId(EntityT entity, URI uri) 
-		throws 
-			UIDLibraryException
+		throws ContainerException
 	{
 		if (idRanges.containsKey(uri)){
-			logger.finest("UIDLibrary: Assigning ID to entity from: " 
-				+ uri.toASCIIString());
-			Range r = (Range)idRanges.get(uri);
+			if (logger.isLoggable(Level.FINEST)) {
+				logger.finest("UIDLibrary: Assigning ID to entity from range " + uri.toASCIIString());
+			}
+			Range r = idRanges.get(uri);
 			r.assignUniqueEntityId(entity);
 		}
 		else{
-			throw new UIDLibraryException(
+			throw new ContainerException(
 				"Cannot find range: " + uri.toASCIIString());
 		}
 	}
@@ -202,11 +182,10 @@ public class UIDLibrary
 	 * Fetch an existing range from the archive and deserialise, only certain
 	 * operations will be permitted.
 	 * @param uri
-	 * @throws UIDLibraryException
+	 * @throws ContainerException
 	 */
 	public void fetchRange(URI uri, String user) 
-		throws 
-			UIDLibraryException
+		throws ContainerException
 	{
 		IdentifierRange idRange = null;
 		try{
@@ -215,7 +194,7 @@ public class UIDLibrary
 			idRange = identifier.getExistingRange(uri.toASCIIString(),user);
 		}
 		catch (NotFound e){
-			throw new UIDLibraryException(e);
+			throw new ContainerException(e);
 		}
 		
 		Range r = new Range(idRange);
@@ -223,7 +202,7 @@ public class UIDLibrary
 			refRanges.put(uri,r);
 		}
 		else{
-			throw new UIDLibraryException(
+			throw new ContainerException(
 				"Range: " + uri.toASCIIString() + " is already in use");
 		}
 	}
@@ -233,20 +212,19 @@ public class UIDLibrary
 	 * ranges.
 	 * @param ref
 	 * @param uri
-	 * @throws UIDLibraryException
+	 * @throws ContainerException
 	 */
 	public void assignUniqueEntityRef(EntityRefT ref, URI uri) 
-		throws 
-			UIDLibraryException
+		throws ContainerException
 	{
 		if (refRanges.containsKey(uri)){
 			logger.finest("UIDLibrary: Assigning ID Ref to entity from: " 
 				+ uri.toASCIIString());
-			Range r = (Range)refRanges.get(uri);
+			Range r = refRanges.get(uri);
 			r.assignUniqueEntityRef(ref);
 		}
 		else{
-			throw new UIDLibraryException(
+			throw new ContainerException(
 				"Cannot find range: " + uri.toASCIIString());
 		}
 	}
