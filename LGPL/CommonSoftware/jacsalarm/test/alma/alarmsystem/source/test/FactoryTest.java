5248ab64777b931f8c05b896d2dfa09cde912c00
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2006-09-25 09:06:42 +0000

    Files imported from ACS-5_0_5
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@59081 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/jacsalarm/test/alma/alarmsystem/source/test/FactoryTest.java
@@ -0,0 +1,120 @@
+package alma.alarmsystem.source.test;
+
+import alma.alarmsystem.source.ACSAlarmSystemInterfaceFactory;
+import alma.alarmsystem.source.ACSFaultState;
+import alma.alarmsystem.source.ACSAlarmSystemInterface;
+
+import si.ijs.maci.Manager;
+import com.cosylab.CDB.JDAL;
+
+public class FactoryTest extends junit.framework.TestCase {
+	
+	
+			
+	// The current directory
+	String curDir=System.getProperty("user.dir");
+	
+	// The reference to the DAL (to make a clear cache)
+	JDAL jdal; 
+	
+	public FactoryTest(String str) {
+		super(str);
+		// Get the reference to the DAL
+		jdal = TestUtil.getDAL(TestUtil.getManager());
+	}
+	
+	/**
+	 * This execute for each test and we want to have
+	 * a cache with some logs 
+	 * 
+	 * @see junit.framework.TestCase
+	 */ 
+	protected void setUp() throws Exception
+	{ 
+	}
+	
+	/** 
+	 * @see junit.framework.TestCase
+	 *
+	 */
+	protected void tearDown() {
+		ACSAlarmSystemInterfaceFactory.done();
+	}
+	
+	/**
+	 * Check if the ACS implementation of the AS is choosen when
+	 * there is no Alarm branch in the CDB
+	 * 
+	 * @throws Exception
+	 */
+	public void testNoALarmBranch() throws Exception {
+		TestUtil.deleteAlarmBranch(curDir);
+		jdal.clear_cache_all();
+		ACSAlarmSystemInterfaceFactory.init(TestUtil.getORB(),TestUtil.getManager(),TestUtil.getLogger(this.getClass().getName()));
+		assertTrue("Wrong implementation in use (no Alarms in CDB case)",ACSAlarmSystemInterfaceFactory.usingACSAlarmSystem());
+	}
+	
+	/**
+	 * Check if the ACS implementation of the AS is choosen when
+	 * there ACS is in the CDB
+	 * 
+	 * @throws Exception
+	 */
+	public void testACSAS() throws Exception {
+		TestUtil.setupAlarmBranch(curDir,"ACS");
+		jdal.clear_cache_all();
+		ACSAlarmSystemInterfaceFactory.init(TestUtil.getORB(),TestUtil.getManager(),TestUtil.getLogger(this.getClass().getName()));
+		assertTrue("Wrong implementation in use (ACS case)",ACSAlarmSystemInterfaceFactory.usingACSAlarmSystem());
+	}
+	
+	/**
+	 * Check if the CERN implementation of the AS is choosen when
+	 * there CERN is in the CDB
+	 * 
+	 * @throws Exception
+	 */
+	public void testCERNAS() throws Exception {
+		TestUtil.setupAlarmBranch(curDir,"CERN");
+		jdal.clear_cache_all();
+		ACSAlarmSystemInterfaceFactory.init(TestUtil.getORB(),TestUtil.getManager(),TestUtil.getLogger(this.getClass().getName()));
+		assertFalse("Wrong implementation in use (CERN case)",ACSAlarmSystemInterfaceFactory.usingACSAlarmSystem());
+	}
+	
+	/**
+	 * Check if the ACS implementation is used when the Implementation property is wrong
+	 * @throws Exception
+	 */
+	public void testWrongImplementationProp() throws Exception {
+		TestUtil.setupAlarmBranch(curDir,"Wrong property");
+		jdal.clear_cache_all();
+		ACSAlarmSystemInterfaceFactory.init(TestUtil.getORB(),TestUtil.getManager(),TestUtil.getLogger(this.getClass().getName()));
+		assertTrue("Wrong implementation in use (wrong prop case)",ACSAlarmSystemInterfaceFactory.usingACSAlarmSystem());
+	}
+	
+	/**
+	 * Test the creation of a FaultState
+	 * 
+	 * @throws Exception
+	 */
+	public void testFaultStateCreation() throws Exception {
+		TestUtil.setupAlarmBranch(curDir,"ACS");
+		jdal.clear_cache_all();
+		ACSAlarmSystemInterfaceFactory.init(TestUtil.getORB(),TestUtil.getManager(),TestUtil.getLogger(this.getClass().getName()));
+		ACSFaultState fs = ACSAlarmSystemInterfaceFactory.createFaultState("Family","Member",0);
+		assertNotNull("Error creating a FS",fs);
+	}
+	
+	/**
+	 * Test the creation of a source (proxy)
+	 * 
+	 * @throws Exception
+	 */
+	public void testAlarmSourceCreation() throws Exception {
+		TestUtil.setupAlarmBranch(curDir,"ACS");
+		jdal.clear_cache_all();
+		ACSAlarmSystemInterfaceFactory.init(TestUtil.getORB(),TestUtil.getManager(),TestUtil.getLogger(this.getClass().getName()));
+		ACSAlarmSystemInterface proxy = ACSAlarmSystemInterfaceFactory.createSource("SourceName");
+		assertNotNull("Error creating an alarm source",proxy);
+	}
+	
+}
