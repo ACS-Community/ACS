10df995a1e5f5f70ae0f8801f9a9ebe623708e01
sharring <sharring@523d945c-050c-4681-91ec-863ad3bb968a>
2006-09-25 19:08:53 +0000

    Adding test files to HEAD
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@59137 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/acsalarm/test/TestAcsAlarmSending.cpp
@@ -0,0 +1,82 @@
+#include <memory>
+
+#include <logging.h>
+#include <loggingGenericLogger.h>
+#include "ACSAlarmSystemInterface.h"
+#include "ACSAlarmSystemInterfaceFactory.h"
+#include "Timestamp.h"
+#include "ACSFaultState.h"
+#include "Properties.h"
+#include "faultStateConstants.h"
+//#include "asiConfigurationConstants.h"
+
+using namespace laserSource;
+using namespace laserUtil;
+
+void printUsageAndExit()
+{
+	std::cout << "\n\nUsage: \n\n" << "TestAcsAlarmSending <NUM_ALARMS_TO_SEND>\n\n" << "where NUM_ALARMS_TO_SEND is how many alarms you wish to send.\n\n";
+	exit(-1);	
+}
+
+/*
+ * Crude test case to send a fault to the laser server using the cpp laser source library.
+ */
+int main(int argc, char *argv[])
+{
+	Logging::Logger::setGlobalLogger(new Logging::GenericLogger("testLogger"));
+
+	if(argc < 2)
+	{
+		printUsageAndExit();
+	}
+
+	int numAlarmsToSend = atoi(argv[1]);
+
+	//try 
+	{
+		// constants we will use when creating the fault
+		string family = "AlarmSource";
+		string member = "ALARM_SOURCE_MOUNT";
+		int code = 1;
+
+		// initialize the AlarmSystemInterfaceFactory 
+		ACSAlarmSystemInterfaceFactory::init(NULL);
+
+		// create the AlarmSystemInterface
+		auto_ptr<ACSAlarmSystemInterface> alarmSource = ACSAlarmSystemInterfaceFactory::createSource();
+
+		// create the FaultState
+		auto_ptr<ACSFaultState> fltstate = ACSAlarmSystemInterfaceFactory::createFaultState(family, member, code);
+
+		// set the fault state's descriptor
+		string stateString = faultState::ACTIVE_STRING;
+		fltstate->setDescriptor(stateString);
+		
+		// create a Timestamp and use it to configure the FaultState
+		Timestamp * tstampPtr = new Timestamp();
+		auto_ptr<Timestamp> tstampAutoPtr(tstampPtr);
+		fltstate->setUserTimestamp(tstampAutoPtr);
+
+		// create a Properties object and configure it, then assign to the FaultState
+		Properties * propsPtr = new Properties();
+		propsPtr->setProperty(faultState::ASI_PREFIX_PROPERTY_STRING, "prefix");
+		propsPtr->setProperty(faultState::ASI_SUFFIX_PROPERTY_STRING, "suffix");
+		propsPtr->setProperty("TEST_PROPERTY", "TEST_VALUE");
+		auto_ptr<Properties> propsAutoPtr(propsPtr);
+		fltstate->setUserProperties(propsAutoPtr);
+
+		for(int i = 0; i < numAlarmsToSend; i++)
+		{
+			// push the FaultState using the AlarmSystemInterface previously created
+			alarmSource->push(*fltstate);
+		}
+	} 
+	/* 
+	// TODO later:
+	catch (ASIException e) 
+	{
+		e.printStackTrace();
+	}
+	*/
+}
