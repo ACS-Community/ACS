b976d8ccdd897983f406bcaeeea5f747cd005e13
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2006-10-05 16:48:32 +0000

    resourceUnreachable impl returns boolean; monitorCallThreadPool no longer static (but still shared among check runners). The static field kept threads across JUnit test which made testing very confusing. added destroy() method.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@60037 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/mastercomp/src/alma/ACS/MasterComponentImpl/SubsysResourceMonitor.java
+++ LGPL/CommonSoftware/mastercomp/src/alma/ACS/MasterComponentImpl/SubsysResourceMonitor.java
@@ -52,53 +52,96 @@ import alma.ACS.ComponentStates;
  */
 public class SubsysResourceMonitor {
 
+	/**
+	 * The delay between two monitoring calls to resource.
+	 */
 	public final int delaySeconds;
 	
 	private final ScheduledExecutorService scheduler;
 	private final Random random;
 	private final Logger logger;
-	private final ThreadFactory threadFactory;
+	private final ExecutorService monitorCallThreadPool;
+	private boolean isShuttingDown;
+	private boolean isShutDown;
 	
+    /**
+     * Ctor, with default value for <code>delaySeconds = 10</code>.
+     * @see #SubsysResourceMonitor(Logger, ThreadFactory, int)
+     */
     SubsysResourceMonitor(Logger logger, ThreadFactory threadFactory) {
         this(logger, threadFactory, 10);
     }
     
+    /**
+     * @param logger  Logger to be used by this object
+     * @param threadFactory  all threads for scheduling and calling the resources will be created by this factory.  
+     * @param delaySeconds  the delay between two monitoring calls to a resource.
+     */
     SubsysResourceMonitor(Logger logger, ThreadFactory threadFactory, int delaySeconds) {
         this.delaySeconds = delaySeconds;
 		this.logger = logger;
-		this.threadFactory = threadFactory;
+		monitorCallThreadPool = Executors.newCachedThreadPool(threadFactory);
+		isShuttingDown = false;
+		isShutDown = false;
 		scheduler = Executors.newScheduledThreadPool(1, threadFactory);
 		random = new Random(System.currentTimeMillis());
 	}
 	
 	/**
-	 * Starts to periodically monitor the resource that <code>checkRunner</code> contains.
-	 * The interval length is given by {@link #delaySeconds}. 
+	 * Starts to periodically monitor the resource that <code>checker</code> contains.
+	 * The monitor interval length is given by {@link #delaySeconds}.
+	 * <p> 
 	 * In order to randomize check times even if many resources get signed up for monitoring one after the other, 
 	 * the initial delay before the first check is run is taken randomly between 1 second and the period time. 
-	 * @param checkRunner
+	 * @param checker
+	 * @throws IllegalStateException if {@link #destroy(long, TimeUnit)} has been called.
 	 */
 	<T> void monitorResource(ResourceChecker<T> checker, ResourceErrorHandler<T> err) {
-        SubsysResourceMonitor.ResourceCheckRunner<T> checkRunner = new SubsysResourceMonitor.ResourceCheckRunner<T>(checker, err, logger, threadFactory);
-        scheduler.scheduleAtFixedRate(checkRunner, random.nextInt(delaySeconds) + 1, delaySeconds, TimeUnit.SECONDS);
+		if (isShuttingDown) {
+			throw new IllegalStateException("Resource monitor is already destroyed.");
+		}
+		SubsysResourceMonitor.ResourceCheckRunner<T> checkRunner = new SubsysResourceMonitor.ResourceCheckRunner<T>(checker, err, logger, monitorCallThreadPool);
+		scheduler.scheduleAtFixedRate(checkRunner, random.nextInt(delaySeconds) + 1, delaySeconds, TimeUnit.SECONDS);
 	}
 	
+	/**
+	 * Cancels monitoring of all resources and leaves this object in an unusable state.
+	 * <p>
+	 * This method is synchronized so that a second call can return immediately, but only if the first call has finished.
+	 * @param timeout the timeout for waiting that the internal threads, queues, jobs etc are freed, or <code>0</code> to not wait at all.
+	 * @param unit
+	 * @throws InterruptedException
+	 */
+	synchronized void destroy(long timeout, TimeUnit unit) throws InterruptedException {
+		isShuttingDown = true;
+		if (isShutDown) {
+			return;
+		}
+		monitorCallThreadPool.shutdownNow();
+		scheduler.shutdownNow();
+		if (timeout > 0) {
+			long timeoutMillis = unit.toMillis(timeout); // to safely divide by 2 in case it was given as 1 second
+			monitorCallThreadPool.awaitTermination(timeoutMillis/2, TimeUnit.MILLISECONDS);
+			scheduler.awaitTermination(timeoutMillis/2, TimeUnit.MILLISECONDS);
+		} else {
+			isShutDown = scheduler.isTerminated();
+		}
+	}
 	
-	private static class ResourceCheckRunner<T> implements Runnable {
+	static class ResourceCheckRunner<T> implements Runnable {
 		
 		public static final int callTimeoutSeconds = 10;
-		private static ExecutorService threadPool;
 		
 		private final ResourceChecker<T> resourceChecker;
 		private final ResourceErrorHandler<T> err;
         private final Logger logger;
-    	private final ThreadFactory threadFactory;
+    	private final ExecutorService threadPool;
 		
-		ResourceCheckRunner(ResourceChecker<T> resourceChecker, ResourceErrorHandler<T> err, Logger logger, ThreadFactory threadFactory) {
+		ResourceCheckRunner(ResourceChecker<T> resourceChecker, ResourceErrorHandler<T> err, Logger logger, ExecutorService threadPool) {
 			this.resourceChecker = resourceChecker;
 			this.err = err;
             this.logger = logger;
-    		this.threadFactory = threadFactory;
+    		this.threadPool = threadPool;
 		}
 		
 		public void run() {
@@ -115,27 +158,44 @@ public class SubsysResourceMonitor {
 					}
 				}
 			};
-			Future future = getThreadPool().submit(timeoutCaller);
+			Future future = threadPool.submit(timeoutCaller);
+			long timeBeforeCall = System.currentTimeMillis();
+			Throwable callError = null;
+			boolean wasTimedOut = false;
 			try {
 				future.get(callTimeoutSeconds, TimeUnit.SECONDS);
 			} catch (TimeoutException e) {
+				wasTimedOut = true;
+			} catch (InterruptedException e) {
+				if (System.currentTimeMillis() - callTimeoutSeconds >= timeBeforeCall) {
+					// most likely a timeout occured. 
+					// TODO: check why we did not get a TimeoutException
+					wasTimedOut = true;
+				}
+				else {
+					// some other strange InterruptedException
+					callError = e;
+				}
+// TODO: check how CORBA timeout behaves, and whether a corba exception would be wrapped as an ExecutionException
+//			} catch (???CORBATimeoutEx??? e) {
+//				wasTimedOut = true;
+//			}
+			} catch (Throwable thr) { // ExecutionException or unexpected
+				callError = thr;
+			}
+			if (wasTimedOut) {
 				try {
+					// notify the error handler
 					err.resourceUnreachable(resourceChecker.getResource()); 
 				} catch (Throwable thr) {
-                    logger.log(Level.WARNING, "Failed to propagate unavailability of resource '" + resourceChecker.getResourceName() + "' to the error handler!", e);
-				} 
-			} catch (Exception e) { // InterruptedException, ExecutionException
+                    logger.log(Level.WARNING, "Failed to propagate unavailability of resource '" + resourceChecker.getResourceName() + "' to the error handler!", thr);
+				} 				
+			}			
+			else if (callError != null) {
                 // the asynchronous call "resourceChecker.checkState()" failed, but not because of a timeout.
-                logger.log(Level.WARNING, "Failed to check the status of resource '" + resourceChecker.getResourceName() + "'.", e);
+                logger.log(Level.WARNING, "Failed to check the status of resource '" + resourceChecker.getResourceName() + "'.", callError);				
 			}
-		}
-		
-		protected synchronized ExecutorService getThreadPool() {
-			if (threadPool == null) {
-				threadPool =  Executors.newCachedThreadPool(threadFactory);
-			}
-			return threadPool;
-		}
+		}		
 	}
 	
 	/**
@@ -216,8 +276,16 @@ public class SubsysResourceMonitor {
         /**
          * Called when the resource could not be reached at all.
          * The resource object is passed to allow using one handler for many resources.
+         * <p>
+         * The return value controls controls whether monitoring of this resource will be stopped:
+         * <ol>
+         * <li><code>true</code> means that the error handler decided that this resource is unreachable beyond repair, 
+         *     and that no further monitoring calls should be made.
+         *     This can avoid potential problems with an increasing number of hanging calls and eventually stopping the respective threads.
+         * <li><code>false</code> means that monitoring calls should continue.
+         * </ol>
          */
-        abstract void resourceUnreachable(T resource);
+        abstract boolean resourceUnreachable(T resource);
         
         /**
          * Called when {@link SubsysResourceMonitor}
