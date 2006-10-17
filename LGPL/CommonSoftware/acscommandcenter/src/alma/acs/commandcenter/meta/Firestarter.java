f9b4fcfc5fffe293dfc620ba2bd7a46b4fa1639c
mschilli <mschilli@523d945c-050c-4681-91ec-863ad3bb968a>
2006-10-17 17:24:39 +0000

    made leaner, not a public class any more
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@60753 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acscommandcenter/src/alma/acs/commandcenter/meta/Firestarter.java
+++ LGPL/CommonSoftware/acscommandcenter/src/alma/acs/commandcenter/meta/Firestarter.java
@@ -6,75 +6,52 @@ package alma.acs.commandcenter.meta;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.omg.PortableServer.POA;
-
-import alma.acs.component.client.ComponentClient;
 import alma.acs.component.client.AdvancedComponentClient;
-import alma.acs.container.ContainerServices;
 import alma.acs.container.corba.AcsCorba;
 import alma.acs.util.AcsLocations;
 
 
 /**
- * Helps to bring an application into the Acs environment.
+ * Helper for advanced Acs clients.
  * <p>
- * Usage: When interested in component retrieval:
- * <ol>
- * <li>fs = new Firestarter();
- * <li>fs.prepareContainerServices(null, null);
- * <li>fs.giveContainerServices()
- * </ol>
- * 
- * When interested in Maci Administration:
- * <ol>
- * <li>fs = new Firestarter();
- * <li>fs.prepareSupervisorFactory(null)
- * <li>fs.giveSupervisorFactory()
- * </ol>
- * 
- * Eventually, in both cases, you should always call:
+ * Create a subclass and, potentially, set some of the fields to desired values:
  * <ul>
- * <li>fs.shutdown();
+ * <li> Used by acscorba:
+ * <li> --- acscorbaLog
+ * <li> --- orbPort
+ * <li> --- orbPortSearchRetry
+ * <li> Used by componentclient:
+ * <li> --- compclientLog
+ * <li> --- managerLoc
+ * <li> Used by this class itself:
+ * <li> --- firestarterLog
+ * </ul>
+ * or leave them <code>null</code> to use defaults.
+ * 
+ * <p>
+ * Then use these methods to have some work done for you:
+ * <ul>
+ * <li>giveOrb()
+ * <li>giveAdvancedComponentClient()
  * </ul>
  * 
- * </p>
- * 
+ * <p>
+ * Finally, stop this firestarter through a call to
+ * <ul>
+ * <li>shutdown()
+ * </ul>
  * 
  * @author mschilli
  */
 public class Firestarter {
 
+
+	// ==========================================================
+	// API to use
+	// ==========================================================
+
+
 	protected String clientName = null;
-
-	protected AcsCorba acsCorba = null;
-	protected POA rootPoa = null;
-	protected ComponentClient componentClient = null;
-	protected MaciSupervisorFactory maciSupervisorFactory = null;
-
-	protected Logger firestarterLog = null;
-	
-	protected Logger acscorbaLog = null;
-	protected Integer orbPort = null;
-	protected Integer orbPortSearchRetry = null;
-
-
-	// ==========================================================
-	// Firestarter
-	// ==========================================================
-
-
-	/**
-	 * Creates a Firestarter for a client with the specified name (like "AcsCommandCenter"
-	 * or "OMC").
-	 * 
-	 * @param clientName will be used as namespace prefix for the default-loggers
-	 * 
-	 * @see 
-	 * @since ACS 5.0
-	 */
-	public Firestarter (String clientName) {
-		this(clientName, null);
-	}
 
 
 	/**
@@ -82,25 +59,56 @@ public class Firestarter {
 	 * or "OMC") and the specified Logger.
 	 * 
 	 * @param clientName will be used as namespace prefix for the default-loggers
-	 * @param firestarterLog will be used to log messages from this class
 	 * 
-	 * @see 
+	 * @see
 	 * @since ACS 5.0
 	 */
-	public Firestarter (String clientName, Logger firestarterLog) {
-
-		if (clientName == null) {
+	protected Firestarter(String clientName) {
+		if (clientName == null)
 			throw new NullPointerException("clientName should not be null");
-		}
-	
-		if (firestarterLog == null) {
-			firestarterLog = Logger.getLogger(clientName + ".Firestarter");
-		}
 
 		this.clientName = clientName;
-		this.firestarterLog = firestarterLog;
 	}
-	
+
+
+	// used when creating the acscorba
+	protected Logger acscorbaLog = null;
+	protected Integer orbPort = null;
+	protected Integer orbPortSearchRetry = null;
+
+	// used when creating the componentclient
+	protected Logger compclientLog = null;
+	protected String managerLoc = null;
+
+	// used when shutting down this firestarter
+	protected Logger firestarterLog = null;
+
+
+	/**
+	 * Should only be used by people who know what they're doing!
+	 * 
+	 * @return an advanced component client
+	 * @throws Exception any connection-related exception
+	 */
+	protected AdvancedComponentClient giveAdvancedComponentClient () throws Exception {
+		prepareAdvancedComponentClient(); // may fail
+
+		return advancedComponentClient;
+	}
+
+
+
+	/**
+	 * Should only be used by people who know what they're doing!
+	 * 
+	 * @return the orb
+	 * @throws Exception any connection-related exception
+	 */
+	protected org.omg.CORBA.ORB giveOrb () throws OrbInitException {
+		prepareAcsCorba(); // may fail
+
+		return acsCorba.getORB();
+	}
 
 
 	/**
@@ -109,21 +117,14 @@ public class Firestarter {
 	public void shutdown () {
 
 		try {
-			if (componentClient != null) {
-				componentClient.tearDown();
-				componentClient = null;
+			if (advancedComponentClient != null) {
+				advancedComponentClient.tearDown();
+				advancedComponentClient = null;
 			}
 		} catch (Throwable t) {
-			firestarterLog.log(Level.INFO, "failed to stop componentclient", t);
-		}
-
-		try {
-			if (maciSupervisorFactory != null) {
-				maciSupervisorFactory.stop();
-			   maciSupervisorFactory = null;
-			}
-		} catch (Throwable t) {
-			firestarterLog.log(Level.INFO, "failed to stop macisupervisor(s)", t);
+			if (firestarterLog == null)
+				firestarterLog = Logger.getLogger(clientName + ".Firestarter");
+			firestarterLog.log(Level.INFO, "failed to stop advancedcomponentclient", t);
 		}
 
 		try {
@@ -136,9 +137,10 @@ public class Firestarter {
 				}.start();
 				acsCorba.blockOnORB();
 				acsCorba = null;
-				rootPoa = null;
 			}
 		} catch (Throwable t) {
+			if (firestarterLog == null)
+				firestarterLog = Logger.getLogger(clientName + ".Firestarter");
 			firestarterLog.log(Level.INFO, "failed to stop orb", t);
 		}
 
@@ -146,172 +148,81 @@ public class Firestarter {
 
 
 	// ==========================================================
-	// ContainerServices
+	// AdvancedComponentClient
 	// ==========================================================
 
-	
-	/**
-	 * This class is <i>fail-fast</i> in that it tries to set up the remote connectivity
-	 * instantly on a call to this method. If this first call goes well, subsequent calls
-	 * will succeed (aside from other general errors).
-	 * <p>
-	 * After the first *successful* execution, it will ignore subsequent invokations. This
-	 * means, this method can be called as often as wanted.</p>
-	 * <p>
-	 * If you don't specify the <code>managerLoc</code>, this will delegate to
-	 * {@link alma.acs.util.AcsLocations} to find the manager. </p>
-	 * 
-	 * @param compclientLog use <code>null</code> for default
-	 * @param managerLoc use <code>null</code> for default
-	 * @throws Exception if construction of {@link Logger} or {@link ComponentClient} fails
-	 */
-	public void prepareContainerServices (Logger compclientLog, String managerLoc) throws Exception {
 
-		if (componentClient != null) {
+	protected AdvancedComponentClient advancedComponentClient = null;
+
+	/**
+	 * Helper, sets up an advanced component client if necessary.
+	 * 
+	 * @throws Exception
+	 */
+	protected void prepareAdvancedComponentClient () throws Exception {
+
+		if (advancedComponentClient != null)
 			return; // already prepared
-		}
-		
-		if (compclientLog == null) {
+
+
+		if (compclientLog == null)
 			compclientLog = Logger.getLogger(clientName + ".ComponentClient");
-		} 
 
-		if (managerLoc == null) {
+		if (managerLoc == null)
 			managerLoc = AcsLocations.figureOutManagerLocation();
-		} 
 
-		/* the following call may fail with exception. if it succeeds, 
-		 * we can continue working with acsCorba and rootPoa
-		 */
-		prepareAcsCorba(); 
-		
-		this.componentClient = createComponentClient(compclientLog, managerLoc);
+
+		prepareAcsCorba(); // may fail
+
+		AdvancedComponentClient x = new AdvancedComponentClient(compclientLog, managerLoc, clientName + ".ComponentClient",
+				acsCorba);
+		advancedComponentClient = x;
+
 	}
 
 
-
-	/**
-	 * Hook for subclasses. This default implementation creates an
-	 * <code>AdvancedComponentClient</code>.
-	 * 
-	 * @throws Exception if creation of component client fails
-	 */
-	protected ComponentClient createComponentClient (Logger compclientLog, String managerLoc) throws Exception {
-		AdvancedComponentClient ret;
-		ret = new AdvancedComponentClient(compclientLog, managerLoc, clientName+".ComponentClient", acsCorba);
-		return ret;
-	}
-
-
-	/**
-	 * Returns the {@link ContainerServices}. Note: {@link #prepareContainerServices()}
-	 * must have been executed successfully for this method to work
-	 * 
-	 * @throws IllegalStateException if {@link #prepareContainerServices()} was not yet
-	 *            executed successfully
-	 * @return the {@link ContainerServices}
-	 */
-	public ContainerServices giveContainerServices () {
-		if (componentClient == null) {
-			throw new IllegalStateException("container services not prepared");
-		}
-		return componentClient.getContainerServices();
-	}
-	
-	
-	// ==========================================================
-	// MaciSupervisors
-	// ==========================================================
-
-
-	/**
-	 * This class is <i>fail-fast</i> in that it tries to set up the remote connectivity
-	 * instantly on a call to this method. If this first call goes well, subsequent calls
-	 * will succeed (aside from other general errors).
-	 * 
-	 * @param factoryLog use <code>null</code> for default
-	 * @throws Exception if construction of {@link MaciSupervisor} fails
-	 */
-	public void prepareSupervisorFactory (Logger factoryLog) throws Exception {
-
-		if (maciSupervisorFactory != null) {
-			return; // already prepared
-		}
-
-		if (factoryLog == null) {
-			factoryLog = Logger.getLogger(clientName + ".Factory");
-		} 
-		
-		/* the following call may fail with exception. if it succeeds, 
-		 * we can continue working with acsCorba and acsCorba.getORB()
-		 */
-		prepareAcsCorba();
-
-		maciSupervisorFactory = new MaciSupervisorFactory(clientName, factoryLog, acsCorba.getORB());
-	}
-
-	/**
-	 * Returns a MaciSupervisorFactory. Note: {@link #prepareSupervisorFactory()} must have been executed
-	 * successfully for this method to work
-	 * 
-	 * @throws IllegalStateException if {@link #prepareSupervisorFactory()} was not yet executed
-	 *            successfully
-	 * @return a MaciSupervisor
-	 */
-	public MaciSupervisorFactory giveSupervisorFactory () {
-		if (maciSupervisorFactory == null) {
-			throw new IllegalStateException("supervisor factory not prepared");
-		}
-		return maciSupervisorFactory;
-	}
-
-	
 
 	// ==========================================================
 	// AcsCorba
 	// ==========================================================
-	
-	
-	/**
-	 * Set AcsCorba's logger and orb-port strategy.
-	 * <p>
-	 * For a description of the parameters <code>orbPort</code> and
-	 * <code>orbPortSearchRetry</code>, see
-	 * {@link AcsCorba#setPortOptions(Integer, Integer)}.
-	 * </p>
-	 * 
-	 * @param acscorbaLog use <code>null</code> for default
-	 * @param orbPort use <code>null</code> for default
-	 * @param orbPortSearchRetry use <code>null</code> for default
-	 */
-	public void setAcsCorbaOptions (Logger acscorbaLog, Integer orbPort, Integer orbPortSearchRetry) {
-		this.acscorbaLog = acscorbaLog;
-		this.orbPort = orbPort;
-		this.orbPortSearchRetry = orbPortSearchRetry;
-	}
 
+
+	protected AcsCorba acsCorba = null;
 
 	/**
-	 * This class is <i>fail-fast</i> in that it tries to create an {@link AcsCorba}
-	 * instantly on a call to this method. If this first call goes well, subsequent calls
-	 * will succeed (aside from other general errors).
+	 * Helper, sets up an acs corba instance if necessary.
 	 * 
-	 * @throws Exception if construction of {@link AcsCorba} fails
+	 * @throws OrbInitException if initialization of {@link AcsCorba} fails
 	 */
-	protected void prepareAcsCorba () throws Exception {
+	protected void prepareAcsCorba () throws OrbInitException {
 
-		if (acsCorba != null && acsCorba.isInitialized()) {
+		if (acsCorba != null && acsCorba.isInitialized())
 			return; // already prepared
-		}
 
-		if (acscorbaLog == null) {
+
+		if (acscorbaLog == null)
 			acscorbaLog = Logger.getLogger(clientName + ".AcsCorba");
-		}
-		
+
+
 		acsCorba = new AcsCorba(acscorbaLog);
 		acsCorba.setPortOptions(orbPort, orbPortSearchRetry);
 		boolean isAdmin = true;
-		acsCorba.initCorbaForClient(isAdmin);
+		
+		try {
+			acsCorba.initCorbaForClient(isAdmin);
+		} catch (Exception exc) {
+			throw new OrbInitException("acsCorba.initCorbaForClient(true) failed", exc);
+		}
 	}
 
+	
+	public static class OrbInitException extends Exception {
+
+		OrbInitException(String message, Throwable cause) {
+			super(message, cause);
+		}
+		
+	}
+	
 
 }
