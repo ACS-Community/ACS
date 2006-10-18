673230689f1d3bf31e6eb4955a064dad55fdd20d
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2006-10-18 15:07:31 +0000

    First skeleton of the dialog to submit queries
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@60842 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/jlog/src/alma/acs/logging/archive/QueryDlg.java
@@ -0,0 +1,79 @@
+
+package alma.acs.logging.archive;
+
+import java.awt.BorderLayout;
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JRootPane;
+
+/**
+ * A class to setup a query to submit to the DB
+ * 
+ * @author acaproni
+ *
+ */
+public class QueryDlg extends JDialog implements ActionListener {
+	
+	private JButton submitBtn;
+	private JButton doneBtn;
+
+	/**
+	 * Empty constructor
+	 */
+	public QueryDlg() {
+		super();
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		initGUI();
+		setBounds(50,50,50,50);
+		pack();
+		setVisible(true);
+	}
+	
+	/**
+	 * @see java.awt.event.ActionListener
+	 * @see java.awt.event.ActionEvent
+	 */
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource()==submitBtn) {
+			System.out.println("Submitting a query");
+		} else if (e.getSource()==doneBtn) {
+			setVisible(false);
+			dispose();
+		} else {
+			System.err.println("Unknown event "+e);
+		}
+	}
+	
+	/**
+	 * Build the GUI
+	 *
+	 */
+	private void initGUI() {
+		JRootPane mainPnl = this.getRootPane();
+		mainPnl.setLayout(new BorderLayout());
+		
+//		 Add the OK, CANCEL buttons
+		JPanel btnPnl = new JPanel();
+		btnPnl.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+		BoxLayout boxLayout = new BoxLayout(btnPnl,BoxLayout.LINE_AXIS);
+		btnPnl.setLayout(boxLayout);
+		btnPnl.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
+		submitBtn = new JButton("Submit");
+		submitBtn.addActionListener(this);
+		doneBtn = new JButton("Cancel");
+		doneBtn.addActionListener(this);
+		btnPnl.add(submitBtn,BorderLayout.WEST);
+		btnPnl.add(Box.createRigidArea(new Dimension(10, 0)));
+		btnPnl.add(doneBtn,BorderLayout.EAST);
+		mainPnl.add(btnPnl,BorderLayout.SOUTH);
+	}
+}
