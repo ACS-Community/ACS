5e6df80cd6d5dbef6557a0a52895df476d506cb8
gchiozzi <gchiozzi@523d945c-050c-4681-91ec-863ad3bb968a>
2006-10-19 07:33:03 +0000

    Added a test receiving a CORBA Sysatem Exception and mapping it to AcsJCORBAProblem.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@60890 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jcontexmpl/src/alma/acsexmpl/clients/ErrorComponentTest.java
+++ LGPL/CommonSoftware/jcontexmpl/src/alma/acsexmpl/clients/ErrorComponentTest.java
@@ -1,11 +1,17 @@
 package alma.acsexmpl.clients;
 
-import alma.ACSErrTypeCommon.ACSErrTypeCommonEx;
+import org.omg.CORBA.SystemException;
+
+import alma.acs.exceptions.AcsJException;
+
 import alma.ACSErrTypeCommon.GenericErrorEx;
+import alma.ACSErrTypeCommon.ACSErrTypeCommonEx;
+
 import alma.ACSErrTypeCommon.wrappers.AcsJACSErrTypeCommonEx;
 import alma.ACSErrTypeCommon.wrappers.AcsJGenericErrorEx;
+import alma.ACSErrTypeCommon.wrappers.AcsJCORBAProblemEx;
+
 import alma.acs.component.client.ComponentClientTestCase;
-import alma.acs.exceptions.AcsJException;
 import alma.acsexmplErrorComponent.ErrorComponent;
 import alma.acsexmplErrorComponent.ErrorComponentHelper;
 
@@ -111,6 +117,31 @@ public class ErrorComponentTest extends ComponentClientTestCase {
 			}						
 		}
 	}
+
+        public void testReceiveCorbaSystemException() {
+	    try {
+	        errorComp.corbaSystemException();
+	    }
+	    /*
+	     * This shows how to map a CORBA System Exception into the
+	     * corresponding ACS Exception wrapper
+	     */
+	    catch(org.omg.CORBA.SystemException ex) {
+	        m_logger.info("Caught CORBA.SystemException");
+		AcsJCORBAProblemEx corbaProblemEx = new AcsJCORBAProblemEx(ex);
+		corbaProblemEx.setMinor(ex.minor);
+		corbaProblemEx.setCompletionStatus(ex.completed.value());
+		corbaProblemEx.setInfo(ex.toString());
+		corbaProblemEx.log(m_logger);
+	    } catch(Throwable th) {
+	        m_logger.info("Caught an unexpected Exception");
+   	        AcsJGenericErrorEx badMethodEx = new AcsJGenericErrorEx(th);
+		badMethodEx.setErrorDesc("corbaSystemException has thrown an UNEXPECTED exception");
+		badMethodEx.log(m_logger);
+		fail("Expected a CORBA System Exception");
+	    }
+
+        }
 
 	public void testCompletionFromException() {
 		fail("Not yet implemented");
