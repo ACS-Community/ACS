5297901ea264127646a6df40a23adf00c47060c0
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2006-11-29 16:07:37 +0000

    reactivated this test class which was lying dormant since this package was completely rewritten
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@63080 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsjlog/test/alma/acs/logging/AcsLoggingHandlerTest.java
+++ LGPL/CommonSoftware/acsjlog/test/alma/acs/logging/AcsLoggingHandlerTest.java
@@ -21,68 +21,91 @@
  */
 package alma.acs.logging;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
 import java.util.logging.LogRecord;
 
-import alma.acs.logging.formatters.AcsXMLLogFormatter;
+import alma.acs.logging.config.LogConfig;
+import alma.acs.testsupport.LogRecordCollectingLogger;
+
+import junit.framework.TestCase;
 
 /**
- * @author rgeorgie
- *
- *Tests the AcsLogLevel
  */
-public class AcsLoggingHandlerTest extends junit.framework.TestCase
+public class AcsLoggingHandlerTest extends TestCase
 {
-//	AcsLoggingHandler m_acsLoggingHandler = new AcsLoggingHandler();
-//	AcsXMLLogFormatter m_acsXMLFormatter = null;
-//
-//	static String localcopy;
-//	/**
-//	 * Manager that controls the default java loggers and services.
-//	 */
-//	private LogManager m_logManager;
-//
-//	/**
-//	 * String corresponding to class that is used in the properties file
-//	 * to define level, cacheSize, formatter, local copy.
-//	 */
-//	private String m_handlerClassName;
-//
-//	private boolean m_verbose = false;
-//
-//	public AcsLoggingHandlerTest(String name)
-//	{
-//		super(name);
-//		m_acsXMLFormatter = new AcsXMLLogFormatter();
-//		m_logManager = LogManager.getLogManager();
-//
-//		m_handlerClassName = AcsLoggingHandler.class.getName();
-//
-//		localcopy = m_logManager.getProperty(m_handlerClassName + ".localcopy");
-//	}
-//
-//	/**
-//	 * Method testFormatRecordLog. Checks whether a log record is formatted 
-//	 * according to what is expected using the Java Formatter.
-//	 */
-//	public void testFormatSimpleLogRecord()
-//	{
-//		//formatSimpleLogRecord
-//		LogRecord logRecord = new LogRecord(AcsLogLevel.INFO, "INFO testFormatSimpleLogRecord message");
-//		assertTrue(m_acsLoggingHandler.formatSimpleLogRecord(logRecord) != null);
-//	}
-//	
-//	/** 
-//	 * According to the specifications, the level set in the properties file should not be less than INFO
-//	 * at the different stages of the project.
-//	 */
-//	public void testPublish()
-//	{
-//		Level curLevel = m_acsLoggingHandler.getLevelProp();
-//		int currentLevel = AcsLogLevel.getNativeLevel(curLevel).getAcsLevel();
-//		LogRecord logRecord = new LogRecord(AcsLogLevel.INFO, "INFO testPublish message");
-//		int testLevel = AcsLogLevel.getNativeLevel(logRecord.getLevel()).getAcsLevel();
-//		assertTrue(currentLevel <= testLevel);
-//	}
+	private AcsLoggingHandler loggingHandler;
+	private TestLogQueue logQueue;
+	
+	protected void setUp() {
+		logQueue = new TestLogQueue();
+		LogConfig logConfig = new LogConfig();
+		loggingHandler = new AcsLoggingHandler(logQueue, logConfig);
+	}
+	
+	/**
+	 * Tests if logs with the silly log level <code>Level.OFF</code> are successfully ignored.
+	 * Note that Level.OFF is only meant to be used as an argument to the setLevel() method, 
+	 * but not for the log methods. Yet the JDK processes logs with Level.OFF, which ACS does not.
+	 */
+	public void testOffLevel() {
+		LogRecordCollectingLogger colLogger = LogRecordCollectingLogger.getCollectingLogger("colLogger");
+		
+		// first an INFO log to warm up
+		colLogger.info("test message INFO");
+		LogRecord testRecord = colLogger.getCollectedLogRecords()[0];
+		assertEquals(Level.INFO.intValue(), testRecord.getLevel().intValue());		
+		loggingHandler.publish(testRecord);
+		assertEquals(1, logQueue.logRecords.size());
+
+		colLogger.clearLogRecords();
+		logQueue.reset();
+
+		// now comes the silly OFF log
+		// the JDK logger and handler are stupid enough to process this log at highest possible level!
+		colLogger.log(Level.OFF, "test message OFF");
+		testRecord = colLogger.getCollectedLogRecords()[0];
+		assertEquals(Level.OFF.intValue(), testRecord.getLevel().intValue());
+		// but our publish method is supposed to catch this...
+		loggingHandler.publish(testRecord);
+		assertEquals(0, logQueue.logRecords.size());
+	}
+	
+	
+	
+	/**
+	 * Test log queue that allows verification of calls to <code>log</code> and <code>flush</code>.
+	 * <p> 
+	 * Actually this class does not support any of the special features of DispatchingLogQueue.
+	 * It will not even forward the log record to it.
+	 * It only extends DispatchingLogQueue because AcsLoggingHandler requires a DispatchingLogQueue in the ctor
+	 * and I see no big advantage in refactoring this.
+	 */
+	private static class TestLogQueue extends DispatchingLogQueue {
+		public int nFlush;
+		public List<LogRecord> logRecords;
+		
+		TestLogQueue() {
+			logRecords = new ArrayList<LogRecord>();
+			reset();
+		}
+		
+		void reset() {
+			nFlush = 0;
+			logRecords.clear();
+		}
+		
+		Future<Boolean> flush() {
+			nFlush++;
+			return super.flush();
+		}
+
+		synchronized boolean log(LogRecord logRecord) {
+			logRecords.add(logRecord);
+			return true;
+		}
+	}
+	
 }
