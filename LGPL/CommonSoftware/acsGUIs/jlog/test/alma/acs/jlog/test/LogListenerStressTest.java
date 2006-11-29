5f1618bc5cde3f5e0b1cee78c7569bc161d35344
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2006-11-29 11:54:59 +0000

    Stress test for the reception of logs.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@63048 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/acsGUIs/jlog/test/alma/acs/jlog/test/LogListenerStressTest.java
@@ -0,0 +1,103 @@
+package alma.acs.jlog.test;
+
+import com.cosylab.logging.engine.ACS.ACSRemoteLogListener;
+import com.cosylab.logging.engine.ACS.ACSRemoteRawLogListener;
+import com.cosylab.logging.engine.ACS.LCEngine;
+import com.cosylab.logging.engine.log.ILogEntry;
+import com.cosylab.logging.engine.log.LogTypeHelper;
+
+public class LogListenerStressTest implements ACSRemoteLogListener, ACSRemoteRawLogListener{
+	
+	private int xmlReceived=0;
+	private int logReceived=0;
+	
+	private int[] logTypeReceived=new int[LogTypeHelper.getNumberOfTypes()];
+	
+	public Object done = new Object();
+	
+	//	 The engine that connects to the logging client
+	private LCEngine engine;
+	
+	boolean msgPrinted = false;
+	
+	public LogListenerStressTest() {
+		engine = new LCEngine();
+		engine.addLogListener(this);
+		engine.addRawLogListener(this);
+		engine.connect();
+	}
+	/**
+	 * Print only the messages received from the client
+	 * 
+	 * @see com.cosylab.logging.engine.ACS.ACSRemoteLogListener
+	 */
+	public void xmlEntryReceived(String str) {
+		if (str.indexOf("logClient.cpp")>=0) {
+			xmlReceived++;
+			if (str.indexOf("Done")>=0) {
+				System.out.println("xmlEntryReceived => Done received");
+			}
+		}
+	}
+	
+	/**
+	 * Print only the messages received from the client
+	 * 
+	 * @see com.cosylab.logging.engine.ACS.ACSRemoteRawLogListener
+	 */
+	public void logEntryReceived(ILogEntry log) {
+		if (log.getField(ILogEntry.FIELD_FILE).toString().indexOf("logClient.cpp")>=0) {
+			logReceived++;
+			Integer logType = ((Integer)log.getField(ILogEntry.FIELD_ENTRYTYPE));
+			logTypeReceived[logType]++;
+			if (log.getField(ILogEntry.FIELD_LOGMESSAGE).toString().indexOf("Done")>=0) {
+				System.out.println("logEntryReceived => Done received");
+				printNums();
+			}
+		}
+	}
+	
+	public void printNums() {
+		msgPrinted = true;
+		System.out.println("XML entries received: "+xmlReceived);
+		System.out.println("Log entries received: "+xmlReceived);
+		for (int t=0; t<logTypeReceived.length; t++) {
+			System.out.print("Num. of received logs of type "+LogTypeHelper.getLogTypeDescription(t));
+			System.out.println(": "+logTypeReceived[t]);
+		}
+		synchronized(this) {
+			notifyAll();
+		}
+	}
+	
+	public void finalize() { 
+		if (!msgPrinted) {
+			System.out.println("Forced to print summary");
+			printNums();
+		}
+	}
+	
+	/**
+	 * Disconnect from the logging channel
+	 *
+	 */
+	public void disconnet() {
+		engine.disconnect();
+	}
+	
+	/**
+	 * Main
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LogListenerStressTest ll = new LogListenerStressTest();
+		synchronized(ll.done) {
+			try {
+				ll.done.wait();
+			} catch (InterruptedException ie) {}
+		} 
+		ll.disconnet();
+		System.out.println("Exiting");
+	}
+}
