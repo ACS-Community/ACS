1aa68ae5325bd2aad67cd6e07aedb0be5313affe
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2006-11-29 12:03:19 +0000

    Refactored
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@63052 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jlog/test/jlogTestRun.sh
+++ LGPL/CommonSoftware/jlog/test/jlogTestRun.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-export ACS_TMP=$PWD/tmp
-. acsstartupAcsPorts
-
-export ACS_INSTANCE=`cat $ACS_TMP/acs_instance`
-
-echo "Running $*"
-if [ "$WIND_BASE" != "" ] 
-then
-    export MANAGER_REFERENCE=corbaloc::`getIP`:`getManagerPort`/Manager
-    export DAL_REFERENCE=corbaloc::`getIP`:`getCDBPort`/CDB
-    sleep 250
-    $*
-else 
-    $*
-fi
-
-echo "All done!"
+acsutilTATTestRunner logClient 2 5000  0 >/dev/null &
+acsutilTATTestRunner logClient 2 5000  0 >/dev/null &
+acsutilTATTestRunner logClient 2 5000  0 >/dev/null &
+acsutilTATTestRunner logClient 2 5000  0 >/dev/null &
+acsutilTATTestRunner logClient 2 5000  0 >/dev/null &
+acsutilTATTestRunner logClient 2 5000  0 >/dev/null &
+acsutilTATTestRunner logClient 2 5000  0 >/dev/null &
+acsutilTATTestRunner logClient 2 5000  0 >/dev/null &
+acsutilTATTestRunner logClient 2 5000  0 >/dev/null &
+wait
+acsutilTATTestRunner logClient 2 1000  1 2>&1 
+sleep 30
