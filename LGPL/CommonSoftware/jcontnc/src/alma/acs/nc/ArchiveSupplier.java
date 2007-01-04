3022a06414dc7dabf14f6e2a188103b2d03dd53e
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2007-01-04 12:28:24 +0000

    fixed comments and variable names in publishEvent
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@64188 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jcontnc/src/alma/acs/nc/ArchiveSupplier.java
+++ LGPL/CommonSoftware/jcontnc/src/alma/acs/nc/ArchiveSupplier.java
@@ -19,110 +19,104 @@
 
 package alma.acs.nc;
 
+import org.omg.CosNotification.Property;
+import org.omg.CosNotification.StructuredEvent;
+
 import alma.acs.container.ContainerServices;
 import alma.acs.exceptions.AcsJException;
-
-import org.omg.CosNotification.StructuredEvent;
-import org.omg.CosNotification.Property;
-
+import alma.acs.util.UTCUtility;
 import alma.acsnc.EventDescription;
 import alma.acsnc.EventDescriptionHelper;
+
 /**
  * Used to supply (BACI property) events to the archiving notification channel.
  * 
  * @author dfugate
- * @version $Id: ArchiveSupplier.java,v 1.7 2006/11/22 09:46:27 cparedes Exp $
+ * @version $Id: ArchiveSupplier.java,v 1.8 2007/01/04 12:28:24 hsommer Exp $
  */
 public class ArchiveSupplier extends SimpleSupplier {
-   /**
-    * Creates a new instance of ArchiveSupplier
-    * 
-    * @param services
-    *           This is used to get the name of the component and to access the
-    *           ACS logging system.
-    * @throws AcsJException
-    *            There are literally dozens of CORBA exceptions that could be
-    *            thrown by the ArchiveSupplier class. Instead, these are
-    *            converted into an ACS Error System exception for the
-    *            developer's convenience.
-    */
-   public ArchiveSupplier(ContainerServices services) throws AcsJException {
-      super(alma.acscommon.ARCHIVING_CHANNEL_NAME.value, services);
-   }
+	/**
+	 * Creates a new instance of ArchiveSupplier
+	 * 
+	 * @param services
+	 *           This is used to get the name of the component and to access the
+	 *           ACS logging system.
+	 * @throws AcsJException
+	 *            There are literally dozens of CORBA exceptions that could be
+	 *            thrown by the ArchiveSupplier class. Instead, these are
+	 *            converted into an ACS Error System exception for the
+	 *            developer's convenience.
+	 */
+	public ArchiveSupplier(ContainerServices services) throws AcsJException {
+		super(alma.acscommon.ARCHIVING_CHANNEL_NAME.value, services);
+	}
 
-   /**
-    * Overridden.
-    * 
-    * @return string
-    */
-   protected String getChannelKind() {
-      return alma.acscommon.ARCHIVING_CHANNEL_KIND.value;
-   }
+	/**
+	 * Overridden.
+	 * 
+	 * @return string
+	 */
+	protected String getChannelKind() {
+		return alma.acscommon.ARCHIVING_CHANNEL_KIND.value;
+	}
 
-   /**
-    * Overridden.
-    * 
-    * @return string
-    */
-   protected String getNotificationFactoryName() {
-      return alma.acscommon.ARCHIVE_NOTIFICATION_FACTORY_NAME.value;
-   }
+	/**
+	 * Overridden.
+	 * 
+	 * @return string
+	 */
+	protected String getNotificationFactoryName() {
+		return alma.acscommon.ARCHIVE_NOTIFICATION_FACTORY_NAME.value;
+	}
 
-   /**
-    * Overridden.
-    * 
-    * @return string
-    */
-   protected String getChannelDomain() {
-      return alma.acscommon.ARCHIVING_DOMAIN.value;
-   }
+	/**
+	 * Overridden.
+	 * 
+	 * @return string
+	 */
+	protected String getChannelDomain() {
+		return alma.acscommon.ARCHIVING_DOMAIN.value;
+	}
 
-   /**
-    * Takes a generic Java object and tries to pack it into a CORBA Any and
-    * publish it to the event channel. This will fail if the parameter is not
-    * CORBA-generated from a user-defined IDL struct. In simple terms, trying to
-    * publish native Java types is impossible because they have no CORBA mapping
-    * to say Python or C++ types.
-    * 
-    * @param customStruct
-    *           An instance of the IDL struct (Java class) to be published.
-    * @throws AcsJException
-    *            There are an enormous amount of possibilities pertaining to why
-    *            an AcsJException would be thrown by publishEvent.
-    */
-   public void publishEvent(java.lang.Object customStruct)throws AcsJException {
+	/**
+	 * Takes an object and tries to pack it into a CORBA Any and
+	 * publish it to the event channel. 
+	 * <p>
+	 * The parameter <code>obj</code> can be one of the JDK classes <code>Integer</code>, <code>Long</code>, 
+	 * <code>Float</code>, <code>Double</code>, <code>String</code>. 
+	 * It could also be <code>null</code>, or come from an IDL-defined struct (thus implementing <code>IDLEntity</code>);
+	 * at least the latter should never occur since baci properties don't have complex values. 
+	 * 
+	 * @param customStruct
+	 *           An instance of the IDL struct (Java class) to be published.
+	 * @throws AcsJException
+	 *            There are an enormous amount of possibilities pertaining to why
+	 *            a subclass of AcsJException would be thrown by publishEvent.
+	 */
+	public void publishEvent(Object value) throws AcsJException {
 
-    //the eventName consists of container named concatenated with the
-    //component and property names, delimited by ':'s.
-      String typeName = customStruct.getClass().getName().substring(
-            customStruct.getClass().getName().lastIndexOf('.') + 1);
-      String containerName = getHelper().getContainerServices().getName();
-      String param = "no_param" ;
-      String device = "no_device" ;
-      String eventName = containerName + ":" + device + ":" + param;
+		//the eventName consists of container named concatenated with the
+		//component and property names, delimited by ':'s.
+		String typeName = value.getClass().getName().substring(value.getClass().getName().lastIndexOf('.') + 1);
+		String containerName = m_services.getName();
+		String param = "no_param";
+		String device = "no_device"; // @TODO use real component/device name
+		String eventName = containerName + ":" + device + ":" + param;
 
-      // event to send
-      StructuredEvent event = getCORBAEvent(typeName, eventName);
+		// event to send
+		StructuredEvent event = getCORBAEvent(typeName, eventName);
+		event.remainder_of_body = m_services.getAdvancedContainerServices().getAny();
 
-      // Store the info for Exec/I&T into the event.
-      // create the any
-      event.remainder_of_body = m_services.getAdvancedContainerServices()
-            .getAny();
-      // get the useful data which includes the component's name, timestamp,
-      // and event count
-      EventDescription descrip = new EventDescription(containerName,
-	     alma.acs.util.UTCUtility
-            .utcJavaToOmg(System.currentTimeMillis()),1 );
-      // store the IDL struct into the structured event
-      EventDescriptionHelper.insert(event.remainder_of_body, descrip);
-      Long time_stamp = new Long(alma.acs.util.UTCUtility.utcJavaToOmg(System.currentTimeMillis()));      
+		// get the useful data which includes the component's name, timestamp, and event count
+		long utcTime = UTCUtility.utcJavaToOmg(System.currentTimeMillis());
+		EventDescription descrip = new EventDescription(containerName, utcTime, 1);
 
-      event.filterable_data = new Property[2];
-      event.filterable_data[0] = new Property(
-            "time_stamp",m_anyAide.objectToCorbaAny(time_stamp)); 
-	     
-      event.filterable_data[1] = new Property(
-            "value", m_anyAide.objectToCorbaAny(customStruct));
-      publishCORBAEvent(event);
-   }
+		// store all data into the structured event
+		EventDescriptionHelper.insert(event.remainder_of_body, descrip);
+		event.filterable_data = new Property[2];
+		event.filterable_data[0] = new Property("time_stamp", m_anyAide.objectToCorbaAny(new Long(utcTime)));
+		event.filterable_data[1] = new Property("value", m_anyAide.objectToCorbaAny(value));
+
+		publishCORBAEvent(event);
+	}
 }
