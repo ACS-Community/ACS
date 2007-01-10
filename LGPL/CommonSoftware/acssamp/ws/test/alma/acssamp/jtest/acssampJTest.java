9ddf6dadd36a4cc265f65b754455f892c272741a
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2007-01-10 14:57:09 +0000

    auto-formatted; added javadoc; fixed disconnect;
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@64392 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acssamp/ws/test/alma/acssamp/jtest/acssampJTest.java
+++ LGPL/CommonSoftware/acssamp/ws/test/alma/acssamp/jtest/acssampJTest.java
@@ -1,67 +1,52 @@
 ////////////////////////////////////////////////////////////////////////////////
 package alma.acssamp.jtest;
+
 ////////////////////////////////////////////////////////////////////////////////
 
 import alma.acs.component.client.ComponentClient;
 import alma.acs.container.ContainerServices;
 
+/**
+ * Runner for {@link acssampConsumer}.
+ * Creates and activates the consumer, waits 50 seconds, then disconnects and returns.
+ */
+public class acssampJTest extends ComponentClient {
+	private acssampConsumer consumer;
 
+	public acssampJTest(String managerLoc, String clientName) throws Exception {
+		super(null, managerLoc, clientName);
+		ContainerServices csrv = getContainerServices();
 
-public class acssampJTest extends ComponentClient
-{
-    private acssampConsumer consumer;
+		String ncChannel = "NC_LAMP1_brightness_1000000_10000000";
 
-    public acssampJTest(String managerLoc, String clientName)
-	throws Exception
-	{
-	    super(null, managerLoc, clientName);
-
-	    String ncChannel = "NC_LAMP1_brightness_1000000_10000000";
-
-	    ContainerServices csrv = getContainerServices();
-	    
-	    consumer = new acssampConsumer(ncChannel, csrv);
-	    try
-		{
-		//After consumerReady() is invoked, push_structured_event(...) is invoked
-		//by the notification channel.  That is, we have no control over when
-		//that method is called.
-		consumer.consumerReady();
-		System.out.println("Waiting for events...");
+		consumer = new acssampConsumer(ncChannel, csrv);
+		try {
+			//After consumerReady() is invoked, push_structured_event(...) is invoked
+			//by the notification channel.  That is, we have no control over when
+			//that method is called.
+			consumer.consumerReady();
+			System.out.println("Waiting for events, for 50 seconds ");
+			Thread.sleep(50000);
+		} 
+		catch (Exception e) {
+			System.err.println(e);
 		}
-	    catch(Exception e)
-		{
-		System.err.println(e);
-		consumer.ncDisconnect();
+		finally {
+			consumer.ncDisconnect();			
 		}
 	}
 
-    public static void main(String[] args)
-	{
-	    String managerLoc = System.getProperty("ACS.manager");
-	    if (managerLoc == null)
-		{
-		System.out.println("Java property 'ACS.manager' " + 
-				   " must be set to the corbaloc of the ACS manager!");
-		System.exit(-1);
+	public static void main(String[] args) {
+		String managerLoc = System.getProperty("ACS.manager");
+		if (managerLoc == null) {
+			System.out.println("Java property 'ACS.manager' must be set to the corbaloc of the ACS manager!");
+			System.exit(-1);
 		}
-	    
-	    String clientName = "acssampJTest1";
-	    
-	    double d;
-	    try
-		{
 
-		
-		acssampJTest test = new acssampJTest(managerLoc, clientName);
-
-		Thread.sleep(50000);
-		    
+		try {
+			new acssampJTest(managerLoc, "acssampJTest1");
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
 		}
-	    catch (Exception e)
-		{
-		e.printStackTrace(System.err);
-		}
-      
 	}
 }
