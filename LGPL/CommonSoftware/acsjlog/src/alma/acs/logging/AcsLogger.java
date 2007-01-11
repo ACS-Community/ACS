4ccd4905f42ddc834cee8cb08024542e13672f51
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2007-01-11 12:13:55 +0000

    bugfix: takes log level from ancestor loggers if it's null, which can happen during shutdown
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@64441 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsjlog/src/alma/acs/logging/AcsLogger.java
+++ LGPL/CommonSoftware/acsjlog/src/alma/acs/logging/AcsLogger.java
@@ -49,7 +49,8 @@ import alma.maci.loggingconfig.UnnamedLogger;
  * created May 30, 2005 4:09:47 PM
  */
 public class AcsLogger extends Logger implements LogConfigSubscriber {
-    // private in base class, need to redeclare here
+
+	// private in base class, need to redeclare here
     protected static final int offValue = Level.OFF.intValue();
 
 	/** the logger class, which must be known to unwind the stack trace. Will be this class unless we use delegation. */
@@ -93,14 +94,21 @@ public class AcsLogger extends Logger implements LogConfigSubscriber {
      *   but additionally including thread name and line of code.
      * </ul>  
      * Note that by overloading this method, we intercept all logging activities of the base class.
-     * 
+     *  
      * @see java.util.logging.Logger#log(java.util.logging.LogRecord)
      */
     public void log(LogRecord record) {
     	
+        // Level could be null and must be inherited from the ancestor loggers, 
+    	// e.g. during JDK shutdown when the log level is nulled by the JDK LogManager 
+    	Logger loggerWithLevel = this;
+    	while (loggerWithLevel.getLevel() == null) {
+    		loggerWithLevel = loggerWithLevel.getParent();
+    	}
+    	
     	// filter by log level to avoid unnecessary retrieval of context data.
     	// The same check will be repeated by the base class implementation of this method that gets called afterwards.
-    	int levelValue = getLevel().intValue();
+    	int levelValue = loggerWithLevel.getLevel().intValue();
     	if (record.getLevel().intValue() < levelValue || levelValue == offValue) {
     	    return;
     	}
