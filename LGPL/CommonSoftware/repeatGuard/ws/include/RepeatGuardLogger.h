b028d1e626e8829516d21b8dd811c35ebe9c089b
gchiozzi <gchiozzi@523d945c-050c-4681-91ec-863ad3bb968a>
2007-03-22 10:56:39 +0000

    Re-implemented according to latest discussions.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@68425 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/repeatGuard/ws/include/RepeatGuardLogger.h
+++ LGPL/CommonSoftware/repeatGuard/ws/include/RepeatGuardLogger.h
@@ -18,7 +18,7 @@
 * License along with this library; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 *
-* "@(#) $Id: RepeatGuardLogger.h,v 1.2 2007/02/27 09:14:35 nbarriga Exp $"
+* "@(#) $Id: RepeatGuardLogger.h,v 1.3 2007/03/22 10:56:32 gchiozzi Exp $"
 *
 * who       when      what
 * --------  --------  ----------------------------------------------
@@ -39,39 +39,135 @@
 
 #include "RepeatGuard.h"
 
+
 namespace Logging {
-        /** @class RepeatGuardLogger
-         *  @brief Guard class against log repetitions.
-         *
-         *   This class implements a logger with protection against logging too often
-         *
-         *   
-         */
-        class RepeatGuardLogger:public ACSLogger{
-                private:
+/**
+ * @class RepeatGuardLogger
+ * @brief Guard template class against log repetitions.
+ *
+ * This is a template class for guarding logs against logging too often.
+ * It inherits from RepeatGuard and adds different
+ * implementations of the log() method.
+ * 
+ * In all cases the log() method checks the guarding condition
+ * and, if it is the case, forwards the log() command
+ * to the given underlying logger object.
+ *
+ * Before doing this, it adds the repeatCount additional data
+ * field to the log to notify that repeated logs have been
+ * skipped according to the guard condition.
+ *
+ * Notes:
+ *
+ * 1. The implementation of the template exploits
+ *    "partial template instantiation" (see Alexandrescu, 
+ *    Modern C++ Design, 1.8).
+ *    The three signatures for the log() method are not supported
+ *    by all loggers.
+ *    What is important is that the log() signarues called for
+ *    each specific instantiation of the template are supported.
+ *    
+ *    Notice that the log() methods cannot be virtual, otherwise
+ *    partial template instantiation would not work.
+ *
+ * 2. The logger object is passed as argument of the log method
+ *    and not in the constructor of the guard to allow using
+ *    the same guard with multiple loggers.
+ *    This (in my opinion) extremely useful additionl flexibility
+ *    comes to the price of one additional parameter in the
+ *    function call.
+ *
+ * 3. Now only the log() method is implemented.
+ *    This automaticaly increments the ocunter.
+ *    It would be probably much better to have instead two methods
+ *    (as was in the original design):
+ *    - log()
+ *    - logAndIncrement()
+ *    Can you confirm this?
+ *    If nobody vote against, I will add the methods.
+ *
+ * 4. Here and in the base RepeatGuard class, the interval
+ *    time is an integer in seconds.
+ *    For consistency with the other interfaces
+ *    it should be instead a TimeInterval (long long in 100ns())
+ *    as defined in acscommon.idl.
+ *    If nobody vote against, I will change the signature.
+ */
+template <class ALogger> class RepeatGuardLogger : RepeatGuard {
 
-                protected:
-                        RepeatGuard *mGuard;
-                public:
-                        /** Constructor
-                         *  @param interval minimum interval between allowing an action(i.e. check returns true)
-                         *  @param maxRepetitions override minimum interval if maxRepetitions is reached.(0 disables this feature)
-                         */
-                        RepeatGuardLogger(const std::string &loggerName,unsigned int interval, unsigned int maxRepetitions=0);
+  private:
 
-                        ~RepeatGuardLogger();
+  protected:
 
-                        //void log(const char *format, ...);
-                        /** This method logs the given message only if, the conditions given in the
-                         *  constructor are met. It accepts printf-like parameters.
-                         *  @param format standard format of printf-like functions.
-                         */
-                        void logAndIncrement(const char *format, ...);
+  public:
 
-                        /** Appends the number of tries for this log
-                         *   @param msg message to which append number of tries
-                         */
-                        virtual void appendCount(std::string &msg);
-        };
+    /** Constructor
+     *  @param interval minimum interval between allowing an action 
+     *         (i.e. check returns true)
+     *  @param maxRepetitions override minimum interval if 
+     *         maxRepetitions is reached.(0 disables this feature)
+     */
+    RepeatGuardLogger(unsigned int interval, 
+                      unsigned int maxRepetitions=0) :  
+	RepeatGuard(interval,maxRepetitions)
+	{
+	};
+
+    virtual ~RepeatGuardLogger() 
+	{
+	};
+
+    void log(ALogger &logger ) 
+	{
+	    increment();
+	    if(check())
+		{
+                std::stringstream strstr;
+                strstr << count();
+
+		LoggingProxy::AddData("repeatCount", strstr.str().c_str() );
+
+		logger.log();
+		}
+	};
+
+    void log(Logging::Logger::LoggerSmartPtr &logger, Logging::BaseLog::Priority priority,
+		     const std::string &message,
+		     const std::string &file,
+		     unsigned long line,
+		     const std::string &method)
+	{
+	    increment();
+	    if(check())
+		{
+                std::stringstream strstr;
+                strstr << count();
+
+		LoggingProxy::AddData("repeatCount", strstr.str().c_str() );
+
+		logger->log(priority, message,
+			   file, line, method);
+		}
+	};
+
+    
+    void log(Logging::Logger::LoggerSmartPtr &logger, 
+		     const Logging::BaseLog::LogRecord &lr)
+	{
+	    increment();
+	    if(check())
+		{
+                std::stringstream strstr;
+                strstr << count();
+
+		LoggingProxy::AddData("repeatCount", strstr.str().c_str() );
+
+		logger->log(lr);
+		}
+	};
+
+
+};
+
 };
 #endif /*!REPEATGUARDLOGGER_H*/
--- LGPL/CommonSoftware/repeatGuard/ws/src/RepeatGuardLogger.cpp
+++ LGPL/CommonSoftware/repeatGuard/ws/src/RepeatGuardLogger.cpp
@@ -16,55 +16,15 @@
 * License along with this library; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 *
-* "@(#) $Id: RepeatGuardLogger.cpp,v 1.2 2007/02/27 09:14:35 nbarriga Exp $"
+* "@(#) $Id: RepeatGuardLogger.cpp,v 1.3 2007/03/22 10:56:39 gchiozzi Exp $"
 *
 * who       when      what
 * --------  --------  ----------------------------------------------
 * nbarriga  2007-02-26  created 
 */
 
-#include "vltPort.h"
+/**
+ * @todo No code here. File to be removed later on is still empty
+ */
 
-static char *rcsId="@(#) $Id: RepeatGuardLogger.cpp,v 1.2 2007/02/27 09:14:35 nbarriga Exp $"; 
-static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
-
-#include <sstream>
-
-#include "RepeatGuardLogger.h"
-
-namespace Logging{
-
-        RepeatGuardLogger::RepeatGuardLogger(const std::string &loggerName,unsigned int interval, unsigned int maxRepetitions) : Logger(loggerName),
-                ACSLogger(loggerName){
-                        mGuard=new RepeatGuard(interval,maxRepetitions);
-                }
-
-        RepeatGuardLogger::~RepeatGuardLogger(){
-                delete mGuard;
-        }
-
-        //void RepeatGuardLogger::log(const char *format, ...){}
-
-        void RepeatGuardLogger::logAndIncrement(const char *format, ...){
-#define MAX_MSG_SIZE 512
-                if(mGuard->checkAndIncrement()){
-                        char buffer[MAX_MSG_SIZE];
-                        va_list arglist;
-                        va_start (arglist, format);
-                        vsnprintf(buffer,MAX_MSG_SIZE,format,arglist);
-                        std::string msg(buffer);
-                        appendCount(msg);
-                        Logger::log(LM_INFO,msg);
-                }
-        }
-
-        void RepeatGuardLogger::appendCount(std::string &msg){
-                msg+="; message repeated ";
-                std::stringstream strstr;
-                strstr<<mGuard->count();
-                msg+=strstr.str();
-                msg+=" times.";
-        }
-
-}
 /*___oOo___*/
