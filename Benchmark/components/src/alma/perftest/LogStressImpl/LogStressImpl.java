bec60ec60edc6b7e0fc706a62939246c89dfdb0d
sharring <sharring@523d945c-050c-4681-91ec-863ad3bb968a>
2007-04-24 21:15:07 +0000

    added java implementation of logging benchmark component
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@70006 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ Benchmark/components/src/alma/perftest/LogStressImpl/LogStressImpl.java
@@ -0,0 +1,135 @@
+/*
+ * ALMA - Atacama Large Millimiter Array (c) European Southern Observatory,
+ * 2002 Copyright by ESO (in the framework of the ALMA collaboration), All
+ * rights reserved
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package alma.perftest.LogStressImpl;
+import java.util.logging.Logger;
+import java.lang.Thread;
+
+import alma.ACS.ComponentStates;
+import alma.acs.component.ComponentLifecycle;
+import alma.acs.container.ContainerServices;
+import alma.perftest.LogStressWithDelayOperations;
+
+/**
+ * A very simple component that can be used to stress test the logging channel.
+ * 
+ * @author sharring
+ */
+public class LogStressImpl implements ComponentLifecycle, LogStressWithDelayOperations
+{
+	private ContainerServices m_containerServices;
+	private Logger m_logger;
+	private boolean threadDone;
+	private int numTimesToLog;
+	private int delay;
+	
+	private class SendingThread extends Thread
+	{
+		/**
+		 * Required method of Thread base class; does the actual work of the thread
+		 * which in this case is to log messages (almost) as fast as possible.
+		 */
+		public void run() 
+		{
+			long i = 0;
+
+			while(i < numTimesToLog)
+			{
+				m_logger.info("Java stress test msg: " + i++);
+				if(delay != 0) 
+				{
+					try {
+						Thread.sleep(delay);
+					}
+					catch(InterruptedException ex) 
+					{
+						m_logger.warning("Thread's sleep was interrupted");
+					}
+				}
+			}
+			setThreadDone(true);
+		}
+
+	}
+
+	/////////////////////////////////////////////////////////////
+	// Implementation of ComponentLifecycle
+	/////////////////////////////////////////////////////////////
+	
+	public void initialize(ContainerServices containerServices) 
+	{
+		m_containerServices = containerServices;
+		m_logger = m_containerServices.getLogger();
+		m_logger.info("initialize() called...");
+		setThreadDone(false);
+	}
+    
+	public void execute() {
+		m_logger.info("execute() called...");
+	}
+    
+	public void cleanUp() {
+		m_logger.info("cleanUp() called..., nothing to clean up.");
+	}
+    
+	public void aboutToAbort() {
+		cleanUp();
+		m_logger.info("managed to abort...");
+        System.out.println("LogStress component managed to abort... you should know this even if the logger did not flush correctly!");
+	}
+	
+	/////////////////////////////////////////////////////////////
+	// Implementation of ACSComponent
+	/////////////////////////////////////////////////////////////
+	
+	public ComponentStates componentState() {
+		return m_containerServices.getComponentStateManager().getCurrentState();
+	}
+	
+	public String name() {
+		return m_containerServices.getName();
+	}
+	
+	/////////////////////////////////////////////////////////////
+	// Implementation of LogStressOperations
+	/////////////////////////////////////////////////////////////
+	
+	public void setThreadDone(boolean threadDone) 
+	{
+		this.threadDone = threadDone;
+	}
+
+	public boolean getThreadDone() 
+	{
+		// TODO Auto-generated method stub
+		return threadDone;
+	}
+
+	public void logNumTimes(int numTimes, int delayBetweenLogs) 
+	{
+		// TODO Auto-generated method stub
+		m_logger.info("logNumTimes called...");
+		this.threadDone = false;
+		this.numTimesToLog = numTimes;
+		this.delay = delayBetweenLogs;
+		SendingThread sendingThread = new SendingThread();
+		sendingThread.start();
+	}
+}
+
