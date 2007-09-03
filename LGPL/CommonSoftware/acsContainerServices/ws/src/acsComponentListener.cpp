8e4021f74d42dc72cf6236a392d2aabefa4318ce
cparedes <cparedes@523d945c-050c-4681-91ec-863ad3bb968a>
2007-09-03 05:59:27 +0000

    *** empty log message ***
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@75939 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/acsContainerServices/ws/src/acsComponentListener.cpp
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ *    ALMA - Atacama Large Millimiter Array
+ *    (c) European Southern Observatory, 2002
+ *    Copyright by ESO (in the framework of the ALMA collaboration)
+ *    and Cosylab 2002, All rights reserved
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ *
+ *
+ * "@(#) $Id: acsComponentListener.cpp,v 1.1 2007/09/03 05:59:27 cparedes Exp $"
+ *
+ * who       when      what
+ * --------  --------  ----------------------------------------------
+ * acaproni  2005-04-06  created
+ */
+
+#include "acsComponentListener.h"
+using namespace maci;
+
+ComponentListener::ComponentListener(){}
+ComponentListener::~ComponentListener(){}
+bool ComponentListener::includeForeignComponents(){return false;}
+    
--- LGPL/CommonSoftware/acsContainerServices/ws/src/acsContainerServices.cpp
+++ LGPL/CommonSoftware/acsContainerServices/ws/src/acsContainerServices.cpp
@@ -19,7 +19,7 @@
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
  *
  *
- * "@(#) $Id: acsContainerServices.cpp,v 1.9 2006/10/23 09:05:04 bjeram Exp $"
+ * "@(#) $Id: acsContainerServices.cpp,v 1.10 2007/09/03 05:59:27 cparedes Exp $"
  *
  * who       when      what
  * --------  --------  ----------------------------------------------
@@ -33,6 +33,7 @@ using namespace maci;
 ContainerServices::ContainerServices(ACE_CString& compName, PortableServer::POA_ptr poa): 
     Logging::Loggable(compName.c_str()),
     m_componentName(compName),
+    withCompListener(false),
     m_poa(PortableServer::POA::_nil()),
     threadManager_m(getLogger())
 {
@@ -50,8 +51,57 @@ ContainerServices::~ContainerServices()
   ACS_TRACE("maci::ContainerServices::~ContainerServices");
 }
 
+void ContainerServices::registerComponentListener(ComponentListener* listener) {
+     compListener = listener;
+     withCompListener = true;
+}
 
-static char *rcsId="@(#) $Id: acsContainerServices.cpp,v 1.9 2006/10/23 09:05:04 bjeram Exp $"; 
+void ContainerServices::fireComponentsAvailable (ACE_CString_Vector& compNames) {
+    if (!withCompListener || compListener == NULL) {
+        return;
+    }
+
+     if (compNames.size() > 0) {
+         try {
+             compListener->componentsAvailable(compNames);
+         } catch (...) {
+             ACS_SHORT_LOG((LM_INFO, "componentsAvailable implementation of client %s failed", m_componentName.c_str()));
+         }
+     }
+}
+
+void ContainerServices::fireComponentsUnavailable(ACE_CString_Vector& compNames){
+         if (!withCompListener || compListener == NULL) {
+             return;
+         }
+
+         // find out which components are interesting for the client
+  /*      //TODO:find a way how to know the used components
+         ACE_CString_Vector & interesting;
+             
+         if (compListener->includeForeignComponents()) {
+             interesting = compNames;
+         }
+         else {
+            int i;
+             for (i=0; i< (int)compNames.size(); i++) {
+                ACE_CString cn = compNames[i];
+                 if (m_usedComponentsMap.containsKey(cn) || m_usedNonStickyComponentsMap.containsKey(cn) ) {
+                     interesting.add(cn);
+                 }
+             }
+         }
+*/
+         if (compNames.size() > 0) {
+            try {
+                 compListener->componentsUnavailable(compNames);
+             } catch (...) {
+                 ACS_SHORT_LOG((LM_INFO, "componentsUnavailable implementation of client %s failed", m_componentName.c_str()));
+             }
+         }
+ }
+
+static char *rcsId="@(#) $Id: acsContainerServices.cpp,v 1.10 2007/09/03 05:59:27 cparedes Exp $"; 
 static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
 
 
