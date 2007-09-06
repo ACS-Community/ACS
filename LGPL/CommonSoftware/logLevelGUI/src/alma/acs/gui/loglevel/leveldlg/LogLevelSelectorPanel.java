96c4ed12d50cd631ec84498e3a07caee6354d368
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2007-09-06 14:58:59 +0000

    The component shown inside a tab allowing the user to set log levels
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@76105 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/logLevelGUI/src/alma/acs/gui/loglevel/leveldlg/LogLevelSelectorPanel.java
@@ -0,0 +1,165 @@
+/*
+ *    ALMA - Atacama Large Millimiter Array
+ *    (c) European Southern Observatory, 2002
+ *    Copyright by ESO (in the framework of the ALMA collaboration)
+ *    and Cosylab 2002, All rights reserved
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *    MA 02111-1307  USA
+ */
+package alma.acs.gui.loglevel.leveldlg;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import si.ijs.maci.LoggingConfigurable;
+
+/**
+ * The panel to select the log level of the named loggers
+ * 
+ * @author acaproni
+ *
+ */
+public class LogLevelSelectorPanel extends JPanel implements ActionListener {
+	
+	// The Button to apply the changes
+	private JButton applyBtn = new JButton("Apply");
+	
+	// The LoggingConfigurable
+	private LoggingConfigurable logConf=null;
+	
+	// The table of log levels
+	private LogLevelTable table;
+	private LogLevelModel model;
+	
+	/**
+	 * Constructor 
+	 * 
+	 * @param owner The windo that owns this dialog (it can be null)
+	 * @param configurable The LoggingConfigurable whose log level
+	 *                     the user wants to read or set
+	 * @param title The name of the configurable to add to the tile
+	 */
+	public LogLevelSelectorPanel(LoggingConfigurable configurable, String name) {
+		if (configurable==null) {
+			throw new IllegalArgumentException("Invalid null LoggingConfigurable in constructor");
+		}
+		logConf=configurable;
+		setName(name);
+		initialize(name);
+	}
+	
+	/**
+	 * Init the GUI
+	 */
+	private void initialize(String name) {
+		BoxLayout layout = new BoxLayout(this,BoxLayout.Y_AXIS);
+		setLayout(layout);
+		
+		// Add the widgets with the log levels at the center
+		add(initLogLevelsPanel());
+		
+		// Set tooltip to buttons
+		applyBtn.setToolTipText("Apply the changes");
+		
+		// Add the botton at the bottom
+		JPanel btnPnl = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		btnPnl.add(applyBtn);
+		applyBtn.addActionListener(this);
+		
+		add(btnPnl);
+	}
+	
+	/**
+	 * Initialize the log level panel (i.e. the table)
+	 * 
+	 * @return The panel with the table of log levels
+	 */
+	private JComponent initLogLevelsPanel() {
+		LogLevelHelper[] levels=null;
+		try {
+			levels = loggersLbl();
+		} catch (Exception e) {
+			JLabel lbl = new JLabel("No log levels available");
+			lbl.setToolTipText("Function not yet implemented");
+			return lbl;
+		}
+		
+		model = new LogLevelModel(levels);
+		table = new LogLevelTable(model);
+		
+		JScrollPane scrollPane = new JScrollPane(table);
+		return scrollPane;
+	}
+	
+	/**
+	 * set the lable for the logger names 
+	 * 
+	 * @return
+	 */
+	private LogLevelHelper[] loggersLbl() throws Exception {
+		String[] logNames = logConf.get_logger_names();
+		if (logNames==null) {
+			return new LogLevelHelper[0];
+		}
+		LogLevelHelper[] ret = new LogLevelHelper[logNames.length];
+		for (int t=0; t<logNames.length; t++) {
+			ret[t]= new LogLevelHelper(logNames[t],logConf.get_logLevels(logNames[t]));
+		}
+		return ret;
+	}
+	
+	/**
+	 * 
+	 * Apply the changes to the log levels, if any
+	 *
+	 */
+	private void applyChanges() {
+		LogLevelHelper[] newLevels = model.getLevels();
+		for (LogLevelHelper logLvl: newLevels) {
+			if (logLvl.modified()) {
+				System.out.println("Applying new log levels to "+logLvl.getName());
+				try {
+					logConf.set_logLevels(logLvl.getName(), logLvl.getLogLevels());
+				} catch (Throwable t) {
+					System.err.println("Exception caught while setting log level "+logLvl.getName()+": "+t.getMessage());
+					t.printStackTrace(System.err);
+				}
+			}
+		}
+		model.changesApplied();
+	}
+	
+	/**
+	 * 
+	 * @see java.awt.event.ActionListener
+	 */
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource()==applyBtn) { 
+			applyChanges();
+		} else {
+			throw new IllegalStateException("Unknown source of events: "+e.getSource());
+		}
+	}
+}
