63eca988432755f50f4686778feefc01752ac52b
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2007-09-28 10:21:18 +0000

    Log a message when an alarm is pushed. The logger is passed in the constructor and must be not null.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@77485 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/ACSLaser/laser-source/src/alma/acs/alarmsystem/binding/ACSLaserSource.java
+++ LGPL/CommonSoftware/ACSLaser/laser-source/src/alma/acs/alarmsystem/binding/ACSLaserSource.java
@@ -1,26 +1,41 @@
 package alma.acs.alarmsystem.binding;
 
 import java.util.Collection;
+import java.util.logging.Logger;
 
 import cern.laser.source.alarmsysteminterface.impl.AlarmSystemInterfaceProxy;
 import cern.laser.source.alarmsysteminterface.FaultState;
 import cern.laser.source.alarmsysteminterface.ASIException;
 
+import alma.acs.util.XmlNormalizer;
 import alma.alarmsystem.source.ACSAlarmSystemInterface;
 import alma.alarmsystem.source.ACSFaultState;
 
 public class ACSLaserSource extends AlarmSystemInterfaceProxy implements ACSAlarmSystemInterface {
 	
-	public ACSLaserSource(String sourceName) throws ASIException {
+	// The logger
+	private Logger logger=null;
+	
+	public ACSLaserSource(String sourceName, Logger logger) throws ASIException {
 		super(sourceName);
+		if (logger==null) {
+			throw new IllegalArgumentException("Invalid null logger in constructor");
+		}
+		this.logger=logger;
 	}
 	/**
 	 * @see cern.laser.source.alarmsysteminterface.AlarmSystemInterface
 	 */
 	public void push(ACSFaultState state){
+		logFaultState(state);
 		try {
 			this.push((FaultState)state);
-		} catch (Exception e) {}
+		} catch (Throwable t) {
+			StringBuilder logStr = new StringBuilder("Exception "+t.getMessage()+" throwing alarm <");
+			logStr.append(state.getFamily()+','+state.getMember()+','+state.getCode()+">");
+			logStr.append(" "+state.getDescriptor());
+			logger.severe(logStr.toString());
+		}
 	}
 	
 	public void push(Collection states) {
@@ -29,5 +44,20 @@ public class ACSLaserSource extends AlarmSystemInterfaceProxy implements ACSAlar
 	
 	public void pushActiveList(Collection active) {
 		this.pushActiveList(active);
+	}
+	
+	/**
+	 * Write the ACSFaultState in the log
+	 * 
+	 * @param fs The ACSFaultState to log
+	 */
+	private void logFaultState(ACSFaultState fs) {
+		if (fs==null) {
+			return;
+		}
+		StringBuilder sb = new StringBuilder("Alarm sent: <");
+		sb.append(fs.getFamily()+','+fs.getMember()+','+fs.getCode()+">");
+		sb.append(" "+fs.getDescriptor());
+		logger.severe(XmlNormalizer.normalize(sb.toString()));
 	}
 }
