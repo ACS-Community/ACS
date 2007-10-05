6b41dfea2a8e7fa84d7ceab1d23e5a10e850e85f
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2007-10-05 08:53:47 +0000

    rolled back to rev. 1.96 (removed set_dynamic_logLevels etc)
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@77874 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jcont/src/alma/acs/container/AcsContainer.java
+++ LGPL/CommonSoftware/jcont/src/alma/acs/container/AcsContainer.java
@@ -1286,7 +1286,6 @@ public class AcsContainer extends ContainerPOA
 	/**
 	 * Gets the names of all loggers, to allow configuring their levels
 	 * individually. The names are those that appear in the log records in the
-
 	 * field "SourceObject". This includes the container logger, ORB logger,
 	 * component loggers, and (only C++) GlobalLogger.
 	 * <p>
@@ -1311,15 +1310,6 @@ public class AcsContainer extends ContainerPOA
 		boolean useDefault = !logConfig.hasCustomConfig(logger_name); 
 		LogLevels ret = new LogLevels(useDefault, (short) levels.getMinLogLevel(), (short) levels.getMinLogLevelLocal());
 		return ret;
-	}
-
-	public void set_dynamic_logLevels(String logger_name, LogLevels levels) {
-		throw new org.omg.CORBA.NO_IMPLEMENT("set_dynamic_logLevels method not implemented yet");
-	}
-
-
-	public void set_dynamic_default_logLevels(LogLevels levels) {
-		throw new org.omg.CORBA.NO_IMPLEMENT("set_dynamic_default_logLevels method not implemented yet");
 	}
 
 	/**
