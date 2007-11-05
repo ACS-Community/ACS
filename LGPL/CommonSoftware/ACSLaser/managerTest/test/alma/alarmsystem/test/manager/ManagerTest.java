dc7daae8200fb81183ec980008d2cd1ff738561a
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2007-11-05 14:12:40 +0000

    Initial empty test for the alarm sent by manager when a container crashes.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@79054 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/ACSLaser/managerTest/test/alma/alarmsystem/test/manager/ManagerTest.java
@@ -0,0 +1,17 @@
+package alma.alarmsystem.test.manager;
+
+/**
+ * @author acaproni
+ *
+ */
+public class ManagerTest {
+
+	/**
+	 * Start the program
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
