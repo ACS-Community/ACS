a62e8c83936661b62dbb845a4b6c57b4df1ae33d
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2007-11-16 17:17:38 +0000

    preliminary first check-in
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@79684 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/acsBUILD/src/alma/acs/releasedoc/Cvs2clXmlEntry.java
@@ -0,0 +1,166 @@
+package alma.acs.releasedoc;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+//changelog
+
+//tagdate
+//  tagisodate	
+//  tagdatetag
+//entry
+//  date
+//  weekday
+//  time
+//  isoDate
+//  author
+//  file
+//    name
+//    cvsstate
+//    revision
+//    tag
+//  utag
+//  commondir 
+//  msg
+
+public class Cvs2clXmlEntry
+{
+	private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
+	private Date date;
+	private String author;
+	private List<EntryFile> files = new ArrayList<EntryFile>();
+	private String commondir; // null if we have only one file 
+	private String msg;
+	
+	public Cvs2clXmlEntry(Element domElem) throws ParseException {
+		NodeList childNodes = domElem.getChildNodes();
+		for (int i = 0; i < childNodes.getLength(); i++) {
+			Node childNode = childNodes.item(i);
+			if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+				if (childNode.getNodeName().equals("isoDate")) {
+					String dateString = childNode.getTextContent();
+					date = parseDate(dateString);
+				}
+				else if (childNode.getNodeName().equals("author")) {
+					author = childNode.getTextContent();
+				}
+				else if (childNode.getNodeName().equals("commondir")) {
+					commondir = childNode.getTextContent();
+				}
+				else if (childNode.getNodeName().equals("file")) {
+					files.add(new EntryFile((Element)childNode));
+				}
+				else if (childNode.getNodeName().equals("msg")) {
+					msg = childNode.getTextContent().trim();
+				}
+			}
+		}
+		if (date == null || author == null) {
+			// todo Give hint as to which element is lacking what data..
+			throw new IllegalArgumentException("Missing required data in entry element.");
+		}
+		
+		System.out.println("data = " + date.toString() + " and author = " + author + " msg = " + msg);
+	}
+	
+
+	Date getDate() {
+		return date;
+	}
+	
+	String getAuthor() {
+		return author;
+	}
+	
+	String getMessage() {
+		return msg;
+	}
+	
+	String getCommonDir() {
+		return this.commondir;
+	}
+	
+	List<EntryFile> getFiles() {
+		return files;
+	}
+	
+	Date parseDate(String dateString) throws ParseException {
+		String parseableDate = ( dateString.endsWith("Z") ? dateString.substring(0, dateString.length()) : dateString ) ;
+		return dateFormat.parse(parseableDate);
+	}
+	
+	
+	public boolean equals(Object obj) {
+		return (
+				obj instanceof Cvs2clXmlEntry && 
+				this.date.equals(((Cvs2clXmlEntry)obj).date) &&
+				this.author.equals(((Cvs2clXmlEntry)obj).author) &&  
+				this.msg.equals(((Cvs2clXmlEntry)obj).msg)  
+			);
+	}
+
+	public int hashCode() {
+		return date.hashCode();
+	}
+	
+
+	public static class EntryFile {
+		private String pathName;
+		private String cvsstate;
+		private String revision;
+		private String tag; // may be null
+			
+		EntryFile(Element domElem) {
+			NodeList childNodes = domElem.getChildNodes();
+			for (int i = 0; i < childNodes.getLength(); i++) {
+				Node childNode = childNodes.item(i);
+				if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+					if (childNode.getNodeName().equals("name")) {
+						pathName = childNode.getTextContent();
+					}
+					else if (childNode.getNodeName().equals("cvsstate")) {
+						cvsstate = childNode.getTextContent();
+					}
+					else if (childNode.getNodeName().equals("revision")) {
+						revision = childNode.getTextContent();
+					}
+					else if (childNode.getNodeName().equals("tag")) {
+						tag = childNode.getTextContent();
+					}
+				}
+			}
+			if (pathName == null || cvsstate == null || revision == null) {
+				// todo Give hint as to which element is lacking what data..
+				throw new IllegalArgumentException("Missing required data in file element.");
+			}
+			System.out.println("file = " + pathName); 
+		}
+
+		String getPathName() {
+			return pathName;
+		}
+
+		String getCvsstate() {
+			return cvsstate;
+		}
+
+		String getRevision() {
+			return revision;
+		}
+
+		String getTag() {
+			return tag;
+		}
+	}
+
+
+
+}
---
+++ LGPL/acsBUILD/src/alma/acs/releasedoc/TWikiFormatter.java
@@ -0,0 +1,124 @@
+package alma.acs.releasedoc;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import alma.acs.releasedoc.Cvs2clXmlEntry.EntryFile;
+
+//2007-08-03 09:45  hsommer
+//
+//* Makefile: added logLevelGUI
+//
+//2007-08-03 09:41  hsommer
+//
+//* LGPL/CommonSoftware/logLevelGUI/src/:
+//  alma/acs/gui/loglevel/LogLevelFrame.java,
+//  alma/acs/gui/loglevel/LogLevelPanel.java,
+//  alma/acs/gui/loglevel/LogLevelWinAdapter.java,
+//  alma/acs/gui/loglevel/leveldlg/LogLevelDlg.java,
+//  alma/acs/gui/loglevel/leveldlg/LogLevelModel.java,
+//  alma/acs/gui/loglevel/leveldlg/LogLevelTable.java,
+//  alma/acs/gui/loglevel/leveldlg/LogTypeCellRenderer.java,
+//  alma/acs/gui/loglevel/tree/AdministratorClient.java,
+//  alma/acs/gui/loglevel/tree/LogLvlTree.java,
+//  alma/acs/gui/loglevel/tree/LogLvlTreeModel.java,
+//  alma/acs/gui/loglevel/tree/ManagerBusyDlg.java,
+//  alma/acs/gui/loglevel/tree/TreeMouseListener.java,
+//  alma/acs/gui/loglevel/tree/TreePopupMenu.java, logLevelPanel:
+//  merged from GUI2-2007-06-B
+//  
+  
+public class TWikiFormatter
+{
+	public static final String LINE_SEPARATOR = System.getProperty("line.separator");
+	
+	List<Cvs2clXmlEntry> sortByDate(Collection<Cvs2clXmlEntry> entries) {
+		List<Cvs2clXmlEntry> list = new ArrayList<Cvs2clXmlEntry>(entries);
+		
+		Comparator<Cvs2clXmlEntry> comp = new Comparator<Cvs2clXmlEntry>() {
+			public int compare(Cvs2clXmlEntry entry1, Cvs2clXmlEntry entry2) {
+				return entry1.getDate().compareTo(entry2.getDate());
+			}
+		};
+		
+		Collections.sort(list, comp);
+		return list;
+	}
+	
+	void printTwiki(List<Cvs2clXmlEntry> entryList) {
+		for (Cvs2clXmlEntry entry : entryList) {
+			printTwiki(entry);
+		}
+	}
+	
+	void printTwiki(Cvs2clXmlEntry entry) {
+		
+		String output = SimpleDateFormat.getDateTimeInstance().format(entry.getDate());
+		output += "  ";
+		output += entry.getAuthor();
+		output += " \n";
+		String wikiFileIndent = "   * ";
+		if (entry.getCommonDir() != null) {
+			output += "   * " + entry.getCommonDir();
+			output += ": \n";
+			wikiFileIndent = "      * ";
+		}
+		Iterator<EntryFile> fileIter = entry.getFiles().iterator();
+		while (fileIter.hasNext()) {
+			EntryFile file = fileIter.next();
+			output += wikiFileIndent;
+			if (file.getCvsstate().equals("dead")) {
+				output += "<strike>" + file.getPathName() + "</strike>";
+			}
+			else {
+				output += "=" + file.getPathName() + "=";
+			}
+			output += " \n";
+		}
+		output += "   * " + formatMessage("     ", entry.getMessage()) + " \n";
+		System.out.println(output);
+	}
+	
+	protected String formatMessage(String indentAfterFirstLine, String message) {
+		StringTokenizer tok = new StringTokenizer(message, "\n\r\f");
+		String ret = maskWikiWords(tok.nextToken());
+		while (tok.hasMoreTokens()) {
+			ret += " <br>" + LINE_SEPARATOR;
+			ret += indentAfterFirstLine;
+			ret += maskWikiWords(tok.nextToken());
+		}
+		return ret;
+	}
+	
+	protected String maskWikiWords(String msgLine) {
+		StringTokenizer wordTok = new StringTokenizer(msgLine);
+		String ret = "";
+		while (wordTok.hasMoreTokens()) {
+			String word = wordTok.nextToken();
+			if (isWikiWord(word)) {
+				ret += "!";
+			}
+			ret += word + " ";
+		}		
+		return ret;
+	}
+	
+	protected boolean isWikiWord(String word) {
+		if (word.length() > 2 && Character.isUpperCase(word.charAt(0)) && Character.isLowerCase(word.charAt(1))) {				
+			for (int i = 2; i < word.length(); i++) {
+				if (Character.isUpperCase(word.charAt(i))) {
+					// got a wiki word
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+}
