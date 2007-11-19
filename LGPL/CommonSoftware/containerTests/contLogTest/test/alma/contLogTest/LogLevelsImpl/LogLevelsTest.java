59689f19d7017cd1be711992f09e884843a260bb
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2007-11-19 18:50:25 +0000

    new helper method getContainerLoggingIF to allow direct logging API calls on all containers
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@79779 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/containerTests/contLogTest/test/alma/contLogTest/LogLevelsImpl/LogLevelsTest.java
+++ LGPL/CommonSoftware/containerTests/contLogTest/test/alma/contLogTest/LogLevelsImpl/LogLevelsTest.java
@@ -22,18 +22,24 @@
 package alma.contLogTest.LogLevelsImpl;
 
 
-import java.util.Vector;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import si.ijs.maci.Administrator;
+import si.ijs.maci.AdministratorPOATie;
+import si.ijs.maci.Container;
+import si.ijs.maci.ContainerInfo;
+import si.ijs.maci.LoggingConfigurableHelper;
+import si.ijs.maci.LoggingConfigurableOperations;
 
 import alma.ACSErrTypeCommon.CouldntPerformActionEx;
 import alma.ACSErrTypeCommon.wrappers.AcsJCouldntPerformActionEx;
+import alma.JavaContainerError.wrappers.AcsJContainerEx;
 import alma.acs.component.client.ComponentClient;
 import alma.acs.component.client.ComponentClientTestCase;
+import alma.acs.container.AcsManagerProxy;
 import alma.contLogTest.LogLevels;
-import alma.maci.loggingconfig.LoggingConfig;
+import alma.maciErrType.NoPermissionEx;
 
 /**
  * Requires Java component "TESTLOG1" of type <code>alma.contLogTest.LogLevels</code> to be running.
@@ -62,28 +68,28 @@ public class LogLevelsTest extends ComponentClientTestCase
 	protected void setUp() throws Exception
 	{
 		super.setUp();
-		if (false) {
-			String managerLoc = System.getProperty("ACS.manager");
-			if (managerLoc == null) {
-				System.out
-						.println("Java property 'ACS.manager' must be set to the corbaloc of the ACS manager!");
-				System.exit(-1);
-			}
-			/*
-			String clientName = "LogLevelsClient";
-			try {
-				hlc = new ComponentClient(null, managerLoc, clientName);
-				m_logger.info("got LogLevel client");
-			} catch (Exception e) {
-				try {
-					Logger logger = hlc.getContainerServices().getLogger();
-					logger.log(Level.SEVERE, "Client application failure", e);
-				} catch (Exception e2) {
-					e.printStackTrace(System.err);
-				}
-			}
-			*/
-		}
+//		if (false) {
+//			String managerLoc = System.getProperty("ACS.manager");
+//			if (managerLoc == null) {
+//				System.out
+//						.println("Java property 'ACS.manager' must be set to the corbaloc of the ACS manager!");
+//				System.exit(-1);
+//			}
+//			/*
+//			String clientName = "LogLevelsClient";
+//			try {
+//				hlc = new ComponentClient(null, managerLoc, clientName);
+//				m_logger.info("got LogLevel client");
+//			} catch (Exception e) {
+//				try {
+//					Logger logger = hlc.getContainerServices().getLogger();
+//					logger.log(Level.SEVERE, "Client application failure", e);
+//				} catch (Exception e2) {
+//					e.printStackTrace(System.err);
+//				}
+//			}
+//			*/
+//		}
 	}
 
 	/**
@@ -91,20 +97,28 @@ public class LogLevelsTest extends ComponentClientTestCase
 	 */
 	protected void tearDown() throws Exception
 	{
-	    if (hlc != null)
-		{
-    	try 
-    		{
-    		hlc.tearDown();
-    		}
-    	catch (Exception e3) 
-    		{
-    		// bad luck
-    		e3.printStackTrace();
-    		}
-		}
+//	    if (hlc != null)
+//		{
+//    	try 
+//    		{
+//    		hlc.tearDown();
+//    		}
+//    	catch (Exception e3) 
+//    		{
+//    		// bad luck
+//    		e3.printStack	Trace();
+//    		}
+//		}
 		super.tearDown();
 	}
+	
+	
+//	public void testGetLoggingConfigurableInterface() throws Exception {
+//		LoggingConfigurableOperations containerLogConfig = getContainerLoggingIF("heikoContainer");
+//		String[] loggerNames = containerLogConfig.get_logger_names();
+//		assertNotNull(loggerNames);
+//	}
+//	
 	
 	public void testGetLevels() throws Exception
 	{
@@ -131,6 +145,48 @@ public class LogLevelsTest extends ComponentClientTestCase
 		}
 	}
 
+	
+	/**
+	 * Gets a reference to the LoggingConfigurableOperations interface of a container with a given name. 
+	 * <p>
+	 * Note that only in test code like here we are allowed to talk directly with the manager.
+	 * For operational code, the ContainerServices methods must be used and extended if necessary.
+	 *  
+	 * @param containerName
+	 */
+	public LoggingConfigurableOperations getContainerLoggingIF(String containerName) throws AcsJContainerEx, NoPermissionEx {
+    	AdministratorPOATie adminpoa = new AdministratorPOATie(new ManagerAdminClient(getName(), m_logger)); 
+		Administrator adminCorbaObj = adminpoa._this(getContainerServices().getAdvancedContainerServices().getORB());
+		
+		// we need an new manager connection because the JUnit test does not have admin right.
+		AcsManagerProxy adminProxy = m_acsManagerProxy.createInstance(); 
+		Container containerRef;
+		try {
+			adminProxy.loginToManager(adminCorbaObj, false);
+			int adminManagerHandle = adminProxy.getManagerHandle();
+			assertTrue(adminManagerHandle > 0);
+			
+			// ask the manager for the container reference
+			ContainerInfo[] containerInfos = adminProxy.getManager().get_container_info(adminManagerHandle, new int[0], containerName);
+			assertEquals(1, containerInfos.length);
+			containerRef = containerInfos[0].reference;
+		} finally {
+			// now that we got our reference, we can logout the manager client again.
+			// Of course this is too dirty for operational code, because manager cannot keep track of 
+			// admin clients holding container references...
+			adminProxy.logoutFromManager();
+		}
+		
+		// cast up to LoggingConfigurable interface
+		return LoggingConfigurableHelper.narrow(containerRef);
+	}
+	
+	
+	/**
+	 * @TODO We usually don't require a main method for a JUnit test to run successfully.
+	 * Therefore instead of getting component names from the arg list, 
+	 * they should be given in some Java property that can be evaluated in the setUp method.
+	 */
 	public static void main(String[] args)
 	{
 	    component = new String[args.length];
