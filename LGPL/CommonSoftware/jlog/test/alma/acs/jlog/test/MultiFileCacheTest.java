e5d2908a9dd9521da6e1eb600e173eba545aef81
mcomin <mcomin@523d945c-050c-4681-91ec-863ad3bb968a>
2007-12-14 13:27:46 +0000

    First version.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@81068 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jlog/test/alma/acs/jlog/test/MultiFileCacheTest.java
+++ LGPL/CommonSoftware/jlog/test/alma/acs/jlog/test/MultiFileCacheTest.java
@@ -21,7 +21,11 @@
  */
 package alma.acs.jlog.test;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 import com.cosylab.logging.client.cache.LogMultiFileCache;
+import com.cosylab.logging.engine.log.ILogEntry;
 
 import junit.framework.TestCase;
 
@@ -37,7 +41,10 @@ public class MultiFileCacheTest extends TestCase {
 	private LogMultiFileCache cache;
 	
 	// The size of the cache
-	private static int TESTCACHE_SIZE=20000;
+	private static int TESTCACHE_SIZE=1500;
+	
+	private Collection<ILogEntry> logCollection;
+	
 	
 	/**
 	 * Constructor
@@ -51,7 +58,12 @@ public class MultiFileCacheTest extends TestCase {
 	public void setUp() throws Exception {
 		super.setUp();
 		cache=new LogMultiFileCache(TESTCACHE_SIZE);
-		assertEqual("The cache size is not as expected",TESTCACHE_SIZE,cache.getMaxFileSize());
+		cache.setDebugTrace(true);
+		
+		assertEquals("The cache size is not as expected",TESTCACHE_SIZE,cache.getMaxFileSize());
+		
+		// Create and populate the cache
+		logCollection = CacheUtils.generateLogsType(100,0);	
 	}
 	
 	public void tearDown() throws Exception {
@@ -60,7 +72,131 @@ public class MultiFileCacheTest extends TestCase {
 		cache=null;
 	}
 	
-	public void testFileCreation() throws Exception {	
+	public void testFileCreation1() throws Exception {
+		long fileSize = 0;
+		
+		Iterator<ILogEntry> ii = logCollection.iterator();
+
+		System.out.println("Start testFileCreation1");
+		
+		cache.setDebugTrace(true);
+		
+		// The cache is empty ... no logs have been added, therefore
+		// the deletion of a log should fail.
+
+		System.out.println("\nACTION : Delete log from empty cache");
+
+		try {
+			cache.deleteLog(12);
+		} catch (Exception e) {
+			System.out.println("Delete Log FAILS : cache is empty");
+		}
+		
+		// Add logs until a new file is generated
+		
+		System.out.println("\nACTION : fill half of the cache with logs");
+		
+		while (fileSize < TESTCACHE_SIZE/2 ) {
+			fileSize = cache.getLogFileSize();
+
+			ILogEntry log = ii.next();
+			
+			cache.add(log);
+		}
+		
+		cache.printFileTableInfo();
+		
+		System.out.println("\nACTION : add single log");
+		
+		ILogEntry log = ii.next();
+		cache.add(log);
+		cache.printFileTableInfo();
+		
+		
+		System.out.println("\nACTION : delete two logs");
+
+		// Delete last log and check that the new file is not deleted
+
+		cache.deleteLog(12);
+		cache.deleteLog(2);
+		cache.printFileTableInfo();
+		
+		System.out.println("\nACTION : fill cache and create a new log file");
+
+		while (fileSize < TESTCACHE_SIZE ) {
+			fileSize = cache.getLogFileSize();
+			log = ii.next();
+			cache.add(log);
+		}
+		
+		cache.printFileTableInfo();
+		
+		
+		System.out.println("\nACTION : delete first log of the new file");
+
+		cache.deleteLog(27);
+		cache.printFileTableInfo();
+
+		System.out.println("\nACTION : Add single log");
+		
+		log = ii.next();
+		cache.add(log);
+		cache.printFileTableInfo();
+
+		// Delete log twice : the log key has already been deleted
+		// therefore the delete should fail.
+
+		System.out.println("\nACTION : Delete a log twice");
+
+		try {
+			cache.deleteLog(27);
+		} catch (Exception e) {
+			System.out.println("Delete Log FAILS : log has already been deleted");
+		}
+	
+		cache.printFileTableInfo();
+		
+		System.out.println("\nACTION : fill cache and create a new log file");
+		
+		while ((fileSize = cache.getLogFileSize()) < TESTCACHE_SIZE ) {
+			log = ii.next();
+			cache.add(log);
+		}
+		
+		cache.printFileTableInfo();
+		
+
+		System.out.println("\nACTION : fill cache and create a new log file");
+		
+		log = ii.next();
+		cache.add(log);
+		
+		while ((fileSize = cache.getLogFileSize()) < TESTCACHE_SIZE ) {
+			log = ii.next();
+			cache.add(log);
+		}
+		
+		cache.printFileTableInfo();
+		
+		
+		System.out.println("\nACTION : add one more log ");
+
+		log = ii.next();
+		cache.add(log);
+		cache.printFileTableInfo();
+	
+		
+		System.out.println("\nACTION : Delete all log keys within a log file");
+
+		cache.setDebugTrace(false);
+		
+		for (int logKey = 28; logKey < 52 ; logKey++) {
+			cache.deleteLog(logKey);
+		}
+		
+		cache.setDebugTrace(true);
+		cache.printFileTableInfo();
+
 	}
 	
 	public void testFileDeletion() throws Exception {
