ceeeee367aff4467e4f1314d0f2f8938faea94ed
eallaert <eallaert@523d945c-050c-4681-91ec-863ad3bb968a>
2008-01-10 14:54:52 +0000

    Workaround in logDummyMessages() for the fact that Python seems to sends logs in packets of 10 logs: add 9 logs at the end to ensure that all the relevant logs get sent across right now.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@82143 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/containerTests/contLogTest/src/pyContLogTest/TestLogLevelsComp.py
+++ LGPL/CommonSoftware/containerTests/contLogTest/src/pyContLogTest/TestLogLevelsComp.py
@@ -17,7 +17,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-# "@(#) $Id: TestLogLevelsComp.py,v 1.3 2008/01/08 10:15:33 eallaert Exp $"
+# "@(#) $Id: TestLogLevelsComp.py,v 1.4 2008/01/10 14:54:52 eallaert Exp $"
 #
 # who       when      what
 # --------  --------  ----------------------------------------------
@@ -90,6 +90,11 @@ class TestLogLevelsComp(contLogTest__POA.TestLogLevelsComp,  #CORBA stubs for ID
         for l in levels:
             mylogger.logAtLevel(l, "dummy log message for core level %d/%s" % (l, getLevelName(l)))
         mylogger.logAtLevel(levels[-2], "===last log message===")
+        # Python seems to sends logs in packets of 10 logs, so add 9 messages to
+        # ensure all the above logs get sent across right now.
+        for i in range(1,10):
+            mylogger.logAtLevel(levels[-2], "packet fill-up messages %d" % i)
+           
         
 #------------------------------------------------------------------------------
 #--Main defined only for generic testing---------------------------------------
