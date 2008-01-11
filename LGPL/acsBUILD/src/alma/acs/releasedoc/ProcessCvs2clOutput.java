d80119dc95c8c5a4a5ddcd1e168eb013af38d89a
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2008-01-11 17:48:33 +0000

    Supports command line options for sorting and cvs-log subtraction
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@82240 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/acsBUILD/src/alma/acs/releasedoc/ProcessCvs2clOutput.java
+++ LGPL/acsBUILD/src/alma/acs/releasedoc/ProcessCvs2clOutput.java
@@ -4,14 +4,12 @@
 package alma.acs.releasedoc;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
@@ -20,57 +18,86 @@ import javax.xml.xpath.XPathFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+
+import alma.acs.util.CmdLineArgs;
+import alma.acs.util.CmdLineRegisteredOption;
 
 /**
- * @author jschwarz
- *
+ * This class bundles the other classes of this package and provides a command line tool.
+ * @author jschwarz, hsommer
  */
-public class ProcessCvs2clOutput {
-	
-	/**
-	 * @param args
-	 */
+public class ProcessCvs2clOutput 
+{	
+	public static enum SortBy {
+		time,
+		user,
+		path
+	}
+
+	private SortBy sortBy;
+	private List<File> subtractFiles = new ArrayList<File>();
+
+
 	public static void main(String[] args) {
-		ProcessCvs2clOutput pc = new ProcessCvs2clOutput();
-		Set<Cvs2clXmlEntry> entries1 = new HashSet<Cvs2clXmlEntry>();
+		try {
+			(new ProcessCvs2clOutput()).run(args);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	void run(String[] args) throws Exception {
+		
+		if (args.length < 1) {
+			throw new IllegalArgumentException("At least the xml file name must be given.");
+		}		
+		File xmlFile1 = new File(args[args.length-1]);
+		
+		if (!xmlFile1.exists()) {
+			System.err.println("File "+xmlFile1.getAbsolutePath()+" does not exist.");
+		}
+
+		setOptions(args);
 		
 		Cvs2clXmlParser parser = new Cvs2clXmlParser();
 		
-		File xmlFile1 = new File(args[0]);
-		if (!xmlFile1.exists()) System.err.println("File "+xmlFile1.getAbsolutePath()+" does not exist.");
+		Document doc = parser.parseXml(xmlFile1);
+		Set<Cvs2clXmlEntry> entries1 = new HashSet<Cvs2clXmlEntry>();
+		storeEntries(doc, entries1);
 
-
-		
-		try {
-			Document doc = parser.parseXml(xmlFile1);
-			storeEntries(doc, entries1);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SAXException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (XPathExpressionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		// optionally subtract entry sets
+		for (File subtractFile : subtractFiles) {
+			Document doc2 = parser.parseXml(subtractFile);
+			Set<Cvs2clXmlEntry> entries2 = new HashSet<Cvs2clXmlEntry>();
+			storeEntries(doc2, entries2);
+			entries1.removeAll(entries2);
+			System.out.println("Subtracted cvs entries from file " + subtractFile.getAbsolutePath());
 		}
 		
 		TWikiFormatter formatter = new TWikiFormatter();
-		List<Cvs2clXmlEntry> sortedEntries = formatter.sortByDate(entries1);
-		formatter.printTwiki(sortedEntries);
+		
+		// sorting
+		List<Cvs2clXmlEntry> entries2 = null;
+		switch (sortBy) {
+		case time:
+			entries2 = formatter.sortByDate(entries1);			
+			break;
+		case user:
+			entries2 = formatter.sortByAuthor(entries1);			
+			break;
+			
+		// @TODO: other sorting options!!
+		default:
+			entries2 = new ArrayList<Cvs2clXmlEntry>(entries1);
+			break;
+		}
+		
+		// formatting
+		formatter.printTwiki(entries2);
 	}
 	
-	private static void storeEntries(Document doc, Set<Cvs2clXmlEntry> entries) throws XPathExpressionException, ParseException {
+	
+	void storeEntries(Document doc, Set<Cvs2clXmlEntry> entries) throws XPathExpressionException, ParseException {
 		XPath xpath = XPathFactory.newInstance().newXPath();
 		NodeList entryNodes = (NodeList) xpath.evaluate("//entry", doc, XPathConstants.NODESET);
 		System.out.println("Number of entries: " + entryNodes.getLength());
@@ -81,6 +108,47 @@ public class ProcessCvs2clOutput {
 			if (!wasNewEntry) {
 				System.out.println("**** Multiple entry " + entry.getDate() + " author = " + entry.getAuthor());
 			}
+		}
+	}
+
+
+	void setOptions(String[] args) {
+		// -- prepare arg parser
+		CmdLineArgs cmdArgs = new CmdLineArgs();
+		
+		// optionally subtract other sets of cvs logs
+		CmdLineRegisteredOption optSubtract = new CmdLineRegisteredOption("-subtract", 1);
+		cmdArgs.registerOption(optSubtract);
+
+		// sort by time or by path (module, file,...) and time
+		CmdLineRegisteredOption optSortBy = new CmdLineRegisteredOption("-sortBy", 1);
+		cmdArgs.registerOption(optSortBy);
+		
+		// -- parse and set args
+		cmdArgs.parseArgs(args);		
+
+		// -- subtract
+		subtractFiles.clear();
+		if (cmdArgs.isSpecified(optSubtract)) {
+			String subtractFileNames = cmdArgs.getValues(optSubtract)[0].trim();
+			for (String fileName : subtractFileNames.split(File.pathSeparator)) {
+				File file = new File(fileName);
+				if (file.exists() && file.isFile()) {
+					subtractFiles.add(file);
+				}
+				else {
+					System.err.println("Will ignore non-existing subtract file " + file.getAbsolutePath());
+				}
+			}
+		}
+		
+		// -- sort by
+		if (cmdArgs.isSpecified(optSortBy)) {
+			String sortByString = cmdArgs.getValues(optSortBy)[0].trim();
+			sortBy = SortBy.valueOf(sortByString);
+		}
+		else {
+			sortBy = SortBy.time;
 		}
 	}
 
