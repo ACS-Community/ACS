7db98616b58386a69c5063ad29a76d7e45bc8533
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2008-01-17 13:14:46 +0000

    added static convenience methods formatDate(date) and formatCurrentDate()
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@82495 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jacsutil/src/alma/acs/util/IsoDateFormat.java
+++ LGPL/CommonSoftware/jacsutil/src/alma/acs/util/IsoDateFormat.java
@@ -1,6 +1,8 @@
 package alma.acs.util;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * ISO 8601 date format.
@@ -11,10 +13,28 @@ import java.text.SimpleDateFormat;
  */
 public class IsoDateFormat extends SimpleDateFormat
 {
+	private static final IsoDateFormat instance = new IsoDateFormat();
+	
 	public static final String pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS";
 
 	public IsoDateFormat() {
 		super(pattern);
 	}
 
+	/**
+	 * Convenience method that works with a shared instance of this class.
+	 * @see DateFormat#format(Date)
+	 */
+	public static String formatDate(Date date) {
+		synchronized (instance) {  // see sync comment for java.text.DataFormat
+			return instance.format(date);			
+		}
+	}
+	
+	/**
+	 * Convenience method that works with a shared instance of this class.
+	 */
+	public static String formatCurrentDate() {
+		return formatDate(new Date());
+	}
 }
