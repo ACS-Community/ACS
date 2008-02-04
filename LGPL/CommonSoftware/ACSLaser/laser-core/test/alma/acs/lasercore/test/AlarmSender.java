47be8b33026c5267ac9448bcd987e9acdab61731
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2008-02-04 14:57:44 +0000

    Some tests moved into testAlarmService
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@83819 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/ACSLaser/laser-core/test/alma/acs/lasercore/test/AlarmSender.java
+++ LGPL/CommonSoftware/ACSLaser/laser-core/test/alma/acs/lasercore/test/AlarmSender.java
@@ -24,10 +24,13 @@ package alma.acs.lasercore.test;
 import java.sql.Timestamp;
 import java.util.Vector;
 
+import alma.alarmsystem.Category;
 import cern.laser.source.alarmsysteminterface.FaultState;
 
 import alma.acs.component.client.ComponentClientTestCase;
 import alma.acs.container.ContainerServices;
+import alma.alarmsystem.AlarmService;
+import alma.alarmsystem.AlarmServiceHelper;
 import alma.alarmsystem.clients.CategoryClient;
 import alma.alarmsystem.clients.SourceClient;
 import alma.alarmsystem.clients.category.AlarmView;
@@ -38,7 +41,7 @@ import alma.alarmsystem.source.ACSAlarmSystemInterfaceFactory;
 import alma.alarmsystem.source.ACSFaultState;
 
 /**
- * This class tests the send/receipt of alarms listenings at the NC 
+ * This class tests the send/receipt of alarms listening at the NC 
  * 
  * Each alarm/fault state received is pushed into a vector
  * 
@@ -62,37 +65,69 @@ public class AlarmSender extends ComponentClientTestCase implements SourceListen
 	private SourceClient sourceClient;
 	
 	// The vector of the received alarms and sources
+	//
+	// For the category we need to remember the number of received alarms
+	// because AlarmView redefines equalsTo and the adding of alarms with the
+	// same triplet causes a replace in the Vector
 	private Vector<FaultState> faultStatesReceived;
 	private Vector<AlarmView> alarmsReceived;
+	private volatile int numOfAlarmsReceived;
 	
-	public AlarmSender() throws Exception
-	{
+	// Container services
+	private ContainerServices contSvcs;
+	
+	// The AS component
+	private AlarmService alarmService;
+	
+	/**
+	 * Constructor 
+	 * 
+	 * @throws Exception
+	 */
+	public AlarmSender() throws Exception {
 		super("AlarmSender");
 	}
 	
+	/**
+	 * @see TestCase
+	 */
 	public void setUp() throws Exception {
 		super.setUp();
 		
-		ContainerServices contSvcs = getContainerServices();
+		contSvcs = getContainerServices();
 		assertNotNull(contSvcs);
 		
 		ACSAlarmSystemInterfaceFactory.init(contSvcs);
 		
-		categoryClient = new CategoryClient(contSvcs);
-		assertNotNull(categoryClient);
+		// Get the AS 
+		alarmService =AlarmServiceHelper.narrow(contSvcs.getComponent("AlarmService"));
+		assertNotNull(alarmService);
 		
 		sourceClient = new SourceClient(contSvcs);
 		assertNotNull(sourceClient);
 		
+		categoryClient = new CategoryClient(contSvcs);
+		assertNotNull(categoryClient);
+		
 		faultStatesReceived=new Vector<FaultState>();
 		alarmsReceived=new Vector<AlarmView>();
+		numOfAlarmsReceived=0;
 		
 		// Connects the listeners
 		categoryClient.addAlarmListener(this);
 		sourceClient.addAlarmListener(this);
+		
+		
 	}
 	
+	/**
+	 * @see TestCase
+	 */
 	public void tearDown() throws Exception {
+		
+		contSvcs.releaseComponent(alarmService.name());
+		alarmService=null;
+		
 		sourceClient.removeListener(this);
 		categoryClient.removeListener(this);
 		
@@ -130,6 +165,7 @@ public class AlarmSender extends ComponentClientTestCase implements SourceListen
 	public void alarmReceived(AlarmView alarm) {
 		synchronized(alarmsReceived) {
 			alarmsReceived.add(alarm);
+			numOfAlarmsReceived++;
 		}
 	}
 	
@@ -167,5 +203,41 @@ public class AlarmSender extends ComponentClientTestCase implements SourceListen
 		assertEquals(fs2.getFamily(), FF);
 		assertEquals(fs2.getMember(), FM);
 		assertEquals(fs2.getCode(), FC);
+	}
+	
+	/**
+	 * Test the alarms received in the categories
+	 * @throws Exception
+	 */
+	public void testCategories() throws Exception {
+		categoryClient.connect();
+		
+		// send 2 alarms active and inactive
+		send_alarm(FF, FM, FC, true);
+		send_alarm(FF, FM, FC, false);
+		
+		// wait until 2 alarms are received in the source NC
+		long now = System.currentTimeMillis();
+		while (numOfAlarmsReceived<2 && now+TIMEOUT*1000>System.currentTimeMillis()) {
+			try {
+				Thread.sleep(500);
+			} catch (Exception e) {}
+		}
+		assertEquals("Not all the alarms appeared in the category channels", 2, alarmsReceived.size());
+		
+		// Check the content of the 2 alarms
+		AlarmView al1 = alarmsReceived.get(0);
+		assertNotNull(al1);
+		assertEquals(al1.alarmID, FF+":"+FM+":"+FC);
+		assertEquals(al1.description,"PS test alarm");
+		assertFalse(al1.active);
+		assertEquals(al1.priority, Integer.valueOf(2));
+		
+		AlarmView al2 = alarmsReceived.get(0);
+		assertNotNull(al2);
+		assertEquals(al2.alarmID, FF+":"+FM+":"+FC);
+		assertEquals(al2.description,"PS test alarm");
+		assertTrue(al2.active);
+		assertEquals(al2.priority, Integer.valueOf(2));
 	}
 }
