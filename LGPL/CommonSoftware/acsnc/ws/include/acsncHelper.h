3a95384c76ae23dcae52aa1fe63fb75a2d58b10a
msekoran <msekoran@523d945c-050c-4681-91ec-863ad3bb968a>
2008-02-07 10:18:17 +0000

    missing include
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@84095 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsnc/ws/include/acsncHelper.h
+++ LGPL/CommonSoftware/acsnc/ws/include/acsncHelper.h
@@ -21,7 +21,7 @@
 *    License along with this library; if not, write to the Free Software
 *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 *
-* "@(#) $Id: acsncHelper.h,v 1.66 2008/02/06 15:26:39 msekoran Exp $"
+* "@(#) $Id: acsncHelper.h,v 1.67 2008/02/07 10:18:17 msekoran Exp $"
 *
 * who       when      what
 * --------  --------  ----------------------------------------------
@@ -46,6 +46,7 @@
 #include "acsncS.h"
 #include "acsncC.h"
 #include "acsncORBHelper.h"
+#include "acsncCDBProperties.h"
 
 #include <ACSErrTypeCommon.h>
 
