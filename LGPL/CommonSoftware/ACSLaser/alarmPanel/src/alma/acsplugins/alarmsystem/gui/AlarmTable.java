192f018eb6df36235ff0421e55c64c07a16dcaa2
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2008-02-14 00:24:10 +0000

    The cell render colors the cells
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@84507 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/ACSLaser/alarmPanel/src/alma/acsplugins/alarmsystem/gui/AlarmTable.java
+++ LGPL/CommonSoftware/ACSLaser/alarmPanel/src/alma/acsplugins/alarmsystem/gui/AlarmTable.java
@@ -19,7 +19,7 @@
 
 /** 
  * @author  aaproni
- * @version $Id: AlarmTable.java,v 1.3 2008/02/13 21:39:40 acaproni Exp $
+ * @version $Id: AlarmTable.java,v 1.4 2008/02/14 00:24:10 acaproni Exp $
  * @since    
  */
 
@@ -35,6 +35,8 @@ import javax.swing.table.TableModel;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableRowSorter;
 
+import cern.laser.client.data.Alarm;
+
 /**
  * 
  * The table of alarms
@@ -42,7 +44,7 @@ import javax.swing.table.TableRowSorter;
  */
 public class AlarmTable extends JTable {
 	// The model of the table
-	private TableModel model;
+	private AlarmTableModel model;
 	
 	// The sorter for sorting the rows of the table
 	private TableRowSorter<TableModel> sorter;
@@ -51,7 +53,7 @@ public class AlarmTable extends JTable {
 	 * Constructor 
 	 * @param model The model for this table
 	 */
-	public AlarmTable(TableModel model) {
+	public AlarmTable(AlarmTableModel model) {
 		super(model);
 		if (model==null) {
 			throw new IllegalArgumentException("Invalid null model in constructor");
@@ -68,6 +70,15 @@ public class AlarmTable extends JTable {
 	public Component prepareRenderer(TableCellRenderer renderer, int rowIndex,
 			int vColIndex) {
 		Component c = super.prepareRenderer(renderer, rowIndex, vColIndex);
+		Alarm alarm = model.getRowAlarm(rowIndex);
+		if (alarm.getStatus().isActive()) {
+			CellColor cellColor = CellColor.fromPriority(alarm.getPriority());
+			c.setForeground(cellColor.foreg);
+			c.setBackground(cellColor.backg);
+		} else {
+			c.setForeground(CellColor.INACTIVE.foreg);
+			c.setBackground(CellColor.INACTIVE.backg);
+		}
 		if (c instanceof JComponent) {
 			JComponent jc = (JComponent) c;
 			jc.setToolTipText("<HTML>"+((AlarmTableModel)model).getCellContent(rowIndex, vColIndex));
