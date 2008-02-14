b6bd01d16eede93ef295b94a18a5f41dddc06007
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2008-02-14 19:35:32 +0000

    Added the renderer to customize the way the cellas are displayed inside the combo box
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@84542 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsGUIs/alarmPanel/src/alma/acsplugins/alarmsystem/gui/toolbar/Toolbar.java
+++ LGPL/CommonSoftware/acsGUIs/alarmPanel/src/alma/acsplugins/alarmsystem/gui/toolbar/Toolbar.java
@@ -18,12 +18,24 @@
  */
 package alma.acsplugins.alarmsystem.gui.toolbar;
 
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.Toolkit;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.ListCellRenderer;
+import javax.swing.SwingConstants;
+
+import alma.acsplugins.alarmsystem.gui.CellColor;
 
 /**
  * The toolbar for the alarm panel
@@ -33,14 +45,155 @@ import javax.swing.JPanel;
  */
 public class Toolbar extends JPanel {
 	
-	// The combo box to choose the auto acknowledge level
-	private static final String[] autoAckLevels = {
-		"None",
-		"Priority 3",
-		"Priority 2",
-		"Priority 1"
-	};
-	private JComboBox autoAckLevelCB=new JComboBox(autoAckLevels);
+	/** 
+	 * The rendered for the auto acknowledge combo box
+	 * This renderer shows each level with it color as defined in CellColor
+	 * 
+	 * @author acaproni
+	 *
+	 */
+	public class ComboBoxRenderer implements ListCellRenderer {
+		
+		/**
+		 * Constructor
+		 */
+		public ComboBoxRenderer() {
+			setOpaque(true);
+		}
+
+		/**
+		 * @see ListCellRenderer
+		 */
+		public Component getListCellRendererComponent(JList list, Object value,
+				int index, boolean isSelected, boolean cellHasFocus) {
+			if (index==-1) {
+				return ((ComboBoxValues)value).normalRenderer;
+			}
+			JLabel renderer=null;
+			if (cellHasFocus) {
+				return (ComboBoxValues.values()[index].focusRenderer);
+			} else {
+				return (ComboBoxValues.values()[index].normalRenderer);
+			}
+		}
+		
+	}
+	
+	/**
+	 * The values  shown in the ComboBox.
+	 * It contains the labels to use as renderer for each cell.
+	 * One label is for the normal situation and the second one
+	 * is used when the cell has focus (inverted colors)
+	 * 
+	 * @author acaproni
+	 *
+	 */
+	public enum ComboBoxValues {
+		NONE("None",CellColor.INACTIVE),
+		PRIORITY3("Priority 3",CellColor.PRI_3),
+		PRIORITY2("Priority 2",CellColor.PRI_2),
+		PRIORITY1("Priority 1",CellColor.PRI_1);
+		
+		public final String title;
+		public final JLabel normalRenderer;
+		public final JLabel focusRenderer;
+		
+		// Width and height of the label
+		// They are calculated from the dimension of the strings
+		// to show
+		private static int height=0;
+		private static int width=0;
+		
+		/**
+		 * Constructor
+		 * 
+		 * @param title
+		 * @param color
+		 */
+		private ComboBoxValues(String tit, CellColor color) {
+			title=tit;
+			normalRenderer = new JLabel(tit);
+			normalRenderer.setBackground(color.backg);
+			normalRenderer.setForeground(color.foreg);
+			normalRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+			normalRenderer.setVerticalAlignment(SwingConstants.CENTER);
+			focusRenderer = new JLabel(tit);
+			focusRenderer.setBackground(color.foreg);
+			focusRenderer.setForeground(color.backg);
+			focusRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+			focusRenderer.setVerticalAlignment(SwingConstants.CENTER);
+		}
+		
+		/**
+		 * Init the sizes of all the lables
+		 */
+		public static void initSizes() {
+			for (ComboBoxValues val: ComboBoxValues.values()) {
+				Font f = val.normalRenderer.getFont();
+				FontMetrics fm = val.normalRenderer.getFontMetrics(f);
+				int h = fm.getHeight()+5;
+				int w = fm.charsWidth(val.title.toCharArray(), 0, val.title.length())+10;
+				setHeight(h);
+				setWidth(w);
+			}
+			
+			Dimension d = new Dimension(ComboBoxValues.getWidth(),ComboBoxValues.getHeight());
+			for (ComboBoxValues val: ComboBoxValues.values()) {
+				val.normalRenderer.setPreferredSize(d);
+				val.focusRenderer.setPreferredSize(d);
+				val.normalRenderer.setMinimumSize(d);
+				val.focusRenderer.setMinimumSize(d);
+				val.normalRenderer.setMaximumSize(d);
+				val.focusRenderer.setMaximumSize(d);
+			}
+			
+			System.out.println(d);
+		}
+		
+		/**
+		 * Return the height of each label
+		 * 
+		 * @return The height of each lable
+		 */
+		public static int getHeight() {
+			return height;
+		}
+
+		/**
+		 * Return the width of each label
+		 * 
+		 * @return The width of each label
+		 */
+		public static int getWidth() {
+			return width;
+		}
+
+		/**
+		 * Set the height (static fields can't be
+		 * called directly by the constructor)
+		 * 
+		 * @param height The new height
+		 */
+		private static void setHeight(int height) {
+			if (height>ComboBoxValues.height) {
+				ComboBoxValues.height = height;
+			}
+		}
+
+		/**
+		 * Set the width (static fields can't be
+		 * called directly by the constructor)
+		 * 
+		 * @param width The new width
+		 */
+		private static void setWidth(int width) {
+			if (width>ComboBoxValues.width) {
+				ComboBoxValues.width = width;
+			}
+		}
+	}
+	
+	private JComboBox autoAckLevelCB=new JComboBox(ComboBoxValues.values());
 
 	private JLabel autoAckLbl = new JLabel("Auto ack: ");
 	
@@ -65,6 +218,15 @@ public class Toolbar extends JPanel {
 		autoAckLbl.setFont(newFont);
 		add(autoAckLbl);
 		autoAckLevelCB.setFont(newFont);
+		autoAckLevelCB.setEditable(false);
+		// Set the colors of the renderers
+		ComboBoxValues.initSizes();
+		autoAckLevelCB.setRenderer(new ComboBoxRenderer());
+		autoAckLevelCB.setSelectedIndex(ComboBoxValues.NONE.ordinal());
+		autoAckLevelCB.setMaximumRowCount(ComboBoxValues.values().length);
+		Dimension d = new Dimension(ComboBoxValues.getWidth(),ComboBoxValues.getHeight());
+		//autoAckLevelCB.setPreferredSize(d);
+		autoAckLevelCB.setMinimumSize(d);
 		add(autoAckLevelCB);
 	}
 }
