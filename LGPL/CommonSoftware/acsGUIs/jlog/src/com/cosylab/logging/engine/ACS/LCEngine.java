1d1a2c7770b3839611b2b205d19499f8a15b1767
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2008-03-13 10:57:07 +0000

    Added the possibility to set filters in the engine. The filters are the same used by the table but used by the engine before sending logs to the listeners.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@86080 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsGUIs/jlog/src/com/cosylab/logging/engine/ACS/LCEngine.java
+++ LGPL/CommonSoftware/acsGUIs/jlog/src/com/cosylab/logging/engine/ACS/LCEngine.java
@@ -21,105 +21,120 @@
  */
 package com.cosylab.logging.engine.ACS;
 
-import java.util.Vector;
-
 import org.omg.CORBA.ORB;
 
 import si.ijs.maci.Manager;
 
+import com.cosylab.logging.engine.Filter;
+import com.cosylab.logging.engine.FiltersVector;
 import com.cosylab.logging.engine.RemoteAccess;
-import com.cosylab.logging.engine.log.ILogEntry;
 
 /**
- * LCEngine connects to the logging NC and sends
- * messages to the listeners
+ * LCEngine connects to the logging NC and sends messages to the listeners.
  * 
- * There are three type of listeners supported:
- *   - ACSLogConnectionListener: listens events related to the conenction
- *                                with the logging NC and reportMessages
- *   - ACSRemoteLogListener: listens for LogEntries
- *   - ACSRemoteRawLogListener: listens for XML strings representing logs
- *   
+ * It is possible to define the filters to apply to the incoming logs. In this
+ * case, only the logs that pass all the filters are sent to the listeners. The
+ * filters do not apply to RAW (i.e. XML) log listeners.
+ * 
+ * There are three type of listeners supported: - ACSLogConnectionListener:
+ * listens events related to the connection with the logging NC and
+ * reportMessages - ACSRemoteLogListener: listens for LogEntries -
+ * ACSRemoteRawLogListener: listens for XML strings representing logs
+ * 
  * It there are no ACSRemoteLogLiestenersRegistered then the string received
  * from the NC is not parsed
  * 
  * @see ACSRemoteLogListener
  * @see ACSRemoteRawLogListener
  * @see ACSLogConnectionListener
- *  
+ * 
  */
 public class LCEngine {
 	/**
 	 * The connection is checked every CHECK_INTERVAL seconds
 	 */
 	private final int CHECK_INTERVAL = 15;
-	
+
 	// The boolean remember if the client was connected before
 	// checking for the connection (needed to understand if the
 	// connection has been lost or never happened)
-	private boolean wasConnected=false;
-	
+	private boolean wasConnected = false;
+
 	// Signal the thread to terminate
-	private AccessChecker connCheckerThread=null;
-	private boolean terminateThread=false;
-	
+	private AccessChecker connCheckerThread = null;
+	private boolean terminateThread = false;
+
 	private RemoteAccess remoteAccess = null;
 
-	/** 
+	/**
+	 * The filters to apply to the incoming logs.
+	 * 
+	 * The filters are applied by <code>ACSLogReceiver.run()</code>
+	 */
+	private FiltersVector filters = null;
+
+	/**
 	 * A thread used to set and initialize RemoteAccess
-	 *
+	 * 
 	 * Constructor parameters:
-	 *
-	 * @accessType (String) name of the RemoteAccessClass e.g. ACS
-	 * class has to be in the package com.cosylab.logging.engine."accessType"
-	 * and has to be named "accessType"RemoteAccess
-	 * e.g. com.cosylab.logging.engine.ACS.ACSRemoteAccess
+	 * 
+	 * @accessType (String) name of the RemoteAccessClass e.g. ACS class has to
+	 *             be in the package com.cosylab.logging.engine."accessType" and
+	 *             has to be named "accessType"RemoteAccess e.g.
+	 *             com.cosylab.logging.engine.ACS.ACSRemoteAccess
 	 */
 	private class AccessSetter extends Thread {
-		
+
 		/**
 		 * Constructor
-		 *
+		 * 
 		 */
 		public AccessSetter() {
 			super("AccessSetter");
 			setDaemon(true);
 		}
-		
+
 		/**
-		 * Connect to the NC.
-		 * It starts the thread to check the status of the connection (LCEngine.run) 
-		 * if it is not already alive (this last case can happen if the connection has been lost)
+		 * Connect to the NC. It starts the thread to check the status of the
+		 * connection (LCEngine.run) if it is not already alive (this last case
+		 * can happen if the connection has been lost)
 		 */
 		public void run() {
 			disconnectRA();
 			listenersDispatcher.publishConnecting();
-			listenersDispatcher.publishReport("Connecting to " + accessType + " remote access...");
+			listenersDispatcher.publishReport("Connecting to " + accessType
+					+ " remote access...");
 			try {
-				
-				Class[] parameters = {listenersDispatcher.getClass()};
+
+				Class[] parameters = { listenersDispatcher.getClass() };
 				String raName = accessType;
 				if (accessType.indexOf(".") == -1)
-					raName = "com.cosylab.logging.engine."
-							+ accessType
-							+ "."
-							+ accessType
-							+ "RemoteAccess"; // com.cosylab.logging.engine.ACS.ACSRemoteAccess
-				//remoteAccess = (RemoteAccess) Class.forName(raName).getConstructors()[0].newInstance(parameters);
-				remoteAccess = (RemoteAccess) Class.forName(raName).getConstructor(parameters).newInstance(listenersDispatcher);
-				//if (remoteAccess == null)
-				//	System.out.println("RemoteAccess == null");
-				remoteAccess.initialize(orb,manager);
+					raName = "com.cosylab.logging.engine." + accessType + "."
+							+ accessType + "RemoteAccess"; // com.cosylab.logging.engine.ACS.ACSRemoteAccess
+				// remoteAccess = (RemoteAccess)
+				// Class.forName(raName).getConstructors()[0].newInstance(parameters);
+				remoteAccess = (RemoteAccess) Class.forName(raName)
+						.getConstructor(parameters).newInstance(
+								listenersDispatcher);
+				// if (remoteAccess == null)
+				// System.out.println("RemoteAccess == null");
+				remoteAccess.initialize(orb, manager);
 			} catch (Throwable e) {
-				listenersDispatcher.publishReport("Exception occurred when initializing " + accessType + " remote access.");
+				listenersDispatcher
+						.publishReport("Exception occurred when initializing "
+								+ accessType + " remote access.");
 				listenersDispatcher.publishConnected(false);
-				System.out.println("Exception in LCEngine$AccessSetter::run(): " + e);
+				System.out
+						.println("Exception in LCEngine$AccessSetter::run(): "
+								+ e);
 				return;
 			}
-			if (remoteAccess!=null && remoteAccess.isInitialized()) {
-				listenersDispatcher.publishReport("Connected to " + accessType + " remote access.");
+			if (remoteAccess != null && remoteAccess.isInitialized()) {
+				listenersDispatcher.publishReport("Connected to " + accessType
+						+ " remote access.");
 				listenersDispatcher.publishConnected(true);
-				LCEngine.this.wasConnected=true;
+				LCEngine.this.wasConnected = true;
+				remoteAccess.setFilters(filters);
 			} else {
 				listenersDispatcher.publishConnected(false);
 			}
@@ -130,32 +145,31 @@ public class LCEngine {
 	 * The thread to check the status of the connection
 	 * 
 	 * @author acaproni
-	 *
+	 * 
 	 */
 	private class AccessChecker extends Thread {
-		
+
 		/**
 		 * Constructor
-		 *
+		 * 
 		 */
 		public AccessChecker() {
 			super("AccessChecker");
 		}
-		
+
 		/**
-		 * The thread that 
-		 * 1- monitors the status of the connection
-		 * 2- reconnect if the autoReconnect option is activated
+		 * The thread that 1- monitors the status of the connection 2- reconnect
+		 * if the autoReconnect option is activated
 		 * 
 		 * The thread is started when the connection is activated and terminated
 		 * when disconnects from the NC.
 		 * 
 		 */
 		public void run() {
-			int currentSec=0;
+			int currentSec = 0;
 			while (!terminateThread) {
 				// wait for CHECK_INTERVAL secs..
-				while (currentSec<CHECK_INTERVAL) {
+				while (currentSec < CHECK_INTERVAL) {
 					try {
 						Thread.sleep(1000);
 						currentSec++;
@@ -166,21 +180,21 @@ public class LCEngine {
 						continue;
 					}
 				}
-				
-				currentSec=0;
+
+				currentSec = 0;
 				// Check the connection!
 				boolean connected = isConnected();
-				//publishConnected(connected);
+				// publishConnected(connected);
 				if (wasConnected && !connected) {
 					listenersDispatcher.publishReport("Connection lost");
-					wasConnected=false;
+					wasConnected = false;
 					disconnectRA();
 					listenersDispatcher.publishConnectionLost();
 					// Better otherwise it tries to reconnect every time
 					if (!autoReconnect) {
 						return; // Terminate the thread
 					}
-					
+
 				}
 				if (autoReconnect && !connected) {
 					connect();
@@ -188,59 +202,81 @@ public class LCEngine {
 			}
 		}
 	}
-	/** accessType, orb and manager are used to connect to
-	 * the logging channel
+
+	/**
+	 * accessType, orb and manager are used to connect to the logging channel
 	 * 
 	 * @see AccessSetter.run
-	 */ 
+	 */
 	private String accessType = "ACS";
 	private ORB orb = null; // Can be null
 	private Manager manager = null; // Can be null
-	
+
 	/**
 	 * If true the engine tries to reconnect automatically
 	 */
 	private boolean autoReconnect = false;
-	
+
 	// Dispatches messages to the listeners
 	private ACSListenersDispatcher listenersDispatcher = new ACSListenersDispatcher();
-	
+
 	/**
 	 * LCEngine constructor.
-	 *
+	 * 
 	 */
 	public LCEngine() {
 	}
-	
+
 	/**
 	 * LCEngine constructor.
-	 *
-	 * @param autoReconn If true the engine automatically reconnects
+	 * 
+	 * @param autoReconn
+	 *            If true the engine automatically reconnects
 	 */
 	public LCEngine(boolean autoReconn) {
-		autoReconnect=autoReconn;
+		autoReconnect = autoReconn;
 	}
-	
+
 	/**
-	 * LCEngine starts an attempt to connect to the remote system.
-	 * Connection is handled in a separate Thread
+	 * LCEngine constructor.
+	 * <P>
+	 * This constructor allows to define a set of filters to apply to incoming
+	 * logs: only the logs passing the filters are sent to the listeners.
+	 * <P>
+	 * 
+	 * @param autoReconn
+	 *            If true the engine automatically reconnects
+	 * @param filters
+	 *            The filters to apply to the incoming logs.
+	 *            <code>filters</code> can be null or empty.
+	 */
+	public LCEngine(boolean autoReconn, FiltersVector filters) {
+		this(autoReconn);
+		this.filters = filters;
+	}
+
+	/**
+	 * LCEngine starts an attempt to connect to the remote system. Connection is
+	 * handled in a separate Thread
+	 * 
 	 * @see LCEngine$AccessSetter
 	 */
 	public void connect() {
 		new AccessSetter().start();
-		if (connCheckerThread==null || !connCheckerThread.isAlive()) {
-			terminateThread=false;
+		if (connCheckerThread == null || !connCheckerThread.isAlive()) {
+			terminateThread = false;
 			connCheckerThread = new AccessChecker();
 			connCheckerThread.setName("LCEngine");
 			connCheckerThread.start();
-		} 
+		}
 	}
-	
+
 	/**
-	 * LCEngine starts an attempt to connect to the remote system.
-	 * Connection is handled in a separate Thread
+	 * LCEngine starts an attempt to connect to the remote system. Connection is
+	 * handled in a separate Thread
 	 * 
-	 * @param accessTyp The access type
+	 * @param accessTyp
+	 *            The access type
 	 * 
 	 * @see LCEngine$AccessSetter
 	 */
@@ -248,133 +284,146 @@ public class LCEngine {
 		setAccessType(newAccessType);
 		connect();
 	}
-	
+
 	/**
-	 * LCEngine starts an attempt to connect to the remote system.
-	 * Connection is handled in a separate Thread
+	 * LCEngine starts an attempt to connect to the remote system. Connection is
+	 * handled in a separate Thread
 	 * 
-	 * @param theORB The ORB (can be null) 
-	 * @param mgr The reference to the manager (can be null)
+	 * @param theORB
+	 *            The ORB (can be null)
+	 * @param mgr
+	 *            The reference to the manager (can be null)
 	 * 
 	 * @see LCEngine$AccessSetter
 	 */
 	public void connect(ORB theORB, Manager mgr) {
-		setConnectionParams(theORB,mgr);
+		setConnectionParams(theORB, mgr);
 		connect();
 	}
-	
+
 	/**
-	 * LCEngine starts an attempt to connect to the remote system.
-	 * Connection is handled in a separate Thread
+	 * LCEngine starts an attempt to connect to the remote system. Connection is
+	 * handled in a separate Thread
 	 * 
 	 * @see LCEngine$AccessSetter
 	 */
 	public void disconnect() {
 		disconnectRA();
 		// Stop the thread to check the status of the connection
-		if (connCheckerThread!=null) {
-			terminateThread=true;
-			while (connCheckerThread!=null && connCheckerThread.isAlive()) {
+		if (connCheckerThread != null) {
+			terminateThread = true;
+			while (connCheckerThread != null && connCheckerThread.isAlive()) {
 				try {
 					Thread.sleep(250);
 				} catch (InterruptedException ie) {
 					continue;
 				}
 			}
-			connCheckerThread=null;
+			connCheckerThread = null;
 		}
 	}
-	
+
 	/**
 	 * Disconnect the remote access
-	 *
+	 * 
 	 */
 	private void disconnectRA() {
 		if (remoteAccess != null && remoteAccess.isInitialized()) {
 			try {
-				listenersDispatcher.publishReport("Disconnecting from " + accessType + " remote access...");
+				listenersDispatcher.publishReport("Disconnecting from "
+						+ accessType + " remote access...");
 				remoteAccess.destroy();
 			} catch (Exception e) {
-				listenersDispatcher.publishReport("Exception occurred when destroying " + accessType + " remote access.");
-				System.out.println("Exception in LCEngine$AccessDestroyer::run(): " + e);
+				listenersDispatcher
+						.publishReport("Exception occurred when destroying "
+								+ accessType + " remote access.");
+				System.out
+						.println("Exception in LCEngine$AccessDestroyer::run(): "
+								+ e);
 			}
-			listenersDispatcher.publishReport("Disconnected from " + accessType + " remote access.");	
+			listenersDispatcher.publishReport("Disconnected from " + accessType
+					+ " remote access.");
 		}
-		if (remoteAccess!=null) {
+		if (remoteAccess != null) {
 			remoteAccess.close(false);
 		}
 		remoteAccess = null;
 		listenersDispatcher.publishConnected(false);
-		LCEngine.this.wasConnected=false;
+		LCEngine.this.wasConnected = false;
 	}
-	
+
 	/**
-	 * Insert the method's description here.
-	 * Creation date: (2/18/2002 9:58:30 AM)
+	 * Insert the method's description here. Creation date: (2/18/2002 9:58:30
+	 * AM)
+	 * 
 	 * @return java.lang.String
 	 */
 	public java.lang.String getAccessType() {
 		return accessType;
 	}
-	
+
 	/**
-	 * Insert the method's description here.
-	 * Creation date: (2/18/2002 9:58:30 AM)
-	 * @param newAccessType java.lang.String
+	 * Insert the method's description here. Creation date: (2/18/2002 9:58:30
+	 * AM)
+	 * 
+	 * @param newAccessType
+	 *            java.lang.String
 	 */
 	public void setAccessType(String newAccessType) {
 		accessType = newAccessType;
 	}
-	
+
 	/**
 	 * Set the connection params fro this
 	 * 
-	 * @param theORB The ORB. It can't be null if the manager is not null
-	 * @param mgr The reference to the Manager 
+	 * @param theORB
+	 *            The ORB. It can't be null if the manager is not null
+	 * @param mgr
+	 *            The reference to the Manager
 	 */
 	public void setConnectionParams(ORB theORB, Manager mgr) {
 		orb = theORB;
 		manager = mgr;
 	}
-	
-	
+
 	/**
 	 * 
 	 * @return ture if the engine is connected to the notification channel
 	 */
 	public boolean isConnected() {
-		if (remoteAccess==null) {
+		if (remoteAccess == null) {
 			return false;
-		} 
+		}
 		return remoteAccess.isConnected();
 	}
-	
+
 	/**
-	 * Suspend resume the notification of logs
-	 * NOTE: When suspended the log discarded are lost forever
+	 * Suspend resume the notification of logs NOTE: When suspended the log
+	 * discarded are lost forever
 	 * 
-	 * @param suspended If true suspend the notification of the logs
+	 * @param suspended
+	 *            If true suspend the notification of the logs
 	 */
 	public void setSupended(boolean suspended) {
 		if (remoteAccess instanceof ACSRemoteAccess) {
-			((ACSRemoteAccess)remoteAccess).setSuspended(suspended);
+			((ACSRemoteAccess) remoteAccess).setSuspended(suspended);
 		}
 	}
-	
+
 	/**
 	 * Enable/disable the auto reconnection
 	 * 
-	 * @param autoRec If true the engine tries to reconnect automatically
+	 * @param autoRec
+	 *            If true the engine tries to reconnect automatically
 	 */
 	public void enableAutoReconnection(boolean autoRec) {
-		autoReconnect=autoRec;
+		autoReconnect = autoRec;
 	}
-	
+
 	/**
-	 * Pause/unpause the publishing of logs to the listener
-	 * The difference between pause and suspended is that
-	 * when the engine is suspended all the received logs are
-	 * discarded. When it is paused, the received logs are
+	 * Pause/unpause the publishing of logs to the listener The difference
+	 * between pause and suspended is that when the engine is suspended all the
+	 * received logs are discarded. When it is paused, the received logs are
 	 * cached and published when the engine will be unpaused.
 	 * 
 	 * @param pause
@@ -382,49 +431,129 @@ public class LCEngine {
 	public void setPaused(boolean pause) {
 		remoteAccess.pause(pause);
 	}
-	
+
 	/**
 	 * Add a log listener
 	 * 
-	 * @param listener The listener to add
+	 * @param listener
+	 *            The listener to add
 	 * 
 	 * @see ACSRemoteLogListener
 	 */
 	public void addLogListener(ACSRemoteLogListener listener) {
 		listenersDispatcher.addLogListener(listener);
 	}
-	
+
 	/**
 	 * Add a RAW log listener
 	 * 
-	 * @param listener The listener to add
+	 * @param listener
+	 *            The listener to add
 	 * 
 	 * @see ACSRemoteRawLogListener
 	 */
 	public void addRawLogListener(ACSRemoteRawLogListener listener) {
 		listenersDispatcher.addRawLogListener(listener);
 	}
-	
+
 	/**
 	 * Add an error listener
 	 * 
-	 * @param listener The error listener to add
+	 * @param listener
+	 *            The error listener to add
 	 * 
 	 * @see ACSRemoteErrorListener
 	 */
 	public void addLogErrorListener(ACSRemoteErrorListener listener) {
 		listenersDispatcher.addErrorListener(listener);
 	}
-	
+
 	/**
 	 * Add a connection listener
 	 * 
-	 * @param listener The listener to add
+	 * @param listener
+	 *            The listener to add
 	 * 
 	 * @see ACSLogConnectionListener
 	 * 
 	 */
 	public void addLogConnectionListener(ACSLogConnectionListener listener) {
 		listenersDispatcher.addLogConnectionListener(listener);
+	}
+
+	/**
+	 * Set the filters to apply before sending the log entries to the listener.
+	 * By setting a null or empty filters disable the filtering.
+	 * 
+	 * @param newFilters
+	 *            The new filters to apply If <code>null</code> or empty the
+	 *            filtering is disabled
+	 * @param append
+	 *            If <code>true</code> the new filters are added to the
+	 *            existing filters (if any).
+	 */
+	public void setFilters(FiltersVector newFilters, boolean append) {
+		if (newFilters==null || newFilters.isEmpty()) {
+			filters=null;
+			return;
+		}
+		if (filters==null) {
+			filters=new FiltersVector();
+		}
+		
+		if (!append) {
+			filters.clear();
+		}
+		for (int t=0; t<newFilters.size(); t++) {
+			filters.addFilter(newFilters.get(t), newFilters.isActive(t));
+		}
+		if (remoteAccess!=null) {
+			remoteAccess.setFilters(filters);
+		}
+	}
+
+	/**
+	 * Add a new filters to the vector of filters to apply to the incoming logs
+	 * before sending to the listeners.
+	 * <P>
+	 * The filter is applied as active.
+	 * 
+	 * @param filter
+	 *            The not null filter to add
+	 */
+	public void addFilter(Filter filter) {
+		if (filter == null) {
+			throw new IllegalArgumentException("The filter can't be null");
+		}
+		if (filters == null) {
+			filters = new FiltersVector();
+		}
+		filters.addFilter(filter, true);
+		if (remoteAccess != null) {
+			remoteAccess.setFilters(filters);
+		}
+	}
+
+	/**
+	 * Remove all the filters.
+	 * 
+	 * After calling this method, all the received logs are forwarded to the
+	 * listeners.
+	 */
+	public void clearFilters() {
+		filters = null;
+		if (remoteAccess != null) {
+			remoteAccess.setFilters(null);
+		}
+	}
+
+	/**
+	 * Return the filters to filter the incoming logs before sending to the
+	 * listeners
+	 * 
+	 * @return The filters (can be <code>null</code>)
+	 */
+	public FiltersVector getFilters() {
+		return filters;
 	}
 }
