13d708a7a8e4810312b4d71a1182d1d7dfa51be4
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2008-03-25 14:04:18 +0000

    Include file of the test component
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@86942 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/ACSLaser/baciPropsTest/include/baciTestImpl.h
@@ -0,0 +1,101 @@
+#ifndef baciTestImpl_h
+#define baciTestImpl_h
+/*******************************************************************************
+*    ALMA - Atacama Large Millimiter Array
+*    (c) European Southern Observatory, 2002
+*    Copyright by ESO (in the framework of the ALMA collaboration)
+*    and Cosylab 2002, All rights reserved
+*
+*    This library is free software; you can redistribute it and/or
+*    modify it under the terms of the GNU Lesser General Public
+*    License as published by the Free Software Foundation; either
+*    version 2.1 of the License, or (at your option) any later version.
+*
+*    This library is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*    Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public
+*    License along with this library; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+*
+*
+* "@(#) $Id: baciTestImpl.h,v 1.1 2008/03/25 14:04:18 acaproni Exp $"
+*
+* who       when      what
+* --------  --------  ----------------------------------------------
+* acaproni 2008-03-17 created
+*/
+
+#ifndef __cplusplus
+#error This is a C++ include file and cannot be used from plain C
+#endif
+
+///Contains the defintion of the standard superclass for C++ components
+#include <baciCharacteristicComponentImpl.h>
+
+///CORBA generated servant stub
+#include <testComponentS.h>
+
+///Includes for each BACI property used in this example
+#include <baciROdouble.h>
+
+///Include the smart pointer for properties
+#include <baciSmartPropertyPointer.h>
+
+using namespace baci;
+
+
+/**
+ * A component for testing if baci sends alarms when a property goes out of range.
+ * 
+ */
+class BaciPropTest: public CharacteristicComponentImpl,     //Standard component superclass
+			     public virtual POA_alarmsystemPropTest::BaciPropTest   //CORBA servant stub
+{
+  public:
+     /**
+     * Constructor
+     * 
+     * @param name component's name. This is also the name that will be used to find the
+     * configuration data for the component in the Configuration Database.
+     * @param containerService The pointer to the container services
+     */
+	  BaciPropTest(ACE_CString name, maci::ContainerServices * containerServices);
+    
+    /**
+     * Destructor
+     */
+    virtual ~BaciPropTest();
+    
+    /* --------------------- [ CORBA interface ] ----------------------*/   
+    /**
+     * 
+     */   
+    virtual void valueTooLow() throw (CORBA::SystemException); 
+    virtual void valueTooHigh() throw (CORBA::SystemException);
+    virtual void valueNormal() throw (CORBA::SystemException);
+    
+    /**
+     * Returns a reference to the cmdEl property
+     * Implementation of IDL interface for the property.
+     * @return pointer to read-only double property cmdEl
+     * @htmlonly
+       <br><hr>
+       @endhtmlonly
+     */
+    virtual ACS::ROdouble_ptr testVar() throw (CORBA::SystemException);
+    
+  private:
+    
+    /**
+     *  The test variable
+     */
+    SmartPropertyPointer<ROdouble> m_testVar_sp;
+
+};
+/*\@}*/
+/*\@}*/
+
+#endif /*!baciTestImpl_H*/
