0d276b63a9cf50c7dc7602aaaf8a850717fd1836
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2008-04-04 09:41:45 +0000

    some existing code has moved to new base class ShutdownHookBase
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@87749 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jcont/src/alma/acs/container/ShutdownHook.java
+++ LGPL/CommonSoftware/jcont/src/alma/acs/container/ShutdownHook.java
@@ -23,72 +23,40 @@ package alma.acs.container;
 
 import java.util.logging.Logger;
 
+import alma.acs.shutdown.ShutdownHookBase;
+
 /**
  * This class will be registered to the JVM Runtime as a shutdown thread to be run at shutdown, 
  * both for regular termination and for interruption like Ctrl-C (SIGINT).
  * <p>
  * TODO: look at <code>void TerminationSignalHandler(int)</code> from the C++ implementation,
  * 		and figure out what else to do at shutdown. 
- * <p>
- * Recommended reading: http://www-106.ibm.com/developerworks/ibm/library/i-signalhandling/ 
- * (also talks about signals for tougher mishaps than Ctrl-c).
- * See also {@link java.lang.Runtime#addShutdownHook(Thread)}
- * 
  * @author hsommer
  */
-public class ShutdownHook extends Thread 
+public class ShutdownHook extends ShutdownHookBase 
 {
-	private boolean m_regularShutdownExpected = false;
-
-	private Logger m_logger; 
-	
 	private AcsContainer m_acsContainer;
-	
 	
 	ShutdownHook(Logger logger)
 	{
-		super("ContainerVMShutdownHook");
-        m_logger = logger;
+		super(logger, "ContainerVM");
 	}
 	
 	
-	/**
-	 * @see java.lang.Runnable#run()
-	 */
-	public void run()
-	{
-		try
-		{
-			if (m_regularShutdownExpected)
-			{
-				regularTermination();
-			}
-			else
-			{
-				interruptDetected();
-			}	
-		}
-		catch (Throwable thr)
-		{
-            System.err.println("failure in ShutdownHook#run: ");
-            thr.printStackTrace();
-		}
-				
-	}
 
-	private void regularTermination()
+	protected void regularTermination()
 	{
 		// regular exit (unless something went wrong after setRegularShutdownExpected() had been called
-		m_logger.info("about to exit the JVM...thanks for using our container, come back soon...");
+		logger.info("about to exit the JVM...thanks for using our container, come back soon...");
 	}
 	
 	/**
 	 * Tidying up when the JVM is about to terminate due to an interrupt signal
 	 */
-	private void interruptDetected()
+	protected void interruptDetected()
 	{
 		// termination
-		m_logger.severe("*** container process has been interrupted - will shut down ***");
+		logger.severe("*** container process has been interrupted - will shut down ***");
 		
 		if (m_acsContainer != null)
 		{
@@ -101,16 +69,7 @@ public class ShutdownHook extends Thread
 		System.err.println("*** emergency shutdown complete, will exit... ***");
 	}
 	
-	
-	/**
-	 * Must be called right before the program ends. This helps the ShutdownHook to decide 
-	 * whether it's being called for a regular program termination or because of an interrupt.
-	 */
-	void setRegularShutdownExpected()
-	{
-		m_regularShutdownExpected = true;
-	}
-	
+
 	/**
 	 * Sets m_acsContainer.
 	 */
