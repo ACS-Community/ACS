9bef827ee9f0d9efae18d80e85c36802f32b3630
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2008-04-11 14:15:06 +0000

    now calls thread.interrupt() instead of thread.stop() on illegally surviving threads. This was necessary to make jbaci code work with this thread factory, see COMP-2362. @TODO: check if we can go back to the more rigorous stop() which should be OK once java properties can terminate their threads more gracefully.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@88174 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jcont/src/alma/acs/container/CleaningDaemonThreadFactory.java
+++ LGPL/CommonSoftware/jcont/src/alma/acs/container/CleaningDaemonThreadFactory.java
@@ -22,13 +22,11 @@
 package alma.acs.container;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 
 /**
  * Thread factory that remembers all threads it creates.
@@ -42,144 +40,153 @@ import java.util.logging.Logger;
  * @author hsommer
  * created Mar 8, 2005 1:27:38 PM
  */
-public class CleaningDaemonThreadFactory implements ThreadFactory {
+public class CleaningDaemonThreadFactory implements ThreadFactory
+{
+	// we keep track of our threads outside of the thread group
+	private final List<Thread> threadList = new ArrayList<Thread>();
 
-    private LoggingThreadGroup group;
-    
-    // we keep track of our threads outside of the thread group
-    private final List<Thread> threadList = new ArrayList<Thread>();
-    
-    private final AtomicInteger threadNumber = new AtomicInteger(1);
-    
-    private final Logger logger;
-    private final String name;
-    private ClassLoader newThreadContextCL;
-    
-//    private boolean DEBUG = true;
+	private final AtomicInteger threadNumber = new AtomicInteger(1);
 
-    
-    /**
-     * Constructor.
-     * @param name  the name of the {@link ThreadGroup} to which all threads created by this factory will belong.
-     * @param logger  the logger to be used by this class
-     */
-    public CleaningDaemonThreadFactory(String name, Logger logger) {        
-        this.logger = logger;
-        this.name = name;
-    }
-    
-    /**
-     * Creates a new daemon thread that is part of the same factory thread group 
-     * as all other threads created by this method.
-     * The thread's name will be that of the group, with an integer value appended.
-     * 
-     * @see java.util.concurrent.ThreadFactory#newThread(java.lang.Runnable)
-     */
-    public Thread newThread(Runnable command) {
-        if (group == null) {
-            group = new LoggingThreadGroup(name, logger);
-            group.setMaxPriority(Thread.currentThread().getPriority()); //Thread.NORM_PRIORITY
-        }
+	private final Logger logger;
+	private final String name;
+	private LoggingThreadGroup group;
 
-        String threadName = group.getName() + "-" + threadNumber.getAndIncrement();
-        Thread t = new Thread(group, command, threadName);
-        t.setDaemon(true);
-        if (newThreadContextCL != null) {
-            t.setContextClassLoader(newThreadContextCL);
-        }
-        threadList.add(t);
-        return t;
-    }
+	private ClassLoader newThreadContextCL;
 
-    
-    void setNewThreadContextClassLoader(ClassLoader cl) {
-        if (cl != null) {
-            newThreadContextCL = cl;
-        }
-    }
-    
-    /**
-     * Gets a copy of the list of all threads created by this factory up to this call.
-     * This method should only be used for testing, but not in operational code.
-     */
-    public List<Thread> _getAllThreadsCreated() {
-    	synchronized (threadList) {
+	
+	/**
+	 * Constructor.
+	 * @param name  the name of the {@link ThreadGroup} to which all threads created by this factory will belong.
+	 * @param logger  the logger to be used by this class
+	 */
+	public CleaningDaemonThreadFactory(String name, Logger logger) {
+		this.logger = logger;
+		this.name = name;
+	}
+
+	/**
+	 * Creates a new daemon thread that is part of the same factory thread group 
+	 * as all other threads created by this method.
+	 * The thread's name will be that of the group, with an integer value appended.
+	 * 
+	 * @see java.util.concurrent.ThreadFactory#newThread(java.lang.Runnable)
+	 */
+	public Thread newThread(Runnable command) {
+		ensureGroupCreated();
+		
+		String threadName = group.getName() + "-" + threadNumber.getAndIncrement();
+		Thread t = new Thread(group, command, threadName);
+		t.setDaemon(true);
+		if (newThreadContextCL != null) {
+			t.setContextClassLoader(newThreadContextCL);
+		}
+		synchronized (threadList) {
+			threadList.add(t);
+		}
+		return t;
+	}
+
+	private synchronized void ensureGroupCreated() {
+		if (group == null) {
+			group = new LoggingThreadGroup(name, logger);
+			group.setMaxPriority(Thread.currentThread().getPriority());
+		}
+	}
+	
+	void setNewThreadContextClassLoader(ClassLoader cl) {
+		if (cl != null) {
+			newThreadContextCL = cl;
+		}
+	}
+
+	/**
+	 * Gets a copy of the list of all threads created by this factory up to this call.
+	 * This method should only be used for testing, but not in operational code.
+	 */
+	public List<Thread> _getAllThreadsCreated() {
+		synchronized (threadList) {
 			return new ArrayList<Thread>(threadList);
 		}
-    }
-    
-    /**
-     * Kills running threads via {@link Thread#stop()}.
-     * Should be called by tbe container or similar classes when all threads 
-     * created by this factory supposedly have terminated anyway thanks to smart applications. 
-     * The safety concerns which led to the deprecation of the stop method thus don't seem to apply here.
-     */
-    public void cleanUp() {
-        if (group == null) {
-            return;
-        }
-        group.setShuttingDown();
-        Thread t = null;
-        for (Iterator<Thread> iter = threadList.iterator(); iter.hasNext();) {
-            try {
-                t = iter.next();
-                if (t.isAlive()) {
-                    logger.warning("forcefully terminating surviving thread " + t.getName());
-                }
-                t.stop();
-            } catch (RuntimeException e) {
-                logger.finer("exception while stopping thread '" + t.getName() + "': " + e.toString());
-            } 
-        }
-        threadList.clear();
-        try {
-            // if there are threads in the group which have never started, destroy() will fail
-            if (!group.isDestroyed() && group.activeCount() == 0 && group.activeGroupCount() == 0) {
-                group.destroy();
-            }
-        } catch (Exception e) {
-            logger.finer("unexpectedly failed to destroy thread group " + group.getName() + e.toString());
-        }
-        // in case this factory gets used again, we should build a new thread group
-        group = null;
-    }
+	}
 
-    
+	/**
+	 * Kills running threads via {@link Thread#stop()}.
+	 * Should be called by the container or similar classes when all threads 
+	 * created by this factory supposedly have terminated anyway thanks to smart applications. 
+	 * The safety concerns which led to the deprecation of the stop method thus don't seem to apply here.
+	 */
+	public synchronized void cleanUp() {
+		if (group == null) {
+			return;
+		}
+		group.setShuttingDown();
+		synchronized (threadList) {
+			for (Thread t : threadList) {
+				try {
+					if (t.isAlive()) {
+						logger.warning("forcefully terminating surviving thread " + t.getName());
+					}
+					// @TODO HSO 2008-04: now that jbaci uses an external ThreadFactory, which is typically supplied by container services,
+					//       we got jbaci test failures as long as stop() is called (see COMP-2362).
+					//       We must check if we fix jbaci and go back to thread.stop, or stay with thread.interrupt.
+					t.interrupt();
+//					t.stop();
+				} catch (RuntimeException e) {
+					logger.finer("exception while stopping thread '" + t.getName() + "': " + e.toString());
+				}
+			}
+			threadList.clear();
+		}
+		try {
+			// if there are threads in the group which have never started, destroy() will fail
+			if (!group.isDestroyed() && group.activeCount() == 0) {
+				group.destroy();
+			}
+		} catch (Exception e) {
+			logger.finer("unexpectedly failed to destroy thread group " + group.getName() + e.toString());
+		}
+		// in case this factory gets used again, we should build a new thread group
+		group = null;
+	}
 
-    private static class LoggingThreadGroup extends ThreadGroup {
-        private Logger logger;
-        private boolean shuttingDown = false;
+	private static class LoggingThreadGroup extends ThreadGroup
+	{
+		private final Logger logger;
+		private boolean shuttingDown = false;
 
-        LoggingThreadGroup(String name, Logger logger) {
-            super(name);
-            this.logger = logger;
-        }
+		LoggingThreadGroup(String name, Logger logger) {
+			super(name);
+			this.logger = logger;
+		}
 
-        void setShuttingDown() {
-            shuttingDown = true;
-        }
-        
-        /**
-         * Called by the JVM if any of the threads in this thread group terminates with an error.
-         * Logs a warning to the logger provided in the ctor.
-         * <p>
-         * The error <code>ThreadDeath</code> is even logged during expected thread lifetime,
-         * because user threads are not supposed to be terminated through the deprecated <code>stop()</code> method
-         * during their normal operation. <br>
-         * During shutdown, exceptions are not logged, because the killing of surviving user threads
-         * is logged already in the <code>cleanUp</code> method. 
-         * 
-         * @see java.lang.ThreadGroup#uncaughtException(java.lang.Thread, java.lang.Throwable)
-         */
-        public void uncaughtException(Thread t, Throwable e) {
-            if (!shuttingDown) {
-                logger.log(Level.WARNING, "User thread '" + t.getName() + "' terminated with error ", e);
-            }
-            // ThreadDeath must move on to really let the thread die
-            if (e instanceof ThreadDeath) {
-                super.uncaughtException(t, e);
-            }
-        }
-        
-    }
+		void setShuttingDown() {
+			shuttingDown = true;
+		}
+
+		/**
+		 * Called by the JVM if any of the threads in this thread group terminates with an error.
+		 * Logs a warning to the logger provided in the ctor.
+		 * <p>
+		 * The error <code>ThreadDeath</code> is even logged during expected thread lifetime,
+		 * because user threads are not supposed to be terminated through the deprecated <code>stop()</code> method
+		 * during their normal operation. <br>
+		 * During shutdown, exceptions are not logged, because the killing of surviving user threads
+		 * is logged already in the <code>cleanUp</code> method. 
+		 * <p>
+		 * @TODO: since JDK 1.5 an {@link UncaughtExceptionHandler} can be attached to each thread,
+		 * which may be an alternative to using a thread group for this purpose.
+		 * 
+		 * @see java.lang.ThreadGroup#uncaughtException(java.lang.Thread, java.lang.Throwable)
+		 */
+		public void uncaughtException(Thread t, Throwable e) {
+			if (!shuttingDown) {
+				logger.log(Level.WARNING, "User thread '" + t.getName() + "' terminated with error ", e);
+			}
+			// ThreadDeath must move on to really let the thread die
+			if (e instanceof ThreadDeath) {
+				super.uncaughtException(t, e);
+			}
+		}
+
+	}
 }
