f5861d762fd488cc713e19ba8edd17656e2e4c5b
jschwarz <jschwarz@523d945c-050c-4681-91ec-863ad3bb968a>
2008-04-24 16:17:16 +0000

    Adjusted size of initial GUI.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@88934 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsGUIs/eventGUI/src/alma/acs/eventbrowser/ApplicationWorkbenchWindowAdvisor.java
+++ LGPL/CommonSoftware/acsGUIs/eventGUI/src/alma/acs/eventbrowser/ApplicationWorkbenchWindowAdvisor.java
@@ -18,7 +18,7 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
     
     public void preWindowOpen() {
         IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-        configurer.setInitialSize(new Point(400, 300));
+        configurer.setInitialSize(new Point(800, 300));
         configurer.setShowCoolBar(false);
         configurer.setShowStatusLine(false);
         configurer.setTitle("ALMA ACS Event Browser");
