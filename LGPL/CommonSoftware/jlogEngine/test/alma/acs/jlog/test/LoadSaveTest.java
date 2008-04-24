832ee15696a8e02b531e97ed8790b8d37ba43026
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2008-04-24 13:56:16 +0000

    Added a test for special formatted logs
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@88920 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jlogEngine/test/alma/acs/jlog/test/LoadSaveTest.java
+++ LGPL/CommonSoftware/jlogEngine/test/alma/acs/jlog/test/LoadSaveTest.java
@@ -21,7 +21,9 @@
  */
 package alma.acs.jlog.test;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.Collection;
 import java.util.Properties;
 import java.util.Vector;
@@ -45,6 +47,52 @@ import alma.acs.logging.engine.io.IOPorgressListener;
  *
  */
 public class LoadSaveTest extends TestCase implements IOPorgressListener, ACSRemoteLogListener, ACSRemoteErrorListener {
+	
+	// Some interesting logs to check 
+	// See COMP-2369 for log1
+	private final String log1 ="<Trace TimeStamp=\"2008-04-14T17:53:22.445\" SourceObject=\"CONTROL/ACC/cppContainer-GL\" "+
+	"File=\"Unavailable\" Line=\"0\" Routine=\"\" Host=\"gas01\" Process=\"CONTROL/ACC/cppContainer\" "+
+	"Context=\"\" Thread=\"CONTROL/Array002/DELAYSERVERServiceThread\">"+
+	"<![CDATA[AlarmSupplier::publishEvent()\n"+
+    "About to send XML of:\n"+
+    "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n"+
+    "<ASI-message xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" backup=\"false\" version=\"0.9\" xsi:type=\"ASI-message\">\n"+
+	"<source-name>ALARM_SYSTEM_SOURCES</source-name>\n"+
+	"<source-hostname>gas01</source-hostname>\n"+
+	"<source-timestamp seconds=\"1208195602\" microseconds=\"433703\"/>\n"+
+	"<fault-states>\n"+
+	"<fault-state family=\"DelayServer\" member=\"CONTROL/Array002\" code=\"1\">\n"+
+	"<descriptor>TERMINATE</descriptor>\n"+
+	"<user-timestamp seconds=\"1208195602\" microseconds=\"433103\"/>\n"+
+	"</fault-state>\n"+
+	"</fault-states>\n"+
+	"</ASI-message>\n"+
+	"]]></Trace>\n"; 
+		
+	private final String log2 = "<Warning TimeStamp=\"2005-12-13T15:09:12.041\" SourceObject=\"ARCHIVE_MASTER_COMP\" " +
+	"File=\"alma.ACS.MasterComponentImpl.MasterComponentImplBase\"  "+
+	"Line=\"164\" Routine=\"doTransition\" Host=\"gas\" Process=\"LoggerName: alma.component.ARCHIVE_MASTER_COMP\" "+
+	"Thread=\"RequestProcessor-10\" StackId=\"unknown\" StackLevel=\"0\" LogId=\"140\">"+
+	"<![CDATA[Illegal event.]]><Data Name=\"LoggedException\">alma.acs.genfw.runtime.sm.AcsStateIllegalEventException: illegal event 'initPass1' in state 'ONLINE'.\n"+
+	"at alma.ACS.MasterComponentImpl.statemachine.AlmaSubsystemContext.illegalEvent(AlmaSubsystemContext.java:248)\n"+
+    "at alma.ACS.MasterComponentImpl.statemachine.AvailableSubStateAbstract.initPass1(AvailableSubStateAbstract.java:30)\n"+
+    "at alma.ACS.MasterComponentImpl.statemachine.AvailableState.initPass1(AvailableState.java:60)\n"+
+    "at alma.ACS.MasterComponentImpl.statemachine.AlmaSubsystemContext.initPass1(AlmaSubsystemContext.java:135)\n"+
+    "at alma.ACS.MasterComponentImpl.MasterComponentImplBase.doTransition(MasterComponentImplBase.java:134)\n"+
+    "at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\n"+
+    "at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:39)\n"+
+    "at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:25)\n"+
+    "at java.lang.reflect.Method.invoke(Method.java:324)\n"+
+    "at alma.acs.container.ContainerSealant.invoke(ContainerSealant.java:134)\n"+
+    "at $Proxy0.doTransition(Unknown Source)\n"+
+    "at alma.archive.ArchiveSubsystemMasterIFPOATie.doTransition(ArchiveSubsystemMasterIFPOATie.java:63)\n"+
+    "at alma.archive.ArchiveSubsystemMasterIFPOA._invoke(ArchiveSubsystemMasterIFPOA.java:76)\n"+
+    "at org.jacorb.poa.RequestProcessor.invokeOperation(Unknown Source)\n"+
+    "at org.jacorb.poa.RequestProcessor.process(Unknown Source)\n"+
+    "at org.jacorb.poa.RequestProcessor.run(Unknown Source)\n"+
+    "</Data></Warning>";
+	
+	private String[] specialLogs = { log1, log2 };
 	
 	// The number of bytes read and written
 	private volatile long bytesRead, bytesWritten;
@@ -131,6 +179,9 @@ public class LoadSaveTest extends TestCase implements IOPorgressListener, ACSRem
 	@Override
 	protected void tearDown() throws Exception {
 		super.tearDown();
+		// Delete the file
+		File f = new File(fileName);
+		f.deleteOnExit();
 		logs.clear();
 		logs=null;
 		logsRead.clear();
@@ -196,5 +247,32 @@ public class LoadSaveTest extends TestCase implements IOPorgressListener, ACSRem
 	@Override
 	public void logsRead(int numOfLogs) {
 		numOfLogsRead=numOfLogs;
+	}
+	
+	/**
+	 * Load a set of special logs that could be interesting.
+	 * <P>
+	 * Initially this method opens an XML file and writes into it the special logs without using
+	 * <code>IOHelper</code>.
+	 * Then it tries to load the file containing the special logs
+	 * 
+	 * 
+	 * @throws Exception
+	 */
+	public void testLoadingSpecialLogs() throws Exception {
+		FileWriter f = new FileWriter(fileName,false);
+		f.write("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n<Log>\n<Header Name=\"NameForXmlDocument\" Type=\"LOGFILE\" />\n");
+		for (String xml: specialLogs) {
+			f.write(xml);
+		}
+		f.write("</Log>");
+		f.flush();
+		f.close();
+		
+		IOHelper ioHelper = new IOHelper();
+		assertNotNull(ioHelper);
+		
+		ioHelper.loadLogs(fileName, this, this, this);
+		assertEquals(specialLogs.length, logsRead.size());
 	}
 }
