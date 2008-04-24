4c2467d4137a5eb281e65afcc5d128b98fe20c23
jschwarz <jschwarz@523d945c-050c-4681-91ec-863ad3bb968a>
2008-04-24 16:20:23 +0000

    Added unit test that generates events on channel "blar".
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@88936 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/acsGUIs/eventGUI/test/alma/acs/eventbrowser/model/SupplierCompOperations.java
@@ -0,0 +1,17 @@
+package alma.acs.eventbrowser.model;
+
+	/**
+	 *	Generated from IDL interface "SupplierComp"
+	 *	@author JacORB IDL compiler V 2.2.3, 10-Dec-2005
+	 */
+
+
+	public interface SupplierCompOperations
+//		extends alma.ACS.ACSComponentOperations
+	{
+		/* constants */
+		/* operations  */
+		void sendEvents(short numEvents) throws alma.ACSErrTypeCommon.CouldntPerformActionEx;
+//		void sendEventsSpecial(alma.FRIDGE.FridgeControlPackage.NestedFridgeEvent[] eventData) throws alma.ACSErrTypeCommon.CouldntPerformActionEx;
+	}
+
