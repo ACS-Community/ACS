43e7501c22e061f9b816b398082dbf165d66e6aa
bjeram <bjeram@523d945c-050c-4681-91ec-863ad3bb968a>
2008-06-03 09:06:41 +0000

    added virtual destructor that gcc 4 does not complain
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@90858 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/logging/ws/include/loggingBaseLog.h
+++ LGPL/CommonSoftware/logging/ws/include/loggingBaseLog.h
@@ -18,7 +18,7 @@
 * License along with this library; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 *
-* "@(#) $Id: loggingBaseLog.h,v 1.12 2008/01/25 10:11:17 cparedes Exp $"
+* "@(#) $Id: loggingBaseLog.h,v 1.13 2008/06/03 09:06:41 bjeram Exp $"
 *
 * who       when      what
 * --------  --------  ----------------------------------------------
@@ -46,6 +46,8 @@ namespace Logging
     class BaseLog
     {
       public:
+
+	virtual ~BaseLog(){}
 
 	/**
 	 * This enumeration is intended to be the various logging levels for
