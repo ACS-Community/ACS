e48dafd132b3827c24582925f7e7be997c177e1c
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2008-06-05 13:12:21 +0000

    Uses Field instead of an integer
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@91021 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsGUIs/jlog/src/com/cosylab/logging/settings/FilterParameterPanel.java
+++ LGPL/CommonSoftware/acsGUIs/jlog/src/com/cosylab/logging/settings/FilterParameterPanel.java
@@ -26,87 +26,110 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 
 import com.cosylab.logging.engine.Filter;
+import com.cosylab.logging.engine.log.ILogEntry.Field;
+
 /**
- * Serves the purpose of filtering according to the parameter. Used by FieldClassChoser, FilterDataPanel, 
- * FilterIntegerPanel, FilterParameterPanel and FilterStringPanel.
- * Creation date: (2/7/02 11:35:17 AM)
- * @author: 
+ * Serves the purpose of filtering according to the parameter. Used by
+ * FieldClassChoser, FilterDataPanel, FilterIntegerPanel, FilterParameterPanel
+ * and FilterStringPanel. Creation date: (2/7/02 11:35:17 AM)
+ * 
+ * @author:
  */
 public abstract class FilterParameterPanel extends javax.swing.JPanel {
-	private int fieldIndex;
+	/**
+	 * The field filtered
+	 */
+	private Field field;
+
 	private boolean lethal;
-/**
- * FilterParameterPanel constructor comment.
- */
-public FilterParameterPanel() {
-	super(new GridBagLayout());
-	createComponents();
-}
-/**
- * Insert the method's description here.
- * Creation date: (2/7/02 11:39:51 AM)
- */
-protected abstract void createComponents();
-/**
- * Insert the method's description here.
- * Creation date: (2/7/02 11:53:41 AM)
- * @return int
- */
-public int getFieldIndex() {
-	return fieldIndex;
-}
-/**
- * Insert the method's description here.
- * Creation date: (2/7/02 11:38:47 AM)
- * @return com.cosylab.logging.engine.Filter
- * @exception com.cosylab.logging.engine.InvalidFilterConstraintException The exception description.
- */
-public abstract Filter getFilter() throws FilterParameterException;
-/**
- * Insert the method's description here.
- * Creation date: (2/7/02 11:53:41 AM)
- * @return boolean
- */
-public boolean isLethal() {
-	return lethal;
-}
-/**
- * Insert the method's description here.
- * Creation date: (2/7/02 10:45:18 AM)
- * @param y int
- * @param insetsTop int
- * @param insetsBottom int
- */
-protected GridBagConstraints newConstraints(int y, int insetsTop, int insetsLeft, int insetsBottom, int insetsRight) {
-	GridBagConstraints g = new GridBagConstraints();
-	g.gridx = 0;
-	g.gridy = y;
-	g.insets = new Insets(insetsTop, insetsLeft, insetsBottom, insetsRight);
-	g.fill = GridBagConstraints.HORIZONTAL;
-	g.weightx = 1.0;
-	g.weighty = 0.0;
-	return g;
-}
-/**
- * Insert the method's description here.
- * Creation date: (2/7/02 11:53:41 AM)
- * @param newFieldIndex int
- */
-public void setFieldIndex(int newFieldIndex) {
-	fieldIndex = newFieldIndex;
-}
-/**
- * Insert the method's description here.
- * Creation date: (2/7/02 11:37:19 AM)
- * @param f com.cosylab.logging.engine.Filter
- */
-public abstract void setFilter(Filter f);
-/**
- * Insert the method's description here.
- * Creation date: (2/7/02 11:53:41 AM)
- * @param newLethal boolean
- */
-public void setLethal(boolean newLethal) {
-	lethal = newLethal;
-}
+
+	/**
+	 * FilterParameterPanel constructor comment.
+	 */
+	public FilterParameterPanel() {
+		super(new GridBagLayout());
+		createComponents();
+	}
+
+	/**
+	 * Insert the method's description here. Creation date: (2/7/02 11:39:51 AM)
+	 */
+	protected abstract void createComponents();
+
+	/**
+	 * Insert the method's description here. Creation date: (2/7/02 11:53:41 AM)
+	 * 
+	 * @return int
+	 */
+	public Field getFieldIndex() {
+		return field;
+	}
+
+	/**
+	 * Insert the method's description here. Creation date: (2/7/02 11:38:47 AM)
+	 * 
+	 * @return com.cosylab.logging.engine.Filter
+	 * @exception com.cosylab.logging.engine.InvalidFilterConstraintException
+	 *                The exception description.
+	 */
+	public abstract Filter getFilter() throws FilterParameterException;
+
+	/**
+	 * Insert the method's description here. Creation date: (2/7/02 11:53:41 AM)
+	 * 
+	 * @return boolean
+	 */
+	public boolean isLethal() {
+		return lethal;
+	}
+
+	/**
+	 * Insert the method's description here. Creation date: (2/7/02 10:45:18 AM)
+	 * 
+	 * @param y
+	 *            int
+	 * @param insetsTop
+	 *            int
+	 * @param insetsBottom
+	 *            int
+	 */
+	protected GridBagConstraints newConstraints(int y, int insetsTop,
+			int insetsLeft, int insetsBottom, int insetsRight) {
+		GridBagConstraints g = new GridBagConstraints();
+		g.gridx = 0;
+		g.gridy = y;
+		g.insets = new Insets(insetsTop, insetsLeft, insetsBottom, insetsRight);
+		g.fill = GridBagConstraints.HORIZONTAL;
+		g.weightx = 1.0;
+		g.weighty = 0.0;
+		return g;
+	}
+
+	/**
+	 * Insert the method's description here. Creation date: (2/7/02 11:53:41 AM)
+	 * 
+	 * @param newFieldIndex
+	 *            int
+	 */
+	public void setFieldIndex(Field newField) {
+		field = newField;
+	}
+
+	/**
+	 * Insert the method's description here. Creation date: (2/7/02 11:37:19 AM)
+	 * 
+	 * @param f
+	 *            com.cosylab.logging.engine.Filter
+	 */
+	public abstract void setFilter(Filter f);
+
+	/**
+	 * Insert the method's description here. Creation date: (2/7/02 11:53:41 AM)
+	 * 
+	 * @param newLethal
+	 *            boolean
+	 */
+	public void setLethal(boolean newLethal) {
+		lethal = newLethal;
+	}
 }
