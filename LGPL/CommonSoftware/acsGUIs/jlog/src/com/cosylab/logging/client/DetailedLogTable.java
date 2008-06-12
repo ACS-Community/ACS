9fd9d2ed10c824791e64c2ea43f8c281c53ea14e
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2008-06-12 12:21:32 +0000

    The log type is displayed with the renderer in order to show the icon.
    Each cell is displayed with a renderer that sets also the height of the table cell in order to show multi-line entries.
    Note that the calculation of the height is not perfect and usually is a little bit greater then desired (I think it is ok as it is; I have found other way to do that on the net but all of them seem to work only up to jdk 1.3!).
    
    The toottip is multiline with a max width of 96 chars.
    
    
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@91466 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsGUIs/jlog/src/com/cosylab/logging/client/DetailedLogTable.java
+++ LGPL/CommonSoftware/acsGUIs/jlog/src/com/cosylab/logging/client/DetailedLogTable.java
@@ -7,19 +7,22 @@
 package com.cosylab.logging.client;
 
 import java.awt.Component;
+import java.awt.FontMetrics;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Vector;
 
 import javax.swing.JComponent;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableCellRenderer;
-import javax.swing.table.TableModel;
 
 import com.cosylab.logging.engine.log.ILogEntry;
+import com.cosylab.logging.engine.log.LogTypeHelper;
 import com.cosylab.logging.engine.log.ILogEntry.Field;
 
+import alma.acs.logging.table.renderer.EntryTypeRenderer;
 import alma.acs.util.IsoDateFormat;
 
 /**
@@ -30,31 +33,140 @@ import alma.acs.util.IsoDateFormat;
  *
  */
 public class DetailedLogTable extends JTable
-{
-	int rowsNum; // The rows in the table
-	String[][] nameValue; // The pairs <name,value>
-	private String NOT_AVAILABLE="N/A";
+{	
 	
-	private TableModel dataModel = new AbstractTableModel() { 
+	/**
+	 * The string shown when the dialog is not displaying a log
+	 */
+	private final String NOT_AVAILABLE="";
+	
+	public class MultilineRenderer extends JTextArea implements
+			TableCellRenderer {
 		
+		public MultilineRenderer() {
+			super (100,20);
+			setWrapStyleWord(true);
+			setLineWrap(true);
+		}
+
+		public Component getTableCellRendererComponent(
+				//
+				JTable table, Object obj, boolean isSelected, boolean hasFocus,
+				int row, int column) {
+
+			setText(obj.toString());
+			int newSize = normalRowHeight*formatText(obj.toString());
+			if (DetailedLogTable.this.getRowHeight(row)!=newSize) {
+				DetailedLogTable.this.setRowHeight(row, newSize);
+			}
+			FontMetrics fm = this.getFontMetrics(getFont());	
+			return this;
+		}
+
+		/**
+		 * Calculate the number of lines to display the string in the
+		 * renderer.
+		 * <P>
+		 * Setting the preferred size worked with jdk 1.3 but
+		 * now does not and I did not find anything on the net so I have
+		 * to find a solution by myself.
+		 * <P>
+		 * The calculation is not 100% perfect but seems to work pretty well.
+		 * In future we should think of changing this strategy with something
+		 * better..
+		 * 
+		 * @param str The string to display into the renderer
+		 * @return The number of lines (probably) needed to display the
+		 *         string into the text area. 
+		 */
+		private int formatText(String str) {
+			if (str==null || str.length()==0) {
+				return 1;
+			}
+			FontMetrics fm = this.getFontMetrics(getFont());
+			int pixels = fm.stringWidth(str);
+			if (pixels<getSize().width) {
+				return 1;
+			}
+			return pixels/getPreferredSize().width+1;
+		}
+
+	}
+
+	
+	/**
+	 * The model for this table.
+	 * 
+	 * @author acaproni
+	 * 
+	 */
+	public class DetailedTableModel extends AbstractTableModel {
+		/**
+		 * An object of <code>LogTypeHelper</code> is needed in order to have 
+		 * <code>EntryTypeRenderer</code> working properly.
+		 */
+		public LogTypeHelper logType=null;
+		
+		@Override
 		public int getColumnCount()
 		{
 			return 2;
 		}
+		
+		@Override
 		public int getRowCount()
 		{
 			return rowsNum;
 		}
+		
+		@Override
 		public Object getValueAt(int row, int col)
 		{
+			if (col==1 && row==Field.ENTRYTYPE.ordinal()) {
+				return logType;
+			}
 			return nameValue[row][col];
 		}
-	};
+	}
+	
+	/**
+	 * The table model
+	 */
+	private DetailedTableModel dataModel = new DetailedTableModel();
+	
+	/**
+	 * The rows in the table
+	 */
+	private int rowsNum;
+	
+	/**
+	 * The height of a row with only one line of text
+	 */
+	private int normalRowHeight;
+	
+	/**
+	 * The pairs <name,value> i.e. all the values displayed in the table.
+	 * <P>
+	 * The first column contains the title of each cell in HTML format;
+	 * the second column contains the value displayed in the right column of the
+	 * table in plain text.
+	 * The renderes will take such a string and format to be properly displayed,
+	 * with the exception of the log type that is stored into the
+	 * <code>DetailedTableModel</code> as a <code>LogTypeHelper</code>.
+	 */
+	private String[][] nameValue;
+	
+	/**
+	 * The multiline renderer for the log message
+	 */
+	private MultilineRenderer logMessageRenderer = new MultilineRenderer();
+	
+	private EntryTypeRenderer entryTypeRenderer = new EntryTypeRenderer(true);
 
 	/**
-	 * Build a table using the dats in the log entry
+	 * Build a table using the data in the log entry
 	 * 
-	 * @param log The logEntry with the datas to display 
+	 * @param log The logEntry with the data to display 
 	 */
 	public DetailedLogTable() {
 		super();
@@ -62,14 +174,16 @@ public class DetailedLogTable extends JTable
 		setRowSelectionAllowed(false);
         setColumnSelectionAllowed(false);
         setCellSelectionEnabled(false);
-        setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
         
 		getColumnModel().getColumn(0).setHeaderValue("Field");
 		getColumnModel().getColumn(1).setHeaderValue("Value");
 		
 		getColumnModel().getColumn(0).setMinWidth(110);
-		getColumnModel().getColumn(1).setMinWidth(10000);
+		getColumnModel().getColumn(0).setMaxWidth(250);
+		getColumnModel().getColumn(1).setMinWidth(250);
 		setEmptyContent();
+		normalRowHeight=getRowHeight();
 	}
 	
 	/**
@@ -84,13 +198,12 @@ public class DetailedLogTable extends JTable
 			nameValue[i][1]=NOT_AVAILABLE;
 		}
 		
-		getTableHeader().resizeAndRepaint();
-		resizeAndRepaint();
+		dataModel.fireTableDataChanged();
 	}
 	
 	/**
-	 * Fill the table with the fields of the given log
-	 *  
+	 * Fill the table with the fields of the given log.
+	 * 
 	 * @param log The log whose content is shown in the table
 	 */
 	public void setupContent(ILogEntry log) {
@@ -115,6 +228,7 @@ public class DetailedLogTable extends JTable
 				if (obj!=null) {
 					if (field==Field.ENTRYTYPE) {
 						nameValue[i][1]=obj.toString();
+						dataModel.logType=(LogTypeHelper)obj;
 					} else if (field==Field.TIMESTAMP) {
 						SimpleDateFormat df = new IsoDateFormat();
 						Date dt = new Date((Long)obj);
@@ -131,31 +245,27 @@ public class DetailedLogTable extends JTable
 			}
 			for (int i=Field.values().length; i < rowsNum; i++) {
 				nameValue[i][0] = "<HTML><B>Additional</B> <I>"+additionalData.get(i-Field.values().length).getName()+"</I>";
-				nameValue[i][1] = additionalData.get(i-Field.values().length).getValue();
+				String temp = new String ((String)additionalData.get(i-Field.values().length).getValue());
+				nameValue[i][1] = temp;
 			}
-
-			// Change the name of the columns
-			getColumnModel().getColumn(0).setHeaderValue("Field");
-			getColumnModel().getColumn(1).setHeaderValue("Value");
-
-			// Force the header to resize and repaint itself
 			
-			getTableHeader().resizeAndRepaint();
-			resizeAndRepaint();
+			dataModel.fireTableDataChanged();
 		}
 	}
 	
 	/**
-	 * Sets a tool tip on all the cells. It pops up when the value is not fully displayed while the mose 
-	 * scrolls over it. 
+	 * Sets a tool tip on all the cells. It pops up when the value is not fully displayed while 
+	 * the mouse scrolls over it.
+	 *  
 	 * @see javax.swing.JTable#prepareRenderer(TableCellRenderer, int, int)
 	 */
 	public Component prepareRenderer(TableCellRenderer renderer, int rowIndex, int vColIndex)
 	{
 		Component c = super.prepareRenderer(renderer, rowIndex, vColIndex);
 
-		int columnWidth = getColumnModel().getColumn(vColIndex).getWidth() / 6;
-		setToolTip((JComponent)c,nameValue[rowIndex][vColIndex].toString(),columnWidth);
+		if (vColIndex==1) {
+			setToolTip((JComponent)c,nameValue[rowIndex][vColIndex].toString(),96);
+		}
 		return c;
 	}
 	
@@ -164,29 +274,66 @@ public class DetailedLogTable extends JTable
 	 * The tooltip is shown only if the text is not visible (i.e. the num.
 	 * of displayed chars for the column containing the text is less then
 	 * the given text).
-	 * 
-	 * To show the string as multine it is transformed in HTML so
-	 * \n are replaced by <BR>/ To show strings containing HTML and/or
-	 * XML the <PRE> tag is used (for this reason existing < and > in
-	 * the original string are replaced by &lt; and &lgt;)
+	 * <P>
+	 * To show the string as multi-line it is transformed in HTML and
+	 * <code>\n</code> are replaced by <code>&lt;BR&gt;</code>. 
+	 * To show strings containing HTML and/or XML the <code>&lt;PRE&gt;</code> tag is used 
+	 * (for this reason existing &lt; and &gt; in the original string are replaced by &lt; and &gt;)
 	 * 
 	 * @param c The component to set the tooltip 
 	 * @param text The string to display in the tooltip
-	 * @param colWidth The width of the column
-	 * 
-	 * @return
+	 * @param colWidth The max number of chars for each line of the tooltip
 	 */
 	private void setToolTip(JComponent  c, String text, int colWidth) {
-		if (text==null)	{
+		if (text==null || text.length()==0)	{
 			c.setToolTipText(null);
-		} else if (text.length() <colWidth) {
-			c.setToolTipText(null);
+			return;
+		}
+		// Insert the 'new line' if text is longer then colWidth
+		StringBuilder str = new StringBuilder();
+		String toolTip;
+		if (text.length()>colWidth) {
+			int count=0;
+			
+			for (int t=0; t<text.length(); t++) {
+				if (++count>=colWidth) {
+					count=0;
+					str.append('\n');
+				}
+				char ch = text.charAt(t);
+				str.append(ch);
+				if (ch=='\n') {
+					count=0;
+				} else {
+					count++;
+				}
+			}
+			toolTip=str.toString();
 		} else {
-			// I am going to print the string in HTML format: new lines become <BR>
-			text=text.replaceAll("<","&lt;");
-			text=text.replaceAll(">","&gt;");
-			// Eventually, set the tooltip
-			c.setToolTipText("<HTML><PRE>"+text+"</PRE></HTML>");
+			toolTip=text;
+		}
+		System.out.println("TEXT: "+text+"\n>>>"+toolTip);
+		// Format the string as HTML
+		toolTip=toolTip.replaceAll("<","&lt;");
+		toolTip=toolTip.replaceAll(">","&gt;");
+		toolTip=toolTip.replaceAll("\n", "<BR>");
+		// Eventually, set the tooltip
+		c.setToolTipText("<HTML><PRE>"+toolTip+"</PRE></HTML>");
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.JTable#getCellRenderer(int, int)
+	 */
+	@Override
+	public TableCellRenderer getCellRenderer(int row, int column) {
+		if (column==0) {
+			return super.getCellRenderer(row, column);
+		} else {
+			if (row!=Field.ENTRYTYPE.ordinal()) {
+				return logMessageRenderer;
+			} else {
+				return entryTypeRenderer;
+			}
 		}
 	}
 
