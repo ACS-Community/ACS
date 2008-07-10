c62e3d8a223918e7ce50568a182f6c5ab8023f60
bjeram <bjeram@523d945c-050c-4681-91ec-863ad3bb968a>
2008-07-10 10:00:37 +0000

    Replaced non portable comparing with _nil() with CORBA::_nil(x)
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@92720 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsexmpl/ws/src/acsexmplAsyncCalls.cpp
+++ LGPL/CommonSoftware/acsexmpl/ws/src/acsexmplAsyncCalls.cpp
@@ -1,6 +1,6 @@
 /*******************************************************************************
 * ALMA - Atacama Large Millimiter Array
-* (c) European Southern Observatory, 2004 
+* (c) European Southern Observatory, 2004
 *
 *This library is free software; you can redistribute it and/or
 *modify it under the terms of the GNU Lesser General Public
@@ -16,11 +16,11 @@
 *License along with this library; if not, write to the Free Software
 *Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 *
-* "@(#) $Id: acsexmplAsyncCalls.cpp,v 1.7 2007/06/15 07:47:22 nbarriga Exp $"
+* "@(#) $Id: acsexmplAsyncCalls.cpp,v 1.8 2008/07/10 10:00:37 bjeram Exp $"
 *
 * who       when      what
 * --------  --------  ----------------------------------------------
-* acaproni  2004-08-09  created 
+* acaproni  2004-08-09  created
 */
 
 /** @file ACSEXMPLCLIENTASYNCCALLSDOC acsexmplAsynCalls
@@ -47,7 +47,7 @@
  It access a MOUNT component to move the antenna to a definite position.
  The movement is asynchronous
  <br>
-  
+
  <h2>What can I gain from this example?</h2>
  <ul>
   	<li>Simple client usage</li>
@@ -70,7 +70,7 @@
 #include <iostream>
 #include <string>
 
-static char *rcsId="@(#) $Id: acsexmplAsyncCalls.cpp,v 1.7 2007/06/15 07:47:22 nbarriga Exp $"; 
+static char *rcsId="@(#) $Id: acsexmplAsyncCalls.cpp,v 1.8 2008/07/10 10:00:37 bjeram Exp $";
 static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
 
 using namespace maci;
@@ -85,49 +85,49 @@ typedef struct threadParamStruct {
 /**
  * The thread to read the actual azimuth and elevation
  * The reads are synchronous
- * 
+ *
  */
-static void 
+static void
 worker(void *threadParam_p) {
 
 	if (threadParam_p==NULL) {
 		ACS_SHORT_LOG((LM_ERROR,"Invalid parameter for thread!"));
 		return;
 	}
-	
+
 	//Get access to the BACI thread this function is being executed from
     BACIThreadParameter *baciParameter_p = static_cast<BACIThreadParameter *>(threadParam_p);
     BACIThread *myself_p = baciParameter_p->getBACIThread();
-    
+
 	// Init the thread
     if (BACIThread::InitThread != 0)  {
 		BACIThread::InitThread("Position thread");
 	}
-    
+
     ACS_SHORT_LOG((LM_INFO,"Thread started with name %s",myself_p->getName().c_str()));
-    
+
     // Get the struct from the parameter
     ThreadParamStruct* param=(ThreadParamStruct*)baciParameter_p->getParameter();
 
     // Get the reference to the actAz property of the mount
     ACS::ROdouble_var actAz = param->mount->actAz();
-    if (actAz.ptr()==ACS::ROdouble::_nil()) {
+    if (CORBA::is_nil(actAz)) {
     	ACS_SHORT_LOG((LM_ERROR,"ERROR getting actAz"));
     	return;
     }
-    
+
     // Get the reference to the actEl property of the mount
     ACS::ROdouble_var actEl = param->mount->actEl();
-    if (actEl.ptr()==ACS::ROdouble::_nil()) {
+    if (CORBA::is_nil(actEl)) {
     	ACS_SHORT_LOG((LM_ERROR,"ERROR getting actEl"));
     	return;
     }
- 	
+
     // The az (azimuth) and el (elevation) are declared here
     // but they are both updated in the callback
 	double az,el;
 	ACSErr::Completion_var completion;
-	
+
 	// Create the structs for the async calls to read the values of az
 	AsyncCBdouble azCallback("actAz",&az);
 	ACS::CBdouble_var actAz_CB = azCallback._this();
@@ -136,26 +136,26 @@ worker(void *threadParam_p) {
 	AsyncCBdouble elCallback("actEl",&el);
 	ACS::CBdouble_var actEl_CB = elCallback._this();
 	ACS::CBDescIn actElDescIn;
-	
+
 	// Create the callback for the objfix method
 	AsyncMethodCBvoid objfixCB("objfix");
 	ACS::CBvoid_var objfix_CB = objfixCB._this();
 	ACS::CBDescIn objfixDescIn;
-	
+
 	// Times handshaking is not yet implemented so
 	// we do not really need to fill these fields
 	//objfixDescIn.normal_timeout=10000000;
 	//objfixDescIn.negotiable_timeout=5000000;
-	
+
 	CORBA::Double newAz=(CORBA::Double)param->az;
 	CORBA::Double newEl=(CORBA::Double)param->el;
-	
+
 	ACS_SHORT_LOG((LM_INFO,"Calling the asynchronous objfix"))
-	
+
 	param->mount->objfix(newAz,newEl,objfix_CB.in(),objfixDescIn);
 
 	while(myself_p->check() == true) {
-		
+
 		if(myself_p->isSuspended() == false) {
 			ACS_SHORT_LOG((LM_INFO,"Calling the async methods to read Az and El"))
 			actAz->get_async(actAz_CB.in(),actAzDescIn);
@@ -163,44 +163,44 @@ worker(void *threadParam_p) {
 		} else {
 			myself_p->sleep();
 		}
-		
+
 		sleep(1);
 	}
-	
+
 	ACS_SHORT_LOG((LM_INFO,"Exiting thread"))
 	sleep(5);
 	if (BACIThread::DoneThread != 0) {
 		BACIThread::DoneThread();
 	}
-	
+
     delete baciParameter_p;
     myself_p->setStopped();
 }
 
 /** @cond
-*/    
-int main(int argc, char* argv[]) 
+*/
+int main(int argc, char* argv[])
 {
     MOUNT_ACS::Mount_var mount;
     BACIThreadManager threadManager;   // The thread manager is used to manage the thread
     ThreadParamStruct param; // The parameter for the thread
-    
+
     // Check the arguments in the command line
 	if (argc<2) {
 		std::cerr<<"Usage: "<<argv[0]<<" component azimuth elevation <options>\n";
 		return -1;
 	}
-	
+
 	double destAz, destEl;
 	if (sscanf(argv[2],"%lf",&destAz)+sscanf(argv[3],"%lf",&destEl)!=2) {
 		std::cerr<<"Format error in azimuth and/or elevation"<<std::endl;
 		std::cerr<<"Usage: "<<argv[0]<<" component azimuth elevation <options>\n";
 		return -1;
 	}
-	
+
 	// Create the SimpleClient object
 	SimpleClient mountClient;
-	
+
 	// Init the client
 	if (mountClient.init(argc,argv)==0) {
 		// Error initing
@@ -209,7 +209,7 @@ int main(int argc, char* argv[])
 	} else {
 		ACS_SHORT_LOG((LM_INFO,"SimpleClient built"));
 	}
-	
+
 	// Login the client
 	if (mountClient.login()==0) {
 		//Error
@@ -234,7 +234,7 @@ int main(int argc, char* argv[])
 	param.mount=mount.ptr();
 	param.az=destAz;
 	param.el=destEl;
-	
+
 	char logStr[128];
 	sprintf(logStr,"Commanded position az=%lf, el=%lf",destAz,destEl);
 	ACS_SHORT_LOG((LM_INFO,logStr));
@@ -246,23 +246,23 @@ int main(int argc, char* argv[])
 		static_cast<void *>(&param));    //the single parameter
 
 	if (thread_p==NULL)  {
-		ACS_SHORT_LOG((LM_ERROR,"Error in spawning thread")); 
+		ACS_SHORT_LOG((LM_ERROR,"Error in spawning thread"));
 	}
         thread_p->resume();
-	
+
 	// The thread will run for 30 secs so we wait until it finishes
 	// There are better (and safer) solution to wait instead of a simple wait
 	// but... this is the faster one!
 	sleep(40);
-	
+
 	// End of computation: begin to clean up
-	
+
 	// Stop all the threads
 	ACS_SHORT_LOG((LM_INFO,"Terminating the thread"));
 	threadManager.terminateAll();
-	
+
 	// Release the component
-	try 
+	try
 	    {
 	    ACS_SHORT_LOG((LM_INFO,"Releasing %s",argv[1]));
 	    mountClient.releaseComponent(argv[1]);
@@ -272,26 +272,26 @@ int main(int argc, char* argv[])
 	    _ex.log();
 	    return -1;
 	    }
-	
+
 	// logout the client
-	try 
+	try
 	    {
 	    ACS_SHORT_LOG((LM_INFO,"Logging out"));
 	    mountClient.logout();
-	    } 
-	catch (...) 
+	    }
+	catch (...)
 	    {
 	    ACS_SHORT_LOG((LM_ERROR,"Error logging out the simple client object"));
-	    ACSErrTypeCommon::UnexpectedExceptionExImpl uex(__FILE__, __LINE__, 
+	    ACSErrTypeCommon::UnexpectedExceptionExImpl uex(__FILE__, __LINE__,
 							    "main");
 	    uex.log();
 	    return -1;
 	    }//try-catch
 
 	ACS_SHORT_LOG((LM_INFO,"Done"));
-	
+
 	return 0;
 }
 /** @endcond
-*/   
- 
+*/
+
