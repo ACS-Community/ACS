3c276333e8e49ca8d48177dbff19fcc94141e39a
bjeram <bjeram@523d945c-050c-4681-91ec-863ad3bb968a>
2008-08-06 10:28:32 +0000

    modified test that it does not use cout but logging system to rpint out messages
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@94386 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsthread/ws/test/testACSThreadJoinableStress.cpp
+++ LGPL/CommonSoftware/acsthread/ws/test/testACSThreadJoinableStress.cpp
@@ -5,24 +5,24 @@ class testJoinableThread : public ACS::Thread {
 public:
     testJoinableThread(const ACE_CString& name, const ACS::TimeInterval& responseTime=ThreadBase::defaultResponseTime,
 		       const ACS::TimeInterval& sleepTime=ThreadBase::defaultSleepTime, bool del=false) :
-	ACS::Thread(name, responseTime*10, sleepTime*50, del, THR_NEW_LWP| THR_JOINABLE) 
+	ACS::Thread(name, responseTime*10, sleepTime*50, del, THR_NEW_LWP| THR_JOINABLE)
 	{
 	    ACS_TRACE("testJoinableThread::testJoinableThread");
 	}
-	
-    ~testJoinableThread() 
+
+    ~testJoinableThread()
 	{
 	    ACS_TRACE("testJoinableThread::~testJoinableThread");
 	}
 
-    virtual void run() 
+    virtual void run()
 	{
-	    std::cout << " run()" << std::endl;
+    	ACS_SHORT_LOG(( LM_INFO, " run()"));
 	    sleep();
 	}
 };
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
 
     int numThread = atoi(argv[1]);
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
     LoggingProxy::init(&logger_m);
 
 
-    for (int i=0; i<numThread; i++) 
+    for (int i=0; i<numThread; i++)
 	{
 	std::stringstream str_thread;
 	str_thread << "thread " << i;
@@ -43,11 +43,11 @@ int main(int argc, char *argv[])
 	threadId[i] = threads[i]->getThreadID();
 	}//for
     ACS_SHORT_LOG((LM_INFO, "# of threads in the thread manager: %d\n", tm.getThreadCount()));
-    for (int i=0; i<numThread; i++) 
+    for (int i=0; i<numThread; i++)
 	{
 	ACS_SHORT_LOG((LM_INFO, "joining thread # %d\n", i));
 	int st = tm.join(threadId[i]);
-	
+
 	if (st==-1)cout<<"thread "<<i<<" join fails"<<endl;
 	ACS_SHORT_LOG((LM_INFO, "=> thread # %d joint with ret: %d\n", i , st));
 	delete threads[i];
