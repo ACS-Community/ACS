94563044f522c52230ca76fdb10ab18a989681f6
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2008-09-08 09:21:52 +0000

    first check-in
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@95761 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/jcont/test/alma/jconttest/DummyComponent2Impl/DummyComponent2Helper.java
@@ -0,0 +1,83 @@
+/*
+ *    ALMA - Atacama Large Millimiter Array
+ *    (c) European Southern Observatory, 2002
+ *    Copyright by ESO (in the framework of the ALMA collaboration),
+ *    All rights reserved
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *    MA 02111-1307  USA
+ */
+
+package alma.jconttest.DummyComponent2Impl;
+
+import java.util.logging.Logger;
+
+import org.omg.PortableServer.Servant;
+import alma.ACS.ACSComponentOperations;
+import alma.acs.component.ComponentLifecycle;
+import alma.acs.container.ComponentHelper;
+import alma.jconttest.DummyComponent2Operations;
+import alma.jconttest.DummyComponent2POATie;
+import alma.jconttest.DummyComponent2Impl.DummyComponent2Impl;
+
+/**
+ * Component helper class. 
+ * Generated for convenience, but can be modified by the component developer. 
+ * Must therefore be treated like any other Java class (CVS, ...). 
+ * <p>
+ * To create an entry for your component in the Configuration Database, 
+ * copy the line below into a new entry in the file $ACS_CDB/MACI/Components/Components.xml 
+ * and modify the instance name of the component and the container: 
+ * <p>
+ * Name="DUMMYCOMPONENT2_1" Code="alma.jconttest.DummyComponent2Impl.DummyComponent2Helper" Type="IDL:alma/jconttest/DummyComponent2:1.0" Container="frodoContainer"
+ * <p>
+ * @author alma-component-helper-generator-tool
+ */
+public class DummyComponent2Helper extends ComponentHelper
+{
+	/**
+	 * Constructor
+	 * @param containerLogger logger used only by the parent class.
+	 */
+	public DummyComponent2Helper(Logger containerLogger)
+	{
+		super(containerLogger);
+	}
+
+	/**
+	* @see alma.acs.container.ComponentHelper#_createComponentImpl()
+	*/
+	protected ComponentLifecycle _createComponentImpl()
+	{
+		return new DummyComponent2Impl();
+	}
+
+	/**
+	* @see alma.acs.container.ComponentHelper#_getPOATieClass()
+	*/
+	protected Class<? extends Servant> _getPOATieClass()
+	{
+		return DummyComponent2POATie.class;
+	}
+
+	/**
+	* @see alma.acs.container.ComponentHelper#getOperationsInterface()
+	*/
+	protected Class<? extends ACSComponentOperations> _getOperationsInterface()
+	{
+		return DummyComponent2Operations.class;
+	}
+
+}
---
+++ LGPL/CommonSoftware/jcont/test/alma/jconttest/DummyComponent2Impl/DummyComponent2Impl.java
@@ -0,0 +1,9 @@
+package alma.jconttest.DummyComponent2Impl;
+
+import alma.jconttest.DummyComponentImpl.DummyComponentImpl;
+import alma.jconttest.DummyComponent2Operations;
+
+public class DummyComponent2Impl extends DummyComponentImpl implements DummyComponent2Operations
+{
+
+}
