efbfa81226c6e178cbeda1b1eea8511ec8ca2d16
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2008-09-12 15:07:14 +0000

    Added a method to get the hexadecimal representation of the entry. Added a constructor starting from an hexadecimal string too.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@96039 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jlogEngine/src/com/cosylab/logging/engine/cache/CacheEntry.java
+++ LGPL/CommonSoftware/jlogEngine/src/com/cosylab/logging/engine/cache/CacheEntry.java
@@ -33,10 +33,22 @@ package com.cosylab.logging.engine.cache;
  *
  */
 public class CacheEntry {
+	
+	/**
+	 * The length of the bytes representing each entry.
+	 * <P>
+	 * This number is useful while caching the entries on a file.
+	 * <P>
+	 * 40 is 8 for the integer and 2 time 16 for the longs.
+	 * 
+	 * @see toBytes()
+	 */
+	public static final int ENTRY_LENGTH = 40; 
+	
 	/**
 	 * The key of the file where the entry is stored
 	 */
-	public final Integer key;
+	public final int key;
 	/**
 	 * The starting position of the entry in the file
 	 */
@@ -54,14 +66,51 @@ public class CacheEntry {
 	 * @param endPos The ending position of the entry in the file
 	 */
 	public CacheEntry(Integer key, long startPos, long endPos) {
-		if (key==null) {
-			throw new IllegalArgumentException("The file name can't be null nor empty");
+		if (key==null || key<0) {
+			throw new IllegalArgumentException("The key must be not null and greater or equal to 0");
 		}
 		if (startPos<0 || endPos<=0 || startPos>=endPos) {
 			throw new IllegalArgumentException("Invalid start/end positions: "+startPos+", "+endPos);
 		}
-		this.key=key;
+		this.key=key.intValue();
 		start=startPos;
 		end=endPos;
+	}
+	
+	/**
+	 * Constructor.
+	 * <P>
+	 * Build the entry by its hexadecimal representation
+	 * 
+	 * @param hexadecimal
+	 * 
+	 * @see toHexadecimal()
+	 */
+	public CacheEntry(String hexadecimal) {
+		key=Integer.decode("0X"+hexadecimal.substring(0, 8));
+		start=Long.decode("0X"+hexadecimal.substring(8, 24));
+		end=Long.decode("0X"+hexadecimal.substring(24, 40));
+	}
+	
+	/**
+	 * Translate the content of the entry in an hexadecimal string composed
+	 * of the key, the start and end.
+	 * <P>
+	 * The array returned by this method can be written on a file.
+	 * Its length is constant for each possible entry so the fields are not
+	 * separated.
+	 * 
+	 * @return An hexadecimal string representing the content of this entry
+	 */
+	public String toHexadecimal() {
+		StringBuilder str = new StringBuilder();
+		String hexKey=String.format("%08X",key);
+		String hexStart=String.format("%016X",start);
+		String hexEnd = String.format("%016X",end);
+		str.append(hexKey);
+		str.append(hexStart);
+		str.append(hexEnd);
+		
+		return str.toString();
 	}
 }
