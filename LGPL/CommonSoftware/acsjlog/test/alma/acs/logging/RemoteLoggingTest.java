6dc0b4a767c7fb00929f1bd7d78fccd018fd2892
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2008-10-08 13:32:17 +0000

    - Tweaked the combinations of simulated network delay and shutdown delay, to hopefully get more deterministic results in this intrinsically indeterministic test. - Added javadoc
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@97002 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsjlog/test/alma/acs/logging/RemoteLoggingTest.java
+++ LGPL/CommonSoftware/acsjlog/test/alma/acs/logging/RemoteLoggingTest.java
@@ -49,14 +49,35 @@ public class RemoteLoggingTest extends TestCase
 	}
 	
 	
+	/**
+	 * Tests the situation in which the ClientLogManager is shut down before it has finished the 
+	 * asynchronous <code>initRemoteLogging</code> call.
+	 * This can happen in a <code>ComponentClient</code>-based application that performs a short task.
+	 * <p>
+	 * It is important to make the stdout printing part of this test,
+	 * e.g. by using TAT without output suppression tricks,
+	 * because among other things we expect the string
+	 * <code>Will abort ClientLogManager#initRemoteLogging because remote logging seems no longer needed.</code>
+	 * when initRemoteLogging is interrupted by a shutdown.
+	 * <p>
+	 * The main test thread that logs some messages and the thread that calls initRemoteLogging 
+	 * compete for the internal lock {@link ClientLogManager#logQueueLock}, whose functioning is being tested here.
+	 * <p>
+	 * <b>Unfortunately this cannot be totally deterministic, so we must accept occasional failures
+	 * of the kind that the output contains "Remote log: <Info .../>" instead of "Will abort ..." strings, or vice versa.</b>
+	 */
 	public void testConcurrentRemoteInitAndStop() throws InterruptedException {
 		
 		DaemonThreadFactory dtf = new DaemonThreadFactory(getName());
 
-		// we try this out for different delays
-		int[] delays = {1, 20, 100, 500};
-		for (int networkDelay : delays) {
-			for (int shutdownDelay : delays) {
+		// we try this out for different simulated network delays and shutdown delays
+		int[] networkDelays = {0, 2, 18, 22, 100}; // @TODO chose values that give deterministic results on most test machines.
+		int[] shutdownDelays = {1, 20, 102}; // @TODO chose values that give deterministic results on most test machines.
+
+		for (int networkDelay : networkDelays) {
+			// shutdown delay is the time in milliseconds between the last log and the call to clientlogmanager#shutdown,
+			// with the additional arrangement that initRemoteLogging was running when the last log was produced.
+			for (int shutdownDelay : shutdownDelays) {
 				System.out.println("\n*** Network delay = " + networkDelay + " and shutdown delay = " + shutdownDelay + " ***");
 				
 				CountDownLatch syncOnPrepareRemoteLogging = new CountDownLatch(1);
@@ -68,7 +89,7 @@ public class RemoteLoggingTest extends TestCase
 
 				// log something before we init the remote logging:
 				logger.info("A healthy info log before initRemoteLogging ("+networkDelay+"/"+shutdownDelay+")");
-				// Thread.sleep(2); // to keep these two logs in order, which makes manual ref file comparison easier. 
+//				Thread.sleep(2); // to keep these two logs in order, which makes manual ref file comparison easier. 
 				logger.fine("now that was a fine log before initRemoteLogging ("+networkDelay+"/"+shutdownDelay+")");
 				
 				clm.setDelayMillis(networkDelay); // simulated network delay for initRemoteLogging-getLogService and write_records
@@ -80,10 +101,9 @@ public class RemoteLoggingTest extends TestCase
 				});
 				initRemoteLoggingThread.start();
 				// wait until this thread is actually running, which we check via notification from the ClientLogManager#prepareRemoteLogging method
-				syncOnPrepareRemoteLogging.await(10, TimeUnit.SECONDS); // timeout should never apply, just used to stop the test if it gets messed up. 
+				assertTrue("initRemoteLogging should have called prepareRemoteLogging by now...", syncOnPrepareRemoteLogging.await(10, TimeUnit.SECONDS)); // timeout should never apply, just used to stop the test if it gets messed up. 
 				
 				logger.info("Another info log after initRemoteLogging ("+networkDelay+"/"+shutdownDelay+")");
-//				logger.fine("And also a fine log after initRemoteLogging ("+networkDelay+"/"+shutdownDelay+")");
 				
 				// depending on the values of networkDelay and shutdownDelay, we may be calling shutdown while 
 				// our ClientLogManager is still delivering the log messages.
@@ -91,16 +111,26 @@ public class RemoteLoggingTest extends TestCase
 				clm.shutdown(true);
 				
 				// wait for the thread that called initRemoteLogging
-				initRemoteLoggingThread.join();
+				initRemoteLoggingThread.join(10000);
 				
 				// wait a bit more for the mock log dispatcher to print out its xml log record
 				Thread.sleep(1000);
 			}
 		}
 	}
+
 	
 	
+	///////////////////////////////////////////////////////////
+	//////////////////    Helper methods    ///////////////////
+	///////////////////////////////////////////////////////////
 	
+	/**
+	 * Calls {@linkplain ClientLogManagerStandalone#initRemoteLogging(ORB, Manager, int, boolean)}
+	 * with appropriate dummy parameters.
+	 * @param clm The instance to call initRemoteLogging on.
+	 * @return true if simulated remote logging was initialized successfully
+	 */
 	private boolean initRemoteLogging(ClientLogManagerStandalone clm) {
 		ORB orb = ORB.init(); // unconfigured ORB will do, just needed to produce Any objects for sending remote logs.
 		Manager managerDummy = new _ManagerStub(); // will only be used for != null check.
@@ -108,20 +138,23 @@ public class RemoteLoggingTest extends TestCase
 		return clm.initRemoteLogging(orb, managerDummy, 1, true);
 	}	
 	
+	
 	/**
 	 * Modified {@link ClientLogManager} which skips the manager call and uses a mock Log service.
 	 * Remote communication delays are simulated, see {@link #setDelayMillis(long)}. 
 	 */
 	private static class ClientLogManagerStandalone extends ClientLogManager {
-		
-		private CountDownLatch syncOnPrepareRemoteLogging;
 
-		ClientLogManagerStandalone(CountDownLatch syncOnPrepareRemoteLogging) {
-			super();
-			this.syncOnPrepareRemoteLogging = syncOnPrepareRemoteLogging;
-		}
-		
-		private LogOperations logServiceMock = new LogEmptyImpl() {
+		private final CountDownLatch syncOnPrepareRemoteLogging;
+		private volatile long delayMillis = 100;
+
+		/**
+		 * Mock impl of the Log service. All methods are total no-ops, 
+		 * except for {@link LogOperations#write_records(Any[]) which prints the xml log records 
+		 * contained in the given Any objects to stdout, and simulates network delay by sleeping 
+		 * via a call to {@link #delay()}.
+		 */
+		private final LogOperations logServiceMock = new LogEmptyImpl() {
 			public void write_records(Any[] records) {
 				for (Any any : records) {
 					// just print to stdout, to be verified by TAT
@@ -130,18 +163,43 @@ public class RemoteLoggingTest extends TestCase
 				delay();
 			}
 		};
-	
-		private long delayMillis = 100;
-				
+
+		/**
+		 * Constructor.
+		 * @param syncOnPrepareRemoteLogging
+		 *          Optional synchronization aid (may be <code>null</code>). 
+		 *          Method {@linkplain #prepareRemoteLogging()} will call <code>countDown()</code>
+		 *          to allow a test to wait until {@linkplain #initRemoteLogging(ORB, Manager, int, boolean)} is actually running
+		 *          in cases where it gets started from a separate thread.
+		 *          Note that the parent constructor will not call <code>countDown()</code> even though it calls <code>prepareRemoteLogging</code>,
+		 *          because we only set the CountDownLatch after calling the parent ctor. Thus passing a <code>CountDownLatch(1)</code> will work.
+		 */
+		ClientLogManagerStandalone(CountDownLatch syncOnPrepareRemoteLogging) {
+			super();
+			this.syncOnPrepareRemoteLogging = syncOnPrepareRemoteLogging;
+		}
+		
+		/** 
+		 * This is called by {@linkplain ClientLogManager#initRemoteLogging(ORB, Manager, int, boolean)}
+		 * and simulates the access to the Log service by sleeping via {@linkplain #delay()}. 
+		 * @see alma.acs.logging.ClientLogManager#getLogService(si.ijs.maci.Manager, int)
+		 */
 		protected LogOperations getLogService(Manager manager, int managerHandle) {
 			delay();
 			return logServiceMock;
 		}
 		
+		/**
+		 * Sets the delay in milliseconds which subsequent calls to {@link #delay()} will sleep for.
+		 * Default is 100 ms if this method does not get called.
+		 */
 		void setDelayMillis(long delayMillis) {
 			this.delayMillis = delayMillis;
 		}
 		
+		/**
+		 * Sleeps for the time given in {@linkplain #setDelayMillis(long)}.
+		 */
 		void delay() {
 			try {
 				Thread.sleep(delayMillis);
@@ -150,6 +208,10 @@ public class RemoteLoggingTest extends TestCase
 			}
 		}
 		
+		/**
+		 * Overloaded only to allow clients to sync with execution of {@link #initRemoteLogging(ORB, Manager, int, boolean)}.
+		 * @see #ClientLogManagerStandalone(CountDownLatch)
+		 */
 		protected void prepareRemoteLogging() {
 			if (syncOnPrepareRemoteLogging != null) {
 				syncOnPrepareRemoteLogging.countDown();
