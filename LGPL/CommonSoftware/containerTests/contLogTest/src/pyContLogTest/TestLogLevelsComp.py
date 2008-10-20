a74ee6d9e409c6b1df97c0bf5dc5892717a08df5
eallaert <eallaert@523d945c-050c-4681-91ec-863ad3bb968a>
2008-10-20 16:03:28 +0000

    Avoid that Python sends logs in packets of 10.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@97762 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/containerTests/contLogTest/src/pyContLogTest/TestLogLevelsComp.py
+++ LGPL/CommonSoftware/containerTests/contLogTest/src/pyContLogTest/TestLogLevelsComp.py
@@ -17,7 +17,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-# "@(#) $Id: TestLogLevelsComp.py,v 1.9 2008/06/24 13:46:25 eallaert Exp $"
+# "@(#) $Id: TestLogLevelsComp.py,v 1.10 2008/10/20 16:03:28 eallaert Exp $"
 #
 # who       when      what
 # --------  --------  ----------------------------------------------
@@ -88,6 +88,9 @@ class TestLogLevelsComp(contLogTest__POA.TestLogLevelsComp,  #CORBA stubs for ID
         void logDummyMessages(in LongSeq levels);
         '''
         mylogger = self.getLogger()
+        # Next is to avoid that Python sends logs in packets of 10 logs.
+        mylogger.acshandler.capacity = 0
+
         # give client time to get ready for logs
         sleep(0.500)
         for l in levels:
