7bd352daffaed63f30658aed27d3dbd21979b6f1
mschilli <mschilli@523d945c-050c-4681-91ec-863ad3bb968a>
2008-10-21 08:59:35 +0000

    added split() and join()
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@97805 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acscommandcenter/src/alma/acs/commandcenter/util/MiscUtils.java
+++ LGPL/CommonSoftware/acscommandcenter/src/alma/acs/commandcenter/util/MiscUtils.java
@@ -3,6 +3,7 @@
  */
 package alma.acs.commandcenter.util;
 
+import java.util.StringTokenizer;
 import java.util.logging.Logger;
 
 
@@ -31,16 +32,16 @@ public class MiscUtils {
 	 * @param requester class which needs a package-wide logger
 	 * @return a logger
 	 */
-	public static Logger getPackageLogger(Class requester) {
+	public static Logger getPackageLogger(Class<?> requester) {
 		String pkg = requester.getPackage().getName();
 		Logger ret = Logger.getLogger(pkg);
 		return ret;
 	}
 	
 	/**
-	 * Returns the specified string parsed as as int, or 0 if parsing fails.
+	 * Returns the specified string parsed as an int, or 0 if parsing fails.
 	 * 
-	 * @return the specified string parsed as as int, or 0 if parsing fails.
+	 * @return the specified string parsed as an int, or 0 if parsing fails.
 	 */
 	public static int parseInt (String text) {
 		try {
@@ -50,7 +51,41 @@ public class MiscUtils {
 		}
 	}
 
+	/**
+	 * Returns the specified strings concatenated as one comma-separated string.
+	 * 
+	 * @return the specified strings concatenated as one comma-separated string.
+	 */
+	public static String join (String[] text) {
+		if (text == null || text.length == 0)
+			return "";
+		StringBuilder ret = new StringBuilder(text[0]);
+		for (int i=1; i<text.length; i++) {
+			ret.append(",").append(text[i]);
+		}
+		return ret.toString();
+	}
 
+	/**
+	 * Returns the specified string parsed into pieces.
+	 * Commas and semi-colons are accepted as separators, whitespace is tolerated.
+	 * Thus, the formats recognized are roughly:<ul>
+	 * <li> "abc,def,ghi"
+	 * <li> "abc, def, ghi"
+	 * <li> "abc;def;ghi"
+	 * <li> "abc; def; ghi"
+	 * </ul>
+	 * @return the specified string parsed into pieces.
+	 */
+	public static String[] split (String text) {
+		StringTokenizer t = new StringTokenizer(text, ",;");
+		String[] ret = new String[t.countTokens()];
+		for (int i=0; i<ret.length; i++) {
+			ret[i] = t.nextToken().trim();
+		}
+		return ret;
+	}
+	
 }
 
 
