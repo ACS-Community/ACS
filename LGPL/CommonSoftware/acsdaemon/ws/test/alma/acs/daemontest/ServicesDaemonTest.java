499ef2e518816f50a649c841a712fd014f9347c2
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2008-10-21 16:06:05 +0000

    Now actually able to run 2 very primitive tests
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@97858 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsdaemon/ws/test/alma/acs/daemontest/ServicesDaemonTest.java
+++ LGPL/CommonSoftware/acsdaemon/ws/test/alma/acs/daemontest/ServicesDaemonTest.java
@@ -5,12 +5,30 @@ import java.util.logging.Logger;
 
 import junit.framework.TestCase;
 
+import org.omg.CORBA.Context;
+import org.omg.CORBA.ContextList;
+import org.omg.CORBA.DomainManager;
+import org.omg.CORBA.ExceptionList;
+import org.omg.CORBA.NVList;
+import org.omg.CORBA.NamedValue;
 import org.omg.CORBA.ORB;
+import org.omg.CORBA.Object;
+import org.omg.CORBA.Policy;
+import org.omg.CORBA.Request;
+import org.omg.CORBA.SetOverrideType;
 
+import sun.security.action.GetLongAction;
+
+import alma.ACSErr.Completion;
+import alma.ACSErrTypeCommon.wrappers.AcsJUnexpectedExceptionEx;
+import alma.JavaContainerError.wrappers.AcsJContainerEx;
 import alma.acs.container.corba.AcsCorba;
 import alma.acs.logging.ClientLogManager;
 import alma.acs.util.ACSPorts;
 import alma.acs.util.AcsLocations;
+import alma.acs.util.StopWatch;
+import alma.acsdaemon.DaemonCallback;
+import alma.acsdaemon.DaemonCallbackHelper;
 import alma.acsdaemon.ServiceDefinitionBuilder;
 import alma.acsdaemon.ServicesDaemon_ACS80;
 import alma.acsdaemon.ServicesDaemon_ACS80Helper;
@@ -46,7 +64,7 @@ public class ServicesDaemonTest extends TestCase
 
 	protected void runTest() throws Throwable {
 		try {
-			super.runTest();			
+			super.runTest();
 		}
 		catch (Throwable thr) {
 			if (logger != null) {
@@ -59,7 +77,7 @@ public class ServicesDaemonTest extends TestCase
 	protected String getFullName() {
 		String fullName = namePrefix + "#" + getName();
 		return fullName;
-	}	
+	}
 
 	
 	protected void setUp() throws Exception {
@@ -75,7 +93,7 @@ public class ServicesDaemonTest extends TestCase
 					"or to set up the container services.", ex);
 			if (acsCorba != null) {
 				try {
-					acsCorba.shutdownORB(true);
+					acsCorba.shutdownORB(true, false);
 					acsCorba.doneCorba();
 				} catch (Exception ex2) {
 					// to JUnit we want to forward the original exception, 
@@ -87,15 +105,14 @@ public class ServicesDaemonTest extends TestCase
 			throw ex;
 		}
 
-// Heiko's Eclipse-on-windows hack, should be "localhost" 
-		host = "alma78.hq.eso.org";
+		host = "localhost"; //or "alma78.hq.eso.org" for Heiko on eclipse
 		
 		daemon = getServicesDaemon(host);
 	}
 
 	protected void tearDown() throws Exception {
 		if (acsCorba != null) {
-			acsCorba.shutdownORB(true);
+			acsCorba.shutdownORB(true, false);
 			acsCorba.doneCorba();
 		}
 		// just in case... should give the OS time to reclaim ORB ports and so on
@@ -110,10 +127,21 @@ public class ServicesDaemonTest extends TestCase
 		ORB orb = acsCorba.getORB();
 		assertNotNull("ORB provided by inherited acsCorba must not be null", daemonLoc);
 		
-		ServicesDaemon_ACS80 ret = ServicesDaemon_ACS80Helper.narrow(orb.string_to_object(daemonLoc));
-		assertNotNull("Corba ref to services daemon must not be null", daemon);
+		org.omg.CORBA.Object obj = orb.string_to_object(daemonLoc);
+		ServicesDaemon_ACS80 ret = ServicesDaemon_ACS80Helper.narrow(obj);
+		assertNotNull("Corba ref to services daemon must not be null", ret);
 		return ret;
 	}
+
+	/**
+	 * Creates a {@link DaemonCallbackImpl} object and registers it with the ORB.
+	 */
+	private DaemonCallback createDaemonCallback() throws AcsJContainerEx, AcsJUnexpectedExceptionEx {
+		DaemonCallbackImpl daemonCallbackImpl = new DaemonCallbackImpl(logger);
+		DaemonCallback daemonCallback = DaemonCallbackHelper.narrow(acsCorba.activateOffShoot(daemonCallbackImpl, acsCorba.getRootPOA()));
+		return daemonCallback;
+	}
+
 
 	/////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////// Test methods //////////////////////////////////////
@@ -122,16 +150,16 @@ public class ServicesDaemonTest extends TestCase
 	public void testServicesBuilder() throws Exception {
 		short instanceNumber = (short) ACSPorts.getBasePort();
 		ServiceDefinitionBuilder sdb = daemon.create_service_definition_builder(instanceNumber);
-		assertNotNull(sdb);		
+		assertNotNull(sdb);
 		assertEquals("ACS instance number must match the one provided to the factory", instanceNumber, sdb.acs_instance_number());
 
 		// now we add services in an order that is not a legal startup order, to check re-ordering
 		sdb.add_notification_service(systemNotificationServiceDefault.value, host);
-		sdb.add_manager(host, null);
+		sdb.add_manager(host, "", true);
 		sdb.add_notification_service(systemNotificationServiceLogging.value, host);
-		sdb.add_logging_service(host, null); // null means that default name "Log" should be used
+		sdb.add_logging_service(host, ""); // "" means that default name "Log" should be used
 		sdb.add_naming_service(host);
-		sdb.add_cdb(host);
+		sdb.add_xml_cdb(host, true, System.getProperty("user.dir"));
 		sdb.add_interface_repository(host, true, false);
 		
 		String xmlSrvDef = sdb.get_services_definition();
@@ -140,5 +168,15 @@ public class ServicesDaemonTest extends TestCase
 		
 		sdb.close();
 	}
+	
+	public void testStartAcsServiceIndividually() throws Exception {
+		short instanceNumber = (short) ACSPorts.getBasePort();
+		DaemonCallback daemonCallback = createDaemonCallback();
+		
+		StopWatch sw = new StopWatch(logger);
+		daemon.start_naming_service(daemonCallback, instanceNumber);
+		sw.logLapTime("call start_naming_service");
+	}
+
 
 }
