a2bdeafd97f3471bcc3b7678dc0e7b4a83c22339
mschilli <mschilli@523d945c-050c-4681-91ec-863ad3bb968a>
2008-10-23 10:59:53 +0000

    added inputfield for "type modifiers'
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@97965 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acscommandcenter/src/alma/acs/commandcenter/gui/EditContainerSettingsDialog.java
+++ LGPL/CommonSoftware/acscommandcenter/src/alma/acs/commandcenter/gui/EditContainerSettingsDialog.java
@@ -7,6 +7,8 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -22,101 +24,107 @@ import alma.acs.commandcenter.gui.thirdparty.SpringUtilities;
 
 
 
+class EditContainerSettingsDialog extends JDialog implements ActionListener {
 
-class EditContainerSettingsDialog extends JDialog implements
-        ActionListener {
+	private final CommandCenterGui master;
+	JRadioButton btnGlobal, btnCustom;
+	JTextField modifF;
+	JTextField defaultScriptBaseF, defaultHostF, defaultAccountF, defaultPasswordF;
+	JTextField customScriptBaseF, customHostF, customAccountF, customPasswordF;
+	ButtonGroup buttonGroup;
+	boolean okChosen; // this dialog's "return value"
 
-    private final CommandCenterGui master;
+	protected EditContainerSettingsDialog(CommandCenterGui gui) {
+		super(gui.frame, "Edit detail settings for container", true);
+		this.master = gui;
+		this.getContentPane().setLayout(new BorderLayout());
+		
+		// --- content
+		JPanel content = new JPanel();
+		content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
 
-    JRadioButton btnGlobal, btnCustom;
+		JPanel pnlModifiers = new JPanel();
+		pnlModifiers.setBorder(master.createTitledBorder(" Tweak type of container "));
+		pnlModifiers.setLayout(new SpringLayout());
+		pnlModifiers.add(new JLabel("Type Modifiers"));
+		pnlModifiers.add(modifF = new JTextField());
+		modifF.setToolTipText("Enter comma-separated Modifiers, e.g. \"archiveContainer\"");
+		SpringUtilities.makeCompactGrid(pnlModifiers, 0, 2);
+		content.add(pnlModifiers);
 
-    JTextField defaultScriptBaseF, defaultHostF, defaultAccountF,
-            defaultPasswordF;
+		JPanel pnlLocation = new JPanel();
+		pnlLocation.setBorder(master.createTitledBorder(" Where to run this container "));
+		pnlLocation.setLayout(new SpringLayout());
+		pnlLocation.add(btnGlobal = new JRadioButton("On same host as manager"));
+		pnlLocation.add(new JLabel("(see Common Settings)"));
+		pnlLocation.add(new JLabel("Acs Instance"));
+		pnlLocation.add(defaultScriptBaseF = new JTextField());
+		pnlLocation.add(new JLabel("Remote Host"));
+		pnlLocation.add(defaultHostF = new JTextField());
+		pnlLocation.add(new JLabel("Remote Username"));
+		pnlLocation.add(defaultAccountF = new JTextField());
+		pnlLocation.add(new JLabel("Remote Password"));
+		pnlLocation.add(defaultPasswordF = new JPasswordField());
+		defaultScriptBaseF.setEditable(false);
+		defaultHostF.setEditable(false);
+		defaultAccountF.setEditable(false);
+		defaultPasswordF.setEditable(false);
 
-    JTextField customScriptBaseF, customHostF, customAccountF,
-            customPasswordF;
+		pnlLocation.add(btnCustom = new JRadioButton("On the following host"));
+		pnlLocation.add(new JLabel("")); // "(implies SSH connection)"
+		pnlLocation.add(new JLabel("Acs Instance"));
+		pnlLocation.add(customScriptBaseF = new JTextField());
+		pnlLocation.add(new JLabel("Remote Host"));
+		pnlLocation.add(customHostF = new JTextField());
+		pnlLocation.add(new JLabel("Remote Username"));
+		pnlLocation.add(customAccountF = new JTextField());
+		pnlLocation.add(new JLabel("Remote Password"));
+		pnlLocation.add(customPasswordF = new JPasswordField());
+		SpringUtilities.makeCompactGrid(pnlLocation, 0, 2);
 
-    ButtonGroup buttonGroup;
+		buttonGroup = new ButtonGroup();
+		buttonGroup.add(btnGlobal);
+		buttonGroup.add(btnCustom);
+		btnGlobal.setSelected(true);
+		content.add(pnlLocation);
+		
+		// --- controls
+		JPanel controls = new JPanel();
+		JButton btnOk = new JButton("Set");
+		controls.add(btnOk);
+		btnOk.addActionListener(this);
 
-    boolean okChosen; // this dialog's "return value"
+		
+		// need 'root' to be able to set a border
+		Box root = Box.createVerticalBox();
+		root.setBorder(new EmptyBorder(10, 10, 10, 10));
+		root.add(content);
+		root.add(Box.createVerticalGlue());
+		root.add(controls);
+		this.getContentPane().add(root);
+		this.pack();
 
-    protected EditContainerSettingsDialog(CommandCenterGui gui, String title,
-            String explanationText, String buttonText) {
-        super(gui.frame, title, true);
-        this.master = gui;
-        this.getContentPane().setLayout(new BorderLayout());
-        // need 'root' to be able to set a border
-        JPanel root = new JPanel(new BorderLayout()); 
-        root.setBorder(new EmptyBorder(10, 10, 10, 10));
-        // --- content
-        JPanel content = new JPanel();
-        content.setLayout(new BorderLayout());
-        JLabel north = new JLabel(explanationText);
-        content.add(north, BorderLayout.NORTH);
-        JPanel center = new JPanel();
-        center.setLayout(new SpringLayout());
-        center.add(btnGlobal = new JRadioButton("On same host as manager"));
-        center.add(new JLabel("(see Common Settings)"));
-        center.add(new JLabel("Acs Instance"));
-        center.add(defaultScriptBaseF = new JTextField());
-        center.add(new JLabel("Remote Host"));
-        center.add(defaultHostF = new JTextField());
-        center.add(new JLabel("Remote Username"));
-        center.add(defaultAccountF = new JTextField());
-        center.add(new JLabel("Remote Password"));
-        center.add(defaultPasswordF = new JPasswordField());
-        defaultScriptBaseF.setEditable(false);
-        defaultHostF.setEditable(false);
-        defaultAccountF.setEditable(false);
-        defaultPasswordF.setEditable(false);
-        center.add(btnCustom = new JRadioButton("On the following host"));
-        center.add(new JLabel("")); // "(implies SSH connection)"
-        center.add(new JLabel("Acs Instance"));
-        center.add(customScriptBaseF = new JTextField());
-        center.add(new JLabel("Remote Host"));
-        center.add(customHostF = new JTextField());
-        center.add(new JLabel("Remote Username"));
-        center.add(customAccountF = new JTextField());
-        center.add(new JLabel("Remote Password"));
-        center.add(customPasswordF = new JPasswordField());
-        SpringUtilities.makeCompactGrid(center, 0, 2);
-        buttonGroup = new ButtonGroup();
-        buttonGroup.add(btnGlobal);
-        buttonGroup.add(btnCustom);
-        btnGlobal.setSelected(true);
-        content.add(center, BorderLayout.CENTER);
-        root.add(content, BorderLayout.CENTER);
-        // --- controls
-        JPanel controls = new JPanel();
-        JButton btnOk = new JButton(buttonText);
-        controls.add(btnOk);
-        btnOk.addActionListener(this);
-        root.add(controls, BorderLayout.SOUTH);
-        this.getContentPane().add(root);
-        this.pack();
-        
-        btnGlobal.setName("rdb_Global");
-        btnCustom.setName("rdb_Custom");
-        defaultScriptBaseF.setName("txt_DefaultInstance");
-        defaultHostF.setName("txt_DefaultHost");
-        defaultAccountF.setName("txt_DefaultUser");
-        defaultPasswordF.setName("txt_DefaultPassword");
-        customScriptBaseF.setName("txt_CustomInstance");
-        customHostF.setName("txt_CustomHost");
-        customAccountF.setName("txt_CustomUser");
-        customPasswordF.setName("txt_CustomPassword");
-        btnOk.setName("btn_Ok");
-    }
+		btnGlobal.setName("rdb_Global");
+		btnCustom.setName("rdb_Custom");
+		defaultScriptBaseF.setName("txt_DefaultInstance");
+		defaultHostF.setName("txt_DefaultHost");
+		defaultAccountF.setName("txt_DefaultUser");
+		defaultPasswordF.setName("txt_DefaultPassword");
+		customScriptBaseF.setName("txt_CustomInstance");
+		customHostF.setName("txt_CustomHost");
+		customAccountF.setName("txt_CustomUser");
+		customPasswordF.setName("txt_CustomPassword");
+		btnOk.setName("btn_Ok");
+	}
 
-    public void bringUp() {
-        this.okChosen = false;
-        this.master.correctDialogLocation(this);
-        super.setVisible(true);
-    }
+	public void bringUp () {
+		this.okChosen = false;
+		this.master.correctDialogLocation(this);
+		super.setVisible(true);
+	}
 
-    public void actionPerformed(ActionEvent evt) {
-        this.okChosen = true;
-        setVisible(false);
-    }
+	public void actionPerformed (ActionEvent evt) {
+		this.okChosen = true;
+		setVisible(false);
+	}
 }
-
