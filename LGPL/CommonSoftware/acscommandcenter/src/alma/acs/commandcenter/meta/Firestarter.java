95896ca75becae0db76842721c4989b1a8fdd106
mschilli <mschilli@523d945c-050c-4681-91ec-863ad3bb968a>
2008-10-24 11:35:15 +0000

    another round of simplifications for this factory class
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@98061 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acscommandcenter/src/alma/acs/commandcenter/meta/Firestarter.java
+++ LGPL/CommonSoftware/acscommandcenter/src/alma/acs/commandcenter/meta/Firestarter.java
@@ -12,122 +12,43 @@ import alma.acs.util.AcsLocations;
 
 
 /**
- * Helper for advanced Acs clients.
- * <p>
- * Create a subclass and, potentially, set some of the fields to desired values:
- * <ul>
- * <li> Used by acscorba:
- * <li> --- acscorbaLog
- * <li> --- orbPort
- * <li> --- orbPortSearchRetry
- * <li> Used by componentclient:
- * <li> --- compclientLog
- * <li> --- managerLoc
- * <li> Used by this class itself:
- * <li> --- firestarterLog
- * </ul>
- * or leave them <code>null</code> to use defaults.
- * 
- * <p>
- * Then use these methods to have some work done for you:
- * <ul>
- * <li>giveOrb()
- * <li>giveAdvancedComponentClient()
- * </ul>
- * 
- * <p>
- * Finally, stop this firestarter through a call to
- * <ul>
- * <li>shutdown()
- * </ul>
- * 
- * @author mschilli
  */
 public class Firestarter {
 
 
+	// Lifecycle
 	// ==========================================================
-	// API to use
-	// ==========================================================
 
+	protected String clientName;
+	protected Logger logger = null;
 
-	protected String clientName = null;
-
-
-	public Firestarter (String clientName, Logger firestarterLog) {
-		this(clientName);
-		this.firestarterLog = firestarterLog;
-	}
-	
-	/**
-	 * Creates a Firestarter for a client with the specified name (like "AcsCommandCenter"
-	 * or "OMC") and the specified Logger.
-	 * 
-	 * @param clientName will be used as namespace prefix for the default-loggers
-	 * 
-	 * @see
-	 * @since ACS 5.0
-	 */
 	protected Firestarter(String clientName) {
+		this(clientName, null, null);
+	}
+
+	/**
+	 * Creates a Firestarter for a client.
+	 * 
+	 * @param clientName - e.g. "AcsCommandCenter" or "OMC"
+	 * @param logger - logger which will be passed to delegates
+	 * @param managerLoc - the corbaloc of the manager
+	 */
+	public Firestarter(String clientName, Logger logger, String managerLoc) {
+
 		if (clientName == null)
 			throw new NullPointerException("clientName should not be null");
-
 		this.clientName = clientName;
+
+		if (logger == null)
+			logger = Logger.getLogger(clientName + ".Firestarter");
+		this.logger = logger;
+
+		if (managerLoc == null)
+			managerLoc = AcsLocations.figureOutManagerLocation();
+		this.managerLoc = managerLoc;
 	}
 
 
-	// used when creating the acscorba
-	protected Logger acscorbaLog = null;
-	protected Integer orbPort = null;
-	protected Integer orbPortSearchRetry = null;
-
-	// used when creating the componentclient
-	protected Logger compclientLog = null;
-	protected String managerLoc = null;
-
-	// used when shutting down this firestarter
-	protected Logger firestarterLog = null;
-
-
-	/**
-	 * Should only be used by people who know what they're doing!
-	 * 
-	 * @return an advanced component client
-	 * @throws Exception any connection-related exception
-	 */
-	protected AdvancedComponentClient giveAdvancedComponentClient () throws Exception {
-		prepareAdvancedComponentClient(); // may fail
-
-		return advancedComponentClient;
-	}
-
-
-
-	/**
-	 * Should only be used by people who know what they're doing!
-	 * 
-	 * @return the orb
-	 * @throws Exception any connection-related exception
-	 */
-	public org.omg.CORBA.ORB giveOrb () throws OrbInitException {
-		return giveAcsCorba().getORB();
-	}
-
-	/**
-	 * Should only be used by people who know what they're doing!
-	 * 
-	 * @return the object that encapsulates orb and poas
-	 * @throws Exception any connection-related exception
-	 */
-	public AcsCorba giveAcsCorba() throws OrbInitException {
-		prepareAcsCorba(); // may fail
-		return acsCorba;
-	}
-
-
-	/**
-	 * Should be invoked to clean up the Firestarter.
-	 */
 	public void shutdown () {
 
 		try {
@@ -136,107 +57,90 @@ public class Firestarter {
 				advancedComponentClient = null;
 			}
 		} catch (Throwable t) {
-			if (firestarterLog == null)
-				firestarterLog = Logger.getLogger(clientName + ".Firestarter");
-			firestarterLog.log(Level.INFO, "failed to stop advancedcomponentclient", t);
+			logger.log(Level.INFO, "failed to stop AdvancedComponentClient", t);
 		}
+
 
 		try {
 			if (acsCorba != null) {
 				new Thread() {
-					@Override public void run () {
-						if (acsCorba != null)
-							acsCorba.shutdownORB(true, false);
+					@Override
+					public void run () {
+						boolean wait_for_completion = true;
+						boolean isOrbThread = false;
+						acsCorba.shutdownORB(wait_for_completion, isOrbThread);
+						acsCorba = null;
 					}
 				}.start();
-				// acsCorba.blockOnORB(); HSO 2008-09 commented out because of the above change to wait_for_completion==true
-				acsCorba = null;
 			}
 		} catch (Throwable t) {
-			if (firestarterLog == null)
-				firestarterLog = Logger.getLogger(clientName + ".Firestarter");
-			firestarterLog.log(Level.INFO, "failed to stop orb", t);
+			logger.log(Level.INFO, "failed to stop AcsCorba", t);
 		}
 
 	}
 
 
-	// ==========================================================
+
 	// AdvancedComponentClient
 	// ==========================================================
 
-
 	protected AdvancedComponentClient advancedComponentClient = null;
+	protected String managerLoc = null;
 
-	/**
-	 * Helper, sets up an advanced component client if necessary.
-	 * 
-	 * @throws Exception
-	 */
-	protected void prepareAdvancedComponentClient () throws Exception {
+	public AdvancedComponentClient giveAdvancedComponentClient () throws Exception {
 
-		if (advancedComponentClient != null)
-			return; // already prepared
+		if (advancedComponentClient == null) {
+			giveAcsCorba(); // may fail
+			advancedComponentClient = new AdvancedComponentClient(logger, managerLoc, clientName + ".ComponentClient", acsCorba);
+		}
 
-
-		if (compclientLog == null)
-			compclientLog = Logger.getLogger(clientName + ".ComponentClient");
-
-		if (managerLoc == null)
-			managerLoc = AcsLocations.figureOutManagerLocation();
-
-
-		prepareAcsCorba(); // may fail
-
-		AdvancedComponentClient x = new AdvancedComponentClient(compclientLog, managerLoc, clientName + ".ComponentClient",
-				acsCorba);
-		advancedComponentClient = x;
-
+		return advancedComponentClient;
 	}
 
 
-
+	// ORB
 	// ==========================================================
+
+	
+	public org.omg.CORBA.ORB giveOrb () throws OrbInitException {
+		giveAcsCorba(); // may fail
+		return acsCorba.getORB();
+	}
+
 	// AcsCorba
 	// ==========================================================
 
 
 	protected AcsCorba acsCorba = null;
-
-	/**
-	 * Helper, sets up an acs corba instance if necessary.
-	 * 
-	 * @throws OrbInitException if initialization of {@link AcsCorba} fails
-	 */
-	protected void prepareAcsCorba () throws OrbInitException {
-
-		if (acsCorba != null && acsCorba.isInitialized())
-			return; // already prepared
+	protected Integer orbPort = null;
+	protected Integer orbPortSearchRetry = null;
 
 
-		if (acscorbaLog == null)
-			acscorbaLog = Logger.getLogger(clientName + ".AcsCorba");
+	public AcsCorba giveAcsCorba () throws OrbInitException {
 
-
-		acsCorba = new AcsCorba(acscorbaLog);
-		acsCorba.setPortOptions(orbPort, orbPortSearchRetry);
-		boolean isAdmin = true;
-		
-		try {
-			acsCorba.initCorbaForClient(isAdmin);
-		} catch (Exception exc) {
-			throw new OrbInitException("acsCorba.initCorbaForClient(true) failed", exc);
+		if (acsCorba == null) {
+			acsCorba = new AcsCorba(logger);
+			acsCorba.setPortOptions(orbPort, orbPortSearchRetry);
 		}
+
+		if (!acsCorba.isInitialized()) {
+			try {
+				boolean isAdmin = true;
+				acsCorba.initCorbaForClient(isAdmin);
+
+			} catch (Exception exc) {
+				throw new OrbInitException("acsCorba.initCorbaForClient(true) failed", exc);
+			}
+		}
+
+		return acsCorba;
 	}
 
-	
 	public static class OrbInitException extends Exception {
-
 		OrbInitException(String message, Throwable cause) {
 			super(message, cause);
 		}
-		
 	}
-	
+
 
 }
