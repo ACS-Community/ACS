0c9b7652166f9156faa8686d0a72492e6f00bf0f
msekoran <msekoran@523d945c-050c-4681-91ec-863ad3bb968a>
2008-10-28 10:45:19 +0000

    added missing includes
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@98255 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/acsdaemon/ws/include/acsRequest.h
@@ -0,0 +1,321 @@
+#ifndef _ACS_REQUEST_H_
+#define _ACS_REQUEST_H_
+
+/*******************************************************************************
+*    ALMA - Atacama Large Millimiter Array
+*    (c) European Southern Observatory, 2002
+*    Copyright by ESO (in the framework of the ALMA collaboration)
+*    and Cosylab 2002, All rights reserved
+*
+*    This library is free software; you can redistribute it and/or
+*    modify it under the terms of the GNU Lesser General Public
+*    License as published by the Free Software Foundation; either
+*    version 2.1 of the License, or (at your option) any later version.
+*
+*    This library is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*    Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public
+*    License along with this library; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+*
+* "@(#) $Id: acsRequest.h,v 1.1 2008/10/28 10:45:19 msekoran Exp $"
+*
+* who       when      what
+* --------  --------  ----------------------------------------------
+* azagar   2008-08-12 created
+*/
+
+#include "acsdaemonS.h"
+#include <acsThread.h>
+#include <acsutilPorts.h>
+#include <queue>
+#include <memory>
+
+// callback call timeout
+#define CORBA_TIMEOUT 5000
+
+/*** exit codes of startup/shutdown scripts ***/
+// all fine
+#define EC_OK 0
+// can't create requested acs instance: already exists
+#define EC_CANNOTCREATE 40
+// can't use requested acs instance: doesn't exist or is owned by somebody else
+#define EC_CANNOTUSE 41
+// general failure, no details known
+#define EC_FAILURE 42
+// command line arguments wrong or missing
+#define EC_BADARGS 43
+// can't run requested servant: no port available
+#define EC_NOPORT 44
+// can't run requested servant: launch attempt timed out
+#define EC_TIMEOUT 45
+
+struct ACSService {
+    const char *xmltag;
+    const char *script;
+    const char *impname;   // Name of services handler (used for logging purposes)
+    const char *imptype;   // CORBA-type for this services handler
+    const char *impport;
+    const char *impexec;
+    const char *svccorbaurl;
+    std::string (*svcport)(int);
+    std::string (*namedsvcport)(int, const char *);
+    bool autorestart;      // Should ACS service automatically restart
+};
+
+enum ACSServiceType {
+    NAMING_SERVICE = 0,
+    NOTIFICATION_SERVICE,
+    CDB,
+    MANAGER,
+    ACS_LOG_SERVICE,
+    LOGGING_SERVICE,
+    INTERFACE_REPOSITORY,
+    UNKNOWN
+};
+
+#define ACS_SERVICE_TYPES UNKNOWN
+#define ACS_SERVICE_INSTANCES 10
+
+const ACSService acsServices[] = {
+    {
+      "naming_service",
+      "acsNamingService",
+      "Naming Service Imp",
+      "NamingServiceImp",
+      "3015",
+      "acsutilBlock -t 15 -s -k -b \"Imp is up and running...\" acsdaemonNamingServiceImp",
+      "corbaloc::%s:%s/NameService",
+      &ACSPorts::getNamingServicePort,
+      NULL,
+      false
+    }, {
+      "notification_service",
+      "acsNotifyService",
+      "Notification Service Imp",
+      "NotificationServiceImp",
+      "3016",
+      "acsutilBlock -t 15 -s -k -b \"Imp is up and running...\" acsdaemonNotificationServiceImp",
+      "corbaloc::%s:%s/%sNotifyEventChannelFactory",
+      NULL,
+      &ACSPorts::getNotifyServicePort,
+      true
+    }, {
+      "cdb",
+      "acsConfigurationDatabase",
+      "CDB Imp",
+      "ConfigurationDatabaseImp",
+      "3017",
+      "acsutilBlock -t 15 -s -k -b \"Imp is up and running...\" acsdaemonConfigurationDatabaseImp",
+      "corbaloc::%s:%s/CDB",
+      &ACSPorts::getCDBPort,
+      NULL,
+      false
+    }, {
+      "manager",
+      "acsManager",
+      "Manager Imp",
+      "ManagerImp",
+      "3018",
+      "acsutilBlock -t 15 -s -k -b \"Imp is up and running...\" acsdaemonManagerImp",
+      "corbaloc::%s:%s/Manager",
+      &ACSPorts::getManagerPort,
+      NULL,
+      false
+    }, {
+      "acs_log",
+      "acsACSLogService",
+      "ACS Log Service Imp",
+      "ACSLogServiceImp",
+      "3019",
+      "acsutilBlock -t 15 -s -k -b \"Imp is up and running...\" acsdaemonACSLogServiceImp",
+      "corbaloc::%s:%s/ACSLogSvc",
+      &ACSPorts::getLogPort,
+      NULL,
+      false
+    }, {
+      "logging_service",
+      "acsLoggingService",
+      "Logging Service Imp",
+      "LoggingServiceImp",
+      "3020",
+      "acsutilBlock -t 15 -s -k -b \"Imp is up and running...\" acsdaemonLoggingServiceImp",
+      "corbaloc::%s:%s/Log",
+      &ACSPorts::getLoggingServicePort,
+      NULL,
+      false
+    }, {
+      "interface_repository",
+      "acsInterfaceRepository",
+      "Interface Repository Imp",
+      "InterfaceRepositoryImp",
+      "3021",
+      "acsutilBlock -t 15 -s -k -b \"Imp is up and running...\" acsdaemonInterfaceRepositoryImp",
+      "corbaloc::%s:%s/InterfaceRepository",
+      &ACSPorts::getIRPort,
+      NULL,
+      false
+    }, { NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, false }
+};
+
+enum ACSServiceRequestType {
+    START_SERVICE,
+    STOP_SERVICE
+};
+
+enum ACSServiceRequestTarget {
+    LOCAL,
+    DAEMON,
+    IMP
+};
+
+ACSServiceType acsServiceXMLTagToEnum(const char *service);
+
+class RequestProcessorThread;
+
+class Request {
+  public:
+    virtual ~Request() {};
+    virtual void abort() = 0;
+    virtual bool execute() = 0;
+};
+
+class RequestProcessorThread : public ACS::Thread {
+  private:
+    ACE_Thread_Mutex *m_mutex;
+    ACE_Condition<ACE_Thread_Mutex> *m_wait;
+    std::queue<Request*> pending;
+    volatile bool running;
+  public:
+    RequestProcessorThread(const ACE_CString &name,
+           const ACS::TimeInterval& responseTime=ThreadBase::defaultResponseTime, 
+           const ACS::TimeInterval& sleepTime=ThreadBase::defaultSleepTime);
+    ~RequestProcessorThread();
+    void onStart();
+    void stop();
+    void exit();
+    void runLoop() ACE_THROW_SPEC ((CORBA::SystemException, ::ACSErrTypeCommon::BadParameterEx));
+    void process(Request* r);
+};
+
+template <class R> class RequestChainContext;
+
+template <class R> class ChainedRequest : public Request {
+  private:
+    RequestChainContext<R> *context;
+    friend class RequestChainContext<R>;
+    void process(RequestChainContext<R> *icontext);
+  protected:
+    virtual void complete();
+  public:
+    ChainedRequest() : context(NULL) {}
+};
+
+template <class R> class RequestChainContext {
+  private:
+    RequestProcessorThread *rpt;
+    std::deque<R*> requests;
+    R *curreq;
+    bool inprocess;
+  protected:
+    virtual bool requestDone(R *request) = 0;
+    virtual void chainDone() = 0;
+    virtual void chainAborted() = 0;
+  public:
+    RequestChainContext(RequestProcessorThread *irpt) : rpt(irpt), curreq(NULL), inprocess(false) {}
+    virtual ~RequestChainContext() {
+        while (!requests.empty()) {
+            delete requests.front();
+            requests.pop_front();
+        }
+    }
+    RequestProcessorThread *getRequestProcessor() { return rpt; }
+    void appendRequest(R *request) { requests.push_back(request); }
+    void prependRequest(R *request) { requests.push_front(request); }
+    void proceed(R *lastreq = NULL);
+};
+
+/*********************** ACS SERVICES SPECIFIC REQUESTS ***********************/
+
+class ACSServiceRequestChainContext;
+
+class ACSServiceRequestDescription {
+  private:
+    ACSServiceType service;
+    int instance_number;
+    const char *host, *name, *domain, *cdbxmldir;
+    bool loadir, wait, recovery;
+    ACE_CString prepareCommand(ACSServiceRequestType request_type, bool log);
+  public:
+    ACSServiceRequestDescription(ACSServiceType iservice, int iinstance_number);
+    ACSServiceRequestDescription(const ACSServiceRequestDescription &desc);
+    ACSErr::Completion_var executeLocal(ACSServiceRequestType request_type);
+    ACSErr::Completion_var executeRemote(ACSServiceRequestType request_type, CORBA::ORB_ptr orb, acsdaemon::DaemonCallback_ptr cbptr, const char *corbaloc);
+    void setFromXMLAttributes(const char **atts);
+    void setName(const char *iname) { name = iname == NULL ? NULL : strdup(iname); }
+    void setDomain(const char *idomain) { domain = idomain == NULL ? NULL : strdup(idomain); }
+    void setLoadIR(bool iloadir) { loadir = iloadir; }
+    void setWaitLoadIR(bool iwait) { wait = iwait; }
+    void setRecovery(bool irecovery) { recovery = irecovery; }
+    void setCdbXMLDir(const char *icdbxmldir) { cdbxmldir = icdbxmldir; }
+    int getInstanceNumber() { return instance_number; }
+    const char *getName() { return name; }
+    const char *getHost() { return host == NULL ? ACSPorts::getIP() : host; }
+    ACSServiceType getACSService() { return service; }
+    const char *getACSServiceName() { return acsServices[service].xmltag; }
+};
+
+class ACSDaemonContext;
+
+class ACSServiceRequest : public ChainedRequest<ACSServiceRequest>, POA_acsdaemon::DaemonCallback {
+  private:
+    ACSDaemonContext *context;
+    ACSServiceRequestTarget target;
+    ACSServiceRequestType request_type;
+    ACSServiceRequestDescription *desc;
+    acsdaemon::DaemonCallback_var callback;
+    const ACSErr::Completion *completion;
+    acsdaemon::DaemonCallback_var cbvar;
+    acsdaemon::DaemonCallback_ptr cbptr();
+    void release();
+  protected:
+    void complete();
+    void abort();
+    bool execute();
+  public:
+    ACSServiceRequest(ACSDaemonContext *icontext, ACSServiceRequestTarget itarget, ACSServiceRequestType itype, ACSServiceRequestDescription *idesc, acsdaemon::DaemonCallback_ptr icallback = NULL);
+    ~ACSServiceRequest();
+    void done(const ::ACSErr::Completion &comp);
+    void working(const ::ACSErr::Completion &comp);
+    const ACSErr::Completion *getCompletion() { return completion; }
+    bool isErrorFree() { return completion == NULL || completion->previousError.length() == 0; }
+    ACSServiceRequestTarget getRequestTarget() { return target; }
+    ACSServiceRequestDescription *getDescription() { return desc; }
+    const char *getACSServiceName() { return desc->getACSServiceName(); }
+    int getInstanceNumber() { return desc->getInstanceNumber(); }
+    const char *getHost() { return desc->getHost(); }
+};
+
+class ACSServiceRequestChainContext : public RequestChainContext<ACSServiceRequest> {
+  private:
+    ACSDaemonContext *context;
+    ACSServiceRequestType request_type;
+    bool reuse_services;
+    acsdaemon::DaemonSequenceCallback_var callback;
+    int instance_number;
+  protected:
+    bool requestDone(ACSServiceRequest *request);
+    void chainDone();
+    void chainAborted();
+  public:
+    ACSServiceRequestChainContext(ACSDaemonContext *icontext, ACSServiceRequestType itype, bool ireuse_services, acsdaemon::DaemonSequenceCallback_ptr icallback);
+    ~ACSServiceRequestChainContext();
+    void addRequest(const char *iservice, const char **atts);
+    void startProcessing() { proceed(); }
+};
+
+
+#endif
---
+++ LGPL/CommonSoftware/acsdaemon/ws/include/acsServiceController.h
@@ -0,0 +1,160 @@
+#ifndef _ACS_SERVICE_CONTROLLER_H_
+#define _ACS_SERVICE_CONTROLLER_H_
+
+/*******************************************************************************
+*    ALMA - Atacama Large Millimiter Array
+*    (c) European Southern Observatory, 2002
+*    Copyright by ESO (in the framework of the ALMA collaboration)
+*    and Cosylab 2002, All rights reserved
+*
+*    This library is free software; you can redistribute it and/or
+*    modify it under the terms of the GNU Lesser General Public
+*    License as published by the Free Software Foundation; either
+*    version 2.1 of the License, or (at your option) any later version.
+*
+*    This library is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*    Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public
+*    License along with this library; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+*
+* "@(#) $Id: acsServiceController.h,v 1.1 2008/10/28 10:45:19 msekoran Exp $"
+*
+* who       when      what
+* --------  --------  ----------------------------------------------
+* azagar   2008-10-21 created
+*/
+
+#include "acsRequest.h"
+#include <acsThreadManager.h>
+#include <map>
+
+const ACE_Time_Value TIME_PERIOD(60);
+
+class ControllerThread : public ACS::Thread {
+  private:
+    ACSDaemonContext *context;
+    ACE_Thread_Mutex *m_mutex;
+    ACE_Condition<ACE_Thread_Mutex> *m_wait;
+    volatile bool running;
+  public:
+    ControllerThread(const ACE_CString &name,
+           const ACS::TimeInterval& responseTime = ThreadBase::defaultResponseTime, 
+           const ACS::TimeInterval& sleepTime = ThreadBase::defaultSleepTime);
+    ~ControllerThread();
+    void setContext(ACSDaemonContext *icontext) { context = icontext; }
+    void onStart();
+    void stop();
+    void exit();
+    void runLoop() ACE_THROW_SPEC ((CORBA::SystemException, ::ACSErrTypeCommon::BadParameterEx));
+};
+
+class ControlledServiceRequest;
+
+class ServiceController {
+  private:
+    ACSDaemonContext *context;
+    bool autorestart;
+    ACE_Thread_Mutex *m_mutex;
+    acsdaemon::ServiceState state;
+    bool active;
+    Request *startreq, *stopreq; // last request in queue (either start or stop, not both)
+    friend class ControlledServiceRequest;
+    // called once the request returned by createControlledServiceRequest begins stopping the service
+    void stopping();
+    // called once the request returned by createControlledServiceRequest is complete
+    void requestComplete(Request *request);
+    bool setState(acsdaemon::ServiceState istate);
+  protected:
+    virtual ControlledServiceRequest *createControlledServiceRequest(ACSServiceRequestType itype, acsdaemon::DaemonCallback_ptr callback = NULL) = 0;
+    virtual acsdaemon::ServiceState getActualState() = 0;
+    virtual void fireAlarm(acsdaemon::ServiceState state) = 0;
+    ACSDaemonContext *getContext() { return context; }
+  public:
+    ServiceController(ACSDaemonContext *icontext, bool iautorestart);
+    virtual ~ServiceController();
+    void restart();
+    bool start(acsdaemon::DaemonCallback_ptr callback = NULL) ACE_THROW_SPEC ((acsdaemonErrType::ServiceAlreadyRunningEx));
+    void stop(acsdaemon::DaemonCallback_ptr callback = NULL) ACE_THROW_SPEC ((acsdaemonErrType::ServiceNotRunningEx));
+    acsdaemon::ServiceState getLastState() { return state; }
+};
+
+class ControlledServiceRequest : public Request {
+  private:
+    ServiceController *controller;
+    Request *request;
+    bool delreq;
+    bool isstopping;
+  protected:
+    void abort();
+    bool execute();
+  public:
+    ControlledServiceRequest(ServiceController *icontroller, Request *irequest, bool iisstopping);
+    ~ControlledServiceRequest();
+};
+
+class ImpRequest : public Request {
+  private:
+    ACE_CString command;
+  protected:
+    void abort() {}
+    bool execute();
+  public:
+    ImpRequest(ACSServiceRequestType itype, ACSServiceType iservice);
+};
+
+class ImpController : public ServiceController {
+  private:
+    ACSServiceType service;
+    ACE_CString corbaloc;
+  protected:
+    ControlledServiceRequest *createControlledServiceRequest(ACSServiceRequestType itype, acsdaemon::DaemonCallback_ptr callback = NULL);
+    acsdaemon::ServiceState getActualState();
+    void fireAlarm(acsdaemon::ServiceState state) {}
+  public:
+    ImpController(ACSDaemonContext *icontext, ACSServiceType iservice, bool iautostart = true);
+    ACSServiceType getACSService() { return service; }
+};
+
+class ACSServiceController : public ServiceController {
+  private:
+    ACSServiceRequestDescription *desc;
+    ACE_CString corbaloc;
+  protected:
+    ControlledServiceRequest *createControlledServiceRequest(ACSServiceRequestType itype, acsdaemon::DaemonCallback_ptr callback = NULL);
+    acsdaemon::ServiceState getActualState();
+    void fireAlarm(acsdaemon::ServiceState state);
+  public:
+    ACSServiceController(ACSDaemonContext *icontext, ACSServiceRequestDescription *idesc, bool iautostart);
+    ~ACSServiceController();
+};
+
+class ACSDaemonContext {
+  private:
+    CORBA::ORB_ptr orb;
+    ACS::ThreadManager tm;
+    RequestProcessorThread *reqproc;
+    ControllerThread *ctrl;
+    ACE_Thread_Mutex *m_mutex;
+    ServiceController **impcontrollers;
+    ServiceController **acsservicecontrollers;
+    std::map<const char *, ServiceController **> acsservicecontrollersmap;
+    ServiceController *getImpController(ACSServiceType service);
+    ServiceController *getACSServiceController(ACSServiceRequestDescription *desc);
+  public:
+    ACSDaemonContext(std::string name);
+    ~ACSDaemonContext();
+    void initialize(CORBA::ORB_ptr iorb);
+    void dispose(CORBA::ORB_ptr iorb);
+    void processRequest(ACSServiceRequestTarget target, ACSServiceRequestType type, ACSServiceRequestDescription *desc, acsdaemon::DaemonCallback_ptr callback = NULL) ACE_THROW_SPEC ((acsdaemonErrType::ServiceAlreadyRunningEx, acsdaemonErrType::ServiceNotRunningEx));
+    RequestProcessorThread *getRequestProcessor() { return reqproc; }
+    CORBA::ORB_ptr getORB() { return orb; }
+    void checkControllers();
+    acsdaemon::ServiceState getACSServiceState(int instance_number, const char *name = NULL);
+};
+
+
+#endif
