f138623a7eb4f0d07fc25ae1bd3cd368e3d28723
cparedes <cparedes@523d945c-050c-4681-91ec-863ad3bb968a>
2008-11-13 02:54:03 +0000

    Adding the check of CDB's schemas
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@99239 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/acsBUILD/test/checkEncoding.sh
+++ LGPL/acsBUILD/test/checkEncoding.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 LIST=$INTROOT:$INTLIST:$ACSROOT
+CDB_LIST=$PWD/../config/
 
 echo "Checking all xml and xsd files with encode different from ISO-8859-1"
 
@@ -17,6 +18,21 @@ echo $LIST |
             grep -i encoding $path/idl/*.xsd | grep -v "ISO-8859-1"
             grep -i encoding $path/idl/*.xml | grep -v "ISO-8859-1"
         fi
+      fi
+      done
+} 
+
+echo $CDB_LIST |
+ {
+    read line
+    IFS=:
+    array=( $line )
+
+    for path in ${array[@]}
+    do
+      if [ X"$path" != X ]; then
+        #grep -R encoding $path | grep "\.xml" | grep -v "ISO-8859-1"
+        grep -R encoding $path | grep "\.xsd" | grep -v "ISO-8859-1"
       fi
       done
 } 
