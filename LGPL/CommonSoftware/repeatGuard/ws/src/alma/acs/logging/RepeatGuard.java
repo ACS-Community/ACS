7ff1aa98c4433990e42a982214357006b177d915
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2008-12-11 10:04:13 +0000

    - Wrote no-nonsense javadoc descriptions; - renamed "counterAtLastCheck" to "counterAtLastExecution" because the returned value gives the value of the counter at the time when check() returned "true" the last time, which means that the guarded activity was to be executed; otherwise the returned value is 0 regardless of whether check was called or not.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@100859 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/repeatGuard/ws/src/alma/acs/logging/RepeatGuard.java
+++ LGPL/CommonSoftware/repeatGuard/ws/src/alma/acs/logging/RepeatGuard.java
@@ -25,20 +25,43 @@
 
 package alma.acs.logging;
 
-import java.security.InvalidParameterException;
 import java.util.concurrent.TimeUnit;
 
 /**
- * This is a copy from Nicolas' C++ RepeatGuard implementation. See there all
- * the description, comments and documentation.
- * 
- * @author hmeuss
+ * This class can be used to reduce the repeated execution of any kind of code, 
+ * by keeping track of the number of intended executions and the time passed since the last actual execution,
+ * and comparing these against configured values (see details below).
+ * <p>
+ * A reduction of the number of executions can be useful if that code is resource intensive (e.g. DB access, remote calls, logging)
+ * or has other unwanted side effects if executed too often (e.g. annoying the user with too frequent error or confirmation dialogues). 
+ * Of course this only makes sense if the nature of the problem allows skipping some of these executions.
+ * <p>
+ * This class is so general that it does not actually execute any code; it simply keeps track of counters and timer.
+ * It can be used directly to manage decisions about skipping or executing some code.
+ * For example, the application can instantiate a <code>RepeatGuard</code> with the timer set to one second. 
+ * Then whenever the repetitive action would be called, the application first calls {@link #check()};
+ * within one second of the last <code>true</code> returned from <code>check()</code>, the subsequent calls will return <code>false</code>,
+ * and the application should skip the repeated block of code.
+ * Alternatively this class can be extended or wrapped to become easier to use for specific purposes.
+ * Then typically the code to be executed (e.g. logging a message) becomes part of the specialized repeat guard class, 
+ * as in {@link RepeatGuardLogger}.
+ * <p>
+ * The concept of repeat guards in ACS is discussed at http://almasw.hq.eso.org/almasw/bin/view/ACS/LoggingRepetitionControl
+ * <p> 
+ * Repetitions can be reduced using
+ * <ol>
+ *   <li>the number of times that code execution should be skipped before the code can be executed again
+ *   <li>the time that must have passed since the last actual execution, 
+ *       no matter how many execution attempts have been made in the meantime,
+ *   <li><code>OR</code> combination of the above: either enough attempts were made or enough time has passed, whatever happens first
+ *   <li><code>AND</code> combination: enough skipped execution attempts, and enough time passed. 
+ * </ol>
  */
 public class RepeatGuard {
 
 	/*** Evaluation logic: TIMER (time based guard), COUNTER (count based guard), AND/OR (conjunction/disjunction or both) */
 	public enum Logic { AND, OR, TIMER, COUNTER }
-
+	
 	private Logic evaluationMethod;
 
 	private int maxRepetitions;
@@ -47,9 +70,12 @@ public class RepeatGuard {
 
 	private int counter;
 
-	private int counterAtLastCheck;
+	/**
+	 * @see #counterAtLastExecution()
+	 */
+	private int counterAtLastExecution;
 
-	private boolean firstTime;
+	private boolean firstTime = true;
 
 	/**
 	 * Constructor.
@@ -57,44 +83,57 @@ public class RepeatGuard {
 	 * @param interval Time interval (in <code>timeUnit</code> units).
 	 * @param timeUnit Time unit of <code>interval</code> parameter.
 	 * @param maxRepetitions Maximum number of repetitions.
-	 * @param logic Evaluation logic.
+	 * @param logic Evaluation logic for <code>interval</code> and <code>maxRepetitions</code>. 
+	 *        The logic will be "reduced" automatically if <code>interval</code> or <code>maxRepetitions</code> 
+	 *        have a value <= 0, so as to be based only on the other positive value.
+	 * @throws IllegalArgumentException if maxRepetitions <= 0 && interval <= 0
 	 */
 	public RepeatGuard(long interval, TimeUnit timeUnit, int maxRepetitions, Logic logic) {
 		reset(interval, timeUnit, maxRepetitions, logic);
-    }
+	}
 
 	/**
-	 * Constructor, convenience for the above, using OR evaluation method.
+	 * Constructor, convenience for the above, using {@link Logic.OR} evaluation method
+	 * if both <code>interval</code> and <code>maxRepetitions</code> are positive values, 
+	 * otherwise {@Logic.TIMER} or {@Logic.COUNTER} to make sure that only the respective parameter with a positive value gets used.
 	 * @param interval Time interval (in <code>timeUnit</code> units).
 	 * @param timeUnit Time unit of <code>interval</code> parameter.
 	 * @param maxRepetitions Maximum number of repetitions.
+	 * @throws IllegalArgumentException if maxRepetitions <= 0 && interval <= 0
 	 */
 	public RepeatGuard(long interval, TimeUnit timeUnit, int maxRepetitions) {
 		this(interval, timeUnit, maxRepetitions, Logic.OR);
 	}
 
 	/**
-	 * Check returns true, if the last call for check was longer ago than
-	 * interval and/or increment has been called more than maxRepetitions.
-	 * @return <code>true</code> if OK, <code>false</code> if should be guarded
+	 * This method checks if the guarded activity is due for execution, or if it should be skipped instead.
+	 * <p>
+	 * For the first call, it always returns true.
+	 * Later it returns <code>true</code> if the last call to <code>check()</code>was longer ago than the <code>interval</code> 
+	 * given in the constructor or in the <code>reset</code> methods, 
+	 * and/or if the internal counter has been incremented more than <code>maxRepetitions</code> 
+	 * by calls to {@link #increment()} or {@link #checkAndIncrement()}.
+	 * @return <code>true</code> if guarded activity should be run, <code>false</code> if it should be skipped.
 	 */
-    public synchronized boolean check() {
-        long now = System.nanoTime();
+	public synchronized boolean check() {
+		
+		long now = System.nanoTime();
 
-        // first time log
-        if (firstTime)
-        {
-	        firstTime = false; 
-	        counterAtLastCheck = counter;
-	        counter = 0;
-	        endTimeNs = now + intervalNs;
-	        return true;
-        }
+		// first time check always returns true, regardless of counter and timer. Then resets timer and counter.
+		if (firstTime) {
+			firstTime = false;
+			
+			counterAtLastExecution = counter;
+			counter = 0;
+			endTimeNs = now + intervalNs;
+			
+			return true;
+		}
 
-        switch (evaluationMethod) {
+		switch (evaluationMethod) {
 			case AND:
 				if ((now >= endTimeNs) && (counter >= maxRepetitions)) {
-	                counterAtLastCheck = counter;
+					counterAtLastExecution = counter;
 					counter = 0;
 					endTimeNs = now + intervalNs;
 					return true;
@@ -103,94 +142,111 @@ public class RepeatGuard {
 				
 			case OR:
 				if ((now >= endTimeNs) || (counter >= maxRepetitions)) {
-					counterAtLastCheck = counter;
+					counterAtLastExecution = counter;
 					counter = 0;
 					endTimeNs = now + intervalNs;
 					return true;
 				}
 				return false;
+				
 			case TIMER:
 				if (now >= endTimeNs) {
-					counterAtLastCheck = counter;
+					counterAtLastExecution = counter;
 					counter = 0;
 					endTimeNs = endTimeNs + intervalNs; //endTime + interval instead of now + interval to prevent drift
 					return true;
 				}
 				return false;
+				
 			case COUNTER:
 				if (counter >= maxRepetitions) {
-					counterAtLastCheck = counter;
+					counterAtLastExecution = counter;
 					counter = 0;
 					endTimeNs = now + intervalNs;
 					return true;
 				}
 				return false;
 		}
-
-        return false;
+		return false;
 	}
 
-    /**
-     * Increments and checks (see {@link #check()}).
+	/**
+	 * Increments the counter and checks (see {@link #check()}).
 	 * @return <code>true</code> if OK, <code>false</code> if should be guarded
-     * @see #check()
-     */
+	 * @see #check()
+	 */
 	public synchronized boolean checkAndIncrement() {
 		counter++;
 		return check();
 	}
 
 	/**
-	 * Reset and reconfigure logic of guard using OR logic.
+	 * Resets and reconfigures this guard using the given interval, maxRepetitions, and {@link Logic.OR} logic.
 	 * @param interval Time interval (in <code>timeUnit</code> units).
 	 * @param timeUnit Time unit of <code>interval</code> parameter.
-	 * @param maxRepetitions Maximum number of repetitions.
+	 * @param maxRepetitions Maximum number of skipped repetitions.
+	 * @throws IllegalArgumentException if maxRepetitions <= 0 && interval <= 0
+	 * @see #RepeatGuard(long, TimeUnit, int, Logic)
 	 */
 	public void reset(long interval, TimeUnit timeUnit, int maxRepetitions) {
 		reset(interval, timeUnit, maxRepetitions, Logic.OR);
 	}
 
 	/**
-	 * Reset and reconfigure logic of guard.
+	 * Resets and reconfigures logic of guard.
 	 * @param interval Time interval (in <code>timeUnit</code> units).
 	 * @param timeUnit Time unit of <code>interval</code> parameter.
-	 * @param maxRepetitions Maximum number of repetitions.
-	 * @param logic Evaluation logic.
+	 * @param maxRepetitions Maximum number of skipped repetitions.
+	 * @param logic Evaluation logic for <code>interval</code> and <code>maxRepetitions</code>. 
+	 *        The logic will be "reduced" automatically if <code>interval</code> or <code>maxRepetitions</code> 
+	 *        have a value <= 0, so as to be based only on the other positive value.
+	 * @throws IllegalArgumentException if <code>maxRepetitions <= 0 && interval <= 0</code> or required arg == null
 	 */
 	public synchronized void reset(long interval, TimeUnit timeUnit, int maxRepetitions, Logic logic) {
-		this.evaluationMethod = logic;
 
-		// check paramaters
+		// check parameters
 		if (maxRepetitions <= 0 && interval <= 0) {
-			throw new InvalidParameterException("maxRepetitions <= 0 && interval <= 0");
+			throw new IllegalArgumentException("maxRepetitions <= 0 && interval <= 0 not allowed.");
 		}
-		// check interval parameter, counter fallback
-		else if (interval <= 0) {
+		
+		if (timeUnit == null && interval > 0) {
+			throw new IllegalArgumentException("A timeUnit must be be specified.");
+		}
+		
+		if (logic == null) {
+			throw new IllegalArgumentException("A 'logic' enum value must be specified.");
+		}
+		
+		// use only maxRepetitions if interval <= 0
+		if (interval <= 0) {
 			evaluationMethod = Logic.COUNTER;
 		}
-		// check max repetitions parameter, timer fallback
+		// use only interval if maxRepetitions <= 0
 		else if (maxRepetitions <= 0) {
 			evaluationMethod = Logic.TIMER;
 		}
- 
-		this.intervalNs = timeUnit.toNanos(interval);
+		else {
+			// use interval and/or maxRepetitions as given by the logic parameter.
+			this.evaluationMethod = logic;
+		}
+
+		this.intervalNs = ( timeUnit != null ? timeUnit.toNanos(interval) : 0 );
 		this.maxRepetitions = maxRepetitions;
 
 		reset();
 	}
 
 	/**
-	 * Reset and reconfigure logic of guard.
+	 * Resets this guard without changing the configuration for timer, counter and logic.
 	 */
 	public synchronized void reset() {
 		counter = 0;
-		counterAtLastCheck = 0;
+		counterAtLastExecution = 0;
 		firstTime = true;
-		this.endTimeNs = System.nanoTime() + intervalNs;
 	}
 
 	/**
-	 * Increase couter value.
+	 * Increase counter value.
 	 */
 	public synchronized void increment() {
 		counter++;
@@ -205,11 +261,14 @@ public class RepeatGuard {
 	}
 
 	/**
-	 * Get count at last check.
-	 * @return count at last check.
+	 * Gets the value of the counter that it had when the {@link #check()} or
+	 * {@link #checkAndIncrement()} method returned <code>true</code> the last time,
+	 * which corresponds to the number of times the activity was skipped before it got executed.
+	 * <p>
+	 * Calling this method does not make sense if only timer logic was used (no counters configured nor incremented)
 	 */
-	public synchronized int count() {
-		return counterAtLastCheck;
+	public synchronized int counterAtLastExecution() {
+		return counterAtLastExecution;
 	}
 	
 }
