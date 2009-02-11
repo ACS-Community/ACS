a4495329f5e5cadc0e8e0396e7772c586ea4bf2c
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2009-02-11 14:00:27 +0000

    Added a never ending test publishing log messages with a random number.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@103437 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jlogEngine/test/logClient.cpp
+++ LGPL/CommonSoftware/jlogEngine/test/logClient.cpp
@@ -20,7 +20,7 @@
 *
 *
 *
-* "@(#) $Id: logClient.cpp,v 1.5 2007/02/08 14:13:51 acaproni Exp $"
+* "@(#) $Id: logClient.cpp,v 1.6 2009/02/11 14:00:27 acaproni Exp $"
 *
 * who       when      what
 * --------  --------  ----------------------------------------------
@@ -28,31 +28,38 @@
 */
 
 #include <maciSimpleClient.h>
+#include <cstdlib>
 
 /**
  * This process is a SimpleClient that sends logs
- * 
+ *
  * Usage:
  *    - logClient 1
  *    - logClient 2 nIter sendDone
- * 
- * If first param is 0 then the process sends 100 logs with a defined 
+ *    - logClient 3
+ *
+ * If first parameter is 1 then the process sends 100 logs with a defined
  * message reporting the number of the log.
  * This is used to check if all the messages are published and received
  * by the LCEngine
- *  
- * If the first param is 1 then the process executes nIter iteration
+ *
+ * If the first parameter is 2 then the process executes nIter iteration
  * and in each iteration it sends one message of each possible
  * log type.
  * If sendDone==0 the process sends a log containing the Done message
- * (that is used as a termination message fro the client listening 
+ * (that is used as a termination message from the client listening
  * for logs)
  * This second format is used as a stress test to based on the number
- * of logs received. The difference between this case and the former 
- * is that the test is now performed on a great numer of messages
+ * of logs received. The difference between this case and the former
+ * is that the test is now performed on a great number of messages
  * produced by several clients (copies of logClient) publishing messages
  * in parallel.
- *  
+ *
+ * if the first parameter is 3 then the process sends the logs but never ends
+ * i.e. it must be killed. This is used for very long run tests.
+ * In this case the body of log messages contains a randomly generated
+ * integer used to distinguish the messages.
+ *
  */
 using namespace maci;
 
@@ -69,16 +76,16 @@ int main(int argc, char *argv[])
         //Must log into manager before we can really do anything
         client.login();
         }
-        
+
     int mode = atol(argv[1]);
-	
+
 	if (mode==1) {
-	    for (int t=0; t<100; t++) 
+	    for (int t=0; t<100; t++)
 		{
 		ACS_SHORT_LOG ((LM_INFO, "Test message %d",t));
 		}
 	} else if (mode==2) {
-		for (int t=0; t<atol(argv[2]); t++) 
+		for (int t=0; t<atol(argv[2]); t++)
 		{
 			ACS_SHORT_LOG ((LM_TRACE, "Test message"));
 			usleep(10);
@@ -99,11 +106,33 @@ int main(int argc, char *argv[])
 			ACS_SHORT_LOG ((LM_EMERGENCY, "Test message %d",t));
 			usleep(250);
 		}
-		if (atol(argv[3])!=0) 
+		if (atol(argv[3])!=0)
 			{
 			ACS_SHORT_LOG ((LM_INFO, "Done"));
 			}
 		sleep(10);
+	} else if (mode==3) {
+		while (true)
+			{
+				ACS_SHORT_LOG ((LM_TRACE, "Random message"));
+				usleep(10);
+				ACS_SHORT_LOG ((LM_DEBUG, "Random message %d",std::rand()));
+				usleep(10);
+				ACS_SHORT_LOG ((LM_INFO, "Random message %d",std::rand()));
+				usleep(10);
+				ACS_SHORT_LOG ((LM_NOTICE, "Random message %d",std::rand()));
+				usleep(10);
+				ACS_SHORT_LOG ((LM_WARNING, "Random message %d",std::rand()));
+				usleep(10);
+				ACS_SHORT_LOG ((LM_ERROR, "Random message %d",std::rand()));
+				usleep(10);
+				ACS_SHORT_LOG ((LM_CRITICAL, "Random message %d",std::rand()));
+				usleep(10);
+				ACS_SHORT_LOG ((LM_ALERT, "Random message %d",std::rand()));
+				usleep(10);
+				ACS_SHORT_LOG ((LM_EMERGENCY, "Random message %d",std::rand()));
+				usleep(250);
+			}
 	}
 
 	ACE_OS::sleep(5);
