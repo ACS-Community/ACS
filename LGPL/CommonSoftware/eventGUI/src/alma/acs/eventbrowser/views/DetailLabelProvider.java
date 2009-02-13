5488b6c42222844d84c185434a7ea4a9d52fd1f6
jschwarz <jschwarz@523d945c-050c-4681-91ec-863ad3bb968a>
2009-02-13 13:56:24 +0000

    Added comment with references to Web pages that discuss various label provider options.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@103555 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/eventGUI/src/alma/acs/eventbrowser/views/DetailLabelProvider.java
+++ LGPL/CommonSoftware/eventGUI/src/alma/acs/eventbrowser/views/DetailLabelProvider.java
@@ -1,5 +1,8 @@
 package alma.acs.eventbrowser.views;
 
+// See https://bugs.eclipse.org/bugs/show_bug.cgi?id=246026 for a tip on how to avoid all this, and get labels that are path
+// dependent; also http://www.eclipsezone.com/eclipse/forums/t113574.html
+
 import org.eclipse.jface.viewers.LabelProvider;
 
 public class DetailLabelProvider extends LabelProvider {
