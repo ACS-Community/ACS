e762f403d46e4989975c3dd971370e0f00605cd9
cmenay <cmenay@523d945c-050c-4681-91ec-863ad3bb968a>
2009-02-19 18:49:56 +0000

    Switched to a HashMap implementation for the properties. Added some comments.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@103947 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jbaci/src/alma/ACS/jbaci/PropertySetImpl.java
+++ LGPL/CommonSoftware/jbaci/src/alma/ACS/jbaci/PropertySetImpl.java
@@ -1,6 +1,27 @@
+/*******************************************************************************
+ * ALMA - Atacama Large Millimiter Array
+ * (c) European Southern Observatory, 2002
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ */
+
 package alma.ACS.jbaci;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.omg.CORBA.Any;
 import org.omg.CORBA.TypeCode;
@@ -20,35 +41,49 @@ import org.omg.CosPropertyService.PropertySetPOA;
 import org.omg.CosPropertyService.ReadOnlyProperty;
 import org.omg.CosPropertyService.UnsupportedProperty;
 import org.omg.CosPropertyService.UnsupportedTypeCode;
-import org.omg.CosPropertyService.PropertySetOperations;
 
-public class PropertySetImpl implements PropertySetOperations
+
+/**
+ * Implementation of <code>org.omg.CosPropertyService.PropertySet</code>.
+ * @author <a href="mailto:cmenayATcsrg.inf.utfsm.cl">Camilo Menay</a>
+ * @author <a href="mailto:cmmaureirATinf.utfsm.cl">Cristian Maureira</a>
+ * @version $id$
+ */
+public class PropertySetImpl extends PropertySetPOA
 								 {
 
-	
-	
-	private ArrayList propList;
-	private ArrayList valueList;
+	private Map propMap; 
 	private ArrayList allowed_property_types;
 	private ArrayList allowed_properties;
 	
+	
+	//others constructors might be created on demand
+	
 	public PropertySetImpl(){
-		
-		propList = new ArrayList();
-		valueList = new ArrayList();
+
 		allowed_property_types = new ArrayList();
 		allowed_properties = new ArrayList();
+		propMap = new HashMap();
 	}
 	
 	public PropertySetImpl(Property[] propSet) throws MultipleExceptions{
-		
-		propList = new ArrayList();
-		valueList = new ArrayList();
+
 		allowed_property_types = new ArrayList();
 		allowed_properties = new ArrayList();
+		propMap = new HashMap();
 		define_properties(propSet);
 	}
 	
+	/***checkTypeAndProperty According to documentation, we jave to check if the new property will be valid
+	 * according to the constraints that might exists. If both allowed lists are empty, then there aren't 
+	 * any constraints.
+	 * @param name
+	 * @param value
+	 * @return boolean True if the type and name of property are valid and can be added
+	 * @throws InvalidPropertyName
+	 * @throws UnsupportedTypeCode
+	 * @throws UnsupportedProperty
+	 */
 	private boolean checkTypeAndProperty(String name, Any value) throws InvalidPropertyName, UnsupportedTypeCode, UnsupportedProperty{
 		
 		if(name==null || name.length()==0)
@@ -106,30 +141,27 @@ public class PropertySetImpl implements PropertySetOperations
 				errorList.add(new PropertyException(ExceptionReason.invalid_property_name,propSet[i].property_name) );
 			}	
 			
-			if(errorList.size()>0)
+			if(errorList.size()>0) //TODO: check if this cast is possibly
 			throw new MultipleExceptions((PropertyException[])errorList.toArray());
 	}
 
 
-	
+
 	public void define_property(String name, Any value)
 			throws ConflictingProperty, UnsupportedProperty,
 			UnsupportedTypeCode, ReadOnlyProperty, InvalidPropertyName {
 
 		checkTypeAndProperty(name, value);
-		
-		int tmp = propList.indexOf(name);
-		if (propList.indexOf(name) != -1) {
-			Property p =  new Property((String)propList.toArray()[tmp],(Any)valueList.toArray()[tmp]) ;
+		if(  propMap.containsKey(name) )
+		{
+			Property p =  new Property(name,(Any)propMap.get(name)) ;
 			if (value.type().kind() != p.property_value.type().kind())
 				throw new ConflictingProperty();
-			else 
-				propList.add(name);
-				valueList.add(value);
+			else {
+				propMap.put(name, value);
+			}
 		} else {
-
-			propList.add(name);
-			valueList.add(value);
+			propMap.put(name, value);
 		}
 
 	}
