1310372777ee2e5d12fe202fc9b1a5adf1bf2456
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2009-02-20 17:23:14 +0000

    Changed implementation to use good old CountDownLatch as the thread gate, after the previous stab at using concurrent.locks.Condition failed to actually run the threads in parallel.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@104117 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jacsutil/src/alma/acs/concurrent/ParallelExecutor.java
+++ LGPL/CommonSoftware/jacsutil/src/alma/acs/concurrent/ParallelExecutor.java
@@ -1,13 +1,9 @@
 package alma.acs.concurrent;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Helper class that allows tests (but also other code) to be executed concurrently.
@@ -22,18 +18,15 @@ import java.util.concurrent.locks.ReentrantLock;
 public class ParallelExecutor
 {
 	private final ThreadFactory tf;
-	private final ReentrantLock lock;
-	private final Condition readyToRun;
-	private final List<Thread> scheduledThreads;
+	private final CountDownLatch threadGate;
+	private volatile boolean executed = false;
 	
 	/**
 	 * @param tf For components, clients and unit tests, this should be taken from container services method getThreadFactory.
 	 */
 	public ParallelExecutor(ThreadFactory tf) {
 		this.tf = tf;
-		lock = new ReentrantLock();
-		readyToRun = lock.newCondition();
-		scheduledThreads = new ArrayList<Thread>();
+		threadGate = new CountDownLatch(1);
 	}
 
 
@@ -46,23 +39,20 @@ public class ParallelExecutor
 	 * @param timeoutMillis  timeout in milliseconds: code will not be run if {@link #execute()} 
 	 *        is not called within this time.
 	 *        This is meant to avoid troubles with deadlocked threads in case something goes wrong.
-	 * @throws InterruptedException 
+	 * @throws InterruptedException
+	 * @throws IllegalStateException If called after {@link #execute()} or {@link #execute(List, long)}. 
 	 */
 	public void scheduleForExecution(Runnable runnable, long timeoutMillis) throws InterruptedException {
+		if (executed) {
+			throw new IllegalStateException();
+		}
 		CountDownLatch confirmWaiting = new CountDownLatch(1);
-		InterceptingRunnable interceptingRunnable = new InterceptingRunnable(runnable, lock, readyToRun, confirmWaiting, timeoutMillis);
+		InterceptingRunnable interceptingRunnable = new InterceptingRunnable(runnable, threadGate, confirmWaiting, timeoutMillis);
 		Thread t = tf.newThread(interceptingRunnable);
-		scheduledThreads.add(t);
 		t.start();
 		// wait till the thread has started, otherwise the execute() call might come too early 
 		// and the threads would wait for it till they time out.
 		confirmWaiting.await(timeoutMillis, TimeUnit.MILLISECONDS);
-		// now that the thread has confirmed that it's running, we also know from the implementation 
-		// of InterceptingRunnable#run that it already holds the lock.
-		// It will release this lock a few lines down by calling condition.await, so that by the time we can get the lock here,
-		// we know that the runnable is really parked on condition and will therefore be woken up by a future call to #execute()
-		lock.lock();
-		lock.unlock();
 	}
 
 
@@ -71,13 +61,8 @@ public class ParallelExecutor
 	 * should then be called immediately.
 	 */
 	public void execute() {
-		lock.lock();
-		try {
-			readyToRun.signalAll();
-		}
-		finally {
-			lock.unlock();
-		}
+		threadGate.countDown();
+		executed = true;
 	}
 	
 	/**
@@ -94,26 +79,23 @@ public class ParallelExecutor
 	
 	private static class InterceptingRunnable implements Runnable {
 		private final Runnable delegate;
-		private final Lock lock;
-		private final Condition condition;
+		private final CountDownLatch threadGate;
 		private final CountDownLatch confirmWaiting;
 		private final long timeoutMillis;
 
-		InterceptingRunnable(Runnable delegate, Lock lock, Condition condition, CountDownLatch confirmWaiting, long timeoutMillis) {
+		InterceptingRunnable(Runnable delegate, CountDownLatch threadGate, CountDownLatch confirmWaiting, long timeoutMillis) {
 			this.delegate = delegate;
-			this.lock = lock;
-			this.condition = condition;
+			this.threadGate = threadGate;
 			this.confirmWaiting = confirmWaiting;
 			this.timeoutMillis = timeoutMillis;
 		}
 		
 		public void run() {
 			boolean success = false;
-			lock.lock();
 			try {
 				confirmWaiting.countDown();
 				// condition.await will unlock the lock and wait...
-				if(condition.await(timeoutMillis, TimeUnit.MILLISECONDS) == false) {
+				if(threadGate.await(timeoutMillis, TimeUnit.MILLISECONDS) == false) {
 					// got a timeout
 					System.out.println("Got a timeout from condition.await()");
 				}
@@ -125,10 +107,10 @@ public class ParallelExecutor
 				// TODO Auto-generated catch block
 				ex.printStackTrace();
 			}
-			finally {
-				lock.unlock();
+			// TODO: handle !success better than this
+			if (!success) {
+				System.out.println("Damn, run() failed in thread " + Thread.currentThread().getName());
 			}
-			// TODO: handle !success
 		}
 	}
 }
