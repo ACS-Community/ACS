2b3ea1cbc1d29aa77542f617504dd3df16f73b9d
msekoran <msekoran@523d945c-050c-4681-91ec-863ad3bb968a>
2009-04-20 13:42:02 +0000

    prototype
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@108005 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/jbaci/src/alma/ACS/impl/CommonROEnumPropertyImpl.java
@@ -0,0 +1,353 @@
+/**
+ * 
+ */
+package alma.ACS.impl;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.omg.CORBA.BAD_PARAM;
+import org.omg.CORBA.NO_IMPLEMENT;
+import org.omg.CORBA.NO_RESOURCES;
+
+import alma.ACS.Alarmpattern;
+import alma.ACS.CBDescIn;
+import alma.ACS.CBDescOut;
+import alma.ACS.CBpattern;
+import alma.ACS.Callback;
+import alma.ACS.Condition;
+import alma.ACS.Monitor;
+import alma.ACS.Monitorpattern;
+import alma.ACS.MonitorpatternHelper;
+import alma.ACS.MonitorpatternPOATie;
+import alma.ACS.NoSuchCharacteristic;
+import alma.ACS.Subscription;
+import alma.ACS.TimeSeqHolder;
+import alma.ACS.jbaci.CallbackDispatcher;
+import alma.ACS.jbaci.DataAccess;
+import alma.ACS.jbaci.PropertyInitializationFailed;
+import alma.ACSErr.Completion;
+
+/**
+ * Base enum class. Contains common methods that does not contain <code>enumClass</code> class in method' signatures.
+ * <code>enumClass</code> must has standard Java CORBA generated metods of called <code>fromInt(int)</code> and <code>value()</code>.
+ * @author msekoranja
+ */
+public class CommonROEnumPropertyImpl extends ROCommonPropertyImpl {
+
+	/**
+	 * @param name
+	 * @param parentComponent
+	 * @throws PropertyInitializationFailed
+	 */
+	CommonROEnumPropertyImpl(Class enumClass, String name,
+			CharacteristicComponentImpl parentComponent)
+			throws PropertyInitializationFailed {
+		super(enumClass, name, parentComponent);
+		initialize();
+	}
+
+	/**
+	 * @param name
+	 * @param parentComponent
+	 * @param dataAccess
+	 * @throws PropertyInitializationFailed
+	 */
+	CommonROEnumPropertyImpl(Class enumClass, String name,
+			CharacteristicComponentImpl parentComponent, DataAccess dataAccess)
+			throws PropertyInitializationFailed {
+		super(enumClass, name, parentComponent, dataAccess);
+		initialize();
+	}
+	
+    private Method from_int_method;
+    private Method value_method;
+    
+    @SuppressWarnings("unchecked")
+	protected void initialize()
+    	throws PropertyInitializationFailed
+    {
+    	try {
+        	from_int_method = propertyType.getMethod("from_int", new Class[] { int.class });
+        	value_method = propertyType.getMethod("value", (Class[])null);
+    	} catch (NoSuchMethodException nse) {
+    		throw new PropertyInitializationFailed("given enum class does not implement required method(s)", nse);
+    	}
+    }
+
+    protected int value(Object value)
+	{
+    	try {
+        	return ((Integer)value_method.invoke(value, (Object[])null)).intValue();
+    	} catch (Throwable th) {
+    		throw new RuntimeException("failed to get int value from enum instance", th);
+    	}
+	}
+	
+	protected Object from_int(int value)
+	{
+    	try {
+        	return from_int_method.invoke(value, new Object[] { value });
+    	} catch (Throwable th) {
+    		throw new RuntimeException("failed to get enum instance from int value", th);
+    	}
+	}
+
+	
+	
+	
+	/* (non-Javadoc)
+	 */
+	public Condition[] condition() {
+		try
+		{
+			int[] temp = characteristicModelImpl.getIntegerSeq("condition");
+			Condition[] ret = new Condition[temp.length];
+			for (int i=0; i<temp.length; i++)
+				ret[i] = Condition.from_int(temp[i]);
+			return ret;
+		} catch (NoSuchCharacteristic e) {
+			throw new NO_RESOURCES();
+		}
+	}
+
+	/* (non-Javadoc)
+	 */
+	public String[] statesDescription() {
+		try {
+			return characteristicModelImpl.getStringSeq("statesDescription");
+		} catch (NoSuchCharacteristic e) {
+			throw new NO_RESOURCES();
+		}
+	}
+
+	/* (non-Javadoc)
+	 */
+	public Subscription new_subscription_AlarmEnum(Alarmpattern cb,
+			CBDescIn desc) {
+		// TODO NO_IMPLEMENT
+		throw new NO_IMPLEMENT();
+	}
+
+	/* (non-Javadoc)
+	 */
+	public Monitorpattern create_monitor(CBpattern cb, CBDescIn desc) {
+		return (Monitorpattern) create_postponed_monitor(0, cb, desc);
+	}
+
+	/* (non-Javadoc)
+	 */
+	public Monitor create_postponed_monitor(long start_time, CBpattern cb, CBDescIn desc) {
+		
+		// create monitor and its servant
+		MonitorpatternImpl monitorImpl = new MonitorpatternImpl(this, cb, desc, start_time);
+		MonitorpatternPOATie monitorTie = new MonitorpatternPOATie(monitorImpl);
+
+		// register and activate		
+		return MonitorpatternHelper.narrow(this.registerMonitor(monitorImpl, monitorTie));
+	}
+
+	/* (non-Javadoc)
+	 */
+	public void get_async(CBpattern cb, CBDescIn desc) {
+		getAsync(cb, desc);
+	}
+	
+	/* (non-Javadoc)
+	 */
+	@Override
+	public Object readPropertyTypeCharacteristic(String name)
+			throws NoSuchCharacteristic {
+		return from_int(characteristicModelImpl.getInteger(name));
+	}
+
+
+	/* (non-Javadoc)
+	 * @see alma.ACS.jbaci.CallbackDispatcher#dispatchCallback(int, java.lang.Object, alma.ACS.Callback, alma.ACSErr.Completion, alma.ACS.CBDescOut)
+	 */
+	public boolean dispatchCallback(int type, Object value, Callback callback,
+			Completion completion, CBDescOut desc) {
+		// NOTE: CBpattern is being used for enums
+		try
+		{	
+			if (type == CallbackDispatcher.DONE_TYPE)
+				((CBpattern)callback).done(value(value), completion, desc);
+			else if (type == CallbackDispatcher.WORKING_TYPE)
+				((CBpattern)callback).working(value(value), completion, desc);
+			else 
+				return false;
+				
+			return true;
+		}
+		catch (Throwable th)
+		{
+			return false;
+		}
+	}
+
+	
+	/* (non-Javadoc)
+	 */
+	private Object getEnumArrayFromCharacteristicModel(String name) {
+		try
+		{
+			int[] temp = characteristicModelImpl.getIntegerSeq(name);
+			Object ret = Array.newInstance(propertyType, temp.length);
+			for (int i=0; i<temp.length; i++)
+				Array.set(ret, i, from_int(temp[i]));
+			return ret;
+		} catch (NoSuchCharacteristic e) {
+			throw new NO_RESOURCES();
+		}
+	}
+	
+	/* (non-Javadoc)
+	 */
+	public Object alarm_on() {
+		return getEnumArrayFromCharacteristicModel("alarm_on");
+	} 
+	
+	/* (non-Javadoc)
+	 */
+	public Object alarm_off() {
+		return getEnumArrayFromCharacteristicModel("alarm_off");
+	} 
+
+	/* (non-Javadoc)
+	 */
+	public Object allStates() {
+		int statesCount = 0;
+		for (; statesCount < Integer.MAX_VALUE; statesCount++)
+		{
+			try
+			{
+				from_int(statesCount);
+			} catch (BAD_PARAM noMore) {
+				break;
+			}
+		}
+
+		Object states = Array.newInstance(propertyType, statesCount);
+		for (int i=0; i<statesCount; i++)
+			Array.set(states, i, from_int(i));
+		
+		return states;
+	}
+
+
+	/* (non-Javadoc)
+	 */
+	public Object default_value() {
+		try {
+			return from_int(characteristicModelImpl.getInteger("default_value"));
+		} catch (NoSuchCharacteristic e) {
+			return from_int(0);
+		}
+	}
+	
+	
+	
+	
+	/**
+	 * Create default in-memory RO enum structure.
+	 * @see #createEnumProperty(Class, Class, String, CharacteristicComponentImpl, DataAccess)
+	 */
+	public static Object createEnumProperty(
+			Class operationsIF,
+			Class propertyType, String name,
+			CharacteristicComponentImpl parentComponent)
+		throws PropertyInitializationFailed
+	{
+		return createEnumProperty(operationsIF, propertyType, name, parentComponent, null);
+	}
+
+	/**
+	 * Create RO enum structure.
+	 * Example: 
+	 * <code>
+	 * BasicStatesOperations basicStatesEnumImpl = 
+	 * 		(BasicStatesOperations)CommonROEnumPropertyImpl.createEnumProperty(
+	 * 			BasicStatesOperations.class,
+	 *			BasicStates.class,
+	 *			"state",
+	 *			parentComponent,
+	 *			dataAccess);
+	 * </code>
+	 * @param operationsIF CORBA <enum type>Operations class.
+	 * @param propertyType CORBA enum class.
+	 * @param name name of the property.
+	 * @param parentComponent property component.
+	 * @param dataAccess data access to be used.
+	 * @return CORBA <enum type>Operations instance.
+	 * @throws PropertyInitializationFailed
+	 */
+	public static Object createEnumProperty(
+			Class operationsIF,
+			Class propertyType, String name,
+			CharacteristicComponentImpl parentComponent,
+			DataAccess dataAccess)
+		throws PropertyInitializationFailed
+	{
+		CommonROEnumPropertyImpl propertyImpl;
+		if (dataAccess == null)
+			propertyImpl = new CommonROEnumPropertyImpl(propertyType, name, parentComponent);
+		else
+			propertyImpl = new CommonROEnumPropertyImpl(propertyType, name, parentComponent, dataAccess);
+		
+		return Proxy.newProxyInstance(
+			propertyImpl.getClass().getClassLoader(),
+			new Class[] { operationsIF }, 
+			new ROEnumProxy(propertyImpl));
+	}
+	
+
+	/**
+	 * RO enum proxy class.
+	 */
+	public static class ROEnumProxy implements InvocationHandler {
+
+		private final CommonROEnumPropertyImpl delegate;
+
+		ROEnumProxy(CommonROEnumPropertyImpl delegate) {
+			this.delegate = delegate;
+		}
+
+		public Object invoke(Object proxy, Method method, Object[] args)
+				throws Throwable
+		{
+			final String methodName = method.getName();
+			
+			if (methodName.equals("get_history"))
+			{
+				int n_last_values = ((Integer)args[0]).intValue();
+				Object seqHolder = args[1];
+				TimeSeqHolder ts = (TimeSeqHolder)args[2];
+
+				Object historyArray = delegate.getHistory(n_last_values, ts);
+				
+				Field valueField = seqHolder.getClass().getField("value");
+				valueField.set(seqHolder, historyArray);
+				
+				return Array.getLength(historyArray);
+				
+			}
+			else
+			{
+				// delegate
+				try {
+					return method.invoke(delegate, args);
+				} catch (InvocationTargetException e) {
+					throw e.getTargetException();
+				} catch (Throwable e) {
+					throw e;
+				}
+			}
+		}
+	}
+	
+	
+}
