cd745bd89221a64b8c8f1bc6f4587b4888b10805
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2009-05-19 16:38:58 +0000

    Reduced sleep times from 15 to 10 seconds. Really should use proper synchronization instead of sleeping though.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@109374 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jcontnc/test/jcontncTATRunTest.sh
+++ LGPL/CommonSoftware/jcontnc/test/jcontncTATRunTest.sh
@@ -4,10 +4,10 @@
 $* >& $ACS_TMP/$$.log
 
 #sleep so things stabilize
-sleep 15
+sleep 10
 
 #stop the container
 acsStopContainer frodoContainer >&  /dev/null
 
 #give the container a chance to flush logs
-sleep 15
+sleep 10
