e6c0607d21c4ff0950d61473c45c0daf838268c3
bjeram <bjeram@523d945c-050c-4681-91ec-863ad3bb968a>
2009-08-26 13:54:58 +0000

    ported from ACS-8_0_0-B: modified joinable test that is possible to give # of threads from the command line
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@114964 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsthread/ws/test/testACSThreadJoinable.cpp
+++ LGPL/CommonSoftware/acsthread/ws/test/testACSThreadJoinable.cpp
@@ -16,7 +16,7 @@
 * License along with this library; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 *
-* "@(#) $Id: testACSThreadJoinable.cpp,v 1.9 2009/07/30 10:16:47 bjeram Exp $"
+* "@(#) $Id: testACSThreadJoinable.cpp,v 1.10 2009/08/26 13:54:58 bjeram Exp $"
 *
 * who       when      what
 * --------  --------  ----------------------------------------------
@@ -31,7 +31,7 @@
 
 #include "acsThreadTest.h"
 
-static char *rcsId="@(#) $Id: testACSThreadJoinable.cpp,v 1.9 2009/07/30 10:16:47 bjeram Exp $"; 
+static char *rcsId="@(#) $Id: testACSThreadJoinable.cpp,v 1.10 2009/08/26 13:54:58 bjeram Exp $"; 
 static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
 
 /**
@@ -52,6 +52,7 @@ class FastACSThread :public ACS::Thread
 {
   public:
     FastACSThread(const ACE_CString& name, 
+		  int i,
 		  const ACS::TimeInterval& responseTime=ThreadBase::defaultResponseTime, 
 		  const ACS::TimeInterval& sleepTime=ThreadBase::defaultSleepTime,
 		  bool del=false
@@ -59,17 +60,25 @@ class FastACSThread :public ACS::Thread
 	ACS::Thread(name, responseTime, sleepTime, del           )
 	{
 	    ACS_TRACE("FastACSThread::FastACSThread");
+	    num = i;
 	}
 
     FastACSThread(const ACE_CString& name, 
+		  int i,
 		  const ACS::TimeInterval& responseTime, 
 		  const ACS::TimeInterval& sleepTime,
 		  bool del,
 		  const long _thrFlags
 	) :
-	ACS::Thread(name, responseTime, sleepTime, del, _thrFlags)
+	ACS::Thread(name, responseTime, sleepTime, del, _thrFlags, 512)
 	{
 	    ACS_TRACE("FastACSThread::FastACSThread");
+	    num = i;
+	}
+
+    virtual ~FastACSThread()
+	{
+//	std::cout << pthread_self() << " deleted " << std::endl; 
 	}
 
     /**
@@ -79,16 +88,17 @@ class FastACSThread :public ACS::Thread
     virtual void runLoop() 
 	{ 
     	/*
-    	 * we removed writing to stdout because on some machine the runLoop was not executed at all
-    	 * on some other was executed for some threads several (different number)
+        	 * we removed writing to stdout because on some machine the runLoop was not executed at all
+        	 * on some other was executed for some threads several (different number)
 
-#ifndef MAKE_VXWORKS
-	std::cout << pthread_self() << " runLoop()" << std::endl; 
-#else
-	std::cout << taskIdSelf() << " runLoop()" << std::endl; 	
-#endif
-*/
+    #ifndef MAKE_VXWORKS
+    	std::cout << pthread_self() << " runLoop()" << std::endl;
+    #else
+    	std::cout << taskIdSelf() << " runLoop()" << std::endl;
+    #endif
+    */
 	}
+    int num;
 };
 
 int main(int argc, char *argv[])
@@ -104,6 +114,21 @@ int main(int argc, char *argv[])
     unsetenv("ACS_LOG_STDOUT");
 #endif
 
+
+    if (argc < 2)
+	{
+	printf ("Usage: %s # of threads\n",argv[0]);
+	return -1;
+	}
+
+    int numOfThread = atoi(argv[1]);
+
+    if (numOfThread > MAX_THREADS)
+	{
+	printf ("ERROR: # of threads should be less than %d\n", MAX_THREADS);
+	return -2;
+	}
+
     LoggingProxy logger_m(0, 0, 31);
     LoggingProxy::init(&logger_m);
 
@@ -111,19 +136,20 @@ int main(int argc, char *argv[])
     int l = 0;
 
     ACE_thread_t threadId;
-    ACE_thread_t threadIdVec[MAX_THREADS];
+    ACE_thread_t threadIdVec[numOfThread];
 
    /*
-     * In the first test we create and destroy MAX_THREADS0
+     * In the first test we create and destroy numOfThread
      * DETACHED threads.
      * This should work without problems.
      */
     ACS_SHORT_LOG(( LM_INFO, "===== 1 - create DETACHED threads"));
     try 
 	{
-	for( l=0; l<MAX_THREADS; l++)
+	for( l=0; l<numOfThread; l++)
 	    {
-	    FastACSThread *a = tm.create<FastACSThread>("TestThreadA",
+	    FastACSThread *a = tm.create<FastACSThread, int>("TestThreadA",
+							     l,
 							5000000, /* 500ms */
 							1000000, /* 100ms */
 							false,
@@ -131,6 +157,7 @@ int main(int argc, char *argv[])
 	    // ACS_SHORT_LOG(( LM_INFO, "Progress: %ld thread created", l+1 ));
             a->resume();
 	    threadId = a->getThreadID();
+//	std::cout << threadId << " created " << std::endl; 
 	    tm.destroy(a);
 	    }
 	}
@@ -145,16 +172,18 @@ int main(int argc, char *argv[])
     ACS_SHORT_LOG(( LM_INFO, "Created threads: %ld", l ));
 
     /*
-     * In the second test we TRY to create and destroy MAX_THREADS
+     * In the second test we TRY to create and destroy numOfThread
      * JOINABLE threads, but we do not join them.
      * This should fail with an exception when resources are exausted.
      */
     ACS_SHORT_LOG(( LM_INFO, "===== 2 - create JOINABLE threads"));
+
     try 
 	{
-	for( l=0; l<MAX_THREADS ; l++)
+	for( l=0; l<numOfThread ; l++)
 	    {
-	    FastACSThread *a = tm.create<FastACSThread>("TestThreadA",
+	    FastACSThread *a = tm.create<FastACSThread, int>("TestThreadA",
+							     l,
 							5000000, /* 500ms */
 							1000000, /* 100ms */
 							false,
@@ -193,16 +222,17 @@ int main(int argc, char *argv[])
 	}
 
     /*
-     * In the second test we TRY to create and destroy MAX_THREADS
+     * In the second test we TRY to create and destroy numOfThread
      * JOINABLE threads, but we join them.
      * This should not fail.
      */
     ACS_SHORT_LOG(( LM_INFO, "===== 4 - create, destroy and JOIN threads"));
     try 
 	{
-	for( l=0; l<MAX_THREADS; l++)
+	for( l=0; l<numOfThread; l++)
 	    {
-	    FastACSThread *a = tm.create<FastACSThread>("TestThreadA",
+	    FastACSThread *a = tm.create<FastACSThread, int>("TestThreadA",
+							     l,
 							5000000, /* 500ms */
 							1000000, /* 100ms */
 							false,
@@ -227,13 +257,14 @@ int main(int argc, char *argv[])
 	}
 
     /*
-     * In the second test we TRY to create and destroy MAX_THREADS
+     * In the second test we TRY to create and destroy 
      * JOINABLE threads, but we join them.
      * This should not fail.
      */
     ACS_SHORT_LOG(( LM_INFO, "===== 5 - create, destroy and join a DETACHED thread"));
     try {
-        FastACSThread *a = tm.create<FastACSThread>("TestThreadA",
+    FastACSThread *a = tm.create<FastACSThread, int>("TestThreadA",
+						     l,
  						    5000000, /* 500ms */
 						    1000000, /* 100ms */
 						    false,
