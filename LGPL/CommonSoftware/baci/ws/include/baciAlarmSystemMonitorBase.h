6d7e29e5f1e819961e0f5d9079a7108c3cfd1f69
bjeram <bjeram@523d945c-050c-4681-91ec-863ad3bb968a>
2009-09-15 10:49:58 +0000

    added the file
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@116153 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/baci/ws/include/baciAlarmSystemMonitorBase.h
@@ -0,0 +1,170 @@
+#ifndef _ALARM_SYSTEM_MONITOR_BASE_H
+#define _ALARM_SYSTEM_MONITOR_BASE_H
+/*******************************************************************************
+ * ALMA - Atacama Large Millimiter Array
+ * (c) European Southern Observatory, 2006
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ *
+ * "@(#) $Id: baciAlarmSystemMonitorBase.h,v 1.1 2009/09/15 10:49:58 bjeram Exp $"
+ *
+ * who       when      what
+ * --------  --------  ----------------------------------------------
+ * bjeram  2006-09-13  created
+ */
+
+/************************************************************************
+ *
+ *----------------------------------------------------------------------
+ */
+
+#ifndef __cplusplus
+#error This is a C++ include file and cannot be used from plain C
+#endif
+
+#include <AlarmSystemInterface.h>
+#include <baciEvent.h>
+
+namespace baci
+{
+
+/**
+ *  Base class for the Alarm System Monitor that is not template class.
+ *  The purpose of this class to have a not template base class for all Alarm System monitors.
+ */
+class baci_EXPORT AlarmSystemMonitorBase : public EventStrategy
+{
+public:
+
+	AlarmSystemMonitorBase(EventDispatcher * eventDispatcher);
+
+	virtual ~AlarmSystemMonitorBase();
+
+	bool failed();
+	void succeeded();
+
+	virtual bool isSuspended() { return false; }
+
+	// here we do not need recovery stuff
+	virtual int getId(void){ return -1; }
+
+	virtual const char* getName(void){ return ""; }
+
+	virtual char* getObjectState(void){ return ""; }
+
+	virtual void setObjectState(const char * state){}
+	// ... and also implementation of  POA_ACS::Subscription can be empty
+	virtual void suspend () {}
+
+	virtual void resume () {}
+
+	virtual void destroy () {}
+
+	virtual void check(BACIValue &val,
+			const ACSErr::Completion & c,
+			const ACS::CBDescOut & desc )=0;
+
+	/**
+	 * Send an alarm to the AlarmSystem
+	 */
+	void sendAlarm(int code, bool active);
+
+
+	/**
+	 * sets property of the alarm.
+	 * If the property was already set it will be set to a new value.
+	 * @parm name - property name
+	 * @parm prop value of the property
+	 *
+	 */
+	void setProperty(const char *name, const char *prop);
+
+	template <class VT>
+	void setProperty(const char *name, VT value)
+	{
+		std::ostringstream ostr;
+		std::string ts;
+
+		ostr << value << std::ends;
+		ts =  ostr.str();
+		setProperty(name, ts.c_str());
+	}//setProperty<>
+
+	/**
+	 * set level of the alarm
+	 * @parm alarm level
+	 */
+	void setLevel(int level){ alarmLevel_m = level; }
+
+	/**
+	 * (re)set Alarm's fault family
+	 * @param fault family
+	 */
+	void setFaultFamily(const char *ff);
+
+	/**
+	 * (re)set Alarm's fault member
+	 * @param fault member
+	 */
+	void setFaultMember(const char *fm);
+
+private:
+
+	/**
+	 * ALMA C++ coding standards state assignment operators should be disabled.
+	 */
+	void operator=(const AlarmSystemMonitorBase&);
+
+	/**
+	 * ALMA C++ coding standards state copy constructors should be disabled.
+	 */
+	AlarmSystemMonitorBase(const AlarmSystemMonitorBase&);
+
+
+protected:
+	ACE_CString name_m;
+
+	bool suspended_m;
+
+	int failureCount_m;
+
+	CBDescIn desc_mIn;
+
+	ACS::TimeInterval interval_m;
+
+	EventDispatcher * eventDispatcher_mp;
+
+	// The alarm system source
+	auto_ptr<acsalarm::AlarmSystemInterface> alarmSource_map;
+
+	int alarmRaised_m;
+
+	std::string faultMember_m;  /// alarm fault member that is used to send alarm
+	std::string faultFamily_m;  /// alarm fault  family that is used to send alarm
+
+	int lastAlarmFaultCode_m; ///fault code of sent alarm
+
+	int alarmLevel_m;  /// alarm's level (priorty)
+
+	// here we can store alarm propertes
+	std::map<std::string, std::string> alarmProperties_m;
+
+	ACE_Recursive_Thread_Mutex faultStructMutex_m;
+};//class AlarmSystemMonitorBase
+
+}// namespace baci
+
+#endif /*!_H*/
+
