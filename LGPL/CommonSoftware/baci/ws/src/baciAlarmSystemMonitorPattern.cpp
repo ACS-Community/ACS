47b63bd9b7186e7a51e74296f2a75e12deb1b41c
bjeram <bjeram@523d945c-050c-4681-91ec-863ad3bb968a>
2009-09-25 13:56:01 +0000

    Added for pattern specialized implementation of clearAlarm. Modified check method that:   - increase/decrease  alarmRaised_m when an alarm is reased/cleared   - is stored value that triggered alarm (lastAlarmValue_m)
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@116688 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/baci/ws/src/baciAlarmSystemMonitorPattern.cpp
+++ LGPL/CommonSoftware/baci/ws/src/baciAlarmSystemMonitorPattern.cpp
@@ -19,6 +19,46 @@ AlarmSystemMonitorPattern::~AlarmSystemMonitorPattern()
 {
 }//~AlarmSystemMonitorPattern
 
+void AlarmSystemMonitorPattern::clearAlarm()
+{
+	ACE_TRACE("baci::AlarmSystemMonitorPattern::clearAlarm");
+	ACE_Guard<ACE_Recursive_Thread_Mutex>  protSect(faultStructMutex_m);
+
+	alarmMask_m = property_mp->alarm_mask();
+	alarmTrigger_m = property_mp->alarm_trigger();
+	bitDescriptions_mp = property_mp->bitDescription();
+
+	ACS::pattern tmpVal = lastValue_m; //tmp storage of the value
+	// here we artifficaly set the last value, so that next check can trigger alarm(s)
+	lastValue_m = (~alarmMask_m & lastValue_m) | (alarmMask_m & ~alarmTrigger_m);
+
+	for(bitPos_m=0; bitPos_m<patternSize_m; bitPos_m++)
+	{
+		// we check if bit by bit
+		if ((alarmMask_m & ~(alarmTrigger_m ^ tmpVal ) & 1) )
+		{
+			setProperty("BACI_BitPosition", bitPos_m);
+			const char *bitDesc;
+			if (bitPos_m<bitDescriptions_mp->length())
+			{
+			   bitDesc = (*bitDescriptions_mp)[bitPos_m];
+			}
+			else
+			{
+				bitDesc = "w/o description";
+			}
+			setProperty("BACI_BitDescription", bitDesc);
+			setProperty("BACI_Value", tmpVal);
+			ACS_SHORT_LOG((LM_ALERT, "Alarm for property: %s, bit %d (%s) cleared. Value change to: %d", property_mp->name(), bitPos_m, bitDesc, tmpVal));
+			this->sendAlarm(1, false);
+			this->alarmRaised_m --;
+		}//if
+		// shift to the next bit
+		tmpVal >>= 1;
+		alarmMask_m >>= 1;
+		alarmTrigger_m >>= 1;
+	}//for
+}//clearAlarm
 
 void AlarmSystemMonitorPattern::check(BACIValue &val,
 								const ACSErr::Completion & c,
@@ -26,7 +66,8 @@ void AlarmSystemMonitorPattern::check(BACIValue &val,
     )
 {
     ACE_UNUSED_ARG(c);
-  
+    ACE_Guard<ACE_Recursive_Thread_Mutex>  protSect(faultStructMutex_m);
+
     ACS::pattern value = val.getValue(static_cast<ACS::pattern*>(0)); //val.patternValue();
     
     // first we just check if the value has been changed since last time
@@ -38,6 +79,7 @@ void AlarmSystemMonitorPattern::check(BACIValue &val,
     	bitDescriptions_mp = property_mp->bitDescription();
 
     	ACS::pattern tmpVal = value; //tmp storage of the value
+
     	for(bitPos_m=0; bitPos_m<patternSize_m; bitPos_m++)
     	{
     		//we check if the bit at position bitPos has been changed
@@ -61,11 +103,15 @@ void AlarmSystemMonitorPattern::check(BACIValue &val,
     			{
     				ACS_SHORT_LOG((LM_ALERT, "Alarm for property: %s, bit %d (%s) raised. Value change to: %d", property_mp->name(), bitPos_m, bitDesc, value));
     				this->sendAlarm(1, true);
+    				this->lastAlarmValue_m = val;
+    				this->alarmRaised_m ++;
       			}
     			else
     			{
     				ACS_SHORT_LOG((LM_ALERT, "Alarm for property: %s, bit %d (%s) cleared. Value change to: %d", property_mp->name(), bitPos_m, bitDesc, value));
     				this->sendAlarm(1, false);
+    				this->lastAlarmValue_m = val;
+    				this->alarmRaised_m --;
       			}//if-else
     		}//if
     		// shift to the next bit 
