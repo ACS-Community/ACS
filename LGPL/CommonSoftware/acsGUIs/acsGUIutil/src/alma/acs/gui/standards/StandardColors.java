1538889dc4d00cd8d34ec5bd4ea16613584077af
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2009-10-06 17:01:43 +0000

    This commit was generated by cvs2svn to compensate for changes in r117272, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@117273 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/acsGUIs/acsGUIutil/src/alma/acs/gui/standards/StandardColors.java
@@ -0,0 +1,62 @@
+package alma.common.gui.standards;
+
+import java.awt.Color;
+
+/**
+ * The predefined colors available to Alma applications.
+ */
+public enum StandardColors {
+
+	// ==> http://www.easycalculation.com/color-coder.php
+	
+	TEXT_FG("333333"), // darkgray-ish
+
+	MAIN_BG("ECE9D8"), // sand-ish
+	EDITOR_BG ("F8F8F8"), // white-ish
+	SELECTION_BG ("A0B4D2"), // blueGray-ish
+	SELECTION_NOFOCUS_BG ("lightGray"),
+
+	STATUS_OK_BG ("54FB3C"), // green-ish
+	STATUS_UNAVAILABLE_BG ("gray"),
+	STATUS_UNKNOWN_BG ("gray"),
+	STATUS_WARNING_BG ("FCD152"), // orange-ish
+	STATUS_DELAY_BG ("F9FD4A"), // yellow-ish
+	STATUS_ERROR_BG ("F95A3C"), // red-ish
+
+	STATE_SHUTDOWN_BG ("F9FD4A"), // yellow-ish
+	STATE_TRANSITING_BG ("F9FD4A"), // yellow-ish
+	STATE_OPERATIONAL_BG ("54FB3C"); // green-ish
+
+
+	/**
+	 * This field contains the actual color needed by clients
+	 */
+	public Color color;
+
+	private StandardColors(String rgb) {
+		this.color = decode(rgb);
+	}
+
+	/**
+	 * Translates colors to strings
+	 */
+	public static String encode (Color c) {
+		return Integer.toHexString(c.getRGB()).toUpperCase().substring(2);
+	}
+
+	/**
+	 * Translates strings to colors
+	 */
+	public static Color decode (String rgb) {
+		try {
+			return Color.decode("0x" + rgb);
+		} catch (NumberFormatException exc) {/* simply continue */}
+
+		try {
+			return (Color) Color.class.getDeclaredField(rgb).get(null);
+		} catch (Exception exc) {/* simply continue */}
+
+		System.err.println("failed to decode " + rgb);
+		return null;
+	}
+}
---
+++ LGPL/CommonSoftware/acsGUIutil/src/alma/acs/gui/standards/StandardIcons.java
@@ -0,0 +1,62 @@
+package alma.common.gui.standards;
+
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+
+/**
+ * The predefined icons available to Alma applications.
+ */
+public enum StandardIcons {
+
+	 START ("play.gif")
+	,STOP ("stop.gif")
+	,CANCEL ("cross_red.gif")
+	,APPROVE ("checkbox_on.gif")
+	,DISMISS ("cross_black.gif")
+	,PAUSE ("pause.gif")
+	,RESUME ("resume.gif")
+	,UNKNOWN ("questionmark.gif")
+	,WARNING ("warning.gif")
+	,WARNING_INACTIVE ("warning_gray.gif")
+	,IDEA ("bulb_yellow.gif")
+	,IDEA_INACTIVE ("bulb_gray.gif")
+	,DOT_GRAY ("dot_gray.gif")
+	,DOT_GREEN ("dot_green.gif")
+	,DOT_RED ("dot_red.gif")
+	,DOT_YELLOW ("dot_yellow.gif")
+	;
+
+	/**
+	 * This field contains the actual icon needed by clients
+	 */
+	public ImageIcon icon;
+
+	private StandardIcons(String loc) {
+		this.icon = decode(loc);
+	}
+
+	/**
+	 * Translates icons to strings
+	 */
+	public static String encode (ImageIcon icon) {
+		return icon.getDescription();
+	}
+
+	/**
+	 * Translates strings to icons
+	 */
+	public static ImageIcon decode (String loc) {
+		String resdir = "alma/common/gui/standards/resources/";
+		try {
+			if (!loc.startsWith("/") && !loc.startsWith(resdir))
+				loc = resdir + loc;
+			URL url = StandardIcons.class.getClassLoader().getResource(loc);
+			return new ImageIcon(url, loc);
+
+		} catch (Exception exc) {
+			System.err.println("failed to decode " + loc);
+			return null;
+		}
+	}
+}
