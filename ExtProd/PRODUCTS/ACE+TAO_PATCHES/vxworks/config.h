c483ba239f54cd5b1f400c7d66d2d139e6b448fd
bjeram <bjeram@523d945c-050c-4681-91ec-863ad3bb968a>
2009-10-12 07:38:04 +0000

    updated for VxWorks 6.7
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@117601 523d945c-050c-4681-91ec-863ad3bb968a

--- ExtProd/PRODUCTS/ACE+TAO_PATCHES/vxworks/config.h
+++ ExtProd/PRODUCTS/ACE+TAO_PATCHES/vxworks/config.h
@@ -1,23 +1,23 @@
 //* -*- C++ -*- */
-// $Id: config.h,v 1.8 2008/10/27 13:37:19 bjeram Exp $
+// $Id: config.h,v 1.9 2009/10/12 07:38:04 bjeram Exp $
 
 // The following configuration file is designed to work for VxWorks
 // 6.6 platforms using one of these compilers:
 // 1) The GNU g++ compiler that is shipped with VxWorks 6.6
 // 2) The Diab compiler that is shipped with VxWorks 6.6
 
-#ifndef ACE_CONFIG_VXWORKS_6_6_H
-#define ACE_CONFIG_VXWORKS_6_6_H
+#ifndef ACE_CONFIG_VXWORKS_6_7_H
+#define ACE_CONFIG_VXWORKS_6_7_H
 #include /**/ "ace/pre.h"
 
 #define ACE_NEEDS_HUGE_THREAD_STACKSIZE 102400
 #define ACE_USE_RCSID 1
 
 #if !defined (ACE_VXWORKS)
-# define ACE_VXWORKS 0x660
+# define ACE_VXWORKS 0x670
 #endif /* ! ACE_VXWORKS */
 
-#include "ace/config-vxworks6.5.h"
+#include "ace/config-vxworks6.6.h"
 
 #if defined (ACE_HAS_PENTIUM)
 # define ACE_LACKS_LOG2
@@ -28,5 +28,5 @@
 #endif
 
 #include /**/ "ace/post.h"
-#endif /* ACE_CONFIG_VXWORKS_6_6_H */
+#endif /* ACE_CONFIG_VXWORKS_6_7_H */
 
