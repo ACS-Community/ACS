4578c859708b292c37e614ca18e66194aa01223a
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2009-11-11 18:19:12 +0000

    Added DELOUSE (but using debug.gif -- still need to define/add its own gif)
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@119452 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsGUIs/jlog/src/com/cosylab/logging/client/EntryTypeIcon.java
+++ LGPL/CommonSoftware/acsGUIs/jlog/src/com/cosylab/logging/client/EntryTypeIcon.java
@@ -42,6 +42,7 @@ public enum EntryTypeIcon {
 	 * 
 	 */
     TRACE_ICON(LogTypeHelper.TRACE,"/trace.gif"),
+    DELOUSE_ICON(LogTypeHelper.DELOUSE,"/debug.gif"),
     DEBUG_ICON(LogTypeHelper.DEBUG,"/debug.gif"),
     INFO_ICON(LogTypeHelper.INFO,"/information.gif"),
     NOTICE_ICON(LogTypeHelper.NOTICE,"/notice.gif"),
