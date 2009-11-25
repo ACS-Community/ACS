e5113b000f0c8c6484be014bd5aa9b37839e1951
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2009-11-25 18:50:54 +0000

    Convert a ILogEntry to a plain ASCII string
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@120288 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/acsGUIs/jlog/src/alma/acs/logging/tools/TextConverter.java
@@ -0,0 +1,48 @@
+/*
+ *    ALMA - Atacama Large Millimiter Array
+ *    (c) European Southern Observatory, 2009
+ *    Copyright by ESO (in the framework of the ALMA collaboration)
+ *    and Cosylab 200, All rights reserved
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *    MA 02111-1307  USA
+ */
+package alma.acs.logging.tools;
+
+/**
+ * Convert a log into a simple ASCII text.
+ * <P> 
+ * The formatted string is composed of plain ASCII, the fields are
+ * separated by a space.
+ * <P>
+ * The processing is done by delegating to the parent CSVConverter. 
+ * 
+ * @author acaproni
+ * @since ACS 8.1.0
+ */
+public class TextConverter extends CSVConverter {
+
+	/**
+	 * Constructor
+	 * 
+	 * @param cols The columns to write in the converted string;
+	 * 				it can be <code>null</code>
+	 * @see LogConverter#colIndex
+	 */
+	public TextConverter(String cols) {
+		super(cols,' ',false);
+	}
+	
+}
