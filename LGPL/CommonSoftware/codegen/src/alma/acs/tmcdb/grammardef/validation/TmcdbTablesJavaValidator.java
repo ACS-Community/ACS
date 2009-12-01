130d46d1a805b860c0629d24f7aee362c69619aa
rtobar <rtobar@523d945c-050c-4681-91ec-863ad3bb968a>
2009-12-01 12:34:59 +0000

    Moved SM generator to alma.acs.sm package. Added new TMCDB tables schema grammar definition, in alma.acs.tmcdb.grammadef
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@120519 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/codegen/src/alma/acs/tmcdb/grammardef/validation/TmcdbTablesJavaValidator.java
@@ -0,0 +1,15 @@
+package alma.acs.tmcdb.grammardef.validation;
+
+import alma.acs.tmcdb.grammardef.validation.AbstractTmcdbTablesJavaValidator;
+ 
+
+public class TmcdbTablesJavaValidator extends AbstractTmcdbTablesJavaValidator {
+
+//	@Check
+//	public void checkTypeNameStartsWithCapital(Type type) {
+//		if (!Character.isUpperCase(type.getName().charAt(0))) {
+//			warning("Name should start with a capital", MyDslPackage.TYPE__NAME);
+//		}
+//	}
+
+}
