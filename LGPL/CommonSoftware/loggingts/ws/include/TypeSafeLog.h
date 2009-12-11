935980cd2b41e3031098acdcdc1a0e5ec2e9ea49
bjeram <bjeram@523d945c-050c-4681-91ec-863ad3bb968a>
2009-12-11 08:56:11 +0000

    ported from ACS_8_1_0-B to TRUNK: added virtual dtor to get rid of warning msg: TypeSafeLog.h:37: warning: class Logging::TypeSafeLog has virtual functions but non-virtual destructor
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@121293 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/loggingts/ws/include/TypeSafeLog.h
+++ LGPL/CommonSoftware/loggingts/ws/include/TypeSafeLog.h
@@ -18,7 +18,7 @@
 * License along with this library; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 *
-* "@(#) $Id: TypeSafeLog.h,v 1.2 2007/03/23 07:53:06 nbarriga Exp $"
+* "@(#) $Id: TypeSafeLog.h,v 1.3 2009/12/11 08:56:11 bjeram Exp $"
 *
 * who       when      what
 * --------  --------  ----------------------------------------------
@@ -36,6 +36,8 @@
 namespace Logging{
         class TypeSafeLog{
                 public:
+						virtual ~TypeSafeLog(){};
+
                         virtual void log()=0;
         };
 };
