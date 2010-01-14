30a071a753453bd900d13f17c5ea247a1a55000f
javarias <javarias@523d945c-050c-4681-91ec-863ad3bb968a>
2010-01-14 17:36:33 +0000

    Modified the acsdataClean script to clean all the data of the other hosts that share the same $ACSDATA dir under the same machine. This patch should close COMP-4053
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@123339 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsstartup/src/acsdataClean.py
+++ LGPL/CommonSoftware/acsstartup/src/acsdataClean.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 ################################################################################################
-# @(#) $Id: acsdataClean.py,v 1.4 2009/09/18 06:39:11 hyatagai Exp $
+# @(#) $Id: acsdataClean.py,v 1.5 2010/01/14 17:36:33 javarias Exp $
 #
 #    ALMA - Atacama Large Millimiter Array
 #    (c) Associated Universities, Inc. Washington DC, USA, 2001
@@ -38,7 +38,7 @@ from os import listdir
 from os import remove, system
 
 from os.path import exists
-from os.path import isfile, isdir
+from os.path import isfile, isdir, islink
 
 from sys import argv
 
@@ -47,68 +47,90 @@ import socket
 from AcsutilPy.ACSDirectory import getAcsTmpDirectoryPath
 
 #------------------------------------------------------------------------------
-__version__ = "$Id: acsdataClean.py,v 1.4 2009/09/18 06:39:11 hyatagai Exp $"
+__version__ = "$Id: acsdataClean.py,v 1.5 2010/01/14 17:36:33 javarias Exp $"
 #------------------------------------------------------------------------------
-
-print "Cleaning up ACS temporary directories"
 
 ################################################################################################
 # Check from the command line if -a (-all) option si give and ALL
 # directories have to be cleaned up
-safeEntries = {}
+def cleanDirectory(dir):
+   safeEntries = {}
+   
+   if argv.count("-all")==0 and argv.count("-a")==0:
+      #these files/directories will NOT be removed.
+      safeEntries = {
+          'ACS_INSTANCE.0'       : '',
+          'ACS_INSTANCE.1'       : '',
+          'ACS_INSTANCE.2'       : '',
+          'ACS_INSTANCE.3'       : '',
+          'ACS_INSTANCE.4'       : '',
+          'ACS_INSTANCE.5'       : '',
+          'ACS_INSTANCE.6'       : '',
+          'ACS_INSTANCE.7'       : '',
+          'ACS_INSTANCE.8'       : '',
+          'ACS_INSTANCE.9'       : '',
+          'USED_CONTAINER_PORTS' : '',
+          '.acs_command_history' : ''
+       }
+      print "Safe directories (i.e., ACS_INSTANCE.*'s) and a few files used by ACS will be"
+      print "preserved. To remove everything, provide the '-all' switch to this script."
+   else:
+      print "Cleaning up ALL directories"
+      print "It is ONLY SAFE and recommended to do this after a machine has been rebooted or"
+      print "killACS has been run! Use at your own risk."
+   
+   ################################################################################################
+   try:
+       myDir = dir
+   
+       #make sure acs temp exists
+       if not exists(myDir):
+           print "'", myDir, "' does not exist!"
+           exit(1)
+   
+       #get a list of all entries in it.
+       tempList = listdir(myDir)
+   
+       #move to it
+       chdir(myDir)
+   
+       #iterate through every entry
+       for entry in tempList:
+           #if OK to remove...
+           if (not safeEntries.has_key(entry)) and (isdir(entry)):
+               try:
+                   system("rm -rf " + entry)
+               except:
+                   print "Unable to remove this directory:", entry
+                   
+           elif (not safeEntries.has_key(entry)) and (isfile(entry)):
+               try:
+                   remove(entry)
+               except:
+                   print "Unable to remove this file:", entry  
+   
+   except Exception, e:
+       print "An exception occured in acsdataClean.py's main:", e
 
-if argv.count("-all")==0 and argv.count("-a")==0:
-   #these files/directories will NOT be removed.
-   safeEntries = {
-       'ACS_INSTANCE.0'       : '',
-       'ACS_INSTANCE.1'       : '',
-       'ACS_INSTANCE.2'       : '',
-       'ACS_INSTANCE.3'       : '',
-       'ACS_INSTANCE.4'       : '',
-       'ACS_INSTANCE.5'       : '',
-       'ACS_INSTANCE.6'       : '',
-       'ACS_INSTANCE.7'       : '',
-       'ACS_INSTANCE.8'       : '',
-       'ACS_INSTANCE.9'       : '',
-       'USED_CONTAINER_PORTS' : '',
-       '.acs_command_history' : ''
-    }
-   print "Safe directories (i.e., ACS_INSTANCE.*'s) and a few files used by ACS will be"
-   print "preserved. To remove everything, provide the '-all' switch to this script."
+
+if argv.count("-h")==0 or argv.count("--help")==0:
+   print "Clean the temporal directories of ACS"
+   print "Options:"
+   print "-h or --help : Show this message and then exit.\n"
+   print "-all : acsdataClean will remove all the directory entries under the tmp directory. By default the ACS_INSTANCE.* directories are preserved.\n"
+   print "-other_hosts : acsdataClean will clean all the directories located under $ACSDATA/tmp/ this will clean the tmp directories of the other hosts sharing the $ACSDATA in the same machine.\n"
+   exit(0)
+
+print "Cleaning up ACS temporary directories"
+myDir = getAcsTmpDirectoryPath()
+if argv.count("-other_hosts")==0:
+   cleanDirectory(myDir)
 else:
-   print "Cleaning up ALL directories"
-   print "It is ONLY SAFE and recommended to do this after a machine has been rebooted or"
-   print "killACS has been run! Use at your own risk."
+   print "Cleanning all the possible hosts' temporary directories"
+   myDir = myDir + "/../"
+   fileList = listdir(myDir)
+   for file in fileList:
+      if isdir(myDir + file):
+         print "Cleanning " + file
+         cleanDirectory(myDir + file)
 
-################################################################################################
-try:
-    myDir = getAcsTmpDirectoryPath()
-
-    #make sure acs temp exists
-    if not exists(myDir):
-        print "'", myDir, "' does not exist!"
-        exit(1)
-
-    #get a list of all entries in it.
-    tempList = listdir(myDir)
-
-    #move to it
-    chdir(myDir)
-
-    #iterate through every entry
-    for entry in tempList:
-        #if OK to remove...
-        if (not safeEntries.has_key(entry)) and (isdir(entry)):
-            try:
-                system("rm -rf " + entry)
-            except:
-                print "Unable to remove this directory:", entry
-                
-        elif (not safeEntries.has_key(entry)) and (isfile(entry)):
-            try:
-                remove(entry)
-            except:
-                print "Unable to remove this file:", entry  
-
-except Exception, e:
-    print "An exception occured in acsdataClean.py's main:", e
