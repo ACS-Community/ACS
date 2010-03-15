382e913c1ad4fbe501165551b9adf329a4f6bd81
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2010-03-15 14:11:09 +0000

    Fixed javadoc that broke doxygen (see COMP-3984); Made hasRepeatGuard and getRepeatGuard synchronized.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@127316 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/repeatGuard/ws/src/alma/acs/logging/MultipleRepeatGuard.java
+++ LGPL/CommonSoftware/repeatGuard/ws/src/alma/acs/logging/MultipleRepeatGuard.java
@@ -7,10 +7,10 @@ import java.util.concurrent.TimeUnit;
 import alma.acs.logging.RepeatGuard.Logic;
 
 /**
- * This class can be used as a convenient map that keeps {@link RepeatGuard} objects associated with {@code String IDs}.
+ * This class can be used as a convenient map that keeps {@link RepeatGuard} objects associated with <code>String IDs</code>.
  * For example, if your application repeatedly sends an event related to the unavailability of some component,
  * and you want to limit these events on a per-component basis, then you either need to keep and use a separate RepeatGuard object for every component instance,
- * or alternatively you can use one instance of {@code MultipleRepeatGuard} and take the component names as {@code ID}s.
+ * or alternatively you can use one instance of <code>MultipleRepeatGuard</code> and take the component names as <code>ID</code>s.
  * <p>
  * The additional benefit is that this class optionally offers a bounded cache for the various RepeatGuards, which limits memory consumption for cases
  * in which the number of possible IDs is large, e.g. if the ID is the log message coming from unknown / 3rd party source. 
@@ -36,7 +36,7 @@ public class MultipleRepeatGuard
 	/////////////////////////////////////////////////////////////////////////////////////
 
 	/**
-	 * Constructor for a {@code MultipleRepeatGuard} with a bounded RepeatGuard cache.
+	 * Constructor for a <code>MultipleRepeatGuard</code> with a bounded RepeatGuard cache.
 	 * <p>
 	 * The first 4 parameters are the same as in {@link RepeatGuard#RepeatGuard(long, TimeUnit, int, Logic)}
 	 * and are used as defaults for all RepeatGuards created by this class.
@@ -50,10 +50,10 @@ public class MultipleRepeatGuard
 	 *        The logic will be "reduced" automatically if <code>interval</code> or <code>maxRepetitions</code> 
 	 *        have a value <= 0, so as to be based only on the other positive value.
 	 * @param maxCacheSize
-	 *        Number of {@code ID}s (as used in ..) whose repetition we'll keep track of.
+	 *        Number of <code>ID</code>s (as used in {@link #check(String)}) whose repetition we'll keep track of.
 	 *        Setting this value higher will use more memory;
 	 *        setting it lower than the actual number of IDs will result in unnecessary execution of the guarded actions,
-	 *        as the matching {@code RepeatGuard}s will be lost and re-created.
+	 *        as the matching <code>RepeatGuard</code>s will be lost and re-created.
 	 * @throws IllegalArgumentException if maxRepetitions <= 0 && interval <= 0
 	 */
 	public MultipleRepeatGuard(long interval, TimeUnit timeUnit, int maxRepetitions, Logic logic, int maxCacheSize) {
@@ -66,7 +66,7 @@ public class MultipleRepeatGuard
 	}
 	
 	/**
-	 * Constructor for a {@code MultipleRepeatGuard} without a bounded RepeatGuard cache 
+	 * Constructor for a <code>MultipleRepeatGuard</code> without a bounded RepeatGuard cache 
 	 * (which means that memory limits indirectly set the bound, with possible side effects).
 	 * @see #MultipleRepeatGuard(long, TimeUnit, int, Logic, int) 
 	 */
@@ -122,17 +122,17 @@ public class MultipleRepeatGuard
 	////////////////////////////// Map administration ///////////////////////////////////
 	/////////////////////////////////////////////////////////////////////////////////////
 
-	public boolean hasRepeatGuard(String ID) {
+	public synchronized boolean hasRepeatGuard(String ID) {
 		return guards.containsKey(ID);
 	}
 	
-	public RepeatGuard getRepeatGuard(String ID) {
+	public synchronized RepeatGuard getRepeatGuard(String ID) {
 		return getOrCreateRepeatGuard(ID);
 	}
 	
 	/**
-	 * Sets a possibly modified {@code RepeatGuard} for the given {@code ID}, 
-	 * which allows for some {@code RepeatGuard}s having different settings than those passed in 
+	 * Sets a possibly modified <code>RepeatGuard</code> for the given <code>ID</code>, 
+	 * which allows for some <code>RepeatGuard</code>s having different settings than those passed in 
 	 * {@link #MultipleRepeatGuard(long, TimeUnit, int, Logic, int)}. 
 	 * @param ID
 	 * @param guard
@@ -178,13 +178,13 @@ public class MultipleRepeatGuard
 
 		/**
 		 * Constructor.
-		 * The initial capacity of this map is determined as {@code Math.min(16, maxEntries)}.
+		 * The initial capacity of this map is determined as <code>Math.min(16, maxEntries)</code>.
 		 * The load factor is taken as 0.75 which is the default for other map types.
 		 * <p>
 		 * @param maxEntries the maximum capacity of this map. 
 		 * @param accessOrder
 		 *            if true, then this map is an LRU (least-recently used) cache that will remove the least recently used entries 
-		 *            when the number of stored entries has reached {@code maxEntries} and a new entry is stored; 
+		 *            when the number of stored entries has reached <code>maxEntries</code> and a new entry is stored; 
 		 *            otherwise insertion order is used (removing the entry that was inserted first).
 		 *            See {@link LinkedHashMap#LinkedHashMap(int, float, boolean)}.
 		 */
@@ -200,7 +200,7 @@ public class MultipleRepeatGuard
 		
 		/**
 		 * Sets a new value for the maximum number of entries in this map,
-		 * overriding the {@code maxEntries} value given in the constructor
+		 * overriding the <code>maxEntries</code> value given in the constructor
 		 * or in previous invocations of this method.
 		 * @param maxEntries must be a positive number
 		 */
