8e3201020a0d34db59c8f4682772ace02a8d7b0d
bjeram <bjeram@523d945c-050c-4681-91ec-863ad3bb968a>
2010-06-08 08:02:58 +0000

    updated patches/config files etc for ACE+TAO 5.7.8
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@133215 523d945c-050c-4681-91ec-863ad3bb968a

--- ExtProd/PRODUCTS/ACE+TAO_PATCHES/vxworks/config.h
+++ ExtProd/PRODUCTS/ACE+TAO_PATCHES/vxworks/config.h
@@ -1,32 +1,60 @@
 //* -*- C++ -*- */
-// $Id: config.h,v 1.9 2009/10/12 07:38:04 bjeram Exp $
+// $Id: config.h,v 1.10 2010/06/08 08:02:03 bjeram Exp $
 
 // The following configuration file is designed to work for VxWorks
-// 6.6 platforms using one of these compilers:
-// 1) The GNU g++ compiler that is shipped with VxWorks 6.6
-// 2) The Diab compiler that is shipped with VxWorks 6.6
+// Based on ACE_VXWORKS it will select the correct config file
 
-#ifndef ACE_CONFIG_VXWORKS_6_7_H
-#define ACE_CONFIG_VXWORKS_6_7_H
+#ifndef ACE_CONFIG_VXWORKS_H
+#define ACE_CONFIG_VXWORKS_H
 #include /**/ "ace/pre.h"
 
 #define ACE_NEEDS_HUGE_THREAD_STACKSIZE 102400
 #define ACE_USE_RCSID 1
 
+// If ACE_VXWORKS is not defined try to figure out the VxWorks version
 #if !defined (ACE_VXWORKS)
-# define ACE_VXWORKS 0x670
+# include "vxWorks.h"
+# if !defined (_WRS_VXWORKS_MAJOR) && !defined (_WRS_VXWORKS_MINOR)
+#  error You must define ACE_VXWORKS
+# else
+#  if (_WRS_VXWORKS_MAJOR == 6)
+#   if (_WRS_VXWORKS_MINOR == 0)
+#    define ACE_VXWORKS 0x600
+#   elif (_WRS_VXWORKS_MINOR == 1)
+#    define ACE_VXWORKS 0x610
+#   elif (_WRS_VXWORKS_MINOR == 2)
+#    define ACE_VXWORKS 0x620
+#   elif (_WRS_VXWORKS_MINOR == 3)
+#    define ACE_VXWORKS 0x630
+#   elif (_WRS_VXWORKS_MINOR == 4)
+#    define ACE_VXWORKS 0x640
+#   elif (_WRS_VXWORKS_MINOR == 5)
+#    define ACE_VXWORKS 0x650
+#   elif (_WRS_VXWORKS_MINOR == 6)
+#    define ACE_VXWORKS 0x660
+#   elif (_WRS_VXWORKS_MINOR == 7)
+#    define ACE_VXWORKS 0x670
+#   elif (_WRS_VXWORKS_MINOR == 8)
+#    define ACE_VXWORKS 0x680
+#   endif
+#  endif
+# endif
 #endif /* ! ACE_VXWORKS */
 
-#include "ace/config-vxworks6.6.h"
-
-#if defined (ACE_HAS_PENTIUM)
-# define ACE_LACKS_LOG2
-#endif
-
-#if !defined (__RTP__)
-# undef ACE_HAS_IOCTL_INT_3_PARAM
+#if (ACE_VXWORKS == 0x640)
+# include "ace/config-vxworks6.4.h"
+#elif (ACE_VXWORKS == 0x650)
+# include "ace/config-vxworks6.5.h"
+#elif (ACE_VXWORKS == 0x660)
+# include "ace/config-vxworks6.6.h"
+#elif (ACE_VXWORKS == 0x670)
+# include "ace/config-vxworks6.7.h"
+#elif (ACE_VXWORKS == 0x680)
+# include "ace/config-vxworks6.8.h"
+#else
+#error Unknown or unsupported VxWorks version
 #endif
 
 #include /**/ "ace/post.h"
-#endif /* ACE_CONFIG_VXWORKS_6_7_H */
+#endif /* ACE_CONFIG_VXWORKS_H */
 
