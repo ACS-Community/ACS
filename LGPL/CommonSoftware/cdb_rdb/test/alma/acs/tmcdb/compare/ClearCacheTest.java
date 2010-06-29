3f9618c05f793a7b48751052880148db8288906f
jschwarz <jschwarz@523d945c-050c-4681-91ec-863ad3bb968a>
2010-06-29 12:40:05 +0000

    For Matej: both tests throw exceptions, but for different reasons. (I don't expect the asserts to work, now that we're comparing arrays.)
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@134614 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/cdb_rdb/test/alma/acs/tmcdb/compare/ClearCacheTest.java
+++ LGPL/CommonSoftware/cdb_rdb/test/alma/acs/tmcdb/compare/ClearCacheTest.java
@@ -22,65 +22,102 @@ public class ClearCacheTest extends TestCase {
 
 	private ORB orb;
 	
-	private final CDBAccess rdbAccess;
-	private final Logger logger;
-	private final JDAL rdbDAL;
+	private CDBAccess rdbAccess;
+	private CDBAccess xmlAccess;
+	private Logger logger;
+	private JDAL rdbDAL;
+	private JDAL xmlDAL;
 	
 	private DAO rdbDao;
+	private DAO xmlDao;
 
 	private String firstNode;
 
 	private StopWatch sw;
 
-	private String before;
+	private String[] before;
 
 	private WDAL wdal;
-
+	private WDAL xmlWdal;
+	
 	private WDAO wdao;
+	private WDAO xmlWdao;
+
+	private String[] xbefore;
+
+	private String[] after;
+
 
 	public ClearCacheTest(String name) {
 		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
 		String rdbIOR = "corbaloc::" + ACSPorts.getIP() + ":" + "3012/CDB"; // ACSPorts.getCDBPort() + "/CDB";
+		String xmlIOR = "corbaloc::" + ACSPorts.getIP() + ":" + "3013/CDB"; // ACSPorts.getCDBPort() + "/CDB";
 		String args[] = {};
 		logger = Logger.getAnonymousLogger();
 		Properties props = new Properties();
 		props.setProperty("org.omg.CORBA.ORBClass","org.jacorb.orb.ORB");
 		props.setProperty("org.omg.CORBA.ORBSingletonClass","org.jacorb.orb.ORBSingleton");
 		orb = ORB.init(args, props);
-
+		
 		rdbDAL = JDALHelper.narrow(orb.string_to_object(rdbIOR));
 		rdbAccess = new CDBAccess(orb,logger);
 		rdbAccess.setDAL(rdbDAL);
-	}
-
-	protected void setUp() throws Exception {
-		super.setUp();
+		
+		xmlDAL = JDALHelper.narrow(orb.string_to_object(xmlIOR));
+		xmlAccess = new CDBAccess(orb,logger);
+		xmlAccess.setDAL(xmlDAL);
+		after = new String[]{"fubar","snafu"};
 		String[] rdbNodes = rdbDAL.list_nodes("MACI/Managers").split(" ");
 		if (rdbNodes.length == 0)
 			fail("No Manager nodes present.");
 		firstNode = "MACI/Managers/"+rdbNodes[0];
 		rdbDao = rdbDAL.get_DAO_Servant(firstNode);
+		xmlDao = xmlDAL.get_DAO_Servant(firstNode);
 		sw = new StopWatch(logger);
-		before = rdbDao.get_string("Startup");
+		before = rdbDao.get_string_seq("Startup");
+		xbefore = xmlDao.get_string_seq("Startup");
+		
 		wdal = WDALHelper.narrow(rdbDAL);
 		wdao = wdal.get_WDAO_Servant(firstNode);
+		xmlWdal = WDALHelper.narrow(xmlDAL);
+		xmlWdao = xmlWdal.get_WDAO_Servant(firstNode);
 	}
 
 	protected void tearDown() throws Exception {
-		wdao.set_string("Startup", before);
+		wdao.set_string_seq("Startup", before);
 		rdbDAL.clear_cache(firstNode);
+//		xmlWdao.set_string("Startup", xbefore);
+//		xmlDAL.clear_cache(firstNode);
 		super.tearDown();
 	}
 	
-	public void testWdalChange() throws Exception {
+	public void testRdbWdalChange() throws Exception {
+		wdao.set_string_seq("Startup", after);
 		System.out.println(sw.getLapTimeMillis()+" ms after writing new record");
-		String after = "fubar";
-		wdao.set_string("Startup", after);
 		assertFalse(before.equals(rdbDao.get_string("Startup")));
-		assertEquals(after,rdbDao.get_string("Startup")); // WDAL changes recognized on next read
+//		assertEquals(after,rdbDao.get_string_seq("Startup")); // WDAL changes recognized on next read
+//		assertEquals(after,rdbDao.get_string_seq("Startup")); // See whether second read causes the failure
 		rdbDAL.clear_cache(firstNode);
+//		rdbDao = rdbDAL.get_DAO_Servant(firstNode); // This makes the test succeed
 		System.out.println(sw.getLapTimeMillis()+" ms after clearing the cache");
-		assertEquals(after,rdbDao.get_string("Startup"));
+		assertEquals(after,rdbDao.get_string_seq("Startup"));
+	}
+	
+	public void testXmlWdalChange() throws Exception {
+		System.out.println("xbefore: "+xbefore[0]+" "+xbefore[1]);
+		xmlWdao.set_string_seq("Startup", after);
+		System.out.println(sw.getLapTimeMillis()+" ms after writing new record");
+//		assertFalse(xbefore.equals(xmlDao.get_string("Startup")));
+//		assertEquals(after,xmlDao.get_string_seq("Startup")); // WDAL changes recognized on next read
+//		assertEquals(after,xmlDao.get_string_seq("Startup")); // See whether second read causes the failure
+		xmlDAL.clear_cache(firstNode);
+		xmlDao = xmlDAL.get_DAO_Servant(firstNode); // This makes the test succeed
+		System.out.println(sw.getLapTimeMillis()+" ms after clearing the cache");
+//		assertEquals(after,xmlDao.get_string_seq("Startup"));		
 	}
 
 }
