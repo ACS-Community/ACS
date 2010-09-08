2de313d3e54439087aefe898e8d6d26457ac51bb
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2010-09-08 14:30:17 +0000

    - Added TODO comment about a desirable factory method for alarm sources, so that ACSAlarmSystemInterface can be used without also using a ACSAlarmSystemInterfaceFactory. - Made push methods for multiple alarms generic, taking Collection<ACSFaultState>
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@139058 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jacsalarm/src/alma/alarmsystem/source/ACSAlarmSystemInterface.java
+++ LGPL/CommonSoftware/jacsalarm/src/alma/alarmsystem/source/ACSAlarmSystemInterface.java
@@ -5,7 +5,10 @@ import java.util.Collection;
 /**
  * This interface is derived from cern.laser.source.alarmsysteminterface.AlarmSystemInterface.
  * This is repeated here to avoid dependency with LASER.
- * 
+ * <p>
+ * @TODO Shouldn't we have a factory method for alarm sources here in this interface, 
+ *       so that alarms can be produced with only the ACSAlarmSystemInterface,
+ *       and without the need to use {@link ACSAlarmSystemInterfaceFactory#createFaultState(String, String, int)}?
  * @see cern.laser.source.alarmsysteminterface.AlarmSystemInterface
  * 
  * @author acaproni
@@ -34,10 +37,10 @@ public interface ACSAlarmSystemInterface {
 	/**
 	 * @see cern.laser.source.alarmsysteminterface.AlarmSystemInterface
 	 */
-	public void push(Collection states);
+	public void push(Collection<ACSFaultState> states);
 
 	/**
 	 * @see cern.laser.source.alarmsysteminterface.AlarmSystemInterface
 	 */
-	public void pushActiveList(Collection active);
+	public void pushActiveList(Collection<ACSFaultState> active);
 }
