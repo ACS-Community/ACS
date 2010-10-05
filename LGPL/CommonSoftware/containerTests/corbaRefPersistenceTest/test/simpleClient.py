264a97cf3db2e5c440f7ff81f8671cd18ddf2997
rtobar <rtobar@523d945c-050c-4681-91ec-863ad3bb968a>
2010-10-05 15:46:11 +0000

    Added automatic testing of auto-closing idle connection from containers to manager, triggered by the manager
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@140918 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/containerTests/corbaRefPersistenceTest/test/simpleClient.py
+++ LGPL/CommonSoftware/containerTests/corbaRefPersistenceTest/test/simpleClient.py
@@ -1,34 +1,42 @@
+#! /usr/bin/env python
 #*******************************************************************************
 # ALMA - Atacama Large Millimiter Array
-# (c) European Southern Observatory, 2002
-# Copyright by ESO (in the framework of the ALMA collaboration)
-# and Cosylab 2002, All rights reserved
-#
+# (c) National Research Council of Canada, 2008 
+# 
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
-#
+# 
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-#
+# 
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
-# MA 02111-1307  USA
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-# "@(#) $Id: TestList.lite,v 1.3 2010/10/05 15:46:11 rtobar Exp $"
+# "@(#) $Id: simpleClient.py,v 1.1 2010/10/05 15:46:11 rtobar Exp $"
 #
-# TestList
+# who       when      what
+# --------  --------  ----------------------------------------------
+# arne  2008-06-16  created
 #
-SOURCE testTATEnvironment
-PROLOGUE acsutilTATPrologue
 
-#1  testComponents "acsStartJava -endorsed alma.test.corbareftest.TestComponents &" "testComponents"
+from Acspy.Clients.SimpleClient import PySimpleClient
 
-1  testComponent                "testComponent"
-2  testAutoCloseIdleConnections "testAutoCloseIdleConnections"
+cl = PySimpleClient.getInstance()
+c1 = cl.getComponent("COMP_JAVA")
+c2 = cl.getComponent("COMP_CPP")
+c3 = cl.getComponent("COMP_PY")
 
-EPILOGUE acsutilTATEpilogue
+try:
+	c1.getOtherComponent()
+	c2.getOtherComponent()
+	c3.getOtherComponent()
+except Exception, e:
+	print "Something went wrong on the server side :("
+	print e
+
+print "Bye bye"
