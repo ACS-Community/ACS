47db4e15c38ec0c6ab4d0a20413b9196f8e5b8aa
rtobar <rtobar@523d945c-050c-4681-91ec-863ad3bb968a>
2010-11-26 12:13:30 +0000

    More elaborated tests for shutdown hook on the java client
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@144098 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jcont/test/alma/acs/component/client/ShutdownHookTest.java
+++ LGPL/CommonSoftware/jcont/test/alma/acs/component/client/ShutdownHookTest.java
@@ -1,22 +1,50 @@
 package alma.acs.component.client;
 
+import java.util.logging.Handler;
+import java.util.logging.Level;
+
 import alma.acs.logging.AcsLogger;
 import alma.acs.logging.ClientLogManager;
+import alma.acs.logging.StdOutConsoleHandler;
 
 public class ShutdownHookTest {
 
 	public static void main(String []args) throws Exception {
 
-		AcsLogger m_logger;
+		AcsLogger logger;
 		ComponentClient m_client;
 
-		m_logger = ClientLogManager.getAcsLogManager().getLoggerForApplication("ShutdownHookTestClient", false);
-		m_client = new ComponentClient(m_logger, System.getProperty("ACS.manager"), "ShutdownHookTestClient");
+		logger = ClientLogManager.getAcsLogManager().getLoggerForApplication("ShutdownHookTestClient", false);
 
-		// Sleep a little bit... we'll kill this baby from the outside
-		Thread.sleep(100*1000);
+		if( Integer.parseInt(args[0]) == 0 )
+			m_client = new ComponentClient(logger, System.getProperty("ACS.manager"), "ShutdownHookTestClient") {
+				public void tearDown() throws Exception {
+					for(Handler h : m_logger.getHandlers())
+						if(h instanceof StdOutConsoleHandler)
+							h.setLevel(Level.FINE); // to get the "duplicate call" log
+					super.tearDown();
+				}
+			};
+		else
+			m_client = new ComponentClient(logger, System.getProperty("ACS.manager"), "ShutdownHookTestClient") {
+				public void tearDown() throws Exception {
+					for(Handler h : m_logger.getHandlers())
+						if(h instanceof StdOutConsoleHandler)
+							h.setLevel(Level.FINE); // to get the "duplicate call" log
+					super.tearDown();
+					Thread.sleep(10*1000);
+				}
+			};
 
-		m_client.tearDown();
+		// Sleep a little bit... depending on the amount of time, we might want to kill
+		// this baby from the outside, who knows
+		Thread.sleep(Integer.parseInt(args[1])*1000);
+
+		// We may want to call tearDown() as many times as we want
+		int iterations = Integer.parseInt(args[2]);
+		for(int i=0; i != iterations; i++)
+			m_client.tearDown();
 
 	}
+
 }
