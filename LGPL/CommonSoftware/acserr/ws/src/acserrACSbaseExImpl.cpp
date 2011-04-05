1cf2e4a80a5843d5e5b460dbf5be3278652c94e7
bjeram <bjeram@523d945c-050c-4681-91ec-863ad3bb968a>
2011-04-05 10:35:07 +0000

    fixed assignment operator suggested by Matej
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@151925 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acserr/ws/src/acserrACSbaseExImpl.cpp
+++ LGPL/CommonSoftware/acserr/ws/src/acserrACSbaseExImpl.cpp
@@ -16,14 +16,14 @@
 *License along with this library; if not, write to the Free Software
 *Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 *
-* "@(#) $Id: acserrACSbaseExImpl.cpp,v 1.8 2010/05/31 09:36:51 bjeram Exp $"
+* "@(#) $Id: acserrACSbaseExImpl.cpp,v 1.9 2011/04/05 10:35:07 bjeram Exp $"
 *
 * who       when      what
 * --------  --------  ----------------------------------------------
 * bjeram  2004-02-17  created 
 */
 
-static char *rcsId="@(#) $Id: acserrACSbaseExImpl.cpp,v 1.8 2010/05/31 09:36:51 bjeram Exp $"; 
+static char *rcsId="@(#) $Id: acserrACSbaseExImpl.cpp,v 1.9 2011/04/05 10:35:07 bjeram Exp $"; 
 static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
 
 #include "acserrACSbaseExImpl.h"
@@ -75,8 +75,10 @@ ACSbaseExImpl& ACSbaseExImpl::operator=(const ACSbaseExImpl &t)
 {
     if (this != &t )
 	{
-	m_errorTracePtr =  &m_errorTrace;
-	m_current = m_errorTracePtr;
+		m_errorTrace = t.m_errorTrace;
+		m_errorTracePtr =  &m_errorTrace;
+		m_current = m_errorTracePtr;
+		m_depth = t.m_depth;
 	}
     return *this;
 }//operator=
