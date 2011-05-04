eee0f7c6c263d91d8ee0c93318ab79d7ba61215f
tstaig <tstaig@523d945c-050c-4681-91ec-863ad3bb968a>
2011-05-04 14:14:53 +0000

    Small fix for longLong and uLongLong which are mapped differently in 32-bit and 64-bit machines. They were specified as "long long" and "unsigned long long" in the template specialization of getValue, which is correct for 32-bit machines, but in 64-bit machines they should be rather mapped to "long" and "unsigned long". Due to this, on 64-bit machines if getValue was called for ACS::longLong, for example it wouldn't find the specialization and throw an exception instead.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@154794 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/cdb/ws/src/cdbDAONode.cpp
+++ LGPL/CommonSoftware/cdb/ws/src/cdbDAONode.cpp
@@ -18,7 +18,7 @@
  *    License along with this library; if not, write to the Free Software
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
  *
- * "@(#) $Id: cdbDAONode.cpp,v 1.10 2010/05/07 09:08:05 bjeram Exp $"
+ * "@(#) $Id: cdbDAONode.cpp,v 1.11 2011/05/04 14:14:53 tstaig Exp $"
  *
  * who       when        what
  * --------  ----------  ----------------------------------------------
@@ -429,10 +429,10 @@ long DAONode::getValue(const char * propertyName)
 
 //we have to cast because from DAO we get just get_long
 template<>
-long long DAONode::getValue(const char * propertyName)
+ACS::longLong DAONode::getValue(const char * propertyName)
 {
 	// until get_long is fixed that it can read also xs:long which is 64 bit we have to read string and cast it to long long
-	long long var;
+	ACS::longLong var;
 	CORBA::String_var str = this->get_string( propertyName );
 	std::istringstream is(str.in());
 	(istream&) is >> var ;
@@ -456,11 +456,11 @@ int DAONode::getValue(const char * propertyName)
 
 //we have to cast because from DAO we get just get_long
 template<>
-unsigned long long  DAONode::getValue(const char * propertyName)
+ACS::uLongLong DAONode::getValue(const char * propertyName)
 {
 	// we can not use get_long to read xs:unisngedLong which is 64 bit we have to read string and cast it to unsigned long long
 	// see: COMP-4268
-		unsigned long long var;
+		ACS::uLongLong var;
 		CORBA::String_var str = this->get_string( propertyName );
 		std::istringstream is(str.in());
 		(istream&) is >> var ;
