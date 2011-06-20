293fd1d0ca0eba7443b98d236f5ee9f02a01c054
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2011-06-20 19:13:14 +0000

    INSTANT_STRING renamed to INSTANT
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@157564 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsalarm/include/faultStateConstants.h
+++ LGPL/CommonSoftware/acsalarm/include/faultStateConstants.h
@@ -6,7 +6,7 @@ namespace faultState
    const char * const ACTIVE_STRING = "ACTIVE";
    const char * const TERMINATE_STRING = "TERMINATE";
    const char * const CHANGE_STRING = "CHANGE";
-   const char * const INSTANT_STRING = "INSTANT_STRING";
+   const char * const INSTANT_STRING = "INSTANT";
    const char * const ASI_PREFIX_PROPERTY_STRING = "ASI_PREFIX_PROPERTY";
    const char * const ASI_SUFFIX_PROPERTY_STRING = "ASI_SUFFIX_PROPERTY";
 };
