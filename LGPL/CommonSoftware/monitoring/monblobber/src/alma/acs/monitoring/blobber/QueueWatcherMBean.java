172242fec7d7544f18c9bce963bef3cc161098eb
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2011-07-01 09:04:49 +0000

    Reverted recent changes by JAO, as agreed with Tzu in email "Re: monblobber broken"
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@158127 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/monitoring/monblobber/src/alma/acs/monitoring/blobber/QueueWatcherMBean.java
+++
@@ -1,9 +0,0 @@
-package alma.acs.monitoring.blobber;
-
-public interface QueueWatcherMBean {
-	//Attributes
-	public long  getMQDataQueueSize();
-	public long  getBlobDataQueueSize();
-
-	// Operations 
-}
