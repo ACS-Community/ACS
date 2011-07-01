046e147de944d8108fe6b6795aa4b0ffe4e8a016
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2011-07-01 08:36:56 +0000

    Reverted changes from r1.2 (as agreed with Tzu in a recent email "Re: monblobber broken"), and also removed method getMonitorData which does not seem to be needed on the interface level.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@158123 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/monitoring/monicd/ws/src/alma/acs/monitoring/DAO/MonitorDAO.java
+++ LGPL/CommonSoftware/monitoring/monicd/ws/src/alma/acs/monitoring/DAO/MonitorDAO.java
@@ -1,36 +1,19 @@
-/** MonitorDAO is the interface that defines the interaccion
+/** MonitorDAO is the interface that defines the interaction
  * between Blobbers and DAO Persistence Layer.
  *
  * @author Pablo Burgos
  * @since ACS-8_0_0-B Jun2009
- * @version "@(#) $Id: MonitorDAO.java,v 1.2 2011/06/03 22:29:54 vgonzale Exp $
+ * @version "@(#) $Id: MonitorDAO.java,v 1.3 2011/07/01 08:36:56 hsommer Exp $
  */
 package alma.acs.monitoring.DAO;
-
-import java.util.List;
-import java.sql.Timestamp;
-import alma.DAOErrType.wrappers.AcsJGettingMonitorCharacteristicsEx;
-import alma.DAOErrType.wrappers.AcsJDynConfigFailureEx;
 
 public interface MonitorDAO {
 
     public void store(ComponentData inData) throws Exception;
-
-    public List getMonitorData(long monitorPointId, Timestamp startTimestamp,
-            Timestamp stopTimestamp);
 
     public void close();
 
     public void openTransactionStore() throws Exception;
 
     public void closeTransactionStore() throws Exception;
-
-    public boolean hasFailedToBeConfigured(ComponentData inData);
-
-    public void setHasFailedToBeConfigured(ComponentData inData);
-
-    public MonitorCharacteristicIDs getMonitorCharacteristicIDs(String configurationName, ComponentData inData)
-            throws AcsJGettingMonitorCharacteristicsEx, AcsJDynConfigFailureEx;
-
-
 }
