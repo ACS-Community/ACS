b2ab01c532a9b35a212add249eb2cb9b4c873adc
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2011-07-31 04:19:24 +0000

    Methods open/closeTransactionStore declare AcsJException instead of Exception
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@159356 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/monitoring/monicd/ws/src/alma/acs/monitoring/DAO/MonitorDAO.java
+++ LGPL/CommonSoftware/monitoring/monicd/ws/src/alma/acs/monitoring/DAO/MonitorDAO.java
@@ -1,11 +1,33 @@
+/*
+ * ALMA - Atacama Large Millimiter Array
+ * Copyright (c) European Southern Observatory, 2011 
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ */
+
+
 /** MonitorDAO is the interface that defines the interaction
  * between Blobbers and DAO Persistence Layer.
  *
  * @author Pablo Burgos
  * @since ACS-8_0_0-B Jun2009
- * @version "@(#) $Id: MonitorDAO.java,v 1.3 2011/07/01 08:36:56 hsommer Exp $
+ * @version "@(#) $Id: MonitorDAO.java,v 1.4 2011/07/31 04:19:24 hsommer Exp $
  */
 package alma.acs.monitoring.DAO;
+
+import alma.acs.exceptions.AcsJException;
 
 public interface MonitorDAO {
 
@@ -13,7 +35,7 @@ public interface MonitorDAO {
 
     public void close();
 
-    public void openTransactionStore() throws Exception;
+    public void openTransactionStore() throws AcsJException;
 
-    public void closeTransactionStore() throws Exception;
+    public void closeTransactionStore() throws AcsJException;
 }
