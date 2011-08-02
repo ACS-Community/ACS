16cb678f81f45231497437b37bde4004eb702468
rtobar <rtobar@523d945c-050c-4681-91ec-863ad3bb968a>
2011-08-02 09:51:27 +0000

    Receiver test client is more flexible, receives stream name and flow names in command line now
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@159505 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/bulkDataNT/test/bdNTReceiverTest.cpp
+++ LGPL/CommonSoftware/bulkDataNT/test/bdNTReceiverTest.cpp
@@ -2,6 +2,10 @@
 #include "bulkDataNTReceiverStream.h"
 #include "bulkDataNTCallback.h"
 #include <iostream>
+#include <ace/Get_Opt.h>
+#include <ace/Tokenizer_T.h>
+
+using namespace std;
 
 class  TestCB:  public BulkDataCallback
 {
@@ -36,21 +40,54 @@ public:
 
 };
 
+void print_usage(char *argv[]) {
+	cout << "Usage: " << argv[0] << " [-s streamName] -f flow1Name[,flow2Name,flow3Name...]" << endl;
+	exit(1);
+}
 
-int main()
+int main(int argc, char *argv[])
 {
+
+	char c;
+	char *streamName = "DefaultStream";
+	list<char *> flows;
+
+	// Parse the args
+	ACE_Get_Opt get_opts (argc, argv, "s:f:");
+	while(( c = get_opts()) != -1 ) {
+
+		switch(c) {
+			case 's':
+				streamName = get_opts.opt_arg();
+				break;
+
+			case 'f':
+				ACE_Tokenizer tok(get_opts.opt_arg());
+				tok.delimiter(',');
+				for(char *p = tok.next(); p; p = tok.next())
+					flows.push_back(p);
+
+				break;
+		}
+
+	}
+
+	if( flows.size() == 0 )
+		print_usage(argv);
+
+	if( !strcmp("DefaultStream", streamName) )
+		cerr << "Warning: using default stream name \"DefaultStream\"" << endl;
+
 	LoggingProxy m_logger(0, 0, 31, 0);
 	LoggingProxy::init (&m_logger);
 	ACS_CHECK_LOGGER;
 
-	AcsBulkdata::BulkDataNTReceiverStream<TestCB> receiverStream("TestFlow");
+	AcsBulkdata::BulkDataNTReceiverStream<TestCB> receiverStream(streamName);
 
-	receiverStream.createFlow("00");
-
-	TestCB *cb = new TestCB();
-	receiverStream.createFlow("01", cb);
-
-	BulkDataNTReceiverFlow *flow0 = receiverStream.getFlow("00");
+	list<char *>::iterator it;
+	for(it = flows.begin(); it != flows.end(); it++) {
+		receiverStream.createFlow((*it));
+	}
 
 	std::cout << "press a key to end.." << std::endl;
 	getchar();
