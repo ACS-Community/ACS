e0cdfca031146ede53fb389a7a9f7c95ba689a7d
bjeram <bjeram@523d945c-050c-4681-91ec-863ad3bb968a>
2011-08-25 11:44:01 +0000

    moved some commented code for testing seg faults and slow receivers
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@160788 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/bulkDataNT/test/bdNTReceiverImplTest.cpp
+++ LGPL/CommonSoftware/bulkDataNT/test/bdNTReceiverImplTest.cpp
@@ -19,12 +19,33 @@ public:
 	int cbReceive(unsigned char* userParam_p, unsigned  int size)
 	{
 		std::cout << "=>cbReceive[" << recvName_m << "/" << flowName_m << "]: got " << size << " :";
-/*		for(unsigned int i=0; i<frame_p->length(); i++)
+		/*		for(unsigned int i=0; i<frame_p->length(); i++)
 		{
 			std::cout <<  *(char*)(frame_p->base()+i);
 		}
-	*/	std::cout << std::endl;
-		return 0;
+		 */	std::cout << std::endl;
+
+	/*	 if (sleep_period!=0)
+		 {
+			 cout << listName << " Rest:" << message.restDataLength << " Going sleep for: " << sleep_period << endl;
+			 //cout <<  message.data.length() << endl;
+			 //cout <<  message.restDataLength << endl;
+			 usleep(sleep_period);
+		 }
+		*/
+		 /* simulate seg fault
+    			if (data_length>100000) {
+    			char *tt=0;
+    			printf("XXX %s\n", tt);
+    			printf("crash\n");
+    			ACE_Time_Value *t=0;
+    			t->sec();
+    			DDS::DataReaderQos *ddr_qos=0;
+    			ddr_qos->reliability.kind = 0;
+    			printf("after crash\n");
+    			} */
+
+		 return 0;
 	}
 
 	int cbStop()
