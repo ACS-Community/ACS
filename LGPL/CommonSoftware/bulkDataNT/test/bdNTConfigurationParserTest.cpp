dc0d0027396d08f2f393c9b6b91b895a739a6698
rtobar <rtobar@523d945c-050c-4681-91ec-863ad3bb968a>
2011-09-02 15:39:45 +0000

    Delete the receivers after using them
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@161243 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/bulkDataNT/test/bdNTConfigurationParserTest.cpp
+++ LGPL/CommonSoftware/bulkDataNT/test/bdNTConfigurationParserTest.cpp
@@ -20,6 +20,7 @@ int main(int args, char *argv[]) {
     ACS_CHECK_LOGGER;
 
     list<BulkDataNTSenderStream *>* senders = 0;
+    list<BulkDataNTReceiverStream<MyCallback> *>* receivers = 0;
 
 	char *correctSenderConfig = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><BulkDataNTSender xmlns:baci=\"urn:schemas-cosylab-com:BACI:1.0\" xmlns=\"urn:schemas-eso-org:BulkDataNTSender:1.0\" xmlns:cdb=\"urn:schemas-cosylab-com:CDB:1.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"urn:schemas-eso-org:BulkDataNTSender:1.0 file:/D:/eclipseWorkshop/bulkDataNT/config/CDB/schemas/BulkDataNTSender.xsd\" recentCommand=\"\" recentTimeStamp=\"\" actionThreadStackSize=\"1024\" monitoringThreadStackSize=\"2048\"><SenderStream Name=\"Name7\">        <DDSSenderStreamQoS>            <participant_qos name=\"name5\">            </participant_qos>        </DDSSenderStreamQoS>    </SenderStream><SenderStream Name=\"Name1\">        <Flow Name=\"Name3\">            <DDSSenderFlowQoS>                <datawriter_qos name=\"name1\">                </datawriter_qos>            </DDSSenderFlowQoS>        </Flow>        <Flow Name=\"Name5\">            <DDSSenderFlowQoS>                <datawriter_qos name=\"name3\">                 </datawriter_qos>            </DDSSenderFlowQoS>        </Flow>    </SenderStream>    </BulkDataNTSender>";
 	try {
@@ -38,7 +39,7 @@ int main(int args, char *argv[]) {
 	char *correctReceiverConfig = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><BulkDataNTReceiver xmlns:baci=\"urn:schemas-cosylab-com:BACI:1.0\" xmlns=\"urn:schemas-eso-org:BulkDataNTSender:1.0\" xmlns:cdb=\"urn:schemas-cosylab-com:CDB:1.0\"  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"urn:schemas-eso-org:BulkDataNTSender:1.0 file:/D:/eclipseWorkshop/bulkDataNT/config/CDB/schemas/BulkDataNTSender.xsd\" recentCommand=\"\" recentTimeStamp=\"\" actionThreadStackSize=\"1024\" monitoringThreadStackSize=\"2048\">    <ReceiverStream Name=\"Name1\">        <ReceiverFlow Name=\"Name3\">            <DDSReceiverFlowQoS>                <datawriter_qos name=\"name1\">                </datawriter_qos>            </DDSReceiverFlowQoS>        </ReceiverFlow>        <ReceiverFlow Name=\"Name5\">            <DDSReceiverFlowQoS>                <datawriter_qos name=\"name3\">                 </datawriter_qos>            </DDSReceiverFlowQoS>        </ReceiverFlow>    </ReceiverStream>    <ReceiverStream Name=\"Name7\">        <DDSReceiverStreamQoS>            <participant_qos name=\"name5\">            </participant_qos>        </DDSReceiverStreamQoS>    </ReceiverStream></BulkDataNTSender>";
 	try {
 		BulkDataConfigurationParser parser;
-		parser.parseReceiverConfig<MyCallback>(correctReceiverConfig);
+		receivers = parser.parseReceiverConfig<MyCallback>(correctReceiverConfig);
 	} catch(CDBProblemExImpl &ex) {
 		std::cout << ex.getDetail() << std::endl;
 	} catch(StreamCreateProblemExImpl &ex) {
@@ -47,12 +48,18 @@ int main(int args, char *argv[]) {
 		std::cout << "Error while creating stream '" << ex.getStreamName() << "', flow '" << ex.getFlowName() <<"'" << std::endl;
 	}
 
-	// cleanup the senders
+	// cleanup the senders and receivers
 	if( senders != 0 ) {
 		list<BulkDataNTSenderStream *>::iterator it;
 		for( it = senders->begin(); it != senders->end(); it++)
 			delete *it;
 		delete senders;
+	}
+	if( receivers != 0 ) {
+		list<BulkDataNTReceiverStream<MyCallback> *>::iterator it;
+		for( it = receivers->begin(); it != receivers->end(); it++)
+			delete *it;
+		delete receivers;
 	}
 
 	m_logger.done();
