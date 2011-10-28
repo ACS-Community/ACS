ff0a041917e9b2b0b1455488aad903fdd02aa18d
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2011-10-28 14:39:18 +0000

    Manually fixed "no LGPL license text" issue reported by addCopyright.py
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@164380 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acserr/ws/src/ACSErrorChecker/ErrorDefinition.py
+++ LGPL/CommonSoftware/acserr/ws/src/ACSErrorChecker/ErrorDefinition.py
@@ -1,32 +1,27 @@
-# @(#) $Id: ErrorDefinition.py,v 1.7 2008/11/12 15:13:11 acaproni Exp $
+#!/usr/bin/env python
+################################################################################################
+# @(#) $Id: ErrorDefinition.py,v 1.8 2011/10/28 14:39:18 hsommer Exp $
 #
-# Copyright (C) 2001
-# Associated Universities, Inc. Washington DC, USA.
+#    ALMA - Atacama Large Millimiter Array
+#    (c) Associated Universities, Inc. Washington DC, USA, 2001
+#    (c) European Southern Observatory, 2002
+#    Copyright by ESO (in the framework of the ALMA collaboration)
+#    and Cosylab 2002, All rights reserved
 #
-# Produced for the ALMA project
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
 #
-# This library is free software; you can redistribute it and/or modify it under
-# the terms of the GNU Library General Public License as published by the Free
-# Software Foundation; either version 2 of the License, or (at your option) any
-# later version.
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
 #
-# This library is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU Library General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Library General Public License
-# adouble with this library; if not, write to the Free Software Foundation, Inc.,
-# 675 Massachusetts Ave, Cambridge, MA 02139, USA.  Correspondence concerning
-# ALMA should be addressed as follows:
-#
-# Internet email: alma-sw-admin@nrao.edu
-# "@(#) $Id: ErrorDefinition.py,v 1.7 2008/11/12 15:13:11 acaproni Exp $"
-#
-# who       when        what
-# --------  ----------  ----------------------------------------------
-# acaproni  2005/02/08  Created.
-#------------------------------------------------------------------------------
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+###############################################################################
 
 import xml.parsers.expat
 
