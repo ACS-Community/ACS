2f0c5b849a4969933aa4f2cf3ccac2208b795e78
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2011-11-08 17:06:58 +0000

    Bugfix for container_logged_in notification
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@164836 523d945c-050c-4681-91ec-863ad3bb968a

--- Benchmark/util/src/acs/benchmark/util/ManagerAdminClient.java
+++ Benchmark/util/src/acs/benchmark/util/ManagerAdminClient.java
@@ -123,6 +123,7 @@ public class ManagerAdminClient implements AdministratorOperations
 			CountDownLatch sync = containerLoggedInSyncMap.get(containerName);
 			if (sync != null) {
 				sync.countDown();
+				containerLoggedInSyncMap.remove(sync);
 			}
 		}
 	}
