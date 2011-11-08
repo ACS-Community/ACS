d26eef7df4f237e61e9ff9393f28727fd378888d
bjeram <bjeram@523d945c-050c-4681-91ec-863ad3bb968a>
2011-11-08 15:08:54 +0000

    fixed dynamic_cast in getCallback
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@164821 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/bulkDataNT/include/bulkDataNTReceiverFlow.h
+++ LGPL/CommonSoftware/bulkDataNT/include/bulkDataNTReceiverFlow.h
@@ -18,7 +18,7 @@
 * License along with this library; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 *
-* "@(#) $Id: bulkDataNTReceiverFlow.h,v 1.12 2011/11/08 11:12:04 bjeram Exp $"
+* "@(#) $Id: bulkDataNTReceiverFlow.h,v 1.13 2011/11/08 15:08:54 bjeram Exp $"
 *
 * who       when      what
 * --------  --------  ----------------------------------------------
@@ -70,7 +70,7 @@ public:
 	BulkDataNTCallback* getCallbackObject() { return callback_m; }
 
 	template<class T>
-	T* getCallback() { return dynamic_cast<T>(callback_m); }
+	T* getCallback() { return dynamic_cast<T*>(callback_m); }
 
 	//TBD:: setCB();
 protected:
