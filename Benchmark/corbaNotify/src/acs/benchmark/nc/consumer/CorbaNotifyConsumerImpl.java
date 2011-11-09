857a0ac7bf4c2fb3fd6b27aae3d9e7302471059b
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2011-11-09 17:06:22 +0000

    First impl, not yet tested
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@164894 523d945c-050c-4681-91ec-863ad3bb968a

--- Benchmark/corbaNotify/src/acs/benchmark/nc/consumer/CorbaNotifyConsumerImpl.java
+++ Benchmark/corbaNotify/src/acs/benchmark/nc/consumer/CorbaNotifyConsumerImpl.java
@@ -20,32 +20,168 @@
  *******************************************************************************/
 package acs.benchmark.nc.consumer;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.omg.CORBA.portable.IDLEntity;
+
 import acs.benchmark.nc.CorbaNotifyBaseImpl;
 
 import alma.ACSErrTypeCommon.CouldntPerformActionEx;
+import alma.ACSErrTypeCommon.wrappers.AcsJCouldntPerformActionEx;
+import alma.JavaContainerError.wrappers.AcsJContainerServicesEx;
+import alma.acs.nc.AcsEventSubscriber;
+import alma.acs.util.StopWatch;
+import alma.acsnc.EventDescription;
 import alma.benchmark.CorbaNotifyConsumerOperations;
+import alma.benchmark.LightweightMountStatusData;
+import alma.benchmark.MountStatusData;
 import alma.benchmark.NcEventSpec;
+import alma.benchmark.SomeOtherEventType;
 
-public class CorbaNotifyConsumerImpl extends CorbaNotifyBaseImpl implements CorbaNotifyConsumerOperations
+public class CorbaNotifyConsumerImpl extends CorbaNotifyBaseImpl<AcsEventSubscriber> implements CorbaNotifyConsumerOperations
 {
+
+//	@Override
+//	public void initialize(ContainerServices containerServices) throws ComponentLifecycleException {
+//		super.initialize(containerServices);
+//	}
+//
+//	@Override
+//	public void cleanUp() throws AcsJComponentCleanUpEx {
+//		super.cleanUp();
+//	}
+
 	@Override
-	public void ncConnect(String[] ncNames) throws CouldntPerformActionEx {
-		// TODO Auto-generated method stub
-		
+	protected AcsEventSubscriber createNcParticipant(String ncName) throws AcsJContainerServicesEx {
+		return m_containerServices.createNotificationChannelSubscriber(ncName);
 	}
 
 	@Override
-	public void ncDisconnect() throws CouldntPerformActionEx {
-		// TODO Auto-generated method stub
-		
+	protected void disconnectNcParticipant(AcsEventSubscriber sub) {
+		sub.disconnect();
 	}
 
+	protected static class TestEventHandler<T extends IDLEntity> implements AcsEventSubscriber.Callback<T> {
+		
+		private final Class<T> clazz;
+		private final String antennaName; 
+		private final CountDownLatch sharedEventCountdown;
+		
+		/**
+		 * @param clazz  Class of the event (IDL struct) 
+		 * @param antennaName  Expected antenna name in MountStatusData or LightweightMountStatusData events. 
+		 *                     May be null for other events or if antenna name does not matter. Currently not used anyway. 
+		 * @param sharedEventCountdown  Used to optionally sync on expected number of events (total, over all NCs). May be null.
+		 */
+		protected TestEventHandler(Class<T> clazz, String antennaName, CountDownLatch sharedEventCountdown) {
+			this.clazz = clazz;
+			this.antennaName = antennaName;
+			this.sharedEventCountdown = sharedEventCountdown;
+		}
+		
+		@Override
+		public Class<T> getEventType() {
+			return clazz;
+		}
 
+		@Override
+		public void receive(T event, EventDescription eventDescrip) {
+			if (sharedEventCountdown != null) {
+				sharedEventCountdown.countDown();
+			}
+		}
+	}
+	
+	/**
+	 * Creates a typed event handler for the given eventName.
+	 * To avoid errors, the String-to-handler mapping is not done in a generic way (classForName etc with guessing the Java package)
+	 * but hardcoded for a few known event struct names that can be used for the tests.
+	 * 
+	 * @param eventName  Currently supported are "MountStatusData", "LightweightMountStatusData", "SomeOtherEventType"
+	 * @return
+	 * @throws IllegalArgumentException  If eventName is not supported.
+	 */
+	protected TestEventHandler<? extends IDLEntity> createEventHandler(String eventName, String antennaName, CountDownLatch sharedEventCountdown) {
+		TestEventHandler<? extends IDLEntity> ret = null;
+		
+		if (eventName.equals("MountStatusData")) {
+			ret= new TestEventHandler<MountStatusData>(MountStatusData.class, antennaName, sharedEventCountdown) {
+				public void receive(MountStatusData event, EventDescription eventDescrip) {
+					super.receive(event, eventDescrip);
+					// TODO: do something with event.antennaName;
+				}
+			};
+		}
+		else if (eventName.equals("LightweightMountStatusData")) {
+			ret= new TestEventHandler<LightweightMountStatusData>(LightweightMountStatusData.class, antennaName, sharedEventCountdown);
+		}
+		else if (eventName.equals("SomeOtherEventType")) {
+			ret= new TestEventHandler<SomeOtherEventType>(SomeOtherEventType.class, antennaName, sharedEventCountdown);
+		}
+		else {
+			throw new IllegalArgumentException("Unsupported event type '" + eventName + "'.");
+		}
+		return ret;
+	}
+	
 	@Override
-	public void receiveEvents(NcEventSpec[] ncEventSpec, int processingDelayMillis, int numberOfEvents)
+	public int receiveEvents(NcEventSpec[] ncEventSpecs, int processingDelayMillis, int numberOfEvents)
 			throws CouldntPerformActionEx {
-		// TODO Auto-generated method stub
+		
+		// Set up receivers
+		
+		CountDownLatch sharedEventCountdown = null;
+		if (numberOfEvents > 0) {
+			sharedEventCountdown = new CountDownLatch(numberOfEvents);
+		}
 
+		StopWatch sw = new StopWatch();
+
+		try {
+			for (NcEventSpec ncEventSpec : ncEventSpecs) {
+				AcsEventSubscriber sub = subsOrPubs.get(ncEventSpec.ncName);
+				if (sub == null) {
+					throw new AcsJCouldntPerformActionEx("No subscriber available for NC '" + ncEventSpec.ncName + "'.");
+				}
+				for (String eventName : ncEventSpec.eventNames) {
+					sub.addSubscription(createEventHandler(eventName, ncEventSpec.antennaName, sharedEventCountdown));
+					sub.startReceivingEvents();
+				}
+			}
+		} catch (AcsJCouldntPerformActionEx ex) {
+			throw ex.toCouldntPerformActionEx();
+		} catch (Exception ex) {
+			throw new AcsJCouldntPerformActionEx(ex).toCouldntPerformActionEx();
+		}
+		m_logger.info(ncEventSpecs.length + " subscriber(s) set up to receive events in " + sw.getLapTimeMillis() + " ms.");
+		
+		if (numberOfEvents > 0) {
+			m_logger.info("Will wait for a total of " + numberOfEvents + " events to be received, with timeout after 10 minutes.");
+			try {
+				boolean cleanTermination = sharedEventCountdown.await(10, TimeUnit.MINUTES);
+				if (cleanTermination) {
+					m_logger.info("Received the expected " + numberOfEvents + " events in " + sw.getLapTimeMillis() + " ms.");
+				}
+				else {
+					m_logger.warning("Unforeseen termination of event suppliers after 10 min (timeout).");
+					cancel = true;
+				}
+			} catch (InterruptedException ex) {
+				cancel = true;
+			}
+		}
+		else {
+			m_logger.info("Will return from receiveEvents now but will keep receiving events.");
+		}
+		
+		if (cancel) {
+			throw new AcsJCouldntPerformActionEx("Event receiving was interrupted or failed otherwise.").toCouldntPerformActionEx();
+		}
+		else {
+			return (int) sw.getLapTimeMillis();
+		}
 	}
+
 
 }
