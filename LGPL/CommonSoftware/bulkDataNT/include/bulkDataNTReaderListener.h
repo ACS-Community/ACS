c02cf8588ac1aabd68362280a7e0bdde6a6a1147
bjeram <bjeram@523d945c-050c-4681-91ec-863ad3bb968a>
2011-11-09 12:01:36 +0000

    added LGPL license
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@164860 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/bulkDataNT/include/bulkDataNTReaderListener.h
+++ LGPL/CommonSoftware/bulkDataNT/include/bulkDataNTReaderListener.h
@@ -1,6 +1,31 @@
 #ifndef BULKDATA_NT_READER_LISTENER
 #define BULKDATA_NT_READER_LISTENER
 
+/*******************************************************************************
+* ALMA - Atacama Large Millimiter Array
+* (c) European Southern Observatory, 2011
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+*
+* "@(#) $Id: bulkDataNTReaderListener.h,v 1.16 2011/11/09 12:01:36 bjeram Exp $"
+*
+* who       when      what
+* --------  --------  ----------------------------------------------
+* bjeram  2011-04-19  created
+*/
+
 
 #include "bulkDataNTSupport.h"
 #include <ndds/ndds_namespace_cpp.h>
--- LGPL/CommonSoftware/bulkDataNT/src/bulkDataNTDDSSubscriber.cpp
+++ LGPL/CommonSoftware/bulkDataNT/src/bulkDataNTDDSSubscriber.cpp
@@ -1,3 +1,28 @@
+/*******************************************************************************
+* ALMA - Atacama Large Millimiter Array
+* (c) European Southern Observatory, 2011
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+*
+* "@(#) $Id: bulkDataNTDDSSubscriber.cpp,v 1.17 2011/11/09 12:01:36 bjeram Exp $"
+*
+* who       when      what
+* --------  --------  ----------------------------------------------
+* bjeram  2011-04-19  created
+*/
+
 #include "bulkDataNTDDSSubscriber.h"
 #include <iostream>
 
