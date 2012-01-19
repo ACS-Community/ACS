3fc3672d6db8c6b0c5659c1499d74fc31cc72b4f
bjeram <bjeram@523d945c-050c-4681-91ec-863ad3bb968a>
2012-01-19 15:53:55 +0000

    create data reader disabled + enable it
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@168233 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/bulkDataNT/src/bulkDataNTDDSSubscriber.cpp
+++ LGPL/CommonSoftware/bulkDataNT/src/bulkDataNTDDSSubscriber.cpp
@@ -16,7 +16,7 @@
 * License along with this library; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 *
-* "@(#) $Id: bulkDataNTDDSSubscriber.cpp,v 1.18 2012/01/19 15:34:39 bjeram Exp $"
+* "@(#) $Id: bulkDataNTDDSSubscriber.cpp,v 1.19 2012/01/19 15:53:55 bjeram Exp $"
 *
 * who       when      what
 * --------  --------  ----------------------------------------------
@@ -55,6 +55,7 @@ DDS::Subscriber* BulkDataNTDDSSubscriber::createDDSSubscriber()
 {
 	AUTO_TRACE(__PRETTY_FUNCTION__);
 	DDS::ReturnCode_t ret;
+	DDS::SubscriberQos sub_qos;
 
 	if (participant_m==NULL)
 	{
@@ -70,6 +71,27 @@ DDS::Subscriber* BulkDataNTDDSSubscriber::createDDSSubscriber()
 		DDSSubscriberCreateProblemExImpl ex(__FILE__, __LINE__, __PRETTY_FUNCTION__);
 		throw ex;
 	}
+
+	ret = sub->get_qos(sub_qos);
+	if (ret!=DDS::RETCODE_OK)
+	{
+		DDSQoSGetProblemExImpl ex(__FILE__, __LINE__, __PRETTY_FUNCTION__);
+		ex.setDDSTypeCode(ret);
+		ex.setQoS("sub->get_qos()");
+		throw ex;
+	}//if
+
+	/// dr has to be created disabled
+	sub_qos.entity_factory.autoenable_created_entities=DDS_BOOLEAN_FALSE;
+
+	ret = sub->set_qos(sub_qos);
+	if (ret!=DDS::RETCODE_OK)
+	{
+		DDSQoSSetProblemExImpl ex(__FILE__, __LINE__, __PRETTY_FUNCTION__);
+		ex.setDDSTypeCode(ret);
+		ex.setQoS("sub->set_qos()");
+		throw ex;
+	}//if
 
 	ret = sub->enable();
 	if (ret!=DDS::RETCODE_OK)
@@ -98,6 +120,7 @@ void BulkDataNTDDSSubscriber::destroyDDSSubscriber()
 ACSBulkData::BulkDataNTFrameDataReader* BulkDataNTDDSSubscriber::createDDSReader(DDS::Topic *topic, DDS::DataReaderListener *listener)
 {
 	AUTO_TRACE(__PRETTY_FUNCTION__);
+	DDS::ReturnCode_t ret;
 
 	if (subscriber_m==NULL || topic==NULL || listener==NULL)
 	{
@@ -115,6 +138,42 @@ ACSBulkData::BulkDataNTFrameDataReader* BulkDataNTDDSSubscriber::createDDSReader
 		DDSDRCreateProblemExImpl ex(__FILE__, __LINE__, __PRETTY_FUNCTION__);
 		throw ex;
 	}//if
+/*
+	if (noOfReaders==1)
+			{
+				//multicast
+				struct DDS_TransportMulticastSettings_t* multicast_locator = NULL;
+				// DDS_TransportMulticastSettingsSeq_ensure_length(&, 1, 1);
+				dr_qos.multicast.value.ensure_length(1,1);
+				// DDS_TransportMulticastSettingsSeq_get_reference(&qos.multicast.value,0);
+				multicast_locator = &dr_qos.multicast.value[0];
+
+				string mcasta="225.3.2.1";
+				DDS_String_replace(&multicast_locator->receive_address,	mcasta.c_str()	);
+				cout << "going to listen on multicast address: " << mcasta << endl;
+
+
+				dr_qos.resource_limits.initial_samples *= 1;//multicast_reader_count;
+				if (dr_qos.resource_limits.initial_samples >
+					dr_qos.resource_limits.max_samples) {
+					dr_qos.resource_limits.max_samples =
+							dr_qos.resource_limits.initial_samples;
+				}
+				dr_qos.reader_resource_limits.max_samples_per_remote_writer =
+						dr_qos.resource_limits.initial_samples;
+			}
+			else
+			{
+				dr_qos.unicast.value.ensure_length(1,1);
+			}
+
+*/
+	ret = dr->enable();
+	if (ret!=DDS::RETCODE_OK)
+	{
+		DDSDREnableProblemExImpl ex(__FILE__, __LINE__, __PRETTY_FUNCTION__);
+		ex.setDDSTypeCode(ret);
+	}
 
 	return ACSBulkData::BulkDataNTFrameDataReader::narrow(dr);
 }
