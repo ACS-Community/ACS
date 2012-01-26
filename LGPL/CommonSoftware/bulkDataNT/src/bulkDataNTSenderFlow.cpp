57f3afccb5eec750c43fe0e0903226fb9a5970f1
bjeram <bjeram@523d945c-050c-4681-91ec-863ad3bb968a>
2012-01-26 14:49:20 +0000

    using dataType2String where possilbe
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@168666 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/bulkDataNT/src/bulkDataNTSenderFlow.cpp
+++ LGPL/CommonSoftware/bulkDataNT/src/bulkDataNTSenderFlow.cpp
@@ -16,14 +16,14 @@
 * License along with this library; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 *
-* "@(#) $Id: bulkDataNTSenderFlow.cpp,v 1.36 2012/01/26 11:11:54 bjeram Exp $"
+* "@(#) $Id: bulkDataNTSenderFlow.cpp,v 1.37 2012/01/26 14:49:20 bjeram Exp $"
 *
 * who       when      what
 * --------  --------  ----------------------------------------------
 * bjeram  2011-04-19  created
 */
 
-static char *rcsId="@(#) $Id: bulkDataNTSenderFlow.cpp,v 1.36 2012/01/26 11:11:54 bjeram Exp $";
+static char *rcsId="@(#) $Id: bulkDataNTSenderFlow.cpp,v 1.37 2012/01/26 14:49:20 bjeram Exp $";
 static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
 
 #include "bulkDataNTSenderFlow.h"
@@ -244,7 +244,7 @@ void BulkDataNTSenderFlow::writeFrame(ACSBulkData::DataType dataType,  const uns
 		}//if-else
 
 		ddsDataWriter_m->get_reliable_writer_cache_changed_status(status); //RTI
-		ACS_SHORT_LOG((LM_DEBUG, "unacknowledged_sample_count (%d) for flow: %s", status.unacknowledged_sample_count, flowName_m.c_str())); //RTI
+		ACS_SHORT_LOG((LM_DEBUG, "unacknowledged_sample_count (%s) %d for flow: %s", dataType2String[dataType], status.unacknowledged_sample_count, flowName_m.c_str())); //RTI
 		// RTI			cout << "\t\t Int unacknowledged_sample_count_peak: " << status.unacknowledged_sample_count_peak << endl;
 		ret = ddsDataWriter_m->wait_for_acknowledgments(ackTimeout_m);
 		if( ret != DDS::RETCODE_OK)
@@ -256,7 +256,7 @@ void BulkDataNTSenderFlow::writeFrame(ACSBulkData::DataType dataType,  const uns
 		}//if
 
 		ddsDataWriter_m->get_reliable_writer_cache_changed_status(status); //RTI
-		ACS_SHORT_LOG((LM_DEBUG, "unacknowledged_sample_count after waiting: (%d)", status.unacknowledged_sample_count)); //RTI
+		ACS_SHORT_LOG((LM_DEBUG, "unacknowledged_sample_count (%s) after waiting: %d", dataType2String[dataType], status.unacknowledged_sample_count)); //RTI
 		//cout << "\t\t Int1 unacknowledged_sample_count_peak: " << status.unacknowledged_sample_count_peak << endl;
 	}//if (ret != DDS::RETCODE_OK)
 
@@ -268,7 +268,7 @@ void BulkDataNTSenderFlow::writeFrame(ACSBulkData::DataType dataType,  const uns
 		if (DDSConfiguration::debugLevel>0)
 		{
 			// the message can cause perfomance penality for small data sizes
-			ACS_SHORT_LOG((LM_DEBUG, "unacknowledged_sample_count (%d) before waiting: %d", dataType, status.unacknowledged_sample_count)); //RTI
+			ACS_SHORT_LOG((LM_DEBUG, "unacknowledged_sample_count (%s) before waiting: %d", dataType2String[dataType], status.unacknowledged_sample_count)); //RTI
 		}
 		ret = ddsDataWriter_m->wait_for_acknowledgments(ackTimeout_m);
 		if( ret != DDS::RETCODE_OK)
