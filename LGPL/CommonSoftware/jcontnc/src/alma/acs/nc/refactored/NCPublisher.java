5cb1a94869a034a15368c69b8a0f50b67ecb6eb5
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2012-02-06 17:44:19 +0000

    - Merged named-proxy change (COMP-6398) from SimpleSupplier. - Merged some other changes from SimpleSupplier. Most of the "CircularQueue eventBuff" must still be merged (added a TODO) - Now "implements AcsEventPublisher<IDLEntity>", which brings back "publishEvent(IDLEntity customStruct)" instead of just Object.   Hope this change in the base interface will work also for the DDS implementation.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@169150 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jcontnc/src/alma/acs/nc/refactored/NCPublisher.java
+++ LGPL/CommonSoftware/jcontnc/src/alma/acs/nc/refactored/NCPublisher.java
@@ -22,6 +22,7 @@ package alma.acs.nc.refactored;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.omg.CORBA.BAD_PARAM;
 import org.omg.CORBA.IntHolder;
 import org.omg.CORBA.NO_IMPLEMENT;
 import org.omg.CORBA.TIMEOUT;
@@ -35,22 +36,29 @@ import org.omg.CosNotification.Property;
 import org.omg.CosNotification.StructuredEvent;
 import org.omg.CosNotification.UnsupportedAdmin;
 import org.omg.CosNotification.UnsupportedQoS;
+import org.omg.CosNotifyChannelAdmin.AdminLimitExceeded;
 import org.omg.CosNotifyChannelAdmin.ClientType;
-import org.omg.CosNotifyChannelAdmin.EventChannel;
 import org.omg.CosNotifyChannelAdmin.InterFilterGroupOperator;
 import org.omg.CosNotifyChannelAdmin.StructuredProxyPushConsumer;
 import org.omg.CosNotifyChannelAdmin.StructuredProxyPushConsumerHelper;
-import org.omg.CosNotifyChannelAdmin.SupplierAdmin;
 import org.omg.CosNotifyComm.InvalidEventType;
 import org.omg.CosNotifyComm.StructuredPushSupplier;
 import org.omg.CosNotifyComm.StructuredPushSupplierHelper;
 
+import gov.sandia.NotifyMonitoringExt.EventChannel;
 import gov.sandia.NotifyMonitoringExt.EventChannelFactory;
+import gov.sandia.NotifyMonitoringExt.NameAlreadyUsed;
+import gov.sandia.NotifyMonitoringExt.NameMapError;
+import gov.sandia.NotifyMonitoringExt.SupplierAdmin;
 
 import alma.ACSErrTypeCORBA.wrappers.AcsJCORBAReferenceNilEx;
+import alma.ACSErrTypeCORBA.wrappers.AcsJNarrowFailedEx;
 import alma.ACSErrTypeCommon.wrappers.AcsJBadParameterEx;
 import alma.ACSErrTypeCommon.wrappers.AcsJCORBAProblemEx;
 import alma.ACSErrTypeCommon.wrappers.AcsJUnexpectedExceptionEx;
+import alma.AcsNCTraceLog.LOG_NC_ConsumerProxyCreation_FAIL;
+import alma.AcsNCTraceLog.LOG_NC_ConsumerProxyCreation_OK;
+import alma.AcsNCTraceLog.LOG_NC_TaoExtensionsSubtypeMissing;
 import alma.JavaContainerError.wrappers.AcsJContainerServicesEx;
 import alma.acs.container.ContainerServicesBase;
 import alma.acs.exceptions.AcsJException;
@@ -58,20 +66,20 @@ import alma.acs.logging.AcsLogLevel;
 import alma.acs.nc.AcsEventPublisher;
 import alma.acs.nc.AcsNcReconnectionCallback;
 import alma.acs.nc.AnyAide;
+import alma.acs.nc.CircularQueue;
 import alma.acs.nc.Helper;
 import alma.acs.nc.ReconnectableSubscriber;
 import alma.acsnc.EventDescription;
 import alma.acsnc.EventDescriptionHelper;
 import alma.acsnc.OSPushSupplierPOA;
-import alma.acsncErrType.wrappers.AcsJPublishEventFailureEx;
 
 /**
  * NCPublisher is the Notificaction Channel implementation to be used with the
  * event channel API to publish events using the Java programming language.
 
- * @author jslopez
+ * @author jslopez, hsommer
  */
-public class NCPublisher extends OSPushSupplierPOA implements AcsEventPublisher, ReconnectableSubscriber {
+public class NCPublisher extends OSPushSupplierPOA implements AcsEventPublisher<IDLEntity>, ReconnectableSubscriber {
 
 	/** Provides useful methods. */
 	protected final Helper helper;
@@ -89,8 +97,7 @@ public class NCPublisher extends OSPushSupplierPOA implements AcsEventPublisher,
 	protected SupplierAdmin supplierAdmin;
 
 	/**
-	 * The proxy consumer object used by supplier to push events onto the
-	 * channel.
+	 * The server-side proxy consumer object used by this supplier to push events onto the channel.
 	 */
 	protected StructuredProxyPushConsumer proxyConsumer;
 
@@ -101,7 +108,7 @@ public class NCPublisher extends OSPushSupplierPOA implements AcsEventPublisher,
 	 */
 	protected volatile long count = 0;
 
-	/** Channel we'll be sending events to */
+	/** Channel we'll be sending events to. Cannot be final because reconnect() modifies it. */
 	protected EventChannel channel;
 
 	/** Provides access to the ACS logging system. */
@@ -113,15 +120,12 @@ public class NCPublisher extends OSPushSupplierPOA implements AcsEventPublisher,
 	/** Helper class used to manipulate CORBA anys */
 	protected final AnyAide anyAide;
 	
-	protected AcsNcReconnectionCallback m_callback;
+	protected AcsNcReconnectionCallback reconnectCallback;
+	
+	protected final CircularQueue eventBuff;
 
 	/** Whether sending of events should be logged */
 	private final boolean isTraceEventsEnabled;
-
-	private IntHolder supplierAdminID;
-
-	private IntHolder proxyID;
-
 
 	/**
 	 * Creates a new instance of NCPublisher. Make sure you call
@@ -196,28 +200,70 @@ public class NCPublisher extends OSPushSupplierPOA implements AcsEventPublisher,
 		// @TODO: handle Corba TIMEOUT 
 		channel = helper.getNotificationChannel(this.channelName, getChannelKind(), getNotificationFactoryName());
 		
-		// get the Supplier admin
-		supplierAdminID = new IntHolder();
+		// Corba NC spec about adminId: a unique identifier assigned by the target EventChannel instance that is unique among all 
+		// SupplierAdmin instances currently associated with the channel.
+		// We are currently not using it, but it could be logged to help with NC debugging.
+		IntHolder adminIdHolder = new IntHolder();
+		org.omg.CosNotifyChannelAdmin.SupplierAdmin supplierAdminBase = null;
 		try {
-			supplierAdmin = channel.new_for_suppliers(InterFilterGroupOperator.AND_OP, supplierAdminID);
+			supplierAdminBase = channel.new_for_suppliers(InterFilterGroupOperator.AND_OP, adminIdHolder);
 		} catch (TIMEOUT ex) { // found in http://jira.alma.cl/browse/COMP-6312
 			throw new AcsJCORBAProblemEx(ex);
 		}
-		if (supplierAdmin == null) {
-			Throwable cause = new Throwable(
-					"Null reference obtained for the supplier admin for channel " + this.channelName);
-			throw new AcsJCORBAReferenceNilEx(cause);
+		if (supplierAdminBase == null) {
+			AcsJCORBAReferenceNilEx ex = new AcsJCORBAReferenceNilEx();
+			ex.setVariable("supplierAdminBase");
+			ex.setContext("Null reference obtained for the supplier admin for channel " + this.channelName);
+			throw ex;
 		}
-
-		// get the Consumer proxy
 		try {
-			proxyID = new IntHolder();
-			proxyConsumer = StructuredProxyPushConsumerHelper
-					.narrow(supplierAdmin.obtain_notification_push_consumer(
-							ClientType.STRUCTURED_EVENT, proxyID));
-		} catch (org.omg.CosNotifyChannelAdmin.AdminLimitExceeded e) {
-			Throwable cause = new Throwable(e.getMessage());
-			throw new alma.ACSErrTypeCommon.wrappers.AcsJCORBAProblemEx(cause);
+			supplierAdmin = gov.sandia.NotifyMonitoringExt.SupplierAdminHelper.narrow(supplierAdminBase);
+		} catch (BAD_PARAM ex) {
+			// This should never happen, since we already enforced the presence of TAO extensions in Helper#initializeNotifyFactory
+			String specialSupplierAdminId = gov.sandia.NotifyMonitoringExt.SupplierAdminHelper.id();
+			String standardSupplierAdminId = org.omg.CosNotifyChannelAdmin.SupplierAdminHelper.id();
+			LOG_NC_TaoExtensionsSubtypeMissing.log(logger, channelName + "-SupplierAdmin", specialSupplierAdminId, standardSupplierAdminId);
+			AcsJNarrowFailedEx ex2 = new AcsJNarrowFailedEx(ex);
+			ex2.setNarrowType(specialSupplierAdminId);
+			throw ex2;
+		}
+		
+		int proxyCreationAttempts = 0;
+		while( proxyConsumer == null) {
+			String randomizedClientName = Helper.createRandomizedClientName(services.getName());
+			// Holder for the unique ID assigned by the admin object. It is different from the name we set, and will be discarded.
+			IntHolder proxyIdHolder = new IntHolder();
+			proxyCreationAttempts++;
+			try {
+				// Create the consumer proxy (to which the published events will be fed) with a name.
+				// The client type parameter selects a StructuredProxyPushConsumer (based on Structured Events),
+				// as opposed to ProxyPushConsumer (based on Anys), or SequenceProxyPushConsumer (based on sequences of Structured Events).
+				org.omg.CORBA.Object tempCorbaObj = supplierAdmin.obtain_named_notification_push_consumer(ClientType.STRUCTURED_EVENT, proxyIdHolder, randomizedClientName.toString());
+				if (tempCorbaObj == null) {
+					AcsJCORBAReferenceNilEx ex = new AcsJCORBAReferenceNilEx();
+					ex.setVariable("tempCorbaObj");
+					ex.setContext("Null reference obtained for the Proxy Push Consumer for publisher " + services.getName());
+					throw ex;
+				}
+				proxyConsumer = StructuredProxyPushConsumerHelper.narrow(tempCorbaObj);
+				LOG_NC_ConsumerProxyCreation_OK.log(logger, proxyIdHolder.value, randomizedClientName, proxyCreationAttempts, services.getName(), channelName, getNotificationFactoryName());
+			} catch (NameAlreadyUsed e) {
+				// Hopefully we won't run into this situation. Still, try to go on in the loop,
+				// with a different client name next time.
+				logger.fine("Consumer proxy name '" + randomizedClientName + "' already in use. Will try again with different random number appended.");
+			} catch (NameMapError ex) {
+				// Default to the unnamed version
+				try {
+					proxyConsumer = StructuredProxyPushConsumerHelper.narrow(supplierAdmin.obtain_notification_push_consumer(ClientType.STRUCTURED_EVENT, proxyIdHolder));
+					LOG_NC_ConsumerProxyCreation_OK.log(logger, proxyIdHolder.value, "-unknown-", proxyCreationAttempts, services.getName(), channelName, getNotificationFactoryName());
+				} catch (AdminLimitExceeded ex2) {
+					LOG_NC_ConsumerProxyCreation_FAIL.log(logger, services.getName(), channelName, getNotificationFactoryName(), ex2.getMessage());
+					throw new AcsJCORBAProblemEx(ex2);
+				}
+			} catch (AdminLimitExceeded e) {
+				LOG_NC_ConsumerProxyCreation_FAIL.log(logger, services.getName(), channelName, getNotificationFactoryName(), e.getMessage());
+				throw new AcsJCORBAProblemEx(e);
+			}
 		}
 
 		// must connect this StructuredPushSupplier to the proxy consumer, or
@@ -235,8 +281,10 @@ public class NCPublisher extends OSPushSupplierPOA implements AcsEventPublisher,
 			throw new AcsJCORBAProblemEx(e);
 		}
 		
-		m_callback = new AcsNcReconnectionCallback(this);
-		m_callback.init(services, helper.getNotifyFactory());
+		eventBuff = new CircularQueue();
+		
+		reconnectCallback = new AcsNcReconnectionCallback(this);
+		reconnectCallback.init(services, helper.getNotifyFactory());
 
 	}
 
@@ -246,38 +294,49 @@ public class NCPublisher extends OSPushSupplierPOA implements AcsEventPublisher,
 	 * this method multiple times either. Once disconnect has been called, all
 	 * of SimpleSupplier's methods will cease to function properly.
 	 */
+	@Override
 	public void disconnect() {
 		if (supplierAdmin == null) {
 			throw new IllegalStateException("Publisher already disconnected");
 		}
 
-		String errMsg = "Failed to cleanly disconnect SimpleSupplier for channel '"
-				+ channelName + "': ";
+		String errMsg = "Failed to cleanly disconnect SimpleSupplier for channel '" + channelName + "': ";
 		try {
 			// handle notification channel cleanup
 			proxyConsumer.disconnect_structured_push_consumer();
 		} catch (Throwable thr) {
-			logger.log(Level.WARNING, errMsg
-					+ "could not disconnect push consumer", thr);
+			logger.log(Level.WARNING, errMsg + "could not disconnect push consumer", thr);
 		}
 
 		try {
 			supplierAdmin.destroy();
 		} catch (Throwable thr) {
-			logger.log(Level.WARNING, errMsg
-					+ "could not destroy supplier admin", thr);
+			logger.log(Level.WARNING, errMsg + "could not destroy supplier admin", thr);
 		}
 
 		try {
 			// clean-up CORBA stuff
 			services.deactivateOffShoot(this);
 		} catch (Throwable thr) {
-			logger.log(Level.WARNING, errMsg
-					+ "could not deactivate the SimpleSupplier offshoot.", thr);
+			logger.log(Level.WARNING, errMsg + "could not deactivate the SimpleSupplier offshoot.", thr);
 		}
+		
+		reconnectCallback = null;
 		proxyConsumer = null;
 		supplierAdmin = null;
 	}
+
+
+//	/**
+//	 * Destroys the notification channel, which may be in use by other suppliers or receivers.
+//	 * @Deprecated  This feature is luckily not used as of ALMA-5_0_1_9 (2007-12). We must first investigate 
+//	 *              when/how we can know that it is safe to destroy a channel object.
+//	 */
+//	protected void destroyNotificationChannel() throws AcsJException {
+//		helper.destroyNotificationChannel(channelName, getChannelKind(), channel);
+//	}
+
+
 
 	/**
 	 * This method returns a constant character pointer to the "kind" of
@@ -318,8 +377,7 @@ public class NCPublisher extends OSPushSupplierPOA implements AcsEventPublisher,
 	 * @return string
 	 */
 	protected String getNotificationFactoryName() {
-		return helper.getNotificationFactoryNameForChannel(channelName,
-				channelNotifyServiceDomainName);
+		return helper.getNotificationFactoryNameForChannel(channelName, channelNotifyServiceDomainName);
 	}
 
 	/**
@@ -328,8 +386,7 @@ public class NCPublisher extends OSPushSupplierPOA implements AcsEventPublisher,
 	 * @see org.omg.CosNotifyComm.NotifySubscribeOperations#subscription_change(org.omg.CosNotification.EventType[],
 	 *      org.omg.CosNotification.EventType[])
 	 */
-	public void subscription_change(EventType[] added, EventType[] removed)
-			throws InvalidEventType {
+	public void subscription_change(EventType[] added, EventType[] removed) throws InvalidEventType {
 		throw new NO_IMPLEMENT();
 	}
 
@@ -351,8 +408,7 @@ public class NCPublisher extends OSPushSupplierPOA implements AcsEventPublisher,
 	 *       SimpleSupplier seemss unaffected of consumers in the ACS NC design.
 	 */
 	public void disconnect_structured_push_supplier() {
-		String msg = "A Consumer has disconnected from the '" + channelName
-				+ "' channel";
+		String msg = "A Consumer has disconnected from the '" + channelName + "' channel";
 		logger.info(msg);
 	}
 
@@ -367,6 +423,8 @@ public class NCPublisher extends OSPushSupplierPOA implements AcsEventPublisher,
 	 */
 	protected void publishCORBAEvent(StructuredEvent se) throws AcsJException {
 		try {
+			// @TODO: Merge eventBuff handling from SimpleSupplier (rev. 1.48) here...
+			
 			// Publish directly the given event (see CORBA NC spec 3.3.7.1)
 			proxyConsumer.push_structured_event(se);
 		} catch (org.omg.CosEventComm.Disconnected e) {
@@ -411,8 +469,7 @@ public class NCPublisher extends OSPushSupplierPOA implements AcsEventPublisher,
 
 		// event.header.fixed_header.event_type
 		EventType event_type = new EventType(getChannelDomain(), typeName);
-		FixedEventHeader fixed_header = new FixedEventHeader(event_type,
-				eventName);
+		FixedEventHeader fixed_header = new FixedEventHeader(event_type, eventName);
 
 		// event.header.variable_header
 		Property[] variable_header = new Property[0];
@@ -436,13 +493,16 @@ public class NCPublisher extends OSPushSupplierPOA implements AcsEventPublisher,
 	 *             There are an enormous amount of possibilities pertaining to
 	 *             why an AcsJException would be thrown by publishEvent.
 	 */
-	public void publishEvent(Object customStruct) throws AcsJException {
+	@Override
+	public void publishEvent(IDLEntity customStruct) throws AcsJException {
 
-		if( !(customStruct instanceof IDLEntity) ) {
-			throw new AcsJPublishEventFailureEx(new ClassCastException("'" + customStruct.getClass().getName() + "' doesn't inherit from org.omg.CORBA.portable.IDLEntity"));
-		}
-
+		// Now that IDLEntity is back in the interface, we may delete the following lines. 
+		// Let's first verify that the use of generics between base class and here is OK also for the DDS side. 
+//		if( !(customStruct instanceof IDLEntity) ) {
+//			throw new AcsJPublishEventFailureEx(new ClassCastException("'" + customStruct.getClass().getName() + "' doesn't inherit from org.omg.CORBA.portable.IDLEntity"));
+//		}
 		IDLEntity customStructEntity = (IDLEntity)customStruct;
+		
 		String typeName = customStruct.getClass().getSimpleName();
 		// event to send
 		StructuredEvent event = getCORBAEvent(typeName, "");
@@ -454,20 +514,17 @@ public class NCPublisher extends OSPushSupplierPOA implements AcsEventPublisher,
 		// get the useful data which includes the component's name, timestamp,
 		// and event count
 		EventDescription descrip = new EventDescription(services.getName(),
-				alma.acs.util.UTCUtility.utcJavaToOmg(System
-						.currentTimeMillis()), count);
+				alma.acs.util.UTCUtility.utcJavaToOmg(System.currentTimeMillis()), count);
 		// store the IDL struct into the structured event
 		EventDescriptionHelper.insert(event.remainder_of_body, descrip);
 
 		// preallocate one name/value pair
 		event.filterable_data = new Property[1];
 		event.filterable_data[0] = new Property(
-				alma.acscommon.DEFAULTDATANAME.value, anyAide
-						.complexObjectToCorbaAny(customStructEntity));
+				alma.acscommon.DEFAULTDATANAME.value, anyAide.complexObjectToCorbaAny(customStructEntity));
 
 		if (isTraceEventsEnabled) {
-			logger.log(Level.INFO, "Channel:" + channelName + ", Event Type:"
-					+ typeName);
+			logger.log(Level.INFO, "Channel:" + channelName + ", Event Type:" + typeName);
 		}
 
 		publishCORBAEvent(event);
@@ -488,10 +545,8 @@ public class NCPublisher extends OSPushSupplierPOA implements AcsEventPublisher,
 		}
 
 		try {
-			channel.set_qos(helper.getChannelProperties().
-					getCDBQoSProps(channelName));
-			channel.set_admin(helper.getChannelProperties().
-					getCDBAdminProps(channelName));
+			channel.set_qos(helper.getChannelProperties().getCDBQoSProps(channelName));
+			channel.set_admin(helper.getChannelProperties().getCDBAdminProps(channelName));
 		} catch (UnsupportedQoS e) {
 		} catch (AcsJException e) {
 		} catch (UnsupportedAdmin ex) {
