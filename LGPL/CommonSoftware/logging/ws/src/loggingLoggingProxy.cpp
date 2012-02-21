50bbba052a7b73f134841fd3e5ca5b94d7350234
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2012-02-21 10:39:04 +0000

    Oops: it was clearing an alarm instead of raising and vice-versa!
    A further check ensures that alarm raising/clearing is disabled when the throttle is disabled (i.e. alarmsPerSecond=-1)
    
    
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@169693 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/logging/ws/src/loggingLoggingProxy.cpp
+++ LGPL/CommonSoftware/logging/ws/src/loggingLoggingProxy.cpp
@@ -19,7 +19,7 @@
 *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 *
 *
-* "@(#) $Id: loggingLoggingProxy.cpp,v 1.86 2012/02/20 16:23:26 acaproni Exp $"
+* "@(#) $Id: loggingLoggingProxy.cpp,v 1.87 2012/02/21 10:39:04 acaproni Exp $"
 *
 * who       when        what
 * --------  ---------   ----------------------------------------------
@@ -59,7 +59,7 @@
 #define LOG_NAME "Log"
 #define DEFAULT_LOG_FILE_NAME "acs_local_log"
 
-ACE_RCSID(logging, logging, "$Id: loggingLoggingProxy.cpp,v 1.86 2012/02/20 16:23:26 acaproni Exp $");
+ACE_RCSID(logging, logging, "$Id: loggingLoggingProxy.cpp,v 1.87 2012/02/21 10:39:04 acaproni Exp $");
 unsigned int LoggingProxy::setClrCount_m = 0;
 bool LoggingProxy::initialized = false;
 int LoggingProxy::instances = 0;
@@ -1606,12 +1606,12 @@ bool LoggingProxy::sendRecord(const Logging::XmlLogRecordSeq &reclist)
 		ace_mon.release();
 		if(canSend) {
 			m_logger->writeRecords(reclist);
-			if (logThrottleAlarm_p!=NULL) {
-				logThrottleAlarm_p->raiseLogThrottleAlarm();
+			if (logThrottleAlarm_p!=NULL && m_maxLogsPerSecond!=-1) {
+				logThrottleAlarm_p->clearLogThrottleAlarm();
 			}
 		} else {
-			if (logThrottleAlarm_p!=NULL) {
-				logThrottleAlarm_p->clearLogThrottleAlarm();
+			if (logThrottleAlarm_p!=NULL && m_maxLogsPerSecond!=-1) {
+				logThrottleAlarm_p->raiseLogThrottleAlarm();
 			}
 		}
 
