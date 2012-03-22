afbd103d8cbbb1432f44071577ccc4509758e516
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2012-03-22 15:16:45 +0000

    Added "-interactive" option, to call the target component only when the user hits ENTER.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@171205 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jcont/src/alma/acs/component/client/ComponentRefTracker.java
+++ LGPL/CommonSoftware/jcont/src/alma/acs/component/client/ComponentRefTracker.java
@@ -1,5 +1,8 @@
 package alma.acs.component.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
@@ -29,10 +32,17 @@ public class ComponentRefTracker extends ComponentClient
 	
 	private ACSComponent targetComp;
 
-	private int pingDelaySec;
+	private int pingDelaySec = -1;
 
 	private volatile CountDownLatch shutdownSync = null;
-	
+
+	private boolean checkExists = false;
+
+	/**
+	 * Reader for System.in, or null if option "-interactive" was not specified.
+	 */
+	private BufferedReader inputReader;
+
 	@Override
 	public void initRemoteLogging() {
 		// do nothing
@@ -62,34 +72,59 @@ public class ComponentRefTracker extends ComponentClient
 		cmdArgs.registerOption(optCurl);
 		CmdLineRegisteredOption optPingDelaySec = new CmdLineRegisteredOption("-delaySec", 1);
 		cmdArgs.registerOption(optPingDelaySec);
+		CmdLineRegisteredOption optCheckExists = new CmdLineRegisteredOption("-checkExists", 0);
+		cmdArgs.registerOption(optCheckExists);
+		CmdLineRegisteredOption optInteractive = new CmdLineRegisteredOption("-interactive", 0);
+		cmdArgs.registerOption(optInteractive);
 
 		cmdArgs.parseArgs(args);
 
 		if (cmdArgs.isSpecified(optHelp)) {
-			System.out.println("Usage: acsStartJava " + getClass().getName() + " " 
-					+ optCurl.getName() + " <curl> "
-					+ "[ " + optPingDelaySec.getName() + " <delay in s between component calls> ]");
+			System.out.println("Usage: acsStartJava " + getClass().getName()  
+					+ "\n\t" + optCurl.getName() + " <curl> "
+					+ "\n\t" + "[ " + optInteractive.getName() + " | " + optPingDelaySec.getName() + " <delay in s between component calls> ]"
+					+ "\n\t" + "[ " + optCheckExists.getName() + " ]"
+					);
 			return false;
 		}
 		else {
+			if (cmdArgs.isSpecified(optInteractive)) {
+				inputReader = new BufferedReader(new InputStreamReader(System.in));
+			} 
+			
 			if (cmdArgs.isSpecified(optPingDelaySec)) {
-				pingDelaySec = Integer.parseInt(cmdArgs.getValues(optPingDelaySec)[0].trim());
+				if (inputReader != null) {
+					m_logger.info("Ignoring setting for ping delay because only manual component calls will be used.");
+				}
+				else {
+					pingDelaySec = Integer.parseInt(cmdArgs.getValues(optPingDelaySec)[0].trim());
+				}
 			} 
 			else {
-				pingDelaySec = 5;
+				if (inputReader == null) {
+					pingDelaySec = 5;
+				}
 			}
+			
 			if (cmdArgs.isSpecified(optCurl)) {
 				targetCompUrl = cmdArgs.getValues(optCurl)[0].trim();
 			} 
 			else {
 				throw new IllegalArgumentException("Option '" + optCurl.getName() + "' must be specified.");
 			}
+			
+			if (cmdArgs.isSpecified(optCheckExists)) {
+				checkExists = true;
+			} 
+			
 		}
 		return true;
 	}
 
+	
 	private void run(String[] args) throws Exception {
 		try {
+			// @TODO: Call setOptions before connecting to the manager etc in the base class ctor
 			if (!setOptions(args)) {
 				return;
 			}
@@ -103,46 +138,73 @@ public class ComponentRefTracker extends ComponentClient
 			}
 			String ior = getContainerServices().getAdvancedContainerServices().corbaObjectToString(targetComp);
 		
-			m_logger.info("Got reference to component '" + targetCompUrl + "', IOR='" + ior + "'. Will call it every " + pingDelaySec + " seconds.");
-			
-			long nextCallTime = -1;
-			while(shutdownSync == null) {
-				if (System.currentTimeMillis() >= nextCallTime) {
-					StopWatch sw = new StopWatch();
-					try {
-						if (targetComp._non_existent()) {
-							// Not sure if we gain anything from calling _non_existent.
-							// It may only return true if our proxy object has been destroyed.
-							// In case of target container ORB problems, it will throw an exception, just like the name() call does.
-							m_logger.info("Component '" + targetCompUrl + "' does not exist (see Corba spec 4.3.5.1). Call took " + sw.getLapTimeMillis() + " ms.");
-						}
-						else {
-							String currentName = targetComp.name();
-							
-							String msg = "Call to " + targetCompUrl + "#name() returned fine";
-							if (!targetCompUrl.equals(currentName)) {
-								msg += " but with unexpected name '" + currentName;
-							}
-							m_logger.info(msg + ". Call took " + sw.getLapTimeMillis() + " ms.");
-						}
-					} catch (Throwable thr) {
-						m_logger.info("Call to component '" + targetCompUrl + "' failed with " + thr.getClass().getName() + ", after " + sw.getLapTimeMillis() + " ms.");
-					}
-					nextCallTime = System.currentTimeMillis() + pingDelaySec * 1000;
-				}
-				else {
-					Thread.sleep(250); // sleep short time only, to be responsive to shutdown
-				}
+			String msg = "Got reference to component '" + targetCompUrl + "', IOR='" + ior + "'.";
+			if (pingDelaySec > 0) {
+				msg += " Will call it every " + pingDelaySec + " seconds.";
 			}
-			tearDown();
-			shutdownSync.countDown();
+			m_logger.info(msg);
+			
+			runLoop();
 		}
 		finally {
+			tearDown();
+			
 			// if we exit from here through an exception rather than external SIGINT, 
 			// the shutdown hook will be called anyway, and we must prepare it for this case... 
 			m_shutdownHook.setRegularShutdownExpected();
+			
+			shutdownSync.countDown(); // free the shutdown hook thread
 		}
 	}
+
+	private void runLoop() throws IOException, InterruptedException {
+		long nextCallTime = -1;
+		
+		if (inputReader != null) {
+			m_logger.info("Press ENTER to call " + targetCompUrl);
+		}
+		
+		while(shutdownSync == null) {
+			
+			// Check if we should simply wait for user input or more time to pass
+			if ( (inputReader != null && !inputReader.ready()) || System.currentTimeMillis() < nextCallTime) {
+				Thread.sleep(100); // sleep short time only, to be responsive to shutdown
+				continue;
+			}
+
+			if (inputReader != null) {
+				// We are in interactive mode.
+				// There is user input, thus we can dare to call the otherwise non-interruptable blocking readLine() method.
+				inputReader.readLine();
+			}
+			
+			StopWatch sw = new StopWatch();
+			try {
+				if (checkExists && targetComp._non_existent()) {
+					// Not sure if we gain anything from calling _non_existent.
+					// It may only return true if our proxy object has been destroyed.
+					// In case of target container ORB problems, it will throw an exception, just like the name() call does.
+					m_logger.info("Component '" + targetCompUrl + "' does not exist (see Corba spec 4.3.5.1). Call took " + sw.getLapTimeMillis() + " ms.");
+				}
+				else {
+					String currentName = targetComp.name();
+					
+					String msg = "Call to " + targetCompUrl + "#name() returned fine";
+					if (!targetCompUrl.equals(currentName)) {
+						msg += " but with unexpected name '" + currentName;
+					}
+					m_logger.info(msg + ". Call took " + sw.getLapTimeMillis() + " ms.");
+				}
+			} catch (Throwable thr) {
+				m_logger.info("Call to component '" + targetCompUrl + "' failed with " + thr.getClass().getName() + ", after " + sw.getLapTimeMillis() + " ms.");
+			}
+			
+			if (inputReader == null) {
+				nextCallTime = System.currentTimeMillis() + pingDelaySec * 1000;
+			}
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 		
