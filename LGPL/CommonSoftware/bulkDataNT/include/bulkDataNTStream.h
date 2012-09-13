a1d3c2407a9a3f316f1e3aaa4854e35419d15e61
bjeram <bjeram@523d945c-050c-4681-91ec-863ad3bb968a>
2012-09-13 10:58:56 +0000

    StreamConfiguration configuration_m should be a member not just a reference.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@179203 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/bulkDataNT/include/bulkDataNTStream.h
+++ LGPL/CommonSoftware/bulkDataNT/include/bulkDataNTStream.h
@@ -18,7 +18,7 @@
 * License along with this library; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 *
-* "@(#) $Id: bulkDataNTStream.h,v 1.15 2012/06/14 11:28:34 bjeram Exp $"
+* "@(#) $Id: bulkDataNTStream.h,v 1.16 2012/09/13 10:58:56 bjeram Exp $"
 *
 * who       when      what
 * --------  --------  ----------------------------------------------
@@ -83,7 +83,7 @@ protected:
 
 	std::string streamName_m;
 
-	const StreamConfiguration  &configuration_m; //configuration
+	const StreamConfiguration  configuration_m; //configuration
 	// those two methods and members should probably go to another class  ??
 	void createDDSFactory();
 	void destroyDDSFactory();
