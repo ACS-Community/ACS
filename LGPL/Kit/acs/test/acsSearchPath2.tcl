760437a51557a1cfa3ab2aac7dc2e2c6b9007ee1
eallaert <eallaert@523d945c-050c-4681-91ec-863ad3bb968a>
2012-09-20 12:09:55 +0000

    added acsSearchPath.py utility/module, including corresponding changes in Makefile and test/subdir - see also COMP-5690.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@179496 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/Kit/acs/test/acsSearchPath2.tcl
@@ -0,0 +1,5 @@
+# -*- tcl -*-
+set ::env(INTROOT) /introot/myIntroot
+set ::env(INTLIST) /introot/herIntroot:/introot/hisIntroot
+set ::env(ACSROOT) /alma/ACS15/ACSSW
+set ::env(MODPATH) 1
---
+++ LGPL/Kit/acs/test/acsSearchPath5.tcl
@@ -0,0 +1,4 @@
+# -*- tcl -*-
+catch {unset ::env(INTROOT)}
+catch {unset ::env(INTLIST)}
+catch {unset ::env(ACSROOT)}
