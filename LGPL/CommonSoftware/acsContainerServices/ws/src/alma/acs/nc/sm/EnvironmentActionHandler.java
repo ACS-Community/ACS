f088aad27ff9ff4934d748af18d250b663f362b1
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2012-10-05 11:50:36 +0000

    Added AcsJStateMachineActionEx
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@180106 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsContainerServices/ws/src/alma/acs/nc/sm/EnvironmentActionHandler.java
+++ LGPL/CommonSoftware/acsContainerServices/ws/src/alma/acs/nc/sm/EnvironmentActionHandler.java
@@ -6,15 +6,21 @@ import java.util.logging.Logger;
 import org.apache.commons.scxml.ErrorReporter;
 import org.apache.commons.scxml.EventDispatcher;
 import org.apache.commons.scxml.SCInstance;
-import org.apache.commons.scxml.SCXMLExpressionException;
 import org.apache.commons.scxml.TriggerEvent;
-import org.apache.commons.scxml.model.ModelException;
 
+import alma.ACSErrTypeCommon.wrappers.AcsJStateMachineActionEx;
+import alma.acs.nc.AcsEventSubscriberImplBase;
 import alma.acs.nc.sm.generated.EventSubscriberAction;
 import alma.acs.nc.sm.generic.AcsScxmlActionExecutor;
 
 /**
- * Handler for actions EnvironmentCreator and EnvironmentDestructor
+ * Handler for actions EnvironmentCreator and EnvironmentDestructor.
+ * <p>
+ * Note that grouping these two actions into one class is an arbitrary choice,
+ * not demanded by the state machine framework.
+ * <p>
+ * In this class we give a fairly empty base implementation which 
+ * can be overridden by subclasses of {@link AcsEventSubscriberImplBase}. 
  * 
  * @author hsommer
  */
@@ -29,7 +35,7 @@ public class EnvironmentActionHandler implements AcsScxmlActionExecutor<EventSub
 	@Override
 	public boolean execute(EventSubscriberAction action, EventDispatcher evtDispatcher, ErrorReporter errRep,
 			SCInstance scInstance, Collection<TriggerEvent> derivedEvents) 
-					throws ModelException, SCXMLExpressionException {
+					throws AcsJStateMachineActionEx {
 		switch (action) {
 		case EnvironmentCreator:
 			create(evtDispatcher, errRep, scInstance, derivedEvents);
@@ -45,7 +51,8 @@ public class EnvironmentActionHandler implements AcsScxmlActionExecutor<EventSub
 		
 	}
 	
-	public void create(EventDispatcher evtDispatcher, ErrorReporter errRep, SCInstance scInstance, Collection<TriggerEvent> derivedEvents) {
+	public void create(EventDispatcher evtDispatcher, ErrorReporter errRep, SCInstance scInstance, Collection<TriggerEvent> derivedEvents) 
+			throws AcsJStateMachineActionEx {
 		logger.info("EnvironmentCreator action called, in the user-supplied action dispatcher");
 	}
 	
