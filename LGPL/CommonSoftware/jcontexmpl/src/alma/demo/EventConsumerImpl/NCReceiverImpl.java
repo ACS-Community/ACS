ab72e24427a2ebd472b5631af1076df4522bbbd9
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2012-10-05 13:51:58 +0000

    Added subscriber type parameter (AcsEventSubscriber<temperatureDataBlockEvent>)
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@180123 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jcontexmpl/src/alma/demo/EventConsumerImpl/NCReceiverImpl.java
+++ LGPL/CommonSoftware/jcontexmpl/src/alma/demo/EventConsumerImpl/NCReceiverImpl.java
@@ -39,27 +39,24 @@ import alma.demo.NCReceiverOperations;
  */
 public class NCReceiverImpl extends ComponentImplBase implements NCReceiverOperations, Callback<temperatureDataBlockEvent>
 {
-    private AcsEventSubscriber m_consumer;
+	private AcsEventSubscriber<temperatureDataBlockEvent> m_consumer;
 
-    public void initialize(ContainerServices containerServices)
-		throws ComponentLifecycleException
-	{
+	public void initialize(ContainerServices containerServices) throws ComponentLifecycleException {
 
-        super.initialize(containerServices);
+		super.initialize(containerServices);
 
-		try
-		{
-	        m_consumer = m_containerServices.createNotificationChannelSubscriber(alma.FRIDGE.CHANNELNAME_FRIDGE.value);
+		try {
+			m_consumer = m_containerServices.createNotificationChannelSubscriber(alma.FRIDGE.CHANNELNAME_FRIDGE.value, temperatureDataBlockEvent.class);
 			m_consumer.addSubscription(this);
 			m_consumer.startReceivingEvents();
 			m_logger.info("ConsumerComp is waiting for 'temperatureDataBlockEvent' events.");
-		}
-		catch (Exception e)
-		{
-			if (m_consumer != null) {				
+			
+		} catch (Exception e) {
+			if (m_consumer != null) {
 				m_consumer.disconnect();
 			}
-			throw new ComponentLifecycleException("failed to connect as an event consumer to channel " + alma.FRIDGE.CHANNELNAME_FRIDGE.value);
+			throw new ComponentLifecycleException("failed to connect as an event consumer to channel "
+					+ alma.FRIDGE.CHANNELNAME_FRIDGE.value);
 		}
 	}
 
@@ -67,7 +64,6 @@ public class NCReceiverImpl extends ComponentImplBase implements NCReceiverOpera
 	public void receive(temperatureDataBlockEvent joe, EventDescription desc) {
 		m_logger.info("Received an event: the temp difference is: " + joe.absoluteDiff);
 	}
-
 
 	@Override
 	public Class<temperatureDataBlockEvent> getEventType() {
