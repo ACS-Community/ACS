07313c4058bf9876773f57f3ea1e52738ef5944f
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2012-10-05 11:44:17 +0000

    Signal methods declare "AcsJIllegalStateEventEx", "AcsJStateMachineActionEx" because now "fireSignalWithErrorFeedback" gets used underneath.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@180105 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsContainerServices/ws/src/alma/acs/nc/sm/generated/EventSubscriberSignalHandler.java
+++ LGPL/CommonSoftware/acsContainerServices/ws/src/alma/acs/nc/sm/generated/EventSubscriberSignalHandler.java
@@ -1,25 +1,37 @@
 package alma.acs.nc.sm.generated;
 
+import alma.ACSErrTypeCommon.wrappers.AcsJIllegalStateEventEx;
+import alma.ACSErrTypeCommon.wrappers.AcsJStateMachineActionEx;
+
 /**
  * Interface to send events to the SM, similar
  * to the IDL-generated component "Operations" interface 
- * 
+ * <p>
+ * TODO: A code-generator option should decide whether the named
+ * event methods delegate to {@link #fireSignal(Enum)}
+ * or to {@link #fireSignalWithErrorFeedback(Enum)}, 
+ * and add the declared exceptions accordingly.
+ * Currently we use fireSignalWithErrorFeedback.
  * @author hsommer
  */
 public interface EventSubscriberSignalHandler
 {
-	boolean setUpEnvironment();
+	boolean setUpEnvironment() throws AcsJIllegalStateEventEx, AcsJStateMachineActionEx;
 
-	boolean startReceivingEvents();
+	boolean startReceivingEvents() throws AcsJIllegalStateEventEx, AcsJStateMachineActionEx;
 
-	boolean suspend();
+	boolean suspend() throws AcsJIllegalStateEventEx, AcsJStateMachineActionEx;
 
-	boolean resume();
+	boolean resume() throws AcsJIllegalStateEventEx, AcsJStateMachineActionEx;
 
-	boolean stopReceivingEvents();
+	boolean stopReceivingEvents() throws AcsJIllegalStateEventEx, AcsJStateMachineActionEx;
 
-	boolean cleanUpEnvironment();
+	boolean cleanUpEnvironment() throws AcsJIllegalStateEventEx, AcsJStateMachineActionEx;
 	
 
 	boolean fireSignal(EventSubscriberSignal signal);
+
+	boolean fireSignalWithErrorFeedback(EventSubscriberSignal signal) 
+			throws AcsJIllegalStateEventEx, AcsJStateMachineActionEx;
+
 }
