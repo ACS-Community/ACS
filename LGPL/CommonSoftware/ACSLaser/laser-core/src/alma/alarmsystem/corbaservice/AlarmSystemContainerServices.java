6803e350ace6aabe9233a8abe620eb5a4bc7f4b7
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2012-10-07 08:06:02 +0000

    Subscriber API change
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@180161 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/ACSLaser/laser-core/src/alma/alarmsystem/corbaservice/AlarmSystemContainerServices.java
+++ LGPL/CommonSoftware/ACSLaser/laser-core/src/alma/alarmsystem/corbaservice/AlarmSystemContainerServices.java
@@ -180,13 +180,13 @@ public class AlarmSystemContainerServices implements ContainerServicesBase {
 
 
 	@Override
-	public AcsEventSubscriber createNotificationChannelSubscriber(String channelName) throws AcsJContainerServicesEx {
+	public <T> AcsEventSubscriber<T> createNotificationChannelSubscriber(String channelName, Class<T> eventType) throws AcsJContainerServicesEx {
 		throw new AcsJContainerServicesEx(new AcsJNotImplementedEx("createNotificationChannelSubscriber not yet implemented in this special alarm service CS class."));
 	}
 
 	@Override
-	public AcsEventSubscriber createNotificationChannelSubscriber(String channelName,
-			String channelNotifyServiceDomainName) throws AcsJContainerServicesEx {
+	public <T> AcsEventSubscriber<T> createNotificationChannelSubscriber(String channelName, 
+		String channelNotifyServiceDomainName, Class<T> eventType) throws AcsJContainerServicesEx {
 		throw new AcsJContainerServicesEx(new AcsJNotImplementedEx("createNotificationChannelSubscriber not yet implemented in this special alarm service CS class."));
 	}
 
