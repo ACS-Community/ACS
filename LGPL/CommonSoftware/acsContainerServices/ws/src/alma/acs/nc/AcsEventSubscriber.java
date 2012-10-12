273a458f9db770d250ed369f35ece8195ff22881
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2012-10-12 16:16:49 +0000

    - Declares AcsJ-exceptions, aligned with the underlying state machine. - Updated and extended javadoc   - Documented semantic change: disconnect() no longer throws an exception if we never called startReceivingEvents,     on the contrary it must always be called.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@180411 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsContainerServices/ws/src/alma/acs/nc/AcsEventSubscriber.java
+++ LGPL/CommonSoftware/acsContainerServices/ws/src/alma/acs/nc/AcsEventSubscriber.java
@@ -19,27 +19,51 @@
 
 package alma.acs.nc;
 
+import alma.ACSErrTypeCommon.wrappers.AcsJCouldntPerformActionEx;
+import alma.ACSErrTypeCommon.wrappers.AcsJIllegalStateEventEx;
+import alma.acsErrTypeLifeCycle.wrappers.AcsJEventSubscriptionEx;
 import alma.acsnc.EventDescription;
 
 /**
- * This interface provides an event subscriber, 
- * that can be implemented on top of Corba NC or DDS, in-memory for local testing, or on top of other technologies.
+ * This interface provides an event subscriber
+ * that can be implemented on top of Corba NC or DDS, in-memory for local testing, 
+ * or hopefully also on top of other technologies such as ZeroMQ.
+ * <p>
+ * Subscriber lifecycle:
+ * <ul>
+ *   <li> Creation of the subscriber object may already allocate remote resources.
+ *        Therefore you <b>must always call {@link #disconnect()} when done using the subscriber</b>,
+ *        even if {@link #startReceivingEvents()} was never called.
+ *   <li> Subscriptions by event type or for all types of events can be set up
+ *        using {@link #addSubscription(Callback)} and {@link #addGenericSubscription(GenericCallback)}. <br>
+ *        Subscriptions can optionally be removed again using {@link #removeSubscription(Class)}
+ *        and {@link #removeGenericSubscription()}.
+ *        TODO: Verify that these can be called both before and after startReceivingEvents. <br>
+ *   <li> Call {@link #startReceivingEvents()} to actually subscribe.
+ *   <li> Call {@link #suspend()} and {@link #resume()} to temporarily suspend and later resume
+ *        the subscription. Depending on the underlying framework, suspending will typically
+ *        cause events to be queued somewhere.
+ *   <li> Call {@link #disconnect()} to destroy all resources. The subscriber cannot be used afterwards.
+ * </ul>
+ * The lifecycle is internally implemented using a state machine, see {@link #getLifecycleState()}.
  * <p>
  * Discussion:
  * <ul>
- *   <li>One AcsEventSubscriber object may subscribe to different types of events,
- *       which for a Corba NC is normal, but for DDS requires tricks with topics and partitions.
- *   <li>Alternatively, we could make this class generic and fix a single event type for the entire object (interface),
- *       so that the java compiler would not allow to add a subscription for a different type.
- *   <li>TODO: The declared exceptions must be cleaned up. Rather no generic AcsJException, and no Corba-NC-specific exceptions.
- *       Also, determine which methods should expose IllegalStateException and which not
- *       ({@link #disconnect()}, {@link #resume()} and {@link #suspend()} depend on the current state of the subscriber).
- *   <li>TODO: We need to check if this API actually works for DDS.
- *   <li>An in-memory shortcutting (simulated channel/topic) could be useful, for testing without external processes running.
+ *   <li> One AcsEventSubscriber object may subscribe to different types of events,
+ *        which for a Corba NC is normal, but for DDS requires tricks with topics and partitions.
+ *        We might change this API to allow only one event type per NC.
+ *   <li> TODO: We need to check if this API actually works for DDS, by integrating Rodrigo's RTI prototype.
+ *   <li> For historical reasons the API is asymmetric with respect to creation and destruction of resources,
+ *        because resources are allocated both in the ctor and in {@link #startReceivingEvents()}, 
+ *        whereas they are released only in the single method {@link #disconnect()}.
+ *        However, the underlying state machine knows two 'connect' and two 'disconnect' transitions
+ *        and artificially maps them to the asymmetric API.
+ *        If we want to make the API symmetric, we could break up 'disconnect' into 'stopReceivingEvents' 
+ *        and 'destroy'. 
  * </ul>
  * @param <T> The event (base) type. If all events are of the same type then that type should be used; otherwise a common
- *          base type for all applicable events should be used, such as Object or IDLEntity. 
- *          
+ *            base type for all applicable events should be used, such as <code>Object</code> or <code>IDLEntity</code>. 
+ * 
  * @author jslopez, hsommer
  */
 public interface AcsEventSubscriber<T> {
@@ -48,63 +72,84 @@ public interface AcsEventSubscriber<T> {
 	/*   Subscription management methods         */
 	/*===========================================*/
 	/**
-	 * Adds a handler that will receive events of a specific type. The even type
+	 * Adds a handler that will receive events of a specific type. The event type
 	 * is determined by the value returned by calling {@link Callback#getEventType()}
-	 * over the <code>receiver</code> parameter.
+	 * on the <code>receiver</code> parameter.
 	 * <p>
 	 * Note that the same event type can only be subscribed to with one handler,
 	 * which means that another handler added for the same type will replace the previous handler.
-	 *
+	 * <p>
+	 * The event type must be that of actual events, not of base classes. 
+	 * For example, the AcsEventSubscriber could be parameterized with base type <code>IDLEntity</code>
+	 * and we could then add two subscriptions for events defined as IDL structs, 
+	 * say AntennaStatus and TemperatureData.
+	 * If you want to subscribe to events without knowing their exact type, 
+	 * use {@link #addGenericSubscription(GenericCallback)} instead.
+	 * 
 	 * @param receiver The callback to use when receiving events for the specified type.
 	 *
-	 * @throws CannotAddSubscriptionException If there is a problem and the receiver cannot
-	 *  be added
+	 * @throws AcsJEventSubscriptionEx If there is a problem and the receiver cannot be added
 	 */
 	public <U extends T> void addSubscription(Callback<U> receiver) 
-		throws CannotAddSubscriptionException;
+		throws AcsJEventSubscriptionEx;
 
 	/**
-	 * Removes the subscription for a specified event type, 
-	 * so that the handler previously registered for that event type 
-	 * will no longer receive events.
+	 * Removes the subscription for a specified event type or for all events types, so that the handler previously
+	 * registered for that event type will no longer receive events.
 	 * 
-	 * @param structClass the event type to be unsubscribed. If <code>null</code>,
-	 *  then all subscriptions but the generic subscription are removed.
-	 *
-	 * @throws SubscriptionNotFoundException if the specified event type has not been
-	 *  previously subscribed.
+	 * @param structClass
+	 *            the event type to be unsubscribed. If <code>null</code>, then all subscriptions but the generic
+	 *            subscription are removed.
+	 * 
+	 * @throws AcsJEventSubscriptionEx
+	 *             if the specified event type has not been previously subscribed or if the removal fails with a
+	 *             technical problem.
 	 */
 	public <U extends T> void removeSubscription(Class<U> structClass) 
-		throws SubscriptionNotFoundException; // TODO use AcsJ style ex
+		throws AcsJEventSubscriptionEx;
 
 	/**
 	 * Adds a generic handler for all types of events.
+	 * This should only be used by monitoring tools and other special applications.
+	 * For normal applications, use one or many calls to {@link #addSubscription(Callback)}, 
+	 * each with a Callback object that matches exactly the subscribed event type.
 	 * <p> 
-	 * It is possible to add a generic handler in addition to event type specific handlers
+	 * It is possible to add a generic handler in addition to event type-specific handlers
 	 * (where the latter will get precedence). 
 	 * Adding another generic handler will replace the previous generic handler.
 	 *
 	 * @param receiver The callback to use when receiving events
 	 *
-	 * @throws CannotAddSubscriptionException If there is a problem and the generic receiver cannot
+	 * @throws AcsJEventSubscriptionEx If there is a problem and the generic receiver cannot
 	 *  be added
 	 */
 	public void addGenericSubscription(GenericCallback receiver)
-		throws CannotAddSubscriptionException;
+		throws AcsJEventSubscriptionEx;
 	
 	/**
 	 * Removes the generic event handler, so that it will no longer receive events.
 	 * Event specific handlers may still receive events.
 	 *
 	 * @throws SubscriptionNotFoundException If a generic receiver has not been previously subscribed
-	 * @throws CannotRemoveSubscriptionExeption If there is any problem while unsubscribing the generic receiver
+	 * @throws AcsJEventSubscriptionEx If there is any problem while unsubscribing the generic receiver
 	 */
 	public void removeGenericSubscription()
-		throws SubscriptionNotFoundException;
+		throws AcsJEventSubscriptionEx;
 
+	
 	/*===========================================*/
 	/*   Lifecycle methods                       */
 	/*===========================================*/
+	
+	/**
+	 * Returns the lifecycle state as obtained from an internal state machine.
+	 * <p>
+	 * This state can be used only for debug output.
+	 * <b>The state names may change over time and should not be used to control execution.</b>
+	 */
+	public String getLifecycleState();
+	
+	
 	/**
 	 * This method must be called to actually start receiving events.
 	 * Typically it is called after the subscriptions are set up.
@@ -112,18 +157,17 @@ public interface AcsEventSubscriber<T> {
 	 * Also, the connection can be suspended and resumed.
 	 * <p>
 	 * No further invocations should be attempted on this method after one
-	 * has been already successful. Otherwise, an {@link IllegalStateException}
+	 * has been already successful. Otherwise, an {@link AcsJIllegalStateEventEx}
 	 * will be thrown. 
 	 * <p>
-	 * <b>If this method is not called, no event will be received</b>
+	 * <b>If this method is not called, no event will ever be received</b>
 	 * 
-	 * @throws CannotStartReceivingEventsException If any error happens while trying
-	 *   to start receiving events
-	 * @throws IllegalStateException If the user calls this method on an object that
+	 * @throws AcsJIllegalStateEventEx If the user calls this method on an object that
 	 *   is already receiving events
+	 * @throws AcsJCouldntPerformActionEx If any error happens while trying
+	 *   to start receiving events
 	 */
-	public void startReceivingEvents() throws CannotStartReceivingEventsException, IllegalStateException;
-
+	public void startReceivingEvents() throws AcsJIllegalStateEventEx, AcsJCouldntPerformActionEx;
 
 	/**
 	 * Disconnects this subscriber from the Notification Channel, and releases all
@@ -133,14 +177,10 @@ public interface AcsEventSubscriber<T> {
 	 * Calling this method over a subscriber object that has been already disconnected
 	 * will throw an {@link IllegalStateException}.
 	 * <p>
-	 * TODO: Define better the semantics for disconnecting a subscriber that was never connected.
-	 * It seems that it should be called in this case because the constructor may already allocate resources
-	 * that must be released here. Yet we currently throw IllegalStateException.
-	 * 
-	 * @throws IllegalStateException If this method is called over an AcsEventSubscriber object
+	 * @throws IllegalStateException If this method is called on an AcsEventSubscriber object
 	 * that has been already disconnected
 	 */
-	public void disconnect() throws IllegalStateException;
+	public void disconnect() throws AcsJIllegalStateEventEx, AcsJCouldntPerformActionEx;
 
 	/** 
 	 * Used to temporarily halt receiving events of all types.
@@ -150,12 +190,18 @@ public interface AcsEventSubscriber<T> {
 	 * unexpired events will be received later, after a call to {@link #resume()}.
 	 * 
 	 * <p>
-	 * This call has no effect if the Subscriber is not connected, or if it is
-	 * connected but already suspended.
+//	 * This call has no effect if the Subscriber is not connected, or if it is
+//	 * connected but already suspended.
 	 *
 	 * @throws IllegalStateException if the subscriber is not connected to an NC.
 	 */
-	public void suspend() throws IllegalStateException;
+	public void suspend() throws AcsJIllegalStateEventEx, AcsJCouldntPerformActionEx;
+
+	/**
+	 * Returns <code>true</code> if this subscriber has been suspended.
+	 */
+	public boolean isSuspended();
+
 
 	/**
 	 * Used to reenable the Subscriber after a call to the
@@ -165,15 +211,22 @@ public interface AcsEventSubscriber<T> {
 	 * This call has no effect if the Subscriber is not connected, or if it is
 	 * connected and already processing events.
 	 */
-	public void resume() throws IllegalStateException;
+	public void resume() throws AcsJIllegalStateEventEx, AcsJCouldntPerformActionEx;
 
 	/**
-	 * This ACS-defined interface replaces the runtime search for the
-	 * "receive(...)" method that works with Java introspection.
+	 * User callback for received events.
+	 * <p>
+	 * This ACS-defined interface replaces the runtime search for the old
+	 * "Consumer#receive(...)" method that was based on Java introspection.
 	 */
 	public static interface Callback<U> {
 		
-		public void receive(U event, EventDescription eventDescrip);
+		/**
+		 * Event delivery, from the framework to user code.
+		 * @param eventData The event data, e.g. an IDL-defined struct.
+		 * @param eventDescrip Event meta data.
+		 */
+		public void receive(U eventData, EventDescription eventDescrip);
 		
 		/**
 		 * This method is needed for adding event-specific subscriptions 
@@ -189,10 +242,7 @@ public interface AcsEventSubscriber<T> {
 	}
 	
 	/**
-	 * This ACS-defined interface replaces the runtime search for the
-	 * "receive(...)" method that works with Java introspection.
-	 * <p>
-	 * TODO: Use T instead of Object, but does not work right away...
+	 * Generic event callback, for use with {@link AcsEventSubscriber#addGenericSubscription(GenericCallback)}.
 	 */
 	public static interface GenericCallback {
 		public void receive(Object event, EventDescription eventDescrip);
