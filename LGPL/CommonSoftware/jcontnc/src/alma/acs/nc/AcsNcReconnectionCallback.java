337da8cdc883b8789b739aef428997ed5387fe99
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2012-10-12 16:31:47 +0000

    - Method "init" renamed to "registerForReconnect" to make clearer what it does.   - Made it robust against missing TAO notify extensions. - Added javadoc and some todos.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@180417 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jcontnc/src/alma/acs/nc/AcsNcReconnectionCallback.java
+++ LGPL/CommonSoftware/jcontnc/src/alma/acs/nc/AcsNcReconnectionCallback.java
@@ -20,38 +20,52 @@
  *******************************************************************************/
 package alma.acs.nc;
 
-import gov.sandia.NotifyMonitoringExt.EventChannelFactory;
-import gov.sandia.NotifyMonitoringExt.EventChannelFactoryHelper;
+import java.util.logging.Logger;
 
+import org.omg.CORBA.BAD_PARAM;
 import org.omg.CORBA.Object;
 
 import NotifyExt.ReconnectionCallback;
 import NotifyExt.ReconnectionCallbackHelper;
 import NotifyExt.ReconnectionRegistry;
 import NotifyExt.ReconnectionRegistryHelper;
+import gov.sandia.NotifyMonitoringExt.EventChannelFactory;
+import gov.sandia.NotifyMonitoringExt.EventChannelFactoryHelper;
+
+import alma.ACS.OffShoot;
+import alma.ACSErrTypeCommon.wrappers.AcsJIllegalArgumentEx;
 import alma.JavaContainerError.wrappers.AcsJContainerServicesEx;
 import alma.acs.container.ContainerServicesBase;
 import alma.acsnc.OSReconnectionCallbackPOA;
 
 /**
- * @TODO comment about design by TAO, and how we use it in ACS
+ * AcsNcReconnectionCallback implements the {@link ReconnectionCallback}
+ * from TAO's notify extensions, using an inheritance structure
+ * compatible with ACS's {@link OffShoot} concept.
+ * The publisher and subscriber classes use this class to register themselves
+ * for future {@link #reconnect(Object)} callbacks after a service restart.
+ * This class encapsulates the handling of the reconnection API.
+ *  
+ * TODO: Merge with event callback offshoot
+ * 
  * @author javarias
  */
 public class AcsNcReconnectionCallback extends OSReconnectionCallbackPOA {
 	
+	private final Logger logger;
 	private EventChannelFactory ecf_;
-	private ReconnectableSubscriber sub_;
+	private final ReconnectableParticipant parti;
 	private int callback_id_;
 	private ContainerServicesBase services;
 	private volatile boolean id_is_valid_;
 
-	public AcsNcReconnectionCallback(ReconnectableSubscriber sub){
+	public AcsNcReconnectionCallback(ReconnectableParticipant parti, Logger logger){
+		this.parti = parti;
+		this.logger = logger;
 		id_is_valid_ = false; 
-		sub_=sub;
 	}
 	
 	/**
-	 * 
 	 * @see NotifyExt.ReconnectionCallbackOperations#is_alive()
 	 */
 	@Override
@@ -59,30 +73,63 @@ public class AcsNcReconnectionCallback extends OSReconnectionCallbackPOA {
 		return true;
 	}
 
+	/**
+	 * Called by the NotifyService after service restart, giving the new EventChannelFactory reference.
+	 * This EventChannelFactory reference is passed to the owning ReconnectableParticipant
+	 * who should then use it to reconnect.
+	 * 
+	 * @see NotifyExt.ReconnectionCallbackOperations#reconnect(org.omg.CORBA.Object)
+	 */
 	@Override
 	public void reconnect(Object new_connection) {
 		ecf_ = EventChannelFactoryHelper.narrow(new_connection);
 		if (ecf_ != null){
-			sub_.reconnect(ecf_);
+			parti.reconnect(ecf_);
 		}
 	}
 	
-	public void init(ContainerServicesBase services, EventChannelFactory ecf ) {
-		if (ecf == null || services == null) {
-			return; // HSO: why not IllegalArgumentException ??
+	/**
+	 * Called by the event subscriber or supplier when connecting to the NC.
+	 * Corba-activates this callback object using <code>services</code> and
+	 * registers it with the NotifyService's {@link ReconnectionRegistry}.
+	 * <p>
+	 * @throws AcsJContainerServicesEx 
+	 * @throws AcsJIllegalArgumentEx 
+	 * @TODO: This call does not do anything if the <code>ecf == null</code>,
+	 * which is also mentioned in comments in the calling code. 
+	 * This should be cleaned up, e.g. checked if at all and under which circumstances
+	 * this null can happen. 
+	 */
+	public void registerForReconnect(ContainerServicesBase services, EventChannelFactory ecf ) throws AcsJContainerServicesEx, AcsJIllegalArgumentEx {
+		// TODO: This is strange...
+		if (ecf == null) {
+			return; 
+		}
+		
+		if (services == null) {
+			AcsJIllegalArgumentEx ex = new AcsJIllegalArgumentEx();
+			ex.setVariable("services");
+			ex.setValue("null");
+			throw ex;
 		}
 		this.services = services;
 		
-		ecf_=(EventChannelFactory) ecf._duplicate();
+		ReconnectionRegistry registry = null;
 		try {
-			ReconnectionCallback callback = ReconnectionCallbackHelper.narrow(services.activateOffShoot(this));
-			ReconnectionRegistry registry = ReconnectionRegistryHelper.narrow(ecf);
-			callback_id_ = registry.register_callback(callback);
-			id_is_valid_ = true;
-		} 
-		catch (AcsJContainerServicesEx e) {
-			e.printStackTrace(); // HSO: logging with repeatguard? Throw ex?
+			registry = ReconnectionRegistryHelper.narrow(ecf);
+		} catch (BAD_PARAM ex) {
+			// TODO: Or should we just return, same deal as above with ecf == null?
+			AcsJIllegalArgumentEx ex2 = new AcsJIllegalArgumentEx(ex);
+			ex2.setErrorDesc("The given EventChannelFactory is not a NotifyExt.ReconnectionRegistry. Make sure TAO extensions are used!");
+			throw ex2;
 		}
+		
+		// HSO: Is this _duplicate call useful, or a C++ literal translation?
+		ecf_=(EventChannelFactory) ecf._duplicate();
+		
+		ReconnectionCallback callback = ReconnectionCallbackHelper.narrow(services.activateOffShoot(this));
+		callback_id_ = registry.register_callback(callback);
+		id_is_valid_ = true;
 	}
 	
 	public void disconnect(){
