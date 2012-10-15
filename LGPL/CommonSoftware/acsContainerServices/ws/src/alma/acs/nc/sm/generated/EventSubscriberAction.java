33f739b18ddc1e850653329979526efe7bc01dc9
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2012-10-15 12:52:16 +0000

    javadoc
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@180511 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsContainerServices/ws/src/alma/acs/nc/sm/generated/EventSubscriberAction.java
+++ LGPL/CommonSoftware/acsContainerServices/ws/src/alma/acs/nc/sm/generated/EventSubscriberAction.java
@@ -3,15 +3,18 @@ package alma.acs.nc.sm.generated;
 import alma.acs.nc.sm.generic.AcsScxmlDispatchingAction;
 
 /**
- * All SM actions are mapped to the generic {@link AcsScxmlDispatchingAction}, 
- * so that SCXML will invoke {@link EventSubscriberActionDispatcher}.
+ * All SM actions get mapped to the generic {@link AcsScxmlDispatchingAction}
+ * that is parametrized with an action enum such as this one. 
+ * <p>
+ * Note that the SM framework mandates that all actions can be listed in an
+ * enum class. 
+ * However, the action implementation can be done in one or many classes. 
+ * {@link EventSubscriberAllActionsHandler} is useful in case you want to implement all actions
+ * in one class. 
  * <p>
  * This enum class replaces the config file config/SMActionMap.txt used in the ESO SM framework, 
  * together with the option of setting arbitrary action handlers.
- * <p>
- * TODO: Rename to uppercase and change the name to start with a verb, e.g. createEnvironment.
- *       The noun style goes back to when a handler class was generated for every action,
- *       and the action name had to provide the class name. Then also rename it in the scxml file.
+ * 
  * @author hsommer
  */
 public enum EventSubscriberAction {
