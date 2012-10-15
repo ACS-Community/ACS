1126096de792455516096add6fdcde0f6db7fad6
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2012-10-15 13:55:27 +0000

    - Ported from old NC Consumer to AcsEventSubscriber - Some other refactoring required because we can no longer subclass the NC consumer   if we want to get it the easy and safe way from the ContainerServices.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@180524 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/nctest/ws/test/alma/nctest/clients/FridgeConsumer.java
+++ LGPL/CommonSoftware/nctest/ws/test/alma/nctest/clients/FridgeConsumer.java
@@ -26,51 +26,76 @@
  */
 package alma.nctest.clients;
 
+import java.util.logging.Logger;
+
+import alma.FRIDGE.temperatureDataBlockEvent;
 import alma.acs.component.client.ComponentClient;
+import alma.acs.exceptions.AcsJException;
+import alma.acs.nc.AcsEventSubscriber;
+import alma.acs.nc.AcsEventSubscriber.Callback;
+import alma.acsnc.EventDescription;
 
 /** 
  * FridgeConsumer is a simple class that connects to the "fridge" notification channel,
  * receives five events, and then disconnects from the channel.
  * @author dfugate
  */
-public class FridgeConsumer extends alma.acs.nc.Consumer
+public class FridgeConsumer extends ComponentClient implements Callback<temperatureDataBlockEvent>
 {
-    /** 
-     * Total number of events that have been consumed.
-     */    
-    int eventCount = 0;
-    
-    volatile boolean readyForShutdown = false;
+	/**
+	 * Total number of events that have been consumed.
+	 */
+	private int eventCount = 0;
 
-    /** Creates a new instance of FridgeConsumer */
-    public FridgeConsumer(alma.acs.container.ContainerServices services) throws Exception
-    {
-        super(alma.FRIDGE.CHANNELNAME_FRIDGE.value, services);
-    }
-    
-    
-    ////////////////////////////////////////////////////////////////////////////
-    /** 
-     * <code>processEvent</code> <B>must</B> be overriden in Consumer subclasses to
-     * do something useful.
-     * 
-     * @param corbaData CORBA data extracted from the event.
-     */    
-    public void processEvent(Object corbaData) {
+	private volatile boolean readyForShutdown = false;
+	
+	private AcsEventSubscriber<temperatureDataBlockEvent> subscriber;
+	
+
+	public FridgeConsumer(Logger logger, String managerLoc) throws Exception {
+		super(logger, managerLoc, FridgeConsumer.class.getSimpleName());
+	}
+	
+	private void run() throws AcsJException {
+		subscriber = getContainerServices().
+				createNotificationChannelSubscriber(alma.FRIDGE.CHANNELNAME_FRIDGE.value, temperatureDataBlockEvent.class);
+
+		// Subscribe to an event type.
+		subscriber.addSubscription(this);
+
+		// After startReceivingEvents() is invoked, the receive(...) callback method is invoked
+		// by the notification channel. That is, we have no control over when
+		// that method is called.
+		subscriber.startReceivingEvents();
+
+		// Sleep until five events have been received... @todo: better sync using a CountDownLatch or similar instead of
+		// sleep&check
+		System.out.println("Waiting for events...");
+		while (!readyForShutdown) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException ex) {
+				Thread.currentThread().interrupt();
+			}
+		}
+	}
+	
+	@Override
+	public void receive(temperatureDataBlockEvent eventData, EventDescription eventDescrip) {
 		// Know how many events this instance has received.
 		eventCount++;
 		try {
 			// Stop receiving events
 			if (eventCount > 5) {
-				suspend();
+				subscriber.suspend(); // not really needed, but nice to see that it works...
 			} 
 			else {
-				// Ensure we're getting the correct type of data from the CORBA
-				// Any.
-				if (corbaData.getClass() == alma.FRIDGE.temperatureDataBlockEvent.class) {
-					System.out.println("The temp difference is: " + ((alma.FRIDGE.temperatureDataBlockEvent) corbaData).absoluteDiff);
+				// Ensure we're getting the correct type of data from the CORBA Any.
+				if (eventData.getClass() == alma.FRIDGE.temperatureDataBlockEvent.class) {
+					System.out.println("The temp difference is: " + eventData.absoluteDiff);
 				} 
 				else {
+					// should never happen
 					System.err.println("The type of filterable_data[0] is not correct!");
 				}
 			}
@@ -84,50 +109,46 @@ public class FridgeConsumer extends alma.acs.nc.Consumer
 			}
 		}
 	}
-    
-    
+
+	@Override
+	public Class<temperatureDataBlockEvent> getEventType() {
+		return temperatureDataBlockEvent.class;
+	}
+	
+	@Override
+	public void tearDown() throws Exception {
+		subscriber.disconnect();
+		super.tearDown();
+	}
+
+
     // //////////////////////////////////////////////////////////////////////////
-    /** Illustrates a simple example outside of the component/container model.
-     * @param args Not used!
-     */    
-    public static void main(String[] args) {
+	
+	/**
+	 * Illustrates a simple example outside of the component/container model.
+	 * 
+	 * @param args
+	 *            Not used!
+	 */
+	public static void main(String[] args) {
 		try {
-			//Setup an ACS Java client. This has little to do with the NC API
+			// Setup an ACS Java client. This has little to do with the NC API
 			String managerLoc = System.getProperty("ACS.manager");
 			if (managerLoc == null) {
 				System.out.println("Java property 'ACS.manager' must be set to the corbaloc of the ACS manager!");
 				System.exit(-1);
 			}
-			
-			String clientName = "FridgeConsumer";
-			ComponentClient myClient = new ComponentClient(null, managerLoc, clientName);
 
-			FridgeConsumer joe = new FridgeConsumer(myClient.getContainerServices());
+			FridgeConsumer fridgeConsumer = new FridgeConsumer(null, managerLoc);
+			fridgeConsumer.run();
 
-			//Subscribe to an event type.
-			joe.addSubscription(alma.FRIDGE.temperatureDataBlockEvent.class);
+			// then destroy everything
+			fridgeConsumer.tearDown();
 
-			//After consumerReady() is invoked, processEvent(...) is invoked
-			//by the notification channel.  That is, we have no control over when
-			//that method is called.
-			joe.consumerReady();
-
-			//Sleep until five events have been received... @todo: better sync using a CountDownLatch or similar instead of sleep&check
-			System.out.println("Waiting for events...");
-			while (!joe.readyForShutdown) {
-				Thread.sleep(1000);
-			}
-
-			//then destroy everything
-			joe.disconnect();
-			myClient.tearDown();
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println("Done...");
 	}
-    
-    
-    ////////////////////////////////////////////////////////////////////////////
+
 }
