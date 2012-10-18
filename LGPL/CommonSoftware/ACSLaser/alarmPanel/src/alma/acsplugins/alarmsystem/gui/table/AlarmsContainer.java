f53d03c84dfc67931ae79bd5ae81de5d40d8060d
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2012-10-18 12:28:35 +0000

    Synchronization based on (EDT) thread confinement
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@180781 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/ACSLaser/alarmPanel/src/alma/acsplugins/alarmsystem/gui/table/AlarmsContainer.java
+++ LGPL/CommonSoftware/ACSLaser/alarmPanel/src/alma/acsplugins/alarmsystem/gui/table/AlarmsContainer.java
@@ -18,11 +18,14 @@
  */
 package alma.acsplugins.alarmsystem.gui.table;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
 
-import cern.laser.client.data.Alarm;
+import alma.acs.gui.util.threadsupport.EDTExecutor;
 
 /**
  * A container for the alarms as needed by the AlarmTableModel
@@ -41,6 +44,9 @@ import cern.laser.client.data.Alarm;
  * In this way it is possible to get the entry of a row by getting
  * the key from the Vector. And it is possible to get an alarm
  * from the HashMap.
+ * <P>
+ * Synchronization is done by thread confinement (inside the EDT).
+ * Invocation of read only methods must be done inside the EDT.
  * 
  * @author acaproni
  *
@@ -96,7 +102,7 @@ public class AlarmsContainer {
 	/**
 	 * The entries in the table
 	 */
-	private HashMap<String, AlarmTableEntry> entries = new HashMap<String, AlarmTableEntry>();
+	private Map<String, AlarmTableEntry> entries = Collections.synchronizedMap(new HashMap<String, AlarmTableEntry>());
 	
 	/**
 	 * The index when the reduction rules are not applied
@@ -104,7 +110,7 @@ public class AlarmsContainer {
 	 * Each item in the vector represents the ID of the entry 
 	 * shown in a table row when the reduction rules are not used.
 	 */
-	private final Vector<String> index = new Vector<String>();
+	private final List<String> index = Collections.synchronizedList(new Vector<String>());
 	
 	/**
 	 * The maximum number of alarms to store in the container
@@ -117,7 +123,7 @@ public class AlarmsContainer {
 	 * @param max The max number of alarms to store in the container
 	 * @param panel The <code>AlarmPanel</code>
 	 */
-	public AlarmsContainer(int max) {
+	protected AlarmsContainer(int max) {
 		maxAlarms=max;
 	}
 	
@@ -126,7 +132,7 @@ public class AlarmsContainer {
 	 * 
 	 * @return The number of alarms in the container
 	 */
-	public synchronized int size() {
+	public int size() {
 		return index.size();
 	}
 	
@@ -141,7 +147,7 @@ public class AlarmsContainer {
 	 * @param alarm The not null entry to add
 	 * @throw {@link AlarmContainerException} If the entry is already in the container
 	 */
-	public synchronized void add(AlarmTableEntry entry) throws AlarmContainerException {
+	protected void add(final AlarmTableEntry entry) throws AlarmContainerException {
 		if (entry==null) {
 			throw new IllegalArgumentException("The entry can't be null");
 		}
@@ -158,8 +164,17 @@ public class AlarmsContainer {
 			// entries contains the key but index not!!!!
 			throw new IllegalStateException("Inconsistency between index and entries");
 		}
-		index.add(entry.getAlarmId());
-		entries.put(entry.getAlarmId(), entry);
+		try {
+			EDTExecutor.instance().executeSync(new Runnable() {
+				@Override
+				public void run() {
+					index.add(entry.getAlarmId());
+					entries.put(entry.getAlarmId(), entry);
+				}
+			});
+		} catch (Throwable t) {
+			throw new AlarmContainerException("Error adding alarm",t);
+		}
 	}
 
 	/**
@@ -169,7 +184,7 @@ public class AlarmsContainer {
 	 * @return true if an entries for an alarm with the given ID exists
 	 *              in the container
 	 */
-	public synchronized boolean contains(String alarmID) {
+	public boolean contains(String alarmID) {
 		if (alarmID==null) {
 			throw new IllegalArgumentException("The ID can't be null");
 		}
@@ -183,19 +198,19 @@ public class AlarmsContainer {
 	 * @param reduced <code>true</code> if the alarms in the table are reduced
 	 * @return The AlarmTableEntry in the given position
 	 */
-	public synchronized AlarmTableEntry get(int pos) {
-		if (pos<0) {
-			throw new IllegalArgumentException("The position must be greater then 0");
-		}
-		if (pos>index.size()) {
+	public AlarmTableEntry get(final int pos) {
+		if (pos<0  || pos>index.size()) {
 			throw new IndexOutOfBoundsException("Can't acces item at pos "+pos+": [0,"+index.size()+"]");
 		}
+		
 		String ID = index.get(pos);
-		AlarmTableEntry ret = get(ID);
-		if (ret==null) {
-			throw new IllegalStateException("Inconsistent state of container");
+		AlarmTableEntry entry=get(ID);
+		
+		
+		if (entry==null) {
+			throw new IllegalStateException("Inconsistent state of AlarmsContainer: entry is null at pos "+pos);
 		}
-		return ret;
+		return entry;
 	}
 	
 	/**
@@ -205,7 +220,7 @@ public class AlarmsContainer {
 	 * @return The AlarmTableEntry with the given position
 	 *         <code>null</code>if the container does not contain an entry for the given id
 	 */
-	public synchronized AlarmTableEntry get(String id) {
+	public AlarmTableEntry get(String id) {
 		if (id==null) {
 			throw new IllegalArgumentException("The ID can't be null");
 		}
@@ -215,9 +230,14 @@ public class AlarmsContainer {
 	/**
 	 * Remove all the elements in the container
 	 */
-	public synchronized void clear() {
-		index.clear();
-		entries.clear();
+	protected void clear() {
+		EDTExecutor.instance().execute(new Runnable() {
+			@Override
+			public void run() {
+				index.clear();
+				entries.clear();
+			}
+		});		
 	}
 	
 	/**
@@ -226,15 +246,38 @@ public class AlarmsContainer {
 	 * @return The removed item
 	 * @throws AlarmContainerException If the container is empty
 	 */
-	public synchronized AlarmTableEntry removeOldest() throws AlarmContainerException {
+	protected AlarmTableEntry removeOldest() throws AlarmContainerException {
 		if (index.size()==0) {
 			throw new AlarmContainerException("The container is empty");
 		}
-		String ID = index.remove(0);
-		if (ID==null) {
-			throw new IllegalStateException("The index vector returned a null item");
+		
+		class OldRemover implements Runnable {
+
+			// The entry removed
+			private AlarmTableEntry removedEntry;
+			
+			public AlarmTableEntry getRemovedEntry() {
+				return removedEntry;
+			}
+			
+			public void run() {
+				String ID = index.remove(0);
+				if (ID==null) {
+					throw new IllegalStateException("The index vector returned a null item");
+				}
+				removedEntry = entries.remove(ID);
+			}
+			
 		}
-		AlarmTableEntry ret = entries.remove(ID);
+		
+		OldRemover remover = new OldRemover();
+		try {
+			EDTExecutor.instance().executeSync(remover);
+		} catch (Throwable t) {
+			throw new IllegalStateException("Error removing the oldest entry",t);
+		}
+		
+		AlarmTableEntry ret = remover.getRemovedEntry();
 		if (ret==null) {
 			throw new IllegalStateException("The entries  HashMap contains a null entry");
 		}
@@ -247,19 +290,30 @@ public class AlarmsContainer {
 	 * @param entry The alarm whose entry must be removed
 	 * @throws AlarmContainerException If the alarm is not in the container
 	 */
-	public synchronized void remove(AlarmTableEntry entry) throws AlarmContainerException {
+	protected void remove(final AlarmTableEntry entry) throws AlarmContainerException {
 		if (entry==null) {
 			throw new IllegalArgumentException("The alarm can't be null");
 		}
-		String ID=entry.getAlarmId();
-		int pos=index.indexOf(ID);
-		if (pos<0) {
-			throw new AlarmContainerException("Alarm not in the container");
-		}
-		index.remove(pos);
-		AlarmTableEntry oldEntry = entries.remove(ID);
-		if (oldEntry==null) {
-			throw new IllegalStateException("The ID was in index but not in entries");
+		try {
+			EDTExecutor.instance().executeSync(new Runnable() {
+				
+				@Override
+				public void run() {
+					String ID=entry.getAlarmId();
+					int pos=index.indexOf(ID);
+					if (pos<0) {
+						IllegalStateException iste =new IllegalStateException("Alarm not in the container");
+						throw iste;
+					}
+					index.remove(pos);
+					AlarmTableEntry oldEntry = entries.remove(ID);
+					if (oldEntry==null) {
+						throw new IllegalStateException("The ID was in index but not in entries");
+					}
+				}
+			});
+		} catch (Throwable t) {
+			throw new AlarmContainerException("Error removing alarm",t);
 		}
 	}
 	
@@ -272,35 +326,66 @@ public class AlarmsContainer {
 	 * @param type The type of the inactive alarms
 	 * @return The number of alarms removed
 	 */
-	public synchronized int removeInactiveAlarms(AlarmGUIType type) {
+	protected int removeInactiveAlarms(final AlarmGUIType type) throws AlarmContainerException {
 		if (type==null) {
 			throw new IllegalArgumentException("The type can't be null");
 		}
-		Vector<String> keys = new Vector<String>();
-		for (String key: entries.keySet()) {
-			keys.add(new String(key));
-		}
-		int ret=0;
-		for (String key: keys) {
-			AlarmTableEntry alarm = entries.get(key);
-			if (alarm==null) {
-				throw new IllegalStateException("Got a null alarm for key "+key);
+		
+		/**
+		 * A class to remove the alarm of the given type inside the EDT
+		 * @author acaproni
+		 *
+		 */
+		class InactiveAlarmsRemover implements Runnable {
+			/**
+			 * The number of alarms removed
+			 */
+			private int removed=0;
+			
+			/**
+			 * 
+			 * @return The number of removed alarms
+			 */
+			public int getRemoved() {
+				return removed;
 			}
-			if (alarm.getStatus().isActive()) {
-				continue;
-			}
-			if (type==AlarmGUIType.INACTIVE || alarm.getPriority()==type.id) {
-				// Remove the alarm
-				try {
-					remove(alarm);
-					ret++;
-				} catch (AlarmContainerException ace) {
-					System.err.println("Exception got removing "+alarm.getAlarmId()+" from the AlarmsContainer: "+ace.getMessage());
-					ace.printStackTrace();
+
+			@Override
+			public void run() {
+				Vector<String> keys = new Vector<String>();
+				for (String key: entries.keySet()) {
+					keys.add(new String(key));
 				}
+				for (String key: keys) {
+					AlarmTableEntry alarm = entries.get(key);
+					if (alarm==null) {
+						throw new IllegalStateException("Got a null alarm for key "+key);
+					}
+					if (alarm.getStatus().isActive()) {
+						continue;
+					}
+					if (type==AlarmGUIType.INACTIVE || alarm.getPriority()==type.id) {
+						// Remove the alarm
+						try {
+							remove(alarm);
+							removed++;
+						} catch (Throwable t) {
+							throw new IllegalStateException("Exception got removing "+alarm.getAlarmId()+" from the AlarmsContainer",t);
+						}
+					}
+				}
+				
 			}
+			
 		}
-		return ret;
+		
+		InactiveAlarmsRemover remover = new InactiveAlarmsRemover();
+		try {
+			EDTExecutor.instance().executeSync(remover);
+		} catch (Throwable t) {
+			throw new AlarmContainerException("Error removing inactive alarms of type"+type,t);
+		}
+		return remover.getRemoved();
 	}
 	
 	/**
@@ -311,25 +396,36 @@ public class AlarmsContainer {
 	 * @param newAlarm The not null new alarm 
 	 * @throws AlarmContainerException if the entry is not in the container
 	 */
-	public synchronized void replace(AlarmTableEntry newAlarm) throws AlarmContainerException {
+	protected void replace(final AlarmTableEntry newAlarm) throws AlarmContainerException {
 		if (newAlarm==null) {
 			throw new IllegalArgumentException("The alarm can't be null");
 		}
-		int pos = index.indexOf(newAlarm.getAlarmId());
-		if (pos<0) {
-			throw new AlarmContainerException("Entry not present in the container");
-		}
-		AlarmTableEntry entry = entries.get(newAlarm.getAlarmId());
-		if (entry==null) {
-			// There was no entry for this ID
-			throw new IllegalStateException("Inconsistent state of index and entries");
-		}
-		entry.updateAlarm(newAlarm);
-		// If active, move the item in the head of the container
-		if (newAlarm.getStatus().isActive()) {
-			String key = index.remove(pos);
-			index.insertElementAt(key, 0);
-		}
+		try {
+			EDTExecutor.instance().executeSync(new Runnable() {
+				
+				@Override
+				public void run() {
+					int pos = index.indexOf(newAlarm.getAlarmId());
+					if (pos<0) {
+						throw new IllegalStateException("Entry not present in the container");
+					}
+					AlarmTableEntry entry = entries.get(newAlarm.getAlarmId());
+					if (entry==null) {
+						// There was no entry for this ID
+						throw new IllegalStateException("Inconsistent state of index and entries");
+					}
+					entry.updateAlarm(newAlarm);
+					// If active, move the item in the head of the container
+					if (newAlarm.getStatus().isActive()) {
+						String key = index.remove(pos);
+						index.add(0,key);
+					}
+				}
+			});
+		} catch (Throwable t) {
+			throw new AlarmContainerException("Error replacing entry "+newAlarm,t);
+		} 	
+		
 	}
 	
 	/**
@@ -342,19 +438,48 @@ public class AlarmsContainer {
 	 * 
 	 * @return  -1 if there are not alarm to acknowledge;
 	 * 			the highest priority of the alarm to acknowledge
+	 * @throws AlarmContainerException in case of error getting the highest priority to ack
 	 */
-	public synchronized int hasNotAckAlarms() {
-		int ret=Integer.MAX_VALUE;
-		Set<String> keys=entries.keySet();
-		for (String key: keys) {
-			AlarmTableEntry entry=entries.get(key);
-			if (entry.getStatus().isActive() && entry.isNew() && entry.getPriority()<ret) {
-				ret=entry.getPriority();
+	protected int hasNotAckAlarms() throws AlarmContainerException {
+		
+		class AckAlarmsChecker implements Runnable {
+			
+			int higestPriorityToAck=Integer.MAX_VALUE;
+
+			/**
+			 * 
+			 * @return The highest priority to ack 
+			 *         or -1 if there are not alarm to ack
+			 */
+			public int getHigestPriorityToAck() {
+				return (higestPriorityToAck==Integer.MAX_VALUE)?-1:higestPriorityToAck;
 			}
-			if (ret==0) {
-				break;
+
+			@Override
+			public void run() {
+				higestPriorityToAck=Integer.MAX_VALUE;
+				Set<String> keys=entries.keySet();
+				for (String key: keys) {
+					AlarmTableEntry entry=entries.get(key);
+					if (entry.getStatus().isActive() && entry.isNew() && entry.getPriority()<higestPriorityToAck) {
+						higestPriorityToAck=entry.getPriority();
+					}
+					if (higestPriorityToAck==0) {
+						break;
+					}
+				}
+				
 			}
+			
 		}
-		return (ret==Integer.MAX_VALUE)?-1:ret;
+		
+		AckAlarmsChecker checker = new AckAlarmsChecker();
+		try {
+			EDTExecutor.instance().executeSync(checker);
+		} catch (Throwable t) {
+			throw new AlarmContainerException("Error getting the alarm highest priority of the alarms to ACK",t);
+		}
+		
+		return checker.getHigestPriorityToAck();
 	}
 }
