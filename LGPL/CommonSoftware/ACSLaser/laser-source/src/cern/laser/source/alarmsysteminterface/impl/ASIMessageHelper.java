6adad4071f21613771207c6f68c3e5a52d59893d
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2012-10-22 11:47:12 +0000

    generics and other cleanup
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@180991 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/ACSLaser/laser-source/src/cern/laser/source/alarmsysteminterface/impl/ASIMessageHelper.java
+++ LGPL/CommonSoftware/ACSLaser/laser-source/src/cern/laser/source/alarmsysteminterface/impl/ASIMessageHelper.java
@@ -5,10 +5,9 @@
  */
 package cern.laser.source.alarmsysteminterface.impl;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Enumeration;
 import java.util.Iterator;
-import java.util.Vector;
 
 import cern.laser.source.alarmsysteminterface.FaultState;
 import cern.laser.source.alarmsysteminterface.impl.message.ASIMessage;
@@ -56,30 +55,28 @@ public class ASIMessageHelper {
     return asi_message;
   }
 
-  /**
-   * DOCUMENT ME!
-   *
-   * @param asiMessage DOCUMENT ME!
-   *
-   * @return DOCUMENT ME!
-   */
-  public static Collection unmarshal(ASIMessage asiMessage) {
-    if (asiMessage == null) {
-      throw (new IllegalArgumentException("ASI message is null"));
-    }
+	/**
+	 * DOCUMENT ME!
+	 * 
+	 * @param asiMessage
+	 *            DOCUMENT ME!
+	 * 
+	 * @return DOCUMENT ME!
+	 */
+	public static Collection<FaultState> unmarshal(ASIMessage asiMessage) {
+		if (asiMessage == null) {
+			throw (new IllegalArgumentException("ASI message is null"));
+		}
+		Collection<FaultState> ret = new ArrayList<FaultState>();
+		
+		FaultStates generated_states = asiMessage.getFaultStates();
+		
+		if (generated_states != null) {
+			for (cern.laser.source.alarmsysteminterface.impl.message.FaultState generated_state : generated_states.getFaultState()) {
+				ret.add(FaultStateHelper.unmarshal(generated_state));
+			}
+		}
 
-    Collection states = new Vector();
-    FaultStates generated_states = asiMessage.getFaultStates();
-
-    if (generated_states != null) {
-      Enumeration enumer = generated_states.enumerateFaultState();
-
-      while (enumer.hasMoreElements()) {
-        cern.laser.source.alarmsysteminterface.impl.message.FaultState generated_state = (cern.laser.source.alarmsysteminterface.impl.message.FaultState) enumer.nextElement();
-        states.add(FaultStateHelper.unmarshal(generated_state));
-      }
-    }
-
-    return states;
-  }
+		return ret;
+	}
 }
