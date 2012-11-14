501a066b584fb74e6ba05cc826fd6c39ac6bceb6
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2012-11-14 18:02:20 +0000

    Got this test to run with JUnit4. Added method 'testOneSupplierOneSubscriberMixedEvents' with more advanced test. TODO: Rename this file to "LocalPubSubTest" because now it tests also the subscriber component.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@182171 523d945c-050c-4681-91ec-863ad3bb968a

--- Benchmark/corbaNotify/test/acs/benchmark/nc/client/LocalSupplierTest.java
+++ Benchmark/corbaNotify/test/acs/benchmark/nc/client/LocalSupplierTest.java
@@ -20,73 +20,119 @@
  *******************************************************************************/
 package acs.benchmark.nc.client;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.junit.Assert.assertThat;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+import junit.framework.JUnit4TestAdapter;
+
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 
-import si.ijs.maci.ComponentSpec;
-
-import acs.benchmark.util.ComponentAccessUtil;
 import acs.benchmark.util.ContainerUtil;
 import acs.benchmark.util.ContainerUtil.ContainerLogLevelSpec;
 
-import alma.acs.component.client.ComponentClientTestCase;
+import alma.ACSErrTypeCommon.CouldntPerformActionEx;
+import alma.ACSErrTypeCommon.wrappers.AcsJCouldntPerformActionEx;
+import alma.acs.component.client.ComponentClient;
 import alma.acs.logging.level.AcsLogLevelDefinition;
+import alma.acs.util.AcsLocations;
 import alma.benchmark.CHANNELNAME_CONTROL_REALTIME;
+import alma.benchmark.CorbaNotifyConsumerOperations;
 import alma.benchmark.CorbaNotifySupplierOperations;
 import alma.benchmark.NcEventSpec;
 import alma.maci.containerconfig.types.ContainerImplLangType;
 
 /**
- * A simple test that uses the daemon / component test framework
- * directly, without declarative layer on top which probably will be needed
- * for larger test setups.
+ * A simple publisher-subscriber test that runs publisher and subscriber components
+ * on the local host. 
+ * It uses the daemon / component test framework.
  * 
  * @author hsommer
  */
-public class LocalSupplierTest extends ComponentClientTestCase
+public class LocalSupplierTest extends ComponentClient
 {
-	public LocalSupplierTest() throws Exception {
-		super(LocalSupplierTest.class.getSimpleName());
-	}
-	
 	private ContainerUtil containerUtil;
-	private String containerName = "localSupplierContainer1";
+	
+	private PubSubComponentAccessUtil componentAccessUtil;
+	
+	private final String supplierContainerName = "localSupplierContainer1";
+	
+	/**
+	 * TODO: Check if this rule and the getMethodName() call in setUp() can be moved up to ComponentClient,
+	 *      if that adds a runtime dependency on junit, and how bad that would be.
+	 *      Probably we should add a class ComponentClientTestCaseJUnit4 that extends ComponentClient
+	 *      and only adds this testname business.
+	 */
+	@Rule 
+	public TestName testName = new TestName();
+	
+	/**
+	 * For compatibility with JUnit3 based TATJUnitRunner
+	 */
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(LocalSupplierTest.class);
+	}
+
+	public LocalSupplierTest() throws Exception {
+		super(null, AcsLocations.figureOutManagerLocation(), LocalSupplierTest.class.getSimpleName());
+	}
+
 
 	@Before
-	public void startContainer() throws Exception {
-		// run a local container
+	public void setUp() throws Exception {
+		String testMethodName = testName.getMethodName();
+		m_logger.info("----------------- " + testMethodName + " ----------------- ");
+
+		// run a local container. Logs are stored under $ACS_TMP/logs/
 		containerUtil = new ContainerUtil(getContainerServices());
 		containerUtil.loginToManager();
-		containerUtil.startContainer(null, ContainerImplLangType.JAVA, containerName, null, true);
-		m_logger.info("Container '" + containerName + "' is ready.");
+		containerUtil.startContainer(null, ContainerImplLangType.JAVA, supplierContainerName, null, true);
+		m_logger.info("Container '" + supplierContainerName + "' is ready.");
 		
 		// configure container log levels
 		ContainerLogLevelSpec contLogLevelSpec = new ContainerLogLevelSpec(AcsLogLevelDefinition.WARNING, AcsLogLevelDefinition.DEBUG);
-		contLogLevelSpec.addNamedLoggerSpec("jacorb@"+containerName, AcsLogLevelDefinition.WARNING, AcsLogLevelDefinition.WARNING);
-		containerUtil.setContainerLogLevels(containerName, contLogLevelSpec);
-		Assert.assertTrue(containerUtil.isContainerLoggedIn(containerName));
+		contLogLevelSpec.addNamedLoggerSpec("jacorb@"+supplierContainerName, AcsLogLevelDefinition.WARNING, AcsLogLevelDefinition.WARNING);
+		containerUtil.setContainerLogLevels(supplierContainerName, contLogLevelSpec);
+		assertThat(containerUtil.isContainerLoggedIn(supplierContainerName), is(true));
+		
+		componentAccessUtil = new PubSubComponentAccessUtil(getContainerServices());
 	}
 
 	@After
-	public void stopContainer() throws Exception {
-		// clean up
+	public void tearDown() throws Exception {
+		if (componentAccessUtil != null) {
+			componentAccessUtil.releaseAllComponents(true);
+		}
+		
 		if (containerUtil != null) {
-			containerUtil.stopContainer(null, containerName);
+			containerUtil.stopContainer(null, supplierContainerName);
 			containerUtil.logoutFromManager();
 		}
+		super.tearDown();
 	}
 
+	/**
+	 * Publishes 10.000 "MountStatusData" events on the NC "CONTROL_REALTIME", 
+	 * using a single supplier component "JavaSupplier-1" for sequential publishing at maximum rate.
+	 * The call to the supplier component is synchronous, returning when all events are published. 
+	 * This allows us to measure the the publishing performance without sync'ing up through a subscriber.
+	 */
 	@Test
-	public void testOneCompOneChannelOneEventType() throws Exception {
-		ComponentAccessUtil componentAccessUtil = null; 
+	public void testOneSupplierNoSubscriber() throws Exception {
+		final int numEvents = 10000;
 		CorbaNotifySupplierOperations supplierComp = null;
 		try {
 			// Create dynamic supplier component
-			componentAccessUtil = new ComponentAccessUtil(getContainerServices());
-			ComponentSpec compSpec = createJavaSupplierComponentSpec("JavaSupplier-1", containerName);
-			supplierComp = componentAccessUtil.getDynamicComponent(compSpec, CorbaNotifySupplierOperations.class);
+			String componentName = "JavaSupplier-1";
+			supplierComp = componentAccessUtil.getDynamicJavaSupplierComponent(componentName, supplierContainerName);
 			
 			// supplier setup
 			String[] ncNames = new String[] {CHANNELNAME_CONTROL_REALTIME.value};
@@ -95,14 +141,18 @@ public class LocalSupplierTest extends ComponentClientTestCase
 			
 			NcEventSpec[] ncEventSpecs = new NcEventSpec[] {
 					new NcEventSpec(ncNames[0], 
-					new String[] {"MountStatusData"}, 
-					"") }; // don't care about antenna name here
+							new String[] {"MountStatusData"}, 
+							"" // don't care about antenna name here
+						) }; 
 			
-			int callTimeInternalMillis = supplierComp.sendEvents(ncEventSpecs, -1, 10000);
-			m_logger.info("Single supplier comp '" + compSpec.component_name + "' sent 10000 MountStatusData events at max speed to '" 
+			// Let publisher component publish these events
+			int callTimeInternalMillis = supplierComp.sendEvents(ncEventSpecs, -1, numEvents);
+			
+			m_logger.info("Single supplier comp '" + componentName + "' sent " + numEvents 
+					+ " MountStatusData events at max speed to NC '" 
 					+ ncNames[0] + "' in " + callTimeInternalMillis + " ms.");
-		} catch (Exception ex) {
-			throw ex;
+			assertThat("Expecting at most 2 ms for publishing an event to a NotifyService.", 
+					callTimeInternalMillis, lessThanOrEqualTo(numEvents * 2));
 		} finally {
 			// clean up
 			if (supplierComp != null) supplierComp.ncDisconnect();
@@ -111,25 +161,74 @@ public class LocalSupplierTest extends ComponentClientTestCase
 	}
 
 	/**
-	 * @TODO: This is a mean hack. Need to find a way to use ComponentClient with JUnit4 tests,
-	 * or otherwise make ComponentClientTestCase fit for Junit4.
+	 * Publishes >= 10.000 "MountStatusData" or "LightweightMountStatusData" events on the NC "CONTROL_REALTIME", 
+	 * using a single supplier component "JavaSupplier-1" for sequential publishing at a rate of 10 events/s,
+	 * and a single subscriber component "" for counting the events.
+	 * The call to the supplier component is asynchronous, returning while the publisher runs in "indefinite publishing" mode. 
+	 * Once the subscriber has received enough data, we terminate the supplier. 
 	 */
 	@Test
-	public void tearDownComponentClient() throws Exception {
-		tearDown();
-	}
+	public void testOneSupplierOneSubscriberMixedEvents() throws Exception {
+
+		final int numEvents = 100;
+		final int eventPeriodMillis = 10;
+		
+		String[] ncNames = new String[] {CHANNELNAME_CONTROL_REALTIME.value};
+		final NcEventSpec[] ncEventSpecs = new NcEventSpec[] {
+				new NcEventSpec(ncNames[0], 
+						new String[] {"MountStatusData", "LightweightMountStatusData"}, 
+						"" // don't care about antenna name here
+					) };
+		
+		// Create, configure and activate dynamic subscriber component.
+		// It must be running before we publish events, to make sure we don't lose any.
+		String subscriberContainerName = "localSubscriberContainer1";
+		String subscriberComponentName = "JavaSubscriber-1";
+		try {
+			containerUtil.startContainer(null, ContainerImplLangType.JAVA, subscriberContainerName, null, true);
+			final CorbaNotifyConsumerOperations subscriberComp = componentAccessUtil.getDynamicJavaSubscriberComponent(subscriberComponentName, subscriberContainerName);
+			subscriberComp.ncConnect(ncNames);
+			m_logger.info("Connected subscriber to NC " + ncNames[0]);
+			final CountDownLatch subscriberSync = new CountDownLatch(1); // or use SynchronousQueue etc to pass back data
+			Runnable runSubscriber = new Runnable() {
+				@Override
+				public void run() {
+					try {
+						int subscriberReceptionTimeMillis = subscriberComp.receiveEvents(ncEventSpecs, 0, numEvents);
+						m_logger.info("Subscriber component done. It received " + numEvents + " events in " + subscriberReceptionTimeMillis + " ms.");
+					} catch (CouldntPerformActionEx ex) {
+						AcsJCouldntPerformActionEx ex2 = AcsJCouldntPerformActionEx.fromCouldntPerformActionEx(ex);
+						m_logger.log(Level.WARNING, "Failure in subscriberComp.receiveEvents", ex2);
+					} catch (Exception ex) {
+						m_logger.log(Level.WARNING, "Failure in subscriberComp.receiveEvents", ex);
+					}
+					subscriberSync.countDown();
+				}
+			};
+			getContainerServices().getThreadFactory().newThread(runSubscriber).start();
 	
 	
-	/**
-	 * Helper for tests that create dynamic supplier components.
-	 * <p>
-	 * @TODO: Find better class to put this in.
-	 */
-	public static ComponentSpec createJavaSupplierComponentSpec(String componentName, String containerName) {
-		ComponentSpec compSpec = new ComponentSpec(componentName, 
-				"IDL:alma/benchmark/CorbaNotifySupplier:1.0", 
-				"acs.benchmark.nc.supplier.CorbaNotifySupplierComponentHelper", 
-				containerName);
-		return compSpec;
+			// Create dynamic supplier component
+			String componentName = "JavaSupplier-1";
+			CorbaNotifySupplierOperations supplierComp = componentAccessUtil.getDynamicJavaSupplierComponent(
+															componentName, supplierContainerName);
+			
+			// supplier setup
+			supplierComp.ncConnect(ncNames);
+			m_logger.info("Connected supplier to NC " + ncNames[0]);
+			
+			// Let publisher component publish events as long as it takes for the subscriber to get enough of them 
+			supplierComp.sendEvents(ncEventSpecs, eventPeriodMillis, -1);
+			
+			boolean subscriberTerminateOK = subscriberSync.await(60, TimeUnit.SECONDS);
+			
+			assertThat("Subscriber component should have received all events without client timeout.", 
+					subscriberTerminateOK, is(true));
+		}
+		finally {
+			componentAccessUtil.releaseComponent(subscriberComponentName, true);
+			containerUtil.stopContainer(null, subscriberContainerName);
+			// for supplier component and container we trust the tearDown method..
+		}
 	}
 }
