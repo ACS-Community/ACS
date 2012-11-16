871391ba0bd5c9441c044447f1d632fceeb61991
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2012-11-16 13:33:18 +0000

    First empty impl of java test component
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@182292 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/ACSLaser/containerTest/test/alma/alarmContainerTest/AlarmTestComponentImpl/AlarmTestComponentImpl.java
@@ -0,0 +1,16 @@
+package alma.alarmContainerTest.AlarmTestComponentImpl;
+
+import alma.ACSErrTypeCommon.CouldntPerformActionEx;
+import alma.acs.component.ComponentImplBase;
+import alma.alarmContainerTest.AlarmTestComponentOperations;
+
+public class AlarmTestComponentImpl extends ComponentImplBase implements AlarmTestComponentOperations
+{
+
+	@Override
+	public void logBurst(short logLevel, int numLogs) throws CouldntPerformActionEx {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
