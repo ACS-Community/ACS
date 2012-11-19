67cfdb9aa3f954ecda7944f278335c4055dcc240
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2012-11-19 18:02:16 +0000

    Moved into acsGUIs
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@182393 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acssampGUI/test/samplingSystemUI.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "=== Running SSG test suite ==="
-acsStartJava -endorsed alma.acs.testsupport.tat.TATJUnitRunner cl.SamplingSystemUIJUnitTest
-echo "=== Done running SSG test suite ==="
--- LGPL/CommonSoftware/errorBrowser/test/doAllTests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-acsStartJava junit.textui.TestRunner cl.utfsm.acs.types.TypesSuite
-acsStartJava -endorsed -DACS.acsroot="`pwd`/xml" -Dtest.xmldirs="`pwd`/xml" junit.textui.TestRunner cl.utfsm.acs.xml.XmlSuite
-acsStartJava -endorsed -DACS.acsroot=$ACSROOT -Dtest.xmldirs="`pwd`/xml" junit.textui.TestRunner cl.utfsm.acs.ebe.EbeSuite
