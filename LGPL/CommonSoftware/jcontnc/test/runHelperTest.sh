de574dfa39db67c891e1094bd62c15ca742a8d3e
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2012-12-03 15:03:02 +0000

    Dumps NamingService bindings with "*ChannelFactory" before running the JUnit test, to investigate a recurrent failure with the MC object retrieval in the test setup.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@183059 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jcontnc/test/runHelperTest.sh
+++ LGPL/CommonSoftware/jcontnc/test/runHelperTest.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+echo "Dumped naming service bindings for 'ChannelFactory' objects: (to investigate recurrent problems with 'Failed to resolve factory's MC extension object in the naming service')" 
+acsStartJava org.jacorb.naming.ContextLister | grep ChannelFactory | sort | paste -s
 
 acsStartJava -endorsed alma.acs.testsupport.tat.TATJUnitRunner   alma.acs.nc.HelperTest
-
