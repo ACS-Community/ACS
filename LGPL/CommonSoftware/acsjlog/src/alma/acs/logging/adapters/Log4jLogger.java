37d913ef240be604d2510c615aaf8601a3de1a16
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2012-12-27 12:16:26 +0000

    Adding adapter (factory, custom logger) that allows integration of log4j logs produced by 3rd party code into the ACS logging framework.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@184265 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/acsjlog/src/alma/acs/logging/adapters/Log4jLogger.java
@@ -0,0 +1,74 @@
+package alma.acs.logging.adapters;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
+
+import alma.acs.logging.AcsLogger;
+
+/**
+ * We intercept the log4j logs at the first level, the Logger itself.
+ * It may have been good enough to inject the ACS logger as a log4j appender, 
+ * but it seems that only the Logger allows us to implement <code>isDebugEnabled</code>
+ * type of methods based on the ACS-configured log levels, 
+ * rather than first creating and later dropping a LoggingEvent object.
+ */
+public class Log4jLogger extends Logger
+{
+
+
+	private final AcsLogger delegate;
+
+	/**
+	 * 
+	 */
+	public Log4jLogger(String name, AcsLogger delegate) {
+		super(name);
+		this.delegate = delegate;
+//		System.out.println("*** Created Log4jAcsLogger ***");
+	}
+
+	@Override
+	public boolean isTraceEnabled() {
+		return delegate.isLoggable(log4jLevelToJdkLevel(Level.TRACE));
+	}
+
+	@Override
+	public boolean isDebugEnabled() {
+		return delegate.isLoggable(log4jLevelToJdkLevel(Level.DEBUG));
+	}
+
+	@Override
+	public boolean isInfoEnabled() {
+		return delegate.isLoggable(log4jLevelToJdkLevel(Level.INFO));
+	}
+
+	
+	@Override
+	protected void forcedLog(String fqcn, Priority level, Object message, Throwable t) {
+//		System.out.println("*** Log4jAcsLogger intercepted a log: " + message + " ***");
+		delegate.log(log4jLevelToJdkLevel(level), message.toString(), t);
+	}
+
+	
+	java.util.logging.Level log4jLevelToJdkLevel(Priority level) {
+		switch (level.toInt()) {
+		case Level.TRACE_INT:
+			return java.util.logging.Level.FINEST;
+
+		case Level.DEBUG_INT:
+			return java.util.logging.Level.FINER; // or FINE ?
+
+		case Level.INFO_INT:
+			return java.util.logging.Level.INFO;
+
+		case Level.FATAL_INT:
+			return java.util.logging.Level.SEVERE;
+			
+		case Level.WARN_INT:
+		case Level.ERROR_INT:
+		default:
+			return java.util.logging.Level.WARNING;
+		}
+	}
+}
---
+++ LGPL/CommonSoftware/acsjlog/test/alma/acs/logging/adapters/Log4jAdapterTest.java
@@ -0,0 +1,75 @@
+package alma.acs.logging.adapters;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.Assert.assertThat;
+import junit.framework.JUnit4TestAdapter;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import alma.acs.logging.ClientLogManager;
+import alma.acs.logging.config.LogConfig;
+import alma.acs.logging.level.AcsLogLevelDefinition;
+import alma.acs.logging.testsupport.JUnit4StandaloneTestBase;
+
+
+public class Log4jAdapterTest extends JUnit4StandaloneTestBase
+{
+	/**
+	 * For compatibility with JUnit3 based TATJUnitRunner
+	 */
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(Log4jAdapterTest.class);
+	}
+
+	public Log4jAdapterTest() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 */
+	@Test
+	public void testLog4jLogger() throws Exception {
+
+		Log4jFactory.enableAcsLogging();
+		
+		// Get a logger using the current class name, which is standard with log4j
+		// The call stack does not come from a recognized framework such as CERN laser libs, so that 
+		// the logger name will be "unknown", with the method name appended.
+		org.apache.log4j.Logger log4jLogger = org.apache.log4j.Logger.getLogger(getClass());
+		log4jLogger.info("log4jLogger speaking INFO ...");
+		
+		// Some other logger, should again be "unknown@<TestMethodName>".
+		org.apache.log4j.Logger log4jLogger2 = org.apache.log4j.Logger.getLogger("someOtherLog4jLogger");
+		assertThat(log4jLogger2, sameInstance(log4jLogger));
+		log4jLogger2.debug("stupid DEBUG msg");
+		
+		// DEBUG should be enabled (and the above log should show up in stdout)
+		assertThat(log4jLogger.isDebugEnabled(), is(true));
+		assertThat(log4jLogger.isTraceEnabled(), is(false));
+		
+		// Now we change log levels 
+		LogConfig sharedLogConfig = ClientLogManager.getAcsLogManager().getLogConfig();
+		sharedLogConfig.setMinLogLevelLocal(AcsLogLevelDefinition.INFO, "unknown@testLog4jLogger");
+		assertThat(log4jLogger.isDebugEnabled(), is(true));
+		sharedLogConfig.setMinLogLevel(AcsLogLevelDefinition.INFO, "unknown@testLog4jLogger");
+		// now that both local and remote logs are set to INFO, isDebugEnabled should be false
+		assertThat(log4jLogger.isDebugEnabled(), is(false));
+		log4jLogger.info("This info message should appear");
+		log4jLogger.debug("This debug message should not appear");
+		
+		log4jLogger.warn("A warning with exception!", new RuntimeException());
+		
+	}
+}
