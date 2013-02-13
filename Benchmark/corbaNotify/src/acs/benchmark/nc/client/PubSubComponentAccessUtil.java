c37e2639ac59d71589ba7c649e2b5c8692513953
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2013-02-13 16:35:50 +0000

    Added API support for comp impl languages other than Java
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@186249 523d945c-050c-4681-91ec-863ad3bb968a

--- Benchmark/corbaNotify/src/acs/benchmark/nc/client/PubSubComponentAccessUtil.java
+++ Benchmark/corbaNotify/src/acs/benchmark/nc/client/PubSubComponentAccessUtil.java
@@ -6,6 +6,7 @@ import acs.benchmark.util.ComponentAccessUtil;
 
 import alma.JavaContainerError.wrappers.AcsJContainerServicesEx;
 import alma.acs.container.ContainerServices;
+import alma.acs.pubsubtest.config.types.ImplLangT;
 import alma.benchmark.CorbaNotifyConsumerHelper;
 import alma.benchmark.CorbaNotifyConsumerOperations;
 import alma.benchmark.CorbaNotifySupplierHelper;
@@ -20,18 +21,52 @@ public class PubSubComponentAccessUtil extends ComponentAccessUtil
 		super(contSrv);
 	}
 
-	public CorbaNotifySupplierOperations getDynamicJavaSupplierComponent(String componentName, String containerName) throws AcsJContainerServicesEx {
+//	/**
+//	 * @deprecated Use {@link #getDynamicSupplierComponent(String, String, ImplLangT)}
+//	 */
+//	public CorbaNotifySupplierOperations getDynamicJavaSupplierComponent(String componentName, String containerName) throws AcsJContainerServicesEx {
+//		return getDynamicSupplierComponent(componentName, containerName, ImplLangT.JAVA);
+//	}
+	
+	public CorbaNotifySupplierOperations getDynamicSupplierComponent(String componentName, String containerName, ImplLangT implLang) throws AcsJContainerServicesEx {
+		String code = null;
+		switch (implLang.getType()) {
+		case ImplLangT.JAVA_TYPE:
+			// Cannot use class#getName because the component helper is not visible to the normal client classloader
+			code = "acs.benchmark.nc.comp.publisher.CorbaNotifySupplierComponentHelper"; 
+			break;
+		default:
+			throw new IllegalArgumentException("Component impl '" + implLang.toString() + "' not yet supported!");
+		}
 		ComponentSpec compSpec = new ComponentSpec(componentName,
 				CorbaNotifySupplierHelper.id(), // "IDL:alma/benchmark/CorbaNotifySupplier:1.0" 
-				"acs.benchmark.nc.comp.publisher.CorbaNotifySupplierComponentHelper", // component helper not visible to normal CL 
+				code, 
 				containerName);
 		return getDynamicComponent(compSpec, CorbaNotifySupplierOperations.class);
 	}
+
 	
+	/**
+	 * @deprecated Use {@link #getDynamicSubscriberComponent(String, String, ImplLangT)}
+	 */
 	public CorbaNotifyConsumerOperations getDynamicJavaSubscriberComponent(String componentName, String containerName) throws AcsJContainerServicesEx {
+		return getDynamicSubscriberComponent(componentName, containerName, ImplLangT.JAVA);
+	}
+	
+	public CorbaNotifyConsumerOperations getDynamicSubscriberComponent(String componentName, String containerName, ImplLangT implLang) throws AcsJContainerServicesEx {
+		String code = null;
+		switch (implLang.getType()) {
+		case ImplLangT.JAVA_TYPE:
+			// Cannot use class#getName because the component helper is not visible to the normal client classloader
+			code = "acs.benchmark.nc.comp.subscriber.CorbaNotifyConsumerComponentHelper"; 
+			break;
+			
+		default:
+			throw new IllegalArgumentException("Component impl '" + implLang.toString() + "' not yet supported!");
+		}
 		ComponentSpec compSpec = new ComponentSpec(componentName,
 				CorbaNotifyConsumerHelper.id(), // "IDL:alma/benchmark/CorbaNotifyConsumer:1.0" 
-				"acs.benchmark.nc.comp.subscriber.CorbaNotifyConsumerComponentHelper", // component helper not visible to normal CL 
+				code,
 				containerName);
 		return getDynamicComponent(compSpec, CorbaNotifyConsumerOperations.class);
 	}
