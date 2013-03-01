d4b14b7e26f2f300a554075ed3af2e3084bec1c3
gchiozzi <gchiozzi@523d945c-050c-4681-91ec-863ad3bb968a>
2013-03-01 21:56:05 +0000

    Implemented multiple receivers. Tested but not exaustively at the OSF while the system was in between two observations. More testing will be done later today or tomorrow.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@187052 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/bulkDataNT/src/bulkDataNTremoteTest.py
+++ LGPL/CommonSoftware/bulkDataNT/src/bulkDataNTremoteTest.py
@@ -17,7 +17,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
-# "@(#) $Id: bulkDataNTremoteTest.py,v 1.16 2013/02/27 17:27:25 gchiozzi Exp $"
+# "@(#) $Id: bulkDataNTremoteTest.py,v 1.17 2013/03/01 21:56:05 gchiozzi Exp $"
 #
 # who       when      what
 # --------  --------  ----------------------------------------------
@@ -58,21 +58,24 @@ class RemoteProcData:
 
 class bulkDataNTtestSuite:
     
-    def __init__(self, startTime, hosts, size=640000, loops=10, sourceFile='.bash_profile', acsdataEnv=None,
-                 throttle=0, qos_lib="BulkDataQoSLibrary", filePrefix='', filePath=None):
+    def __init__(self, startTime, receivers, senders, size=640000, loops=10, sourceFile='.bash_profile', acsdataEnv=None,
+                 throttle=0, qos_lib="BulkDataQoSLibrary", filePrefix='', filePath=None, multicast=False):
         self.startTime       = startTime
-        self.hosts           = hosts
-        self.numOfHosts      = len(hosts)
+        self.senders         = senders
+        self.numOfSenders    = len(senders)
+        self.receivers       = receivers
+        self.numOfSenders    = len(receivers)
         self.dataSizeInBytes = size
         self.loops           = loops
         self.sendersData     = {}
-        self.receiverData    = None
+        self.receiversData   = {}
         self.sourceFile      = sourceFile
         self.acsdataEnv      = acsdataEnv
         self.throttle        = throttle
         self.qos_lib         = qos_lib
         self.filePrefix      = filePrefix
         self.filePath        = filePath
+        self.multicast       = multicast
         
         self.preCmd = "source " + self.sourceFile + "; export ENABLE_BULKDATA_NT=true; export BULKDATA_NT_DEBUG=1; export ACS_LOG_STDOUT=2; "
 
@@ -91,7 +94,7 @@ class bulkDataNTtestSuite:
         # Prepares sendersData structures with output file names
         #
         flow = 0
-        for h in self.hosts:
+        for h in self.senders:
             # Builds the flows names
             flowString = format(flow, "02d")
             outFile    = self.filePath+self.filePrefix+"GenSender."+h+"-"+flowString
@@ -105,6 +108,44 @@ class bulkDataNTtestSuite:
             self.sendersData[h+'-'+flowString]= RemoteProcData(h, sndCmd, None, outFile)
             flow+=1
 
+        #
+        # Prepares receiversData structures with output file names
+        # Let's add a sequential number (even thought it is not the flow as above)
+        # to make sure we can run more on the same host without collisions
+        #
+        # Assume there is one flow per each host entry
+        noOfFlows = self.numOfSenders
+
+        flow = 0
+        comma=""
+        flowString=""
+        
+        print 'Prepare list of flows for receivers with ' + str(noOfFlows) + ' flows'
+        for fn in range(noOfFlows):
+            flowString+=comma
+            flowString+=format(flow, "02d")
+            comma=","
+            flow+=1
+                  
+        if self.multicast == True:
+            xxxcastString=""
+        else:
+            xxxcastString="-u"
+
+        seqNum = 0
+        for h in self.receivers:
+            # Builds the flows names
+            seqString = format(seqNum, "02d")
+            outFile    = self.filePath+self.filePrefix+"GenReceiver."+h+"-"+seqString
+
+            rcvCmd = ("ssh " + os.environ['USER']+ "@" + h + 
+                       " '" + self.preCmd + " bulkDataNTGenReceiver -n -s TS -f "+flowString+
+                       " "+xxxcastString+
+                       " --qos_lib="+self.qos_lib+
+                       self.postCmd+outFile+"'")
+            self.receiversData[h+'-'+seqString]= RemoteProcData(h, rcvCmd, None, outFile)
+            seqNum+=1
+
     ######################################
     #   Starts the senders
     #   Send data
@@ -113,7 +154,7 @@ class bulkDataNTtestSuite:
     def runSenders(self):
         flow = 0
         i=0
-        print 'Going to start remote senders on hosts: ' + str(self.hosts)
+        print 'Going to start remote senders on hosts: ' + str(self.senders)
         
         for h in sorted(self.sendersData):
             print '----> Starting host: '+self.sendersData[h].host
@@ -151,75 +192,60 @@ class bulkDataNTtestSuite:
         time.sleep(10)                
                  
     #######################################################
-    #   Starts the receiver
+    #   Starts the receivers
     #
     #   Gets the number of flows and builds their names
     #   May be we could pass the list of names directly?
     ######################################################
-    def startReceiver(self, rcvHost, multicast):
-        # Assume there is one flow per each host entry
-        noOfFlows = self.numOfHosts
+    def startReceivers(self):
 
         flow = 0
-        comma=""
-        flowString=""
+        i=0
+        print 'Going to start remote receivers on hosts: ' + str(self.receivers)
         
-        print 'Going to start remote receiver on host: ' + rcvHost + ' with ' + str(noOfFlows) + ' flows'
-        for fn in range(noOfFlows):
-            flowString+=comma
-            flowString+=format(flow, "02d")
-            comma=","
-            flow+=1
-                  
-        #
-        # GCH: Why is there a -u option here?
-        #        rcvCmd = ("ssh " + os.environ['USER']+ "@" + rcvHost + 
-        #                  " '" + self.preCmd + " bulkDataNTGenReceiver -n -u -s TS -f "+flowString+
-        #                  self.postCmd+"bulkDataNTGenReceiver.$HOST'")
-        #
-        if multicast == True:
-            xxxcastString=""
-        else:
-            xxxcastString="-u"
-        outFile = self.filePath+self.filePrefix+"GenReceiver."+rcvHost
-        rcvCmd = ("ssh " + os.environ['USER']+ "@" + rcvHost + 
-                  " '" + self.preCmd + " bulkDataNTGenReceiver -n -s TS -f "+flowString+
-                  " "+xxxcastString+
-                  " --qos_lib="+self.qos_lib+
-                  self.postCmd+outFile+"'")
-        print ' Executing command: '+ rcvCmd
-        process = Popen(rcvCmd, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
-        if process is not None:
-            self.receiverData=RemoteProcData(rcvHost, rcvCmd, process, outFile)
-        else:
-            print 'Problem to execute: ' + rcvCmd
-            return
-        time.sleep(2)  # Wait for the receiver to be fully initialized.
+        for h in sorted(self.receiversData):
+            print '----> Starting host: '+self.receiversData[h].host
+            print '#',i,' Executing command: '+ self.receiversData[h].command
+            self.receiversData[h].process = Popen(self.receiversData[h].command,
+                                                stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
+            if self.receiversData[h].process is None:
+                print 'Problem to execute: ' + self.receiversData[h].command
+                
+
+        time.sleep(10)  # Wait for the receivers to be fully initialized.
         print 'Going to wait for data'
 
     ######################################
-    #   Stops the receiver
+    #   Stops the receivers
     ######################################
-    def stopReceiver(self):        
-        rp = self.receiverData.process           
-        rp.poll()
-        if rp.returncode is None:
-            rp.stdin.write('\n') # ENTER
-        else:
-            print 'command  : '+self.receiverData.command+' exited with an error: ', rp.returncode, rp.stderr.read()
+    def stopReceivers(self):
+
+
+        for h in sorted(self.receiversData):
+            print '----> Asking receiver to exit for host: '+h
+            sp = self.receiversData[h].process           
+            sp.poll()
+            # Sends \n to request exit
+            if sp.returncode is None:
+                sp.stdin.write('\n') # ENTER
+            else:
+                print 'Command for host: '+h+' : '+self.receiversData[h].command+' exited with an error: ', sp.returncode, sp.stderr.read()
                 
-        rp = self.receiverData.process                
-        print '----> Waiting termination for receiver'
-        rp.wait()
-        print '----> Waiting termination for receiver - DONE'
-        self.receiverData.output=o=rp.stdout.read().strip()
-        if rp.returncode is 0:
-            print "Receiver Command ", rp.returncode, " : "+o
-        else:
-            print 'Command : '+self.receiverData.command+' exited with an error: ', rp.returncode, rp.stderr.read()
+        for h in sorted(self.receiversData):
+            print '----> Waiting termination for host: '+h
+            sp = self.receiversData[h].process                
+            sp.wait()
+            print '----> Waiting termination for host: '+h+' TERMINATED'
+            self.receiversData[h].output=o=sp.stdout.read().strip()
+            if sp.returncode is 0:
+                print "Command for: "+h+" ", sp.returncode, " : "+o
+            else:
+                print 'Command for host: '+h+' : '+self.receiversData[h].command+' exited with an error: ', sp.returncode, sp.stderr.read()
+        print '----> All receivers terminated. Waiting a few seconds for synchronization'
+        time.sleep(10)                
 
     ######################################
-    #   Stops the receiver
+    #   Process testy results
     ######################################
     def processTestResults(self):
 
@@ -371,8 +397,8 @@ class bulkDataNTtestSuite:
 if __name__ == '__main__':
     b           = 640000
     l           = 50
-    senderHosts = None
-    receiverHost= None
+    senders     = None
+    receivers   = None
     acsdata     = None
     sf          = ".bash_profile"
     verbose     = False
@@ -403,7 +429,7 @@ if __name__ == '__main__':
             print '  -h or --help                    : This help'
             sys.exit()
         elif o in ("-s", "--senders"):
-            senderHosts=a.split(",")
+            senders=a.split(",")
         elif o=="-b":
             b=a
         elif o=="-l":
@@ -411,8 +437,7 @@ if __name__ == '__main__':
         elif o=="-m":
             multicast=True
         elif o in ("-r", "--receivers"):
-            receiverHost=a
-            print 'Warning: option -r/--receivers= only one receiver supported in this version'
+            receivers=a.split(",")
         elif o=="--source":
             sf=a
         elif o=="--acsdata":
@@ -426,8 +451,12 @@ if __name__ == '__main__':
         elif o in ("-v", "--verbose"):
             verbose=True
            
-    if senderHosts is None:
+    if senders is None:
         print 'No sender has been given (use option -s/--senders=)!'
+        sys.exit()
+
+    if receivers is None:
+        print 'No receiver has been given (use option -r/--receivers=)!'
         sys.exit()
 
     #
@@ -456,16 +485,17 @@ if __name__ == '__main__':
     # command line parameters reeived from the command line.
     #
     testSuite = bulkDataNTtestSuite(startTime=startTime, filePrefix=filePrefix,
-                                    hosts=senderHosts, size=b, loops=l,
-                                    sourceFile=sf,
-                                    throttle=throttle, qos_lib=qos_lib)
+                                    receivers=receivers,
+                                    senders=senders, size=b, loops=l,
+                                    sourceFile=sf, acsdataEnv=acsdata,
+                                    throttle=throttle, qos_lib=qos_lib, multicast=multicast)
 
     #
     # Starts the receiver, collecting from all senders
     #
-    if receiverHost is not None:
-        print '----> Starting receiver'
-        testSuite.startReceiver(receiverHost, multicast) 
+    if receivers is not None:
+        print '----> Starting receivers'
+        testSuite.startReceivers() 
     print '---------------------------------------------------------------------------'
 
     #
@@ -478,10 +508,10 @@ if __name__ == '__main__':
     #
     # Stops the receiver
     #
-    if receiverHost is not None:
+    if receivers is not None:
         print '---------------------------------------------------------------------------'
-        print '----> Stopping receiver'
-        testSuite.stopReceiver()
+        print '----> Stopping receivers'
+        testSuite.stopReceivers()
 
     #
     # Prints the time all processes have exited
