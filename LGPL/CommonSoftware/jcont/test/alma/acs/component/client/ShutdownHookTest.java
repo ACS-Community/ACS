bec59c6ba2ddbee84a14b9a3bedbcb28f933b3db
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2013-03-08 10:50:10 +0000

    Javadoc and minor cleanup as part of checking test failures.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@187273 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jcont/test/alma/acs/component/client/ShutdownHookTest.java
+++ LGPL/CommonSoftware/jcont/test/alma/acs/component/client/ShutdownHookTest.java
@@ -6,44 +6,68 @@ import java.util.logging.Level;
 import alma.acs.logging.AcsLogger;
 import alma.acs.logging.ClientLogManager;
 import alma.acs.logging.StdOutConsoleHandler;
+import alma.acs.util.AcsLocations;
 
 public class ShutdownHookTest {
 
-	public static void main(String []args) throws Exception {
+	/**
+	 * Creates the ComponentClient "ShutdownHookTestClient" and tests its cleanup
+	 * using calls to tearDown coming both from the shutdown hook and from explicit invocations.
+	 * <p>
+	 * In the <code>tearDown</code> method, it enables FINE logs for stdout 
+	 * before calling {@link ComponentClient#tearDown()},
+	 * in order to not miss the "duplicate call" log that the base class creates 
+	 * if <code>tearDown</code> is called more than once.
+	 * <p>
+	 * Meaning of the 3 int args:
+	 * <ol>
+	 *   <li> <code>0</code>: tearDown calls super.tearDown and exits. <br>
+	 *        <code>!=0</code>: tearDown sleeps 10 seconds after calling super.tearDown
+	 *   <li> Sleep time in seconds after creating the ComponentClient, before returning from this main method call.
+	 *   <li> Number of additional calls to the ComponentClient's tearDown method.
+	 * </ol>
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
 
 		AcsLogger logger;
 		ComponentClient m_client;
 
 		logger = ClientLogManager.getAcsLogManager().getLoggerForApplication("ShutdownHookTestClient", false);
 
-		if( Integer.parseInt(args[0]) == 0 )
-			m_client = new ComponentClient(logger, System.getProperty("ACS.manager"), "ShutdownHookTestClient") {
+		if (Integer.parseInt(args[0]) == 0)
+			m_client = new ComponentClient(logger, AcsLocations.figureOutManagerLocation(), "ShutdownHookTestClient") {
 				public void tearDown() throws Exception {
-					for(Handler h : m_logger.getHandlers())
-						if(h instanceof StdOutConsoleHandler)
+					for (Handler h : m_logger.getHandlers()) {
+						if (h instanceof StdOutConsoleHandler) {
 							h.setLevel(Level.FINE); // to get the "duplicate call" log
+						}
+					}
 					super.tearDown();
 				}
 			};
 		else
-			m_client = new ComponentClient(logger, System.getProperty("ACS.manager"), "ShutdownHookTestClient") {
+			m_client = new ComponentClient(logger, AcsLocations.figureOutManagerLocation(), "ShutdownHookTestClient") {
 				public void tearDown() throws Exception {
-					for(Handler h : m_logger.getHandlers())
-						if(h instanceof StdOutConsoleHandler)
+					for (Handler h : m_logger.getHandlers())
+						if (h instanceof StdOutConsoleHandler) {
 							h.setLevel(Level.FINE); // to get the "duplicate call" log
+						}
 					super.tearDown();
-					Thread.sleep(10*1000);
+					Thread.sleep(10 * 1000);
 				}
 			};
 
 		// Sleep a little bit... depending on the amount of time, we might want to kill
 		// this baby from the outside, who knows
-		Thread.sleep(Integer.parseInt(args[1])*1000);
+		Thread.sleep(Integer.parseInt(args[1]) * 1000);
 
 		// We may want to call tearDown() as many times as we want
 		int iterations = Integer.parseInt(args[2]);
-		for(int i=0; i != iterations; i++)
+		for (int i = 0; i != iterations; i++) {
 			m_client.tearDown();
+		}
 
 	}
 
