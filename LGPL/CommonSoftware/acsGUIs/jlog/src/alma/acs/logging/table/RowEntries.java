94b36f3789fbc6cf6a12cc9866c0f70ad75d8f06
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2013-07-03 13:44:23 +0000

    To improve performances new keys are always appended at the end of the list.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@191747 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsGUIs/jlog/src/alma/acs/logging/table/RowEntries.java
+++ LGPL/CommonSoftware/acsGUIs/jlog/src/alma/acs/logging/table/RowEntries.java
@@ -26,14 +26,20 @@ import java.util.List;
 
 /**
  * The data structure containing all the rows of the table
- * (in the model coordinate space).
+ * (in the model coordinate space). It encapsulate a list of keys.
  * <P>
- * The entry in position <i>i</i> contains the key of the
- * log to show in the row <i>i</i> of the table.
+ * For the user point of view, </CODE>RowEntries</CODE> contains the keys of the 
+ * logs in the table: the keys in position <i>i</i> is the key of the log
+ * shown in the table in position <i>i</i>.
+ * <P>
+ * <B>Implementation note.</B><BR>
+ * Even if logically, the entry in position <i>i</i> contains the key of the
+ * log to show in the row <i>i</i> of the table, to improve performances, 
+ * new keys are appended in the tail of the vector instead
+ * of being inserted in the head i.e. the key of the first log (in row 0) is 
+ * is the last key of the vector (in position size-1). For this reason, {@link #get(int)}
+ * convert from the passed index to the real position in the list.
  * 
- * TODO: add at the beginning of the list results in very poor performance:
- *       we should always append new items and convert the indexes properly!
- *         
  * @author acaproni
  *
  */
@@ -45,12 +51,12 @@ public class RowEntries {
 	private final List<Integer> entries = Collections.synchronizedList(new ArrayList<Integer>(20000));
 
 	/**
-	 * Remove the first <code>numOfEntries</code> entries from the 
+	 * Remove the oldest <code>numOfEntries</code> entries from the 
 	 * array.
 	 * 
 	 * @param numOfEntries
 	 */
-	public List<Integer> removeFirstEntries(int numOfEntries) {
+	public List<Integer> removeLastEntries(int numOfEntries) {
 		if (numOfEntries<=0) {
 			throw new IllegalArgumentException("Invalid number of entries to remove: "+numOfEntries);
 		}
@@ -65,13 +71,13 @@ public class RowEntries {
 	}
 	
 	/**
-	 * Remove the last <code>numOfEntries</code> entries from the 
+	 * Remove the first <code>numOfEntries</code> entries from the 
 	 * array.
 	 * 
 	 * @param numOfEntries
 	 * @return the keys removed
 	 */
-	public List<Integer> removeLastEntries(int numOfEntries) {
+	public List<Integer> removeFirstEntries(int numOfEntries) {
 		if (numOfEntries<=0) {
 			throw new IllegalArgumentException("Invalid number of entries to remove: "+numOfEntries);
 		}
@@ -93,24 +99,34 @@ public class RowEntries {
 	public int size() {
 		return entries.size();
 	}
-
+	
 	/**
-	 * TODO: for better performance we should always add new elements at the end
-	 * @param index
-	 * @param element
-	 * @see java.util.List#add(int, java.lang.Object)
+	 * Add the passed key to the list of entries.
+	 * <P>
+	 * To improve performances, the key is appended at the end of the vector
+	 * 
+	 * @param key The integer key to add to the list
 	 */
-	public void add(int index, Integer element) {
-		entries.add(index, element);
+	public void add(Integer key) {
+		entries.add(key);
 	}
 
 	/**
-	 * @param index
-	 * @return
-	 * @see java.util.List#get(int)
+	 * Return the key at the given row of the table.
+	 * <P>
+	 * For a deeper understanding of the algorithm, read the implementation note
+	 * in {@link RowEntries}.
+	 *  
+	 * @param index The index of the key to get
+	 * @return The key at passed row of the table
 	 */
-	public Integer get(int index) {
-		return entries.get(index);
+	public Integer get(int row) {
+		// The index must be converted from the passed row 
+		// to the right index in the list
+		synchronized (entries) {
+			int indxInTheList=entries.size()-1-row;
+			return entries.get(indxInTheList);	
+		}
 	}
 
 	/**
@@ -122,11 +138,22 @@ public class RowEntries {
 	}
 
 	/**
-	 * @param o
-	 * @return
-	 * @see java.util.List#indexOf(java.lang.Object)
+	 * Return the position (i.e. the row in the table) that contains the passed key 
+	 * <P>
+	 * For a deeper understanding of the algorithm, read the implementation note
+	 * in {@link RowEntries}.
+	 * 
+	 * @param key The key to look for in the table
+	 * @return The position in the list of the passed key or
+	 *         -1 if the list does not contain the passed key
 	 */
-	public int indexOf(Object o) {
-		return entries.indexOf(o);
+	public int indexOf(Integer key) {
+		// Get the real position in the list
+		synchronized (entries) {
+			// Get the real position in the list
+			int pos=entries.indexOf(key);
+			// return the converted index
+			return pos==-1?pos:entries.size()-pos-1;
+		}
 	}
 }
