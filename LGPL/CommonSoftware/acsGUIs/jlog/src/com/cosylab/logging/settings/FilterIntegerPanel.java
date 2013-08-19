ffb8cd505918f5849a06b46efed45925b64dba34
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2013-08-19 16:26:00 +0000

    Abeans JIntegerTextField widget replaced by a JTextField plus a filter to allow the user to insert only digits. TODO: limit the number of digits
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@193870 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsGUIs/jlog/src/com/cosylab/logging/settings/FilterIntegerPanel.java
+++ LGPL/CommonSoftware/acsGUIs/jlog/src/com/cosylab/logging/settings/FilterIntegerPanel.java
@@ -24,31 +24,125 @@ package com.cosylab.logging.settings;
 import java.awt.GridBagLayout;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.regex.Pattern;
 
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
 
-import com.cosylab.gui.components.r2.JIntegerTextField;
-import com.cosylab.logging.engine.Filter;
+import alma.acs.gui.util.threadsupport.EDTExecutor;
+
 import com.cosylab.logging.engine.ExactFilter;
-import com.cosylab.logging.engine.MinMaxFilter;
+import com.cosylab.logging.engine.Filter;
 import com.cosylab.logging.engine.InvalidFilterConstraintException;
+import com.cosylab.logging.engine.MinMaxFilter;
 
 /**
- * Serves the purpose of filtering according to the integer value. Used by FilterParameterDialog. 
- * Creation date: (2/7/02 10:42:17 AM)
- * @author: 
+ * Serves the purpose of filtering according to the integer value. Used by
+ * FilterParameterDialog. Creation date: (2/7/02 10:42:17 AM)
+ * 
+ * @author:
  */
 public class FilterIntegerPanel extends FilterParameterPanel {
-	private JIntegerTextField minimum;
-	private JIntegerTextField maximum;
-	private JIntegerTextField exact;
+	
+	/**
+	 * The filter to 
+	 * <UL>
+	 * 	<LI>accept only digits ({@link #digitsRegExp}
+	 *  <LI>Limit the size of the input string to 9 digits
+	 * </UL>
+	 * 
+	 * @author acaproni
+	 * @since ACS 12.1
+	 */
+	private class FormattedIntDocFilter extends DocumentFilter {
+		
+		/**
+		 * The regular expression used to check if the text contains only digits
+		 */
+		private final Pattern digitsRegExp = Pattern.compile("[0-9]*");
+		
+		/**
+		 * The max number of digits that the user can insert in the text field
+		 */
+		public final int maxNumOfDigits;
+
+		/**
+		 * Constructor
+		 * 
+		 * @param size
+		 *            The max number of digits that the user can insert in the
+		 *            text field
+		 */
+		public FormattedIntDocFilter(int size) {
+			this.maxNumOfDigits = size;
+		}
+
+		@Override
+		public void remove(FilterBypass fb, int offset, int length)
+				throws BadLocationException {
+			// Removing is always allowed
+			super.remove(fb, offset, length);
+		}
+
+		@Override
+		public void insertString(FilterBypass fb, int offset, String string,
+				AttributeSet attr) throws BadLocationException {
+			if (digitsRegExp.matcher(string).matches()) {
+				super.insertString(fb, offset, string, attr);
+			}
+		}
+
+		@Override
+		public void replace(FilterBypass fb, int offset, int length,
+				String text, AttributeSet attrs) throws BadLocationException {
+			if (digitsRegExp.matcher(text).matches()) {
+				super.replace(fb, offset, length, text, attrs);
+			} 
+		}
+	}
+
+	/**
+	 * The text field to insert the minimum value
+	 */
+	private JTextField minimum;
+
+	/**
+	 * The text field to insert the maximum value
+	 */
+	private JTextField maximum;
+	
+	/**
+	 * The text field to insert the exact value
+	 */
+	private JTextField exact;
+	
+	
 	protected JCheckBox minimumCheck;
 	protected JCheckBox maximumCheck;
 	protected JCheckBox exactCheck;
-	
-	private JCheckBox notCheck; // NOT policy
 
+	private JCheckBox notCheck; // NOT policy
+	
+	/**
+	 * All the integer text fields have the same properties:
+	 * this method avoid code repetition.
+	 *   
+	 * @param initialValue The initial value in the text field
+	 * @return The JTextField
+	 * 
+	 */
+	private JTextField buildIntegerTextField(int initialValue) {
+		JTextField ret=new JTextField(""+initialValue);
+		ret.setColumns(10);
+		AbstractDocument document = (AbstractDocument) ret.getDocument();
+		document.setDocumentFilter(new FormattedIntDocFilter(10));
+		return ret;
+	}
 
 	private class MinmaxChecked implements ItemListener {
 		public void itemStateChanged(ItemEvent e) {
@@ -65,157 +159,143 @@ public class FilterIntegerPanel extends FilterParameterPanel {
 			}
 		}
 	}
-/**
- * FilterIntegerPanel constructor comment.
- */
-public FilterIntegerPanel() {
-	super();
 
-}
-/**
- * Insert the method's description here.
- * Creation date: (2/7/02 10:44:28 AM)
- */
-protected void createComponents() {
-	MinmaxChecked mmc = new MinmaxChecked();
-	
-	JPanel panelTop = new JPanel(new GridBagLayout());
-	add(panelTop, newConstraints(0, 4, 4, 4, 4));
-	
-	notCheck = new JCheckBox("Discard entries matching this filter");
-	notCheck.setToolTipText("Keep/discard entries matching this filter");
-	panelTop.add(notCheck,newConstraints(0,4,4,4,4));
+	/**
+	 * Insert the method's description here. Creation date: (2/7/02 10:44:28 AM)
+	 */
+	protected void createComponents() {
+		EDTExecutor.instance().execute(new Runnable() {
+			@Override
+			public void run() {
+				MinmaxChecked mmc = new MinmaxChecked();
 
-	minimumCheck = new JCheckBox("Minimum value");
-	minimumCheck.addItemListener(mmc);
-	panelTop.add(minimumCheck, newConstraints(1, 4, 0, 0, 0));
-	
-	minimum = new JIntegerTextField();
-	panelTop.add(minimum, newConstraints(2, 0, 0, 4, 0));
+				JPanel panelTop = new JPanel(new GridBagLayout());
+				add(panelTop, newConstraints(0, 4, 4, 4, 4));
 
-	maximumCheck = new JCheckBox("Maximum value");
-	maximumCheck.addItemListener(mmc);
-	panelTop.add(maximumCheck, newConstraints(3, 4, 0, 0, 0));
+				notCheck = new JCheckBox("Discard entries matching this filter");
+				notCheck.setToolTipText("Keep/discard entries matching this filter");
+				panelTop.add(notCheck, newConstraints(0, 4, 4, 4, 4));
 
-	maximum = new JIntegerTextField();
-	panelTop.add(maximum, newConstraints(4, 0, 0, 4, 0));
+				minimumCheck = new JCheckBox("Minimum value");
+				minimumCheck.addItemListener(mmc);
+				panelTop.add(minimumCheck, newConstraints(1, 4, 0, 0, 0));
 
-	JPanel panelBottom = new JPanel(new GridBagLayout());
-	add(panelBottom, newConstraints(1, 4, 4, 4, 4));
+				minimum = buildIntegerTextField(1);
+				panelTop.add(minimum, newConstraints(2, 0, 0, 4, 0));
 
-	exactCheck = new JCheckBox("Exact value");
-	exactCheck.addItemListener(new ExactChecked());
-	panelBottom.add(exactCheck, newConstraints(0, 4, 0, 0, 0));
+				maximumCheck = new JCheckBox("Maximum value");
+				maximumCheck.addItemListener(mmc);
+				panelTop.add(maximumCheck, newConstraints(3, 4, 0, 0, 0));
 
-	exact = new JIntegerTextField();
-	panelBottom.add(exact, newConstraints(1, 0, 0, 4, 0));
-	
-	
-}
-/**
- * Insert the method's description here.
- * Creation date: (2/7/02 11:08:31 AM)
- * @return com.cosylab.logging.engine.Filter
- * @exception com.cosylab.logging.settings.FilterParameterException The exception description.
- */
-public Filter getFilter() throws FilterParameterException {
-	boolean bmin = minimumCheck.isSelected();
-	boolean bmax = maximumCheck.isSelected();
-	boolean bexact = exactCheck.isSelected();
+				maximum = buildIntegerTextField(2);
+				panelTop.add(maximum, newConstraints(4, 0, 0, 4, 0));
 
-	Integer min = null;
-	Integer max = null;
-	
-	if (bexact) {
+				JPanel panelBottom = new JPanel(new GridBagLayout());
+				add(panelBottom, newConstraints(1, 4, 4, 4, 4));
+
+				exactCheck = new JCheckBox("Exact value");
+				exactCheck.addItemListener(new ExactChecked());
+				panelBottom.add(exactCheck, newConstraints(0, 4, 0, 0, 0));
+
+				exact = buildIntegerTextField(1);
+				panelBottom.add(exact, newConstraints(1, 0, 0, 4, 0));
+			}
+		});
+	}
+
+	/**
+	 * Insert the method's description here. Creation date: (2/7/02 11:08:31 AM)
+	 * 
+	 * @return com.cosylab.logging.engine.Filter
+	 * @exception com.cosylab.logging.settings.FilterParameterException
+	 *                The exception description.
+	 */
+	public Filter getFilter() throws FilterParameterException {
+		boolean bmin = minimumCheck.isSelected();
+		boolean bmax = maximumCheck.isSelected();
+		boolean bexact = exactCheck.isSelected();
+
+		Integer min = null;
+		Integer max = null;
+
+		if (bexact) {
+			try {
+				return new ExactFilter(getFieldIndex(), isLethal(),
+						Integer.parseInt(exact.getText()),
+						notCheck.isSelected());
+			} catch (InvalidFilterConstraintException e) {
+				throw new FilterParameterException(e.getMessage());
+			}
+		}
+
+		if (bmin) {
+			min = Integer.parseInt(minimum.getText());
+		}
+		if (bmax) {
+			max = Integer.parseInt(maximum.getText());
+		}
+
+		if ((min != null) && (max != null)) {
+			if (min.compareTo(max) > -1) {
+				throw new FilterParameterException(
+						"Minimum must be less than maximum");
+			}
+		}
+
 		try {
-			return new ExactFilter(
-					getFieldIndex(), 
-					isLethal(), 
-					Integer.valueOf(exact.getIntegerValue()),
+			return new MinMaxFilter(getFieldIndex(), isLethal(), min, max,
 					notCheck.isSelected());
 		} catch (InvalidFilterConstraintException e) {
 			throw new FilterParameterException(e.getMessage());
 		}
 	}
 
-	if (bmin) {
-		min = Integer.valueOf(minimum.getIntegerValue());
-	}
-	if (bmax) {
-		max = Integer.valueOf(maximum.getIntegerValue());
-	}
+	/**
+	 * Insert the method's description here. Creation date: (2/7/02 11:00:09 AM)
+	 * 
+	 * @param f
+	 *            com.cosylab.logging.engine.Filter
+	 */
+	public void setFilter(Filter f) {
+		if (f == null)
+			return;
 
-	if ((min != null) && (max != null)) {
-		if (min.compareTo(max) > -1) {
-			throw new FilterParameterException("Minimum must be less than maximum");
-		}
-	}
-
-	try {
-		return new MinMaxFilter(getFieldIndex(), isLethal(), min, max,notCheck.isSelected());
-	} catch (InvalidFilterConstraintException e) {
-		throw new FilterParameterException(e.getMessage());
-	}
-}
-/**
- * Insert the method's description here.
- * Creation date: (2/7/02 11:16:32 AM)
- * @param min int
- * @param max int
- */
-public void setBounds(int min, int max) {
-	minimum.setMinimum(min);
-	minimum.setMaximum(max);
-
-	maximum.setMinimum(min);
-	maximum.setMaximum(max);
-
-	exact.setMinimum(min);
-	exact.setMaximum(max);
-}
-/**
- * Insert the method's description here.
- * Creation date: (2/7/02 11:00:09 AM)
- * @param f com.cosylab.logging.engine.Filter
- */
-public void setFilter(Filter f) {
-	if (f == null)
-		return;
-
-	switch (f.getConstraint()) {
+		switch (f.getConstraint()) {
 		case EXACT:
-			exactCheck.setSelected(true);
-			exact.setIntegerValue(((Number) ((ExactFilter)f).getExact()).intValue());
+			setField(exactCheck, ((Number)((MinMaxFilter)f).getMinimum()).intValue(),exact);
 			break;
-		case MINIMUM :
-			setMinimum(f);
+		case MINIMUM:
+			setField(minimumCheck, ((Number)((MinMaxFilter)f).getMinimum()).intValue(),minimum);
 			break;
-		case MAXIMUM :
-			setMaximum(f);
+		case MAXIMUM:
+			setField(maximumCheck, ((Number)((MinMaxFilter)f).getMinimum()).intValue(),maximum);
 			break;
-		case MINMAX :
-			setMinimum(f);
-			setMaximum(f);
+		case MINMAX:
+			setField(minimumCheck, ((Number)((MinMaxFilter)f).getMinimum()).intValue(),minimum);
+			setField(maximumCheck, ((Number)((MinMaxFilter)f).getMinimum()).intValue(),maximum);
 			break;
+		}
+		notCheck.setSelected(f.notPolicyApplyed());
 	}
-	notCheck.setSelected(f.notPolicyApplyed());
-}
-/**
- * Insert the method's description here.
- * Creation date: (2/7/02 11:06:35 AM)
- * @param f com.cosylab.logging.engine.Filter
- */
-private void setMaximum(Filter f) {
-	maximumCheck.setSelected(true);
-	maximum.setIntegerValue(((Number) ((MinMaxFilter)f).getMaximum()).intValue());
-}
-/**
- * Insert the method's description here.
- * Creation date: (2/7/02 11:06:06 AM)
- * @param f com.cosylab.logging.engine.Filter
- */
-private void setMinimum(Filter f) {
-	minimumCheck.setSelected(true);
-	minimum.setIntegerValue(((Number)  ((MinMaxFilter)f).getMinimum()).intValue());
-}
+	
+	/**
+	 * Select the checkbox and set the value in the text field
+	 * 
+	 * @param checkBox The checkbox to select
+	 * @param val The value to set in the text field
+	 * @param textField The text field to set the value into
+	 */
+	private void setField(JCheckBox checkBox, int val, JTextField textField) {
+		if (checkBox==null) {
+			throw new IllegalArgumentException("The check box can't be null");
+		}
+		if (textField==null) {
+			throw new IllegalArgumentException("The text field can't be null");
+		}
+		if (val<0) {
+			throw new IllegalArgumentException("The value can't be less then 0! Rejected value is "+val);
+		}
+		checkBox.setSelected(true);
+		textField.setText(""+val);
+	}
 }
