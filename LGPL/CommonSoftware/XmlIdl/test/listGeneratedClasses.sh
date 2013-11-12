9fb5f9a2fe74159f0772b549daf9caba1018af87
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2013-11-12 17:50:11 +0000

    - Added a TAT test that unpacks all java sources from the IDL jar(s) that were generated by the Makefile,   so that their concatenated content gets compared to a reference file.   This exercises the call from acsMakefile to XmlIdl and ensures we generate correct code   (especially w/o name clashes with the standard JacORB IDL build). - Added the dummy xml binding classes as a fixed jar because they must be available when IDL gets built,   which I believe the Makefile always does before turning to "JARFILES".   Note that module acsTestEntities only comes after XmlIdl so that we cannot use those castor classes. - Clean-up: RemovedIDL compilation of acscourseMount.idl and associated error definition.   The goal is to have several test IDL files that are specific to our module.   Also removed the old no-op "allTests"
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@197556 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/CommonSoftware/XmlIdl/test/listGeneratedClasses.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+#
+# Extract all java sources from all xmltest*.jar files to tmp directories.
+# TAT can then compare the content of all files with the generated ref file.
+# We do not remove the java files that the normal IDL compiler has generated
+# because if our XmlIdl compiler erroneously creates files with clashing names
+# then we should also detect this in case of files getting overwritten.
+#
+
+idlJarFiles=$(find "`pwd`/../lib" -name "xmltest*.jar")
+
+mkdir -p unpacked_jars
+cd unpacked_jars
+
+for jarFile in $idlJarFiles; do
+  idlName=$(basename $jarFile .jar)
+  echo "======================= IDL file $idlName"
+  echo
+  mkdir -p $idlName
+  cd $idlName
+  jar xf $jarFile src
+  cd src
+  javaFiles=$(find . -name "*.java" | sort)
+  for i in $javaFiles; do
+    echo "----------------------- Java file $i"
+    cat "$i"
+    echo
+  done
+  cd ../..
+done
+
+cd ..
+
