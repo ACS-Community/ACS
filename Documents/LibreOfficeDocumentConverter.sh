2ccdce552262ca7ca36aacc7a5387cc6b11bc52f
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2014-01-16 15:01:02 +0000

    Conversion to PDF is done by LibreOffice instead of OpenOffice.
    Makefile could directly invoke LibreOffice because now it is much easier then
    it was with OpenOffice. 
    I preferred the have LibreOfficeDocumentConverter.sh (replacing OpenOfficeDocumentConverter.sh
    and OpenOfficeDocumentConverter.py) to maintain the same structure we had with OpenOffice.
    
    
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@199965 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ Documents/LibreOfficeDocumentConverter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# This is a very crude script to drive the conversion
+# of documents using Open Office.
+# It should be made nicer, more protectec from errors, 
+# less verbose and, probably, re-written in Python
+# to avoid using the bash, that is not nicely portable.
+#
+echo "Converting docs in PDF using LibreOffice"
+echo "Dest dir $1"
+
+libreoffice4.1 --headless --nologo --convert-to pdf --outdir $*
+
+echo "Done PDF conversion from LibreOffice"
+
+#__oOo__
+
