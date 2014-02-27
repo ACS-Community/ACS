c2207ce4aae096a3125f141596117fbd18fdda9a
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2014-02-27 14:29:43 +0000

    Header
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@201720 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/containerTests/contHandleTest/test/alma/containerTests/contHandleTest/ContainerHandleTest.java
+++ LGPL/CommonSoftware/containerTests/contHandleTest/test/alma/containerTests/contHandleTest/ContainerHandleTest.java
@@ -1,3 +1,21 @@
+/*
+ALMA - Atacama Large Millimiter Array
+* Copyright (c) European Southern Observatory, 2014 
+* 
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+* 
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+* 
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+*/
 package alma.containerTests.contHandleTest;
 
 import junit.framework.TestCase;
@@ -12,6 +30,9 @@ import alma.acs.logging.AcsLogger;
 import alma.acs.logging.ClientLogManager;
 import alma.acs.util.AcsLocations;
 
+/** 
+ * @since ACS 12.3
+ */
 public class ContainerHandleTest extends TestCase {
 
 	private static final String APP_NAME = ContainerHandleTest.class.getName();
