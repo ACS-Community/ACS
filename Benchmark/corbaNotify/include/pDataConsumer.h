a69a0399fe57dcdf7906864fe62d47d686b55f36
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2014-08-29 21:21:20 +0000

    Merged from 2014.4
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/ACS@208356 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ Benchmark/corbaNotify/include/pDataConsumer.h
@@ -0,0 +1,38 @@
+#ifndef PDATACONSUMER_H
+#define PDATACONSUMER_H
+/*******************************************************************************
+* ALMA - Atacama Large Millimiter Array
+* Copyright (c) European Southern Observatory, 2014 
+* 
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+* 
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+* 
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+*
+*
+* who       when      what
+* --------  --------  ----------------------------------------------
+* almadev  2014-08-28  created
+*/
+
+/************************************************************************
+ *
+ *----------------------------------------------------------------------
+ */
+
+#ifndef __cplusplus
+#error This is a C++ include file and cannot be used from plain C
+#endif
+
+
+
+#endif /*!PDATACONSUMER_H*/
