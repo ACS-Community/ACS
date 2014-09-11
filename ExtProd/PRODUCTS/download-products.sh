8a257598517a1e69b6a55bd06041c3710e5092fe
Jorge Avarias <jorge.avarias@gmail.com>
2014-09-10 20:03:30 -0600

    Fixed names of the ext prods files downloaded.
    Now the file name is the name declared in the first segment of extprods.links.txt (before the space)

--- ExtProd/PRODUCTS/download-products.sh
+++ ExtProd/PRODUCTS/download-products.sh
@@ -5,8 +5,10 @@ DOWNLOAD_LIST=extprods.links.txt
 for item in `cat $DOWNLOAD_LIST`; do 
 	p=`echo $item |cut -d':' -f1`
   	if [ $p = "http" ]; then 
-		wget -c $item 
+		wget -c -O $PACKAGE_NAME $item 
 	elif [ $p = "https" ]; then
-		wget -c --no-check-certificate $item
+		wget -c -O $PACKAGE_NAME --no-check-certificate $item
+	else
+		PACKAGE_NAME=$item
 	fi 
 done
