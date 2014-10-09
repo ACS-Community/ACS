0389324c20eb0feeb58ea54bd805b58148ea7027
hsommer <hsommer@523d945c-050c-4681-91ec-863ad3bb968a>
2014-10-09 11:53:12 +0000

    Added new HelperTestStandalone to TAT.
    git-svn-id: https://svn.alma.cl/p2/trunk/ACS@209968 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jcontnc/test/runHelperTest.sh
+++ LGPL/CommonSoftware/jcontnc/test/runHelperTest.sh
@@ -6,4 +6,5 @@
 #echo "Dumped naming service bindings for 'ChannelFactory' objects: (to investigate recurrent problems with 'Failed to resolve factory's MC extension object in the naming service')" 
 #acsStartJava org.jacorb.naming.ContextLister | grep ChannelFactory | sort | paste -s
 
-acsStartJava -endorsed alma.acs.testsupport.tat.TATJUnitRunner   alma.acs.nc.HelperTest
+acsStartJava -endorsed alma.acs.testsupport.tat.TATJUnitRunner alma.acs.nc.HelperTest
+acsStartJava -endorsed alma.acs.testsupport.tat.TATJUnitRunner alma.acs.nc.HelperTestStandalone
