fc907ca435464c733b3ff74a406d31b88d33ceb3
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2015-07-07 15:21:15 +0000

    The icon in the toolbar triggers the saving of the statistics
    git-svn-id: https://svn.alma.cl/p2/trunk/ACS@219498 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsGUIs/AlarmSystemProfiler/src/alma/acs/alarmsystemprofiler/handlers/SaveHandler.java
+++ LGPL/CommonSoftware/acsGUIs/AlarmSystemProfiler/src/alma/acs/alarmsystemprofiler/handlers/SaveHandler.java
@@ -13,20 +13,20 @@ package alma.acs.alarmsystemprofiler.handlers;
 
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.swt.widgets.Shell;
 
 public class SaveHandler {
 
-	@CanExecute
-	public boolean canExecute(EPartService partService) {
-		if (partService != null) {
-			return !partService.getDirtyParts().isEmpty();
-		}
-		return false;
-	}
+//	@CanExecute
+//	public boolean canExecute(EPartService partService) {
+//		if (partService != null) {
+//			return !partService.getDirtyParts().isEmpty();
+//		}
+//		return false;
+//	}
 
 	@Execute
-	public void execute(EPartService partService) {
-		partService.saveAll(false);
+	public void execute(Shell shell) {
+		System.out.println("Saving...");
 	}
 }
