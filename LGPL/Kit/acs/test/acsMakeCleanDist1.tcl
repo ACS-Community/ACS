419af0e813f1a3c1969c41c1ccd7dbbb55aa99cd
eallaert <eallaert@523d945c-050c-4681-91ec-863ad3bb968a>
2015-08-12 12:18:37 +0000

    ICT-5630: directories containing only symlinks should not be considered empty, hence should not be removed by the clean_dist target. Added a test to the test-suite to demonstrate this (tat-test 21).
    git-svn-id: https://svn.alma.cl/p2/trunk/ACS@221017 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ LGPL/Kit/acs/test/acsMakeCleanDist1.tcl
@@ -0,0 +1,4 @@
+# -*- tcl -*-
+file delete -force ../idl
+file mkdir ../idl
+file link -symbolic ../idl/mySymLink ../test/TestList.lite
--- LGPL/Kit/acs/test/acsMakeInstall2.tcl
+++ LGPL/Kit/acs/test/acsMakeInstall2.tcl
@@ -1,6 +1,7 @@
 # -*- tcl -*-
-file mkdir ../include/subdir1 ../include/subdir1/subdir1.1 ../include/subdir2
+file mkdir ../include/subdir1 ../include/subdir1/subdir1.1 ../include/subdir2 ../include/CVS
 exec touch ../include/duplicateFile \
            ../include/subdir1/duplicateFile \
            ../include/subdir1/subdir1.1/duplicateFile \
            ../include/subdir2/duplicateFile \
+           ../include/CVS/duplicateFile
