4b24303dcb8aa79f2e3b53679f8ae7cb3a0e4485
pcolomer <pcolomer@523d945c-050c-4681-91ec-863ad3bb968a>
2015-12-09 11:30:40 +0000

    ICTJ:ICT-4730 Modified NCPublisher to reconnect to the channel when the exception OBJECT_NOT_EXIST is thrown while publishing an event. - Added autoreconnect attribute (by default set to false) that indicates whether the publisher should reconnect to the channel. - Created init method by moving the code from the constructor in order to be used when reconnecting to the channel. - Caught OBJECT_NOT_EXIST exception in NCPublisher#publishCORBAEvent to reconnect to the channel when autoreconnect attribute is on. - Thrown an exception when catching a TRANSIENT exception while publishing an event. In this way, in the case of sending a buffered event, we prevent the publication method to be blocked and it will work in the same way C++ Supplier does. - Added 4 test cases to check the modifications done: autoreconnect ON/OFF and Notify Service restarted or stopped.
    git-svn-id: https://svn.alma.cl/p2/trunk/ACS@225939 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/jcontnc/test/alma/acs/nc/SimpleSupplierReconnClient.java
+++ LGPL/CommonSoftware/jcontnc/test/alma/acs/nc/SimpleSupplierReconnClient.java
@@ -3,6 +3,9 @@ package alma.acs.nc;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.omg.CORBA.portable.IDLEntity;
 
@@ -15,8 +18,10 @@ import alma.acs.component.client.ComponentClient;
 import alma.acs.exceptions.AcsJException;
 import alma.acs.logging.ClientLogManager;
 import alma.acs.nc.AcsEventPublisher;
+import alma.acs.nc.AcsEventPublisher.EventProcessingHandler;
 import alma.acs.nc.AcsEventSubscriber;
 import alma.acs.nc.NCSubscriber;
+import alma.acs.nc.NCPublisher;
 import alma.acs.nc.AcsEventSubscriber.Callback;
 import alma.acsnc.EventDescription;
 
@@ -37,9 +42,14 @@ public class SimpleSupplierReconnClient implements Callback<EventDescription> {
 	private Logger m_logger = ClientLogManager.getAcsLogManager().getLoggerForApplication("SimpleSupplierReconnClient", false);
 	private static final String CHANNEL_NAME = "test_reconn";
 
+    public static final String NS_RESTARTED = "NS_RESTARTED";
+    public static final String NS_STOPPED = "NS_STOPPED";
+
 	private volatile int received = 0;
 	private int m_interval;
 	private int m_nEvents;
+	private boolean m_autoreconnect;
+    private String m_nsAction;
 	
 	/**
 	 * We'll use this publisher to publish events of different types
@@ -51,9 +61,76 @@ public class SimpleSupplierReconnClient implements Callback<EventDescription> {
 	
 	private ComponentClient m_client;
 
-	public SimpleSupplierReconnClient(int nEvents, int interval) {
+    public class CallbackObject implements EventProcessingHandler<IDLEntity> {
+
+        public List<Integer> m_transitions;
+        public int m_currentEvent;
+        public int m_nEventsSent;
+        public int m_nEventsStored;
+        public int m_nEventsDropped;
+        public int m_nExceptions;
+        protected boolean m_sent;
+
+        public CallbackObject() {
+            reset();
+        }
+
+        public void reset() {
+            this.m_transitions = new ArrayList<Integer>();
+            this.m_currentEvent = 0;
+            this.m_nEventsSent = 0;
+            this.m_nEventsStored = 0;
+            this.m_nEventsDropped = 0;
+            this.m_nExceptions = 0;
+            this.m_sent = true;
+        }
+
+        public void eventSent(IDLEntity event) {
+            this.m_currentEvent++;
+            this.m_nEventsSent++;
+            if(false == this.m_sent) {
+                this.m_transitions.add(this.m_currentEvent);
+                this.m_sent = true;
+            }
+        }        
+
+        public void eventStoredInQueue(IDLEntity event) {
+            this.m_currentEvent++;
+            this.m_nEventsStored++;
+            if(true == this.m_sent) {
+                this.m_transitions.add(this.m_currentEvent);
+                this.m_sent = false;
+            }
+        }
+        
+        public void eventDropped(IDLEntity event) {
+            this.m_currentEvent++;
+            this.m_nEventsDropped++;
+            if(true == this.m_sent) {
+                this.m_transitions.add(this.m_currentEvent);
+                this.m_sent = false;
+            }
+        }
+
+        public void exceptionThrown() {
+            this.m_currentEvent++;
+            this.m_nExceptions++;
+            if(true == this.m_sent) {
+                this.m_transitions.add(this.m_currentEvent);
+                this.m_sent = false;
+            }
+        }
+
+    }
+
+	private CallbackObject m_cbObj;
+
+	public SimpleSupplierReconnClient(int nEvents, int interval,boolean autoreconnect,String nsAction) {
 		m_nEvents = nEvents;
 		m_interval = interval;
+		m_autoreconnect = autoreconnect;
+        m_nsAction = nsAction;
+        m_cbObj = new CallbackObject();
 		m_logger.setLevel(Level.INFO);
 	}
 
@@ -62,6 +139,9 @@ public class SimpleSupplierReconnClient implements Callback<EventDescription> {
 			try {
 				m_client = new ComponentClient(m_logger, System.getProperty("ACS.manager"), "SimpleSupplierReconnClient");
 				m_publisher = m_client.getContainerServices().createNotificationChannelPublisher(CHANNEL_NAME, IDLEntity.class);
+				((NCPublisher)m_publisher).setAutoreconnect(m_autoreconnect);
+                m_publisher.enableEventQueue(100, m_cbObj); 
+				//m_publisher.setAutoreconnect(m_autoreconnect);
 			} catch (AcsJContainerServicesEx e) {
 				// Silently ignore the errors
 			} catch (AcsJException e) {
@@ -82,36 +162,72 @@ public class SimpleSupplierReconnClient implements Callback<EventDescription> {
 		event1.onOff = OnOffStates.ON;
 		event1.period = 0.2f;
 
-		int n_errors = 0;
-		int n_changes = 0;
-		boolean publishing = true;
+        m_logger.info("NS action: " + m_nsAction);
+        if(m_autoreconnect) {
+            m_logger.info("Autoreconnection: ON");
+        } else {
+            m_logger.info("Autoreconnection: OFF");
+        }
+        m_logger.info("Start publishing " + String.valueOf(m_nEvents) + " events");
 
 		// publish events
 		for(int j=0; j!=m_nEvents; j++) {
 			try {
-				//m_logger.info("Publishing event " + String.valueOf(j));
 				m_publisher.publishEvent(event1);
-		
-				if(false == publishing) {
-					publishing = true;
-					n_changes++;
-				}
 				
-				// Sleep and get events
+				// Sleep
 				try {
 					Thread.sleep(m_interval * 100);
 				} catch (InterruptedException e) { }
-			} catch(Exception e) {
-				n_errors++;
-				if(true == publishing) {
-					publishing = false;
-					n_changes++;
-				}
+
+			} catch(Throwable e) {
+                m_cbObj.exceptionThrown();
 			}
+            m_logger.info("Published events at iteration " + String.valueOf(j));
 		}
 
-		m_logger.info(String.valueOf(n_errors) + " exceptions caught");
-		m_logger.info(String.valueOf(n_changes) + " changes found");
+        String transitions = "";
+        Iterator<Integer> it = m_cbObj.m_transitions.iterator();
+        while(it.hasNext()) {
+            transitions += String.valueOf(it.next()) + ",";
+        }
+
+        m_logger.info("===   Number of events sent: " + String.valueOf(m_cbObj.m_nEventsSent));
+        m_logger.info("===   Number of events queued: " + String.valueOf(m_cbObj.m_nEventsStored));
+        m_logger.info("===   Number of events dropped: " + String.valueOf(m_cbObj.m_nEventsDropped));
+        m_logger.info("===   Number of exceptions: " + String.valueOf(m_cbObj.m_nExceptions));
+        m_logger.info("===   Transitions: " + transitions);
+
+
+        if(m_autoreconnect) {
+            if(m_nsAction.equals(NS_RESTARTED)) {
+                if(m_cbObj.m_transitions.size() != 2) {
+                    m_logger.info("===   Wrong number of transitions: " 
+                        + String.valueOf(m_cbObj.m_transitions.size()) + ". We expected 0 or 2");
+                }
+            } else if(m_nsAction.equals(NS_STOPPED)) {
+                if(m_cbObj.m_transitions.size() != 1) {
+                    m_logger.info("===   Wrong number of transitions: " 
+                        + String.valueOf(m_cbObj.m_transitions.size()) + ". We expected 1");
+                }
+            } else {
+                m_logger.info("===   Wrong Notify Service action: " + m_nsAction);
+            }
+        } else {
+            if(m_nsAction.equals(NS_RESTARTED)) {
+                if(m_cbObj.m_transitions.size() != 1) {
+                    m_logger.info("===   Wrong number of transitions: " 
+                        + String.valueOf(m_cbObj.m_transitions.size()) + ". We expected 1");
+                }
+            } else if(m_nsAction.equals(NS_STOPPED)) {
+                if(m_cbObj.m_transitions.size() != 1) {
+                    m_logger.info("===   Wrong number of transitions: " 
+                        + String.valueOf(m_cbObj.m_transitions.size()) + ". We expected 1");
+                }
+            } else {
+                m_logger.info("===   Wrong Notify Service action: " + m_nsAction);
+            }
+        }
 	}
 
 	public void disconnectAndReport() throws Exception {
@@ -135,26 +251,38 @@ public class SimpleSupplierReconnClient implements Callback<EventDescription> {
 	 */
 	public static void main(String[] args) throws Exception {
 
-		if( args.length < 2  ) {
-			System.err.println("Usage: SimpleSupplierReconnClient <# events> <# seconds between publications>");
+		if( args.length < 4  ) {
+			System.err.println("Usage: SimpleSupplierReconnClient <# events> <# seconds between publications> <autoreconnect> <NS action>");
 			System.exit(1);
 		}
+
+        String nsAction = args[3];
+        if(nsAction.equals(SimpleSupplierReconnClient.NS_RESTARTED) == false 
+            && nsAction.equals(SimpleSupplierReconnClient.NS_STOPPED) == false) {
+            System.err.println("Wrong NS action: " + nsAction + ". Must be: "
+                + SimpleSupplierReconnClient.NS_RESTARTED + " or "
+                + SimpleSupplierReconnClient.NS_STOPPED);
+            System.exit(1);
+        } 
 
 		SimpleSupplierReconnClient client = null;
 		try {
 			client = new SimpleSupplierReconnClient(
 			   Integer.parseInt(args[0]),
-			   Integer.parseInt(args[1])
+			   Integer.parseInt(args[1]),
+			   args[2].equals("autoreconnect") || args[2].equals("AUTORECONNECT"),
+               nsAction
 			);
 		} catch(NumberFormatException e) {
 			System.err.println("Invalid arguments, must all be integers");
-			System.out.println("Usage: SimpleSupplierReconnClient <# events> <# seconds between transfers>");
+			System.out.println("Usage: SimpleSupplierReconnClient <# events> <# seconds between transfers> <autoreconnect>");
 			System.exit(1);
 		}
 		
 		client.createPublisherAndSubscriber();
 		client.startReceiving();
 		client.disconnectAndReport();
+        System.out.println("Test finished");
 	}
 
 }
