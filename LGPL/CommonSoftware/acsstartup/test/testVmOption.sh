4c54ae94b6cfaa86a55a74d5fafd78728ec139ae
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2016-04-13 13:00:56 +0000

    This tested never worked before because started a mock container instead of a real java container. Fixed the script and the ref file.
    git-svn-id: https://svn.alma.cl/p2/trunk/ACS@230802 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsstartup/test/testVmOption.sh
+++ LGPL/CommonSoftware/acsstartup/test/testVmOption.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# The purpose of this test is to check if the parameter passed in the command line
+# with --passthroughProcessStart when starting a conmtainer arrives to java
+
 getChildProc() {
 	name=$1
 	ppid=$2
@@ -8,8 +11,15 @@ getChildProc() {
 
 LOGFILE=/dev/null
 
+# For this test we want to use acsStartJavaContainer from ACSROOT because the one 
+# provided in this module start a mock
+# The dirt trick is to rename theversion in ../bin just for this test
+mv ../bin/acsStartJavaContainer ../bin/acsStartJavaContainer.original 
+
+
 acsStart -noloadifr -b 9 > $LOGFILE 2>&1
 acsStartContainer -java -b 9 --passthroughProcessStart='-maxHeapSize 99m' jCont > $LOGFILE 2>&1 &
+
 start_container_pid=$!
 sleep 10
 
@@ -21,3 +31,6 @@ ps -p $java_pid -o args |
 sed -e 's/java \(.*\)\( -Xmx[^ ]*\) \(.*\)/java many-options \2 other-options/'
 
 acsStop -b 9 > $LOGFILE 2>&1
+
+# Restore ../bin/acsStartJavaContainer
+mv ../bin/acsStartJavaContainer.original ../bin/acsStartJavaContainer
