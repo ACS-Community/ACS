51b0d92be5d1ec67c832daca3c09baddb85fba4a
Camilo Valenzuela <cacke91@gmail.com>
2016-05-13 18:11:59 -0300

    Added 32 bits eclipse to download products

--- ExtProd/PRODUCTS/download-products.sh
+++ ExtProd/PRODUCTS/download-products.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 DOWNLOAD_LIST=extprods.links.txt
-
-for item in `cat $DOWNLOAD_LIST`; do 
-	p=`echo $item |cut -d':' -f1`
-  	if [ $p = "http" ]; then 
-		wget -c -O $PACKAGE_NAME $item 
-	elif [ $p = "https" ]; then
-		wget -c -O $PACKAGE_NAME --no-check-certificate $item
-	else
-		PACKAGE_NAME=$item
-	fi 
+sufix='-x86_64'
+arch=`uname -m`
+for item in `cat $DOWNLOAD_LIST`; do
+        if [[ "$item" == *"eclipse"* ]]
+        then
+                echo $arch
+                if [[ "$arch" == *"i"* ]]
+                then
+                        item=${item/-x86_64/}
+                fi
+        fi
+        p=`echo $item |cut -d':' -f1`
+        if [ $p = "http" ]; then
+                wget -c -O $PACKAGE_NAME $item
+        elif [ $p = "https" ]; then
+                wget -c -O $PACKAGE_NAME --no-check-certificate $item
+        else
+                PACKAGE_NAME=$item
+        fi
 done
+
