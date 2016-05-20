8f6270682f54d5e66289749a93ab7d37e6131aa8
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2016-05-20 12:48:48 +0000

    Save the logs into the timestamped queue (ICTJ:ICT-5105)
    git-svn-id: https://svn.alma.cl/p2/trunk/ACS@232014 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsExtras/acsXmlFileStore/src/alma/acs/xmlfilestore/alarm/impl/XmlFileStoreAlarmImpl.java
+++ LGPL/CommonSoftware/acsExtras/acsXmlFileStore/src/alma/acs/xmlfilestore/alarm/impl/XmlFileStoreAlarmImpl.java
@@ -18,18 +18,20 @@ ALMA - Atacama Large Millimiter Array
 */
 package alma.acs.xmlfilestore.alarm.impl;
 
+import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import alma.acs.component.ComponentImplBase;
 import alma.acs.component.ComponentLifecycleException;
 import alma.acs.container.ContainerServices;
+import alma.acs.logging.AcsLogLevel;
+import alma.acs.util.stringqueue.TimestampedStringQueue;
+import alma.acs.xmlfilestore.common.QueueFileHandler;
 import alma.alarmsystem.clients.SourceClient;
 import alma.alarmsystem.clients.source.SourceListener;
 import alma.xmlFileStore.AlarmsXmlStoreOperations;
-
 import cern.laser.source.alarmsysteminterface.FaultState;
-
 
 /** 
  * @author  acaproni
@@ -39,26 +41,34 @@ import cern.laser.source.alarmsysteminterface.FaultState;
 public class XmlFileStoreAlarmImpl extends ComponentImplBase implements AlarmsXmlStoreOperations, SourceListener {
 	
 	/**
-	 * @see SourceListener#sourceXMLMsgReceived(String)
+	 * The name of the java property to set the max size of each file of logs
 	 */
-	@Override
-	public void sourceXMLMsgReceived(String xml) {
-		// ASI message begins with XML start tag that we will remove
-		xml=xml.trim();
-		if (xml.startsWith("<?xml")) {
-			int retChar=xml.indexOf('>');
-			xml=xml.substring(retChar+1).trim();
-		}
-		System.out.println(xml);
-		
-	}
+	static public final String MAXFILESIZE_PROPNAME = "alma.acs.extras.alarmLogger.maxFileSize";
 	
 	/**
-	 * This method does nothing as we are interested in storing the XMLs
-	 * @see SourceListener#faultStateReceived(cern.laser.source.alarmsysteminterface.FaultState)
+	 * The default max size of each file of logs
 	 */
-	@Override
-	public void faultStateReceived(FaultState fs) {}
+	static public final int DEFAULTMAXFILESIZE = 134217728;
+	
+	/**
+	 * The name of the java property to set the max number of file of logs
+	 */
+	static public final String MAXNUMBEROFFILES_PROPNAME = "alma.acs.extras.alarmlogger.maxNumberFiles";
+	
+	/**
+	 * The default max number of file of logs
+	 */
+	static public final int DEFAULTMAXNUMBEROFFILES = 750;
+	
+	/**
+	 * The name of the java property to set the path to save the file of logs
+	 */
+	static public final String LOGDIR_PROPNAME = "alma.acs.extras.alarmlogger.logDir";
+	
+	/**
+	 * The default max number of file of logs
+	 */
+	static public final String DEFAULLOGDIR = "/mnt/gas02/data1/AcsAlarmLogs-8.1";
 
 	/**
 	 * Container services
@@ -69,6 +79,11 @@ public class XmlFileStoreAlarmImpl extends ComponentImplBase implements AlarmsXm
 	 * The logger
 	 */
 	private Logger m_logger;
+	
+	/**
+	 * The queue of XMLs
+	 */
+	private TimestampedStringQueue queue;
 	
 	
 	
@@ -95,6 +110,41 @@ public class XmlFileStoreAlarmImpl extends ComponentImplBase implements AlarmsXm
 		cs = containerServices;
 		m_logger = cs.getLogger();
 		
+		// Prepare the queue
+		String folderPath = System.getProperty(XmlFileStoreAlarmImpl.LOGDIR_PROPNAME, XmlFileStoreAlarmImpl.DEFAULLOGDIR);
+		File f = new File(folderPath);
+		if (!f.exists()) {
+			f.mkdirs();
+		}
+		
+		int fileMax = Integer.getInteger(XmlFileStoreAlarmImpl.MAXNUMBEROFFILES_PROPNAME, XmlFileStoreAlarmImpl.DEFAULTMAXNUMBEROFFILES);
+		int fileSizeLimit = Integer.getInteger(XmlFileStoreAlarmImpl.MAXFILESIZE_PROPNAME, XmlFileStoreAlarmImpl.DEFAULTMAXFILESIZE);
+		if (fileMax < 1 || fileSizeLimit < 100000 ) {
+			StringBuilder str = new StringBuilder(XmlFileStoreAlarmImpl.MAXNUMBEROFFILES_PROPNAME);
+			str.append(" must be greater then 1 and ");
+			str.append(XmlFileStoreAlarmImpl.MAXFILESIZE_PROPNAME);
+			str.append(" must be greater then 100000");
+			throw new ComponentLifecycleException(str.toString());
+		}
+		
+		StringBuilder str = new StringBuilder("Will save alarms files in : ");
+		str.append(folderPath);
+		str.append(" (max log file size: ");
+		str.append(fileSizeLimit);
+		str.append(", max # log files: " );
+		str.append(fileMax);
+		str.append(')');
+		m_logger.info(str.toString());
+		QueueFileHandler qFileHandler;
+		try {
+			qFileHandler = new QueueFileHandler(cs, folderPath,fileMax, fileSizeLimit, "alarmSources", "AlarmsLogger");
+		} catch (Throwable t) {
+			throw new ComponentLifecycleException("Could not create the queue file handler",t);
+		}
+		queue = new TimestampedStringQueue(qFileHandler, "<source-timestamp>");
+		queue.start();
+		
+		// Connects to the alarm source NC
 		try {
 			alarmSourceClient = new SourceClient(cs);
 			alarmSourceClient.connect();
@@ -111,7 +161,33 @@ public class XmlFileStoreAlarmImpl extends ComponentImplBase implements AlarmsXm
 	@Override
 	public void cleanUp() throws alma.maciErrType.wrappers.AcsJComponentCleanUpEx {
 		alarmSourceClient.close();
+		queue.close(true);
 		if (m_logger.isLoggable(Level.FINE)) m_logger.fine("cleaning up");
 		super.cleanUp();
 	}
+	
+	/**
+	 * @see SourceListener#sourceXMLMsgReceived(String)
+	 */
+	@Override
+	public void sourceXMLMsgReceived(String xml) {
+		// ASI message begins with XML start tag that we will remove
+		xml=xml.trim();
+		if (xml.startsWith("<?xml")) {
+			int retChar=xml.indexOf('>');
+			xml=xml.substring(retChar+1).trim();
+		}
+		try {
+			queue.push(xml);
+		} catch (Throwable t) {
+			m_logger.log(AcsLogLevel.ERROR,"Error pushing an alarm in the queue: alarm lost",t);
+		}
+	}
+	
+	/**
+	 * This method does nothing as we are interested in storing the XMLs
+	 * @see SourceListener#faultStateReceived(cern.laser.source.alarmsysteminterface.FaultState)
+	 */
+	@Override
+	public void faultStateReceived(FaultState fs) {}
 }
