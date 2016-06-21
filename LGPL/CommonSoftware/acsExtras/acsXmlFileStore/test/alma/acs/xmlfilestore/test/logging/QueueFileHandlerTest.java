7d743c73e6333e21943216b9cbc189edaf1354c1
acaproni <acaproni@523d945c-050c-4681-91ec-863ad3bb968a>
2016-06-21 08:04:50 +0000

    Test fixed for the renaming defined in ICTJ:ICT-5523
    git-svn-id: https://svn.alma.cl/p2/trunk/ACS@233036 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/CommonSoftware/acsExtras/acsXmlFileStore/test/alma/acs/xmlfilestore/test/logging/QueueFileHandlerTest.java
+++ LGPL/CommonSoftware/acsExtras/acsXmlFileStore/test/alma/acs/xmlfilestore/test/logging/QueueFileHandlerTest.java
@@ -3,19 +3,21 @@ package alma.acs.xmlfilestore.test.logging;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.Arrays;
-import java.util.Properties;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 import alma.acs.component.client.ComponentClientTestCase;
+import alma.acs.util.IsoDateFormat;
 import alma.acs.xmlfilestore.common.QueueFileHandler;
-import junit.framework.TestCase;
 
 /**
  * 
- * @author almadev
+ * @author acaproni
+ * @since 2016.6
  */
 public class QueueFileHandlerTest extends ComponentClientTestCase {
 	private final static Logger LOG = Logger.getLogger(QueueFileHandlerTest.class.getSimpleName());
@@ -58,6 +60,7 @@ public class QueueFileHandlerTest extends ComponentClientTestCase {
 	}
 
 	/**
+	 * This test checks the correctness of the format of the name of the file against a regular expresison.
 	 * 
 	 * @throws Exception
 	 */
@@ -66,25 +69,64 @@ public class QueueFileHandlerTest extends ComponentClientTestCase {
 		QueueFileHandler handler = new QueueFileHandler(this.getContainerServices(), logDir.getAbsolutePath(), 1, 1025L,"log","Logging");
 		File f = handler.getNewFile();
 		assertNotNull(f);
-		assertTrue(f.getName(), f.getName().matches(filenamePattern));
+		assertTrue(f.getName()+" has a wrong format!", f.getName().matches(filenamePattern));
 		LOG.info(f.getAbsolutePath());
 	}
 	
 	/**
+	 * This test checks if the file has been renamed after calling {@link QueueFileHandler#fileProcessed(File, String, String)}.
+	 * <P>
+	 * The name of the file, after renaming, must match with the creation and closing time of the file
+	 * and not depend on the content of the timestamped strings it contains.
+	 * <BR>
+	 * This test creates a file th invokes {@link QueueFileHandler#fileProcessed(File, String, String)} passing
+	 * two timestamps and checks that they are ignored in favour of the actual timestamp 
 	 * 
 	 * @throws Exception
 	 */
 	public void testCloseFile() throws Exception {
-		File oldLogFile = new File(logDir, "log1970-01-21T00:00:00.000_YYYY-MM-DDTHH:MM:SS.mmm.xml");
+		// The format of the file after renaming
+		final String filenamePattern = "log\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}_\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}\\.xml";
+		// Create a file whose name contains the actual time
+		String now = IsoDateFormat.formatCurrentDate();
+		File oldLogFile = new File(logDir, "log"+now+"_YYYY-MM-DDTHH:MM:SS.mmm.xml");
 		// touch the log file
 		try (FileOutputStream ofs = new FileOutputStream(oldLogFile)) {;;}
 		QueueFileHandler handler = new QueueFileHandler(this.getContainerServices(), logDir.getAbsolutePath(), 1, 1025L,"log","Logging");
+		// The earliest timestamp must be before the actual timestamp to ensure it is ignored
 		String earliestLogTimestamp = "2014-12-06T13:00:00.000";
-		String lastLogTimestamp = "2014-12-06T15:12:34.567";
-		handler.fileProcessed(oldLogFile, earliestLogTimestamp, lastLogTimestamp);
+		// The oldest timestamp must be after the actual timestamp to ensure it is ignored
+		int nextYear = Calendar.getInstance().get(Calendar.YEAR)+1;
+		String oldestLogTimestamp = ""+nextYear+"-12-06T15:12:34.567";
+		// Wait 10 seconds before closing the file tobe sure the creatin and closing timestamps in the
+		// name of the file differ
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException ie) {}
+		handler.fileProcessed(oldLogFile, earliestLogTimestamp, oldestLogTimestamp);
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException ie) {}
+		String timeAfterProcessing = IsoDateFormat.formatCurrentDate();
 		assertFalse(oldLogFile.exists());
-		File newLogFile = new File(logDir, String.format("log%s_%s.xml", earliestLogTimestamp, lastLogTimestamp));
-		assertTrue(Arrays.toString(logDir.list()), newLogFile.exists());
+		File newLogFile = new File(logDir, String.format("log%s_%s.xml", earliestLogTimestamp, oldestLogTimestamp));
+		assertEquals("Too many log files in "+logDir.getAbsolutePath(),1,logDir.list().length);
+		String fileName = logDir.list()[0];
+		// Check the format
+		assertTrue(fileName+" renamed with wrong format!", fileName.matches(filenamePattern));
+		// Check if the name contains the oldest or the newest timepstamp
+		assertEquals("The file name should not contain the oldest timestamp", -1,fileName.indexOf(oldestLogTimestamp));
+		assertEquals("The file name should not contain the newest timestamp", -1,fileName.indexOf(earliestLogTimestamp));
+		// The creationtimestamp (now) should be in the name of the file (i.e. not changed)
+		assertTrue("The start timestamp has been changed!",fileName.startsWith("log"+now+"_") );
+		// Check that the oldest timestamp in the name of the file is before
+		// the fileProcessed had been executed
+		int posUnderscore = fileName.lastIndexOf('_');
+		int posFileNameExtension = fileName.lastIndexOf(".xml");
+		String lastIsoTimestamp=fileName.substring(posUnderscore+1,posFileNameExtension);
+		Date afterClosingDate = IsoDateFormat.parseIsoTimestamp(timeAfterProcessing);
+		Date closingTimestamp=IsoDateFormat.parseIsoTimestamp(lastIsoTimestamp);
+		assertTrue("The closing timestamp in the file is after fileProcessed had been called",closingTimestamp.before(afterClosingDate));
 	}
 	
 	/**
