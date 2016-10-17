d181619b591ea8794976853888ecf60686d9a136
eallaert <eallaert@523d945c-050c-4681-91ec-863ad3bb968a>
2016-10-17 14:31:29 +0000

    Incorporated all of acsSaveEnv.tcl into acsRestore.tcl, and removed the former file.
    git-svn-id: https://svn.alma.cl/p2/trunk/ACS@236554 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/Kit/acs/test/acsSaveEnv.tcl
+++
@@ -1,9 +0,0 @@
-# -*- tcl -*-
-# Create our own namespace
-namespace eval ::acsMakefile {
-    foreach var {INTROOT INTLIST ACSROOT MODPATH} {
-	if {[info exists ::env($var)]} {
-	    set $var $::env($var)
-	}
-    }
-}
