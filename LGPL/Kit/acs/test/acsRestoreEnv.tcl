55abe8e379b7e8e948ea00526cbdf1b3c48cf14b
scornejo <scornejo@523d945c-050c-4681-91ec-863ad3bb968a>
2016-10-20 15:33:39 +0000

    ICT-8405 reverted changes as requested by EAllaert
    git-svn-id: https://svn.alma.cl/p2/branches/COMMON-OCT2016-TEMP/ACS@236671 523d945c-050c-4681-91ec-863ad3bb968a

--- LGPL/Kit/acs/test/acsRestoreEnv.tcl
+++ LGPL/Kit/acs/test/acsRestoreEnv.tcl
@@ -1,10 +1,31 @@
 # -*- tcl -*-
 namespace eval ::acsMakefile {
-    foreach var {INTROOT INTLIST ACSROOT MODPATH} {
-	if {[info exists $var]} {
-	    set ::env($var) [set $var]
-	} else {
-	    catch {unset ::env($var)}
+    variable savedVars
+    if {[array exists savedVars]} {
+        # The env vars have been saved before, so restore them now
+        foreach var {INTROOT INTLIST ACSROOT} {
+            catch {unset ::$var}
+            catch {unset ::env($var)}
+	}
+	foreach {var val} [array get savedVars] {
+	    set $var $val
+	}
+    } else {
+        # The env vars have not been saved before, so save them now
+	foreach var {INTROOT INTLIST ACSROOT MODPATH} {
+	    if {[info exists ::env($var)]} {
+		array set savedVars [list ::env($var) $::env($var)]
+	    }
+	    if {[info exists ::$var] && [set ::$var] == "undefined"} {
+		# tat has the bad habit of setting global vars INTROOT, INTLIST and ACSROOT
+                # to "undefined" if the corresponding env var does not exist; it will then
+                # pass these global vars on to make - which will barf on the non-existing 
+                # directory "undefined". So better undo this tat behaviour.
+		unset ::$var
+	    }
+	    if {[info exists ::$var]} {
+		array set savedVars [list ::$var [set ::$var]]
+	    }
 	}
     }
 }
